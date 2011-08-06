ec068820a511e598bc814c5c0d4b30a0abf56207
jendave <jendave@cardforge.org>
2011-08-06 17:54:51 +0000

    - Converted Phyrexian Rager to AF Trigger.

--- src/forge/CardFactory_Creatures.java
+++ src/forge/CardFactory_Creatures.java
@@ -835,6 +835,7 @@ public class CardFactory_Creatures {
         }//*************** END ************ END **************************
         */
         
+        /* Converteded to AF Trigger
         //*************** START *********** START **************************
         else if(cardName.equals("Phyrexian Rager")) {
             final SpellAbility ability = new Ability(card, "0") {
@@ -860,7 +861,7 @@ public class CardFactory_Creatures {
             };
             card.addComesIntoPlayCommand(intoPlay);
         }//*************** END ************ END ************************** 
-        
+        */
         
         //*************** START *********** START **************************
         else if(cardName.equals("Brawn")) {
