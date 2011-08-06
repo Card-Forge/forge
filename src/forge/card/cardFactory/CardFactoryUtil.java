6512ba29dc78da868f6ff8b2da00589e0ea569f8
jendave <jendave@cardforge.org>
2011-08-06 21:31:42 +0000

    - "CARDNAME can't be the target of Aura spells." will no longer protect from abilities of auras (like Chime of Night) - Fixed Dense Foliage.

--- src/forge/card/cardFactory/CardFactoryUtil.java
+++ src/forge/card/cardFactory/CardFactoryUtil.java
@@ -3100,7 +3100,7 @@ public class CardFactoryUtil {
                 }
                 
                 if(kw.equals("CARDNAME can't be the target of Aura spells.")) {
-                    if(spell.isAura()) return false;
+                    if(spell.isAura() && spell.isSpell()) return false;
                 }
                 
                 if(kw.equals("CARDNAME can't be the target of red spells or abilities from red sources.")) {
