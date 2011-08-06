0780be7a273be431659a43bf20755961dead61cb
jendave <jendave@cardforge.org>
2011-08-06 09:58:20 +0000

    1) Fix AbilityFactory_Fetch not respecting Tapped$True for AI. 2) add optional Tgt$TgtP to AbilityFactory_Fetch

--- src/forge/AbilityFactory_Fetch.java
+++ src/forge/AbilityFactory_Fetch.java
@@ -7,7 +7,7 @@ import java.util.Random;
 public class AbilityFactory_Fetch {
 	// An AbilityFactory subclass for Fetching Cards from the Library
 	
-	//Destination$Battlefield|Valid$Basic|FetchNum$2|UpTo$True
+	//Destination$Battlefield|Valid$Basic|FetchNum$2|UpTo$True|[Tgt$TgtP]
 	
 	public static SpellAbility createAbilityFetch(final AbilityFactory AF){
 		final SpellAbility abFetch = new Ability_Activated(AF.getHostCard(), AF.getAbCost(), AF.getAbTgt()){
@@ -26,7 +26,9 @@ public class AbilityFactory_Fetch {
 			
 			@Override
 			public void resolve() {
-				doFetch(af, this, this.getActivatingPlayer());
+				Player tgt = getTargetPlayer();
+				if(null == tgt) tgt = this.getActivatingPlayer();
+				doFetch(af, this, tgt);
 			}
 		
 		};
@@ -50,7 +52,9 @@ public class AbilityFactory_Fetch {
 			
 			@Override
 			public void resolve() {
-				doFetch(af, this, this.getActivatingPlayer());
+				Player tgt = getTargetPlayer();
+				if(null == tgt) tgt = this.getActivatingPlayer();
+				doFetch(af, this, tgt);
 			}		
 		};
 		return spFetch;
@@ -145,8 +149,11 @@ public class AbilityFactory_Fetch {
                     	AllZone.Display.getChoiceOptional(af.getHostCard().getName() + " - Computer picked:", l.toArray());
             		AllZone.Computer_Hand.add(c);
             	}//move to hand
-            	else if (destination.equals("Battlefield")) 
+            	else if (destination.equals("Battlefield")) {
                 	AllZone.getZone(Constant.Zone.Play, player).add(c); //move to battlefield
+                	if (params.containsKey("Tapped"))
+                		c.tap();
+            	}
             	else if (destination.equals("Library")) {
                 	CardList l = new CardList();
                 	l.add(c);
@@ -186,7 +193,11 @@ public class AbilityFactory_Fetch {
 		Random r = new Random();
 		// prevent run-away activations - first time will always return true
 		boolean chance = r.nextFloat() <= Math.pow(.6667, source.getAbilityUsed());
-		
+		if(af.getAbTgt().canTgtPlayer()) {
+			sa.setTargetPlayer(AllZone.ComputerPlayer);
+			return chance;
+		}
+
 		// todo: add more decision making for Fetching
 		// if Type is Land or a Land Type, improve chances for each Landfall card you control
 		
