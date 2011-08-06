b349f14ce56ff1645883f5bf37d85e592b17e7c3
jendave <jendave@cardforge.org>
2011-08-06 05:55:23 +0000

    - Added SVar AntiBuffedby to be used on permanents the human controls to make the AI play certain cards in Main1. - Added AntiBuffedby SVar to Timid Drake.

--- src/forge/ComputerAI_General.java
+++ src/forge/ComputerAI_General.java
@@ -153,12 +153,21 @@ public class ComputerAI_General implements Computer {
             	
                 if(c.isCreature() && (c.getKeyword().contains("Haste")) || c.getKeyword().contains("Exalted")) return true;
 
-                CardList buffed = new CardList(AllZone.Computer_Play.getCards());
-                // buffed.getKeyword("BuffedBy");  //get all cards the computer controls with BuffedBy
+                CardList buffed = new CardList(AllZone.Computer_Play.getCards()); //get all cards the computer controls with BuffedBy
                 for(int i = 0; i < buffed.size(); i++) {
                 	Card buffedcard = buffed.get(i);
                 	if (buffedcard.getSVar("BuffedBy").length() > 0) {
                 			String buffedby = buffedcard.getSVar("BuffedBy");
+                			final String bffdby[] = buffedby.split(",");
+                			if (c.isValidCard(bffdby)) return true;
+                	}       
+                }
+
+                CardList antibuffed = new CardList(AllZone.Human_Play.getCards()); //get all cards the computer controls with AntiBuffedBy
+                for(int i = 0; i < buffed.size(); i++) {
+                	Card buffedcard = antibuffed.get(i);
+                	if (buffedcard.getSVar("AntiBuffedBy").length() > 0) {
+                			String buffedby = buffedcard.getSVar("AntiBuffedBy");
                 			final String bffdby[] = buffedby.split(",");
                 			if (c.isValidCard(bffdby)) return true;
                 	}       
