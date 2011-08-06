84706d47ca0af0dc1b973913132af7a97c0457d2
jendave <jendave@cardforge.org>
2011-08-06 18:44:32 +0000

    *Added Blade of the Bloodchief.

--- src/forge/CardFactory_Equipment.java
+++ src/forge/CardFactory_Equipment.java
@@ -799,7 +799,34 @@ class CardFactory_Equipment {
             card.addUnEquipCommand(onUnEquip);
             
         } //*************** END ************ END **************************
-        
+
+        //*************** START *********** START **************************
+        else if(cardName.equals("Blade of the Bloodchief"))
+        {
+            final Ability triggeredAbility = new Ability(card,"0") {
+                @Override
+                public void resolve() {
+                    if(card.getEquipping().size() != 0)
+                    {
+                        Card equipping = card.getEquipping().get(0);
+                        if(equipping.isType("Vampire"))
+                        {
+                            equipping.addCounter(Counters.P1P1,2);
+                        }
+                        else
+                        {
+                            equipping.addCounter(Counters.P1P1,1);
+                        }
+                    }
+                }
+            };
+
+            final Trigger myTrigger = TriggerHandler.parseTrigger("Mode$ ChangesZone | Origin$ Battlefield | Destination$ Graveyard | ValidCard$ Creature | Execute$ TrigOverride | TriggerDescription$ Whenever a creature is put into a graveyard from the battlefield, put a +1/+1 counter on equipped creature. If equipped creature is a Vampire, put two +1/+1 counters on it instead.",card);
+            myTrigger.setOverridingAbility(triggeredAbility);
+
+            card.addTrigger(myTrigger);
+        } //*************** END ************ END **************************
+
         if (shouldEquip(card) != -1) {
             int n = shouldEquip(card);
             if (n != -1) {
