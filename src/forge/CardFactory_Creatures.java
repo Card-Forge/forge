9cd3c8d416488be720d4f553c4d37dc45db310c6
jendave <jendave@cardforge.org>
2011-08-06 17:18:53 +0000

    - Converted Cloudthresher to AF Trigger.

--- src/forge/CardFactory_Creatures.java
+++ src/forge/CardFactory_Creatures.java
@@ -547,7 +547,7 @@ public class CardFactory_Creatures {
             card.addComesIntoPlayCommand(intoPlay);
         }//*************** END ************ END **************************
 
-        
+        /* Converteded to AF Trigger
         //*************** START *********** START **************************
         else if(cardName.equals("Cloudthresher")) {
         	final SpellAbility ability = new Ability(card, "0") {
@@ -594,6 +594,7 @@ public class CardFactory_Creatures {
                 }
             });
         }//*************** END ************ END **************************
+        */
         
         /* Converteded to AF Trigger
         //*************** START *********** START **************************
