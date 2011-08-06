edc8c79a2c5a6fb6e5b8bc1124e943453aad2738
jendave <jendave@cardforge.org>
2011-08-06 05:59:58 +0000

    - Fixed Breath of Life; Resurrection; False Defeat; Zombify. The AI will no longer target a creature with the keyword "At the beginning of the end step, sacrifice CARDNAME."

--- src/forge/CardFactory_Sorceries.java
+++ src/forge/CardFactory_Sorceries.java
@@ -944,6 +944,7 @@ public class CardFactory_Sorceries {
                     PlayerZone zone = AllZone.getZone(Constant.Zone.Graveyard, card.getController());
                     creature.addAll(zone.getCards());
                     creature = creature.getType("Creature");
+                    creature = creature.getNotKeyword("At the beginning of the end step, sacrifice CARDNAME.");
                     return creature.toArray();
                 }
                 
