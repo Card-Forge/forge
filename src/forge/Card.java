ebf1d6bd5ab553047133f63d8d1287edc5f5ced1
jendave <jendave@cardforge.org>
2011-08-07 01:09:10 +0000

    - Added M1M1/P1P1 counters exchange to addCounterFromNonEffect.

--- src/forge/Card.java
+++ src/forge/Card.java
@@ -505,6 +505,32 @@ public class Card extends MyObservable {
             counters.put(counterName, Integer.valueOf(n));
         }
         
+        if (counterName.equals(Counters.P1P1) || counterName.equals(Counters.M1M1)){
+        	// +1/+1 counters should erase -1/-1 counters
+        	int plusOneCounters = 0;
+        	int minusOneCounters = 0;
+        	
+        	Counters p1Counter = Counters.P1P1;
+        	Counters m1Counter = Counters.M1M1;
+        	if (counters.containsKey(p1Counter))
+        		plusOneCounters = counters.get(p1Counter);
+        	if (counters.containsKey(m1Counter))
+        		minusOneCounters = counters.get(m1Counter);
+
+        	if (plusOneCounters == minusOneCounters){
+        		counters.remove(m1Counter);
+        		counters.remove(p1Counter);
+        	}
+        	if (plusOneCounters > minusOneCounters){
+        		counters.remove(m1Counter);
+        		counters.put(p1Counter, (Integer)(plusOneCounters - minusOneCounters));	
+        	}
+        	else{
+        		counters.put(m1Counter, (Integer)(minusOneCounters - plusOneCounters));
+        		counters.remove(p1Counter);
+        	}
+        }
+        
         /////////////////
         //
         // Not sure if we want to fire triggers on addCounterFromNonEffect
@@ -530,32 +556,6 @@ public class Card extends MyObservable {
             counters.put(counterName, aux);
         } else {
             counters.put(counterName, Integer.valueOf(multiplier * n));
-        }
-        
-        if (counterName.equals(Counters.P1P1) || counterName.equals(Counters.M1M1)){
-        	// +1/+1 counters should erase -1/-1 counters
-        	int plusOneCounters = 0;
-        	int minusOneCounters = 0;
-        	
-        	Counters p1Counter = Counters.P1P1;
-        	Counters m1Counter = Counters.M1M1;
-        	if (counters.containsKey(p1Counter))
-        		plusOneCounters = counters.get(p1Counter);
-        	if (counters.containsKey(m1Counter))
-        		minusOneCounters = counters.get(m1Counter);
-
-        	if (plusOneCounters == minusOneCounters){
-        		counters.remove(m1Counter);
-        		counters.remove(p1Counter);
-        	}
-        	if (plusOneCounters > minusOneCounters){
-        		counters.remove(m1Counter);
-        		counters.put(p1Counter, (Integer)(plusOneCounters - minusOneCounters));	
-        	}
-        	else{
-        		counters.put(m1Counter, (Integer)(minusOneCounters - plusOneCounters));
-        		counters.remove(p1Counter);
-        	}
         }
 
         //Run triggers
