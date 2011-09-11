0136e9d5f1565d42fe6dd194f12c36b9fe1f29db
Doublestrike <doublestrike@cardforge.org>
2011-09-11 06:15:21 +0000

    Javadoc and functionality comments updated.

--- src/main/java/forge/Quest_Assignment.java
+++ src/main/java/forge/Quest_Assignment.java
@@ -6,6 +6,8 @@ import forge.card.CardPrinted;
 
 /**
  * <p>Quest_Assignment class.</p>
+ * An instance of a single quest assignment.  Holds values that aren't defined in 
+ * Deck class, such as credit reward.
  *
  * @author Forge
  * @version $Id$
@@ -29,6 +31,165 @@ public class Quest_Assignment {
     private CardList humanExtraCards    = new CardList();
     private CardList aiExtraCards       = new CardList();
     private ArrayList<CardPrinted> cardRewardList = new ArrayList<CardPrinted>(); 
+
+    // No constructor required; fields are set dynamically.
+    
+    // Setters and getters, in alphabetical order.
+    
+    /**
+     * <p>Setter for the field <code>aiExtraCards</code>.</p>
+     * Sets list of cards automatically played at start of match.
+     *
+     * @param s a CardList object.
+     */
+    public final void setAIExtraCards(final CardList cl) {
+        this.aiExtraCards = cl;
+    }
+    
+    /**
+     * <p>Getter for the field <code>aiExtraCards</code>.</p>
+     * Gets list of cards automatically played at start of match.
+     *
+     * @return a {@link java.util.ArrayList} object.
+     */
+    public final CardList getAIExtraCards() {
+        return aiExtraCards;
+    }
+    /**
+     * <p>Setter for the field <code>cardReward</code>.</p>
+     * Sets string notated card reward received if match is won.
+     *
+     * @param cardRewardIn a {@link java.lang.String} object.
+     */
+    public final void setCardReward(final String cardRewardIn) {
+        this.cardReward = cardRewardIn;
+    }
+
+    /**
+     * <p>Getter for the field <code>cardReward</code>.</p>
+     * Gets string notated card reward received if match is won.
+     *
+     * @return a {@link java.lang.String} object.
+     */
+    public final String getCardReward() {
+        return cardReward;
+    }
+    
+    /**
+     * <p>Setter for the field <code>cardRewardList</code>.</p>
+     * Sets card reward list for cards received if match is won.
+     *
+     * @return a {@link java.util.ArrayList} object.
+     */
+    public void setCardRewardList(final ArrayList<CardPrinted> cp) {
+        this.cardRewardList = cp;
+    }
+    
+    /**
+     * <p>Getter for the field <code>cardRewardList</code>.</p>
+     * Gets card reward list for cards received if match is won.
+     *
+     * @return a {@link java.util.ArrayList} object.
+     */
+    public final ArrayList<CardPrinted> getCardRewardList() {
+        return cardRewardList;
+    }
+
+    /**
+     * <p>Setter for the field <code>computerLife</code>.</p>
+     * Sets computer life at start of match.
+     *
+     * @param computerLifeIn a int.
+     */
+    public final void setComputerLife(final int computerLifeIn) {
+        this.computerLife = computerLifeIn;
+    }
+
+    /**
+     * <p>Getter for the field <code>computerLife</code>.</p>
+     * Gets computer life at start of match.
+     *
+     * @return a int.
+     */
+    public final int getComputerLife() {
+        return computerLife;
+    }
+    
+    /**
+     * <p>Setter for the field <code>creditsReward</code>.</p>
+     * Sets credit reward received if match is won.
+     *
+     * @param creditsRewardIn a long.
+     */
+    public final void setCreditsReward(final long creditsRewardIn) {
+        this.creditsReward = creditsRewardIn;
+    }
+
+    /**
+     * <p>Getter for the field <code>creditsReward</code>.</p>
+     * Gets credit reward received if match is won.
+     *
+     * @return a long.
+     */
+    public final long getCreditsReward() {
+        return creditsReward;
+    }
+    
+    /**
+     * <p>Setter for the field <code>desc</code>.</p>
+     *
+     * @param descIn a {@link java.lang.String} object.
+     */
+    public final void setDesc(final String descIn) {
+        this.desc = descIn;
+    }
+
+    /**
+     * <p>Getter for the field <code>desc</code>.</p>
+     *
+     * @return a {@link java.lang.String} object.
+     */
+    public final String getDesc() {
+        return desc;
+    }
+    
+    /**
+     * <p>Setter for the field <code>difficulty</code>.</p>
+     *
+     * @param difficultyIn a {@link java.lang.String} object.
+     */
+    public final void setDifficulty(final String difficultyIn) {
+        this.difficulty = difficultyIn;
+    }
+    
+    /**
+     * <p>Setter for the field <code>humanExtraCards</code>.</p>
+     * Sets list of cards automatically played at start of match.
+     *
+     * @param s a CardList object.
+     */
+    public final void setHumanExtraCards(final CardList cl) {
+        this.humanExtraCards = cl;
+    }
+    
+    /**
+     * <p>Getter for the field <code>humanExtraCards</code>.</p>
+     * Gets list of cards automatically played at start of match.
+     *
+     * @return a {@link forge.CardList} object.
+     */
+    public final CardList getHumanExtraCards() {
+        return humanExtraCards;
+    }
+
+    /**
+     * <p>Getter for the field <code>difficulty</code>.</p>
+     *
+     * @return a {@link java.lang.String} object.
+     */
+    public final String getDifficulty() {
+        return difficulty;
+    }
     
     /**
      * <p>Setter for the field <code>id</code>.</p>
@@ -47,23 +208,25 @@ public class Quest_Assignment {
     public final int getId() {
         return id;
     }
-
+    
     /**
-     * <p>Setter for the field <code>creditsReward</code>.</p>
+     * <p>Setter for the field <code>iconName</code>.</p>
+     * Sets filename of icon used when displaying this quest.
      *
-     * @param creditsRewardIn a long.
+     * @param s a {@link java.lang.String} object.
      */
-    public final void setCreditsReward(final long creditsRewardIn) {
-        this.creditsReward = creditsRewardIn;
+    public final void setIconName(final String s) {
+        iconName = s;
     }
 
     /**
-     * <p>Getter for the field <code>creditsReward</code>.</p>
+     * <p>Getter for the field <code>iconName</code>.</p>
+     * Gets filename of icon used when displaying this quest.
      *
-     * @return a long.
+     * @return a {@link java.lang.String} object.
      */
-    public final long getCreditsReward() {
-        return creditsReward;
+    public final String getIconName() {
+        return iconName;
     }
 
     /**
@@ -82,42 +245,6 @@ public class Quest_Assignment {
      */
     public final String getName() {
         return name;
-    }
-
-    /**
-     * <p>Setter for the field <code>desc</code>.</p>
-     *
-     * @param descIn a {@link java.lang.String} object.
-     */
-    public final void setDesc(final String descIn) {
-        this.desc = descIn;
-    }
-
-    /**
-     * <p>Getter for the field <code>desc</code>.</p>
-     *
-     * @return a {@link java.lang.String} object.
-     */
-    public final String getDesc() {
-        return desc;
-    }
-
-    /**
-     * <p>Setter for the field <code>difficulty</code>.</p>
-     *
-     * @param difficultyIn a {@link java.lang.String} object.
-     */
-    public final void setDifficulty(final String difficultyIn) {
-        this.difficulty = difficultyIn;
-    }
-
-    /**
-     * <p>Getter for the field <code>difficulty</code>.</p>
-     *
-     * @return a {@link java.lang.String} object.
-     */
-    public final String getDifficulty() {
-        return difficulty;
     }
 
     /**
@@ -140,6 +267,7 @@ public class Quest_Assignment {
 
     /**
      * <p>Setter for the field <code>requiredNumberWins</code>.</p>
+     * Sets number of wins required to unlock this quest.
      *
      * @param requiredNumberWinsIn a int.
      */
@@ -149,119 +277,12 @@ public class Quest_Assignment {
 
     /**
      * <p>Getter for the field <code>requiredNumberWins</code>.</p>
+     * Gets number of wins required to unlock this quest.
      *
      * @return a int.
      */
     public final int getRequiredNumberWins() {
         return requiredNumberWins;
-    }
-
-    /**
-     * <p>Setter for the field <code>computerLife</code>.</p>
-     *
-     * @param computerLifeIn a int.
-     */
-    public final void setComputerLife(final int computerLifeIn) {
-        this.computerLife = computerLifeIn;
-    }
-
-    /**
-     * <p>Getter for the field <code>computerLife</code>.</p>
-     *
-     * @return a int.
-     */
-    public final int getComputerLife() {
-        return computerLife;
-    }
-
-    /**
-     * <p>Setter for the field <code>cardReward</code>.</p>
-     *
-     * @param cardRewardIn a {@link java.lang.String} object.
-     */
-    public final void setCardReward(final String cardRewardIn) {
-        this.cardReward = cardRewardIn;
-    }
-
-    /**
-     * <p>Getter for the field <code>cardReward</code>.</p>
-     *
-     * @return a {@link java.lang.String} object.
-     */
-    public final String getCardReward() {
-        return cardReward;
-    }
-
-    /**
-     * <p>Setter for the field <code>iconName</code>.</p>
-     *
-     * @param s a {@link java.lang.String} object.
-     */
-    public final void setIconName(final String s) {
-        iconName = s;
-    }
-
-    /**
-     * <p>Getter for the field <code>iconName</code>.</p>
-     *
-     * @return a {@link java.lang.String} object.
-     */
-    public final String getIconName() {
-        return iconName;
-    }
-
-    /**
-     * <p>Setter for the field <code>humanExtraCards</code>.</p>
-     *
-     * @param s a CardList object.
-     */
-    public final void setHumanExtraCards(final CardList cl) {
-        this.humanExtraCards = cl;
-    }
-    
-    /**
-     * <p>Getter for the field <code>humanExtraCards</code>.</p>
-     *
-     * @return a {@link forge.CardList} object.
-     */
-    public final CardList getHumanExtraCards() {
-        return humanExtraCards;
-    }
-
-    /**
-     * <p>Setter for the field <code>aiExtraCards</code>.</p>
-     *
-     * @param s a CardList object.
-     */
-    public final void setAIExtraCards(final CardList cl) {
-        this.aiExtraCards = cl;
-    }
-    
-    /**
-     * <p>Getter for the field <code>compy</code>.</p>
-     *
-     * @return a {@link java.util.ArrayList} object.
-     */
-    public final CardList getAIExtraCards() {
-        return aiExtraCards;
-    }
-
-    /**
-     * <p>Setter for the field <code>cardRewardList</code>.</p>
-     *
-     * @return a {@link java.util.ArrayList} object.
-     */
-    public void setCardRewardList(final ArrayList<CardPrinted> cp) {
-        this.cardRewardList = cp;
-    }
-    
-    /**
-     * <p>Getter for the field <code>cardRewardList</code>.</p>
-     *
-     * @return a {@link java.util.ArrayList} object.
-     */
-    public final ArrayList<CardPrinted> getCardRewardList() {
-        return cardRewardList;
-    }
+    }    
     
 }
