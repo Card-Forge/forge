ccded0a982a1736930cfd082591b22d4175f6908
slapshot5 <slapshot5@cardforge.org>
2011-09-12 00:43:21 +0000

    remove unneeded code in Trigger_Taps.getCopy (I had only added it to this trigger.  The rest of the Trigger classes do not need this change.)

--- src/main/java/forge/card/trigger/Trigger_Taps.java
+++ src/main/java/forge/card/trigger/Trigger_Taps.java
@@ -18,14 +18,15 @@ public class Trigger_Taps extends Trigger {
      *
      * @param params a {@link java.util.HashMap} object.
      * @param host a {@link forge.Card} object.
+     * @param intrinsic a boolean
      */
-    public Trigger_Taps(HashMap<String, String> params, Card host, boolean intrinsic) {
+    public Trigger_Taps(final HashMap<String, String> params, final Card host, final boolean intrinsic) {
         super(params, host, intrinsic);
     }
 
     /** {@inheritDoc} */
     @Override
-    public boolean performTest(java.util.Map<String, Object> runParams2) {
+    public final boolean performTest(final java.util.Map<String, Object> runParams2) {
         Card tapper = (Card) runParams2.get("Card");
 
         if (mapParams.containsKey("ValidCard")) {
@@ -39,21 +40,20 @@ public class Trigger_Taps extends Trigger {
 
     /** {@inheritDoc} */
     @Override
-    public Trigger getCopy() {
+    public final Trigger getCopy() {
         Trigger copy = new Trigger_Taps(mapParams, hostCard, isIntrinsic);
         if (overridingAbility != null) {
             copy.setOverridingAbility(overridingAbility);
         }
         copy.setName(name);
         copy.setID(ID);
-        copy.setSuppressed(suppressed);
 
         return copy;
     }
 
     /** {@inheritDoc} */
     @Override
-    public void setTriggeringObjects(SpellAbility sa) {
+    public final void setTriggeringObjects(final SpellAbility sa) {
         sa.setTriggeringObject("Card", runParams.get("Card"));
     }
 }
