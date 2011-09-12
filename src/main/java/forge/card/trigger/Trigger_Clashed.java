ab668eba049f611891ee6ad4c1e30124c496dc4c
Sloth <sloth@cardforge.org>
2011-09-12 19:39:22 +0000

    - Fixed performTest in Trigger_Clashed.

--- src/main/java/forge/card/trigger/Trigger_Clashed.java
+++ src/main/java/forge/card/trigger/Trigger_Clashed.java
@@ -27,7 +27,7 @@ public class Trigger_Clashed extends Trigger {
     @Override
     public boolean performTest(java.util.Map<String, Object> runParams2) {
         if (mapParams.containsKey("ValidPlayer")) {
-            if (!matchesValid(runParams.get("Player"), mapParams.get("ValidPlayer").split(","), hostCard)) {
+            if (!matchesValid(runParams2.get("Player"), mapParams.get("ValidPlayer").split(","), hostCard)) {
                 return false;
             }
         }
