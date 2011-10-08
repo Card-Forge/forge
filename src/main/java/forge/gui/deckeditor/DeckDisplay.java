0f748398bf56123d9c2717beed107aa9cdeee556
jendave <jendave@cardforge.org>
2011-10-08 07:37:21 +0000

    Checkstyle fixes

--- src/main/java/forge/gui/deckeditor/DeckDisplay.java
+++ src/main/java/forge/gui/deckeditor/DeckDisplay.java
@@ -15,20 +15,59 @@ import forge.item.ItemPoolView;
  * @version $Id$
  */
 public interface DeckDisplay {
+    /**
+     * 
+     * setDeck.
+     * @param top ItemPoolView<CardPrinted>
+     * @param bottom ItemPoolView<CardPrinted>
+     * @param gameType GameType
+     */
     void setDeck(ItemPoolView<CardPrinted> top, ItemPoolView<CardPrinted> bottom, GameType gameType);
+    /**
+     * 
+     * setItems.
+     * @param <T> InventoryItem
+     * @param topParam ItemPoolView<T>
+     * @param bottomParam ItemPoolView<T>
+     * @param gt GameType
+     */
     <T extends InventoryItem> void setItems(ItemPoolView<T> topParam, ItemPoolView<T> bottomParam, GameType gt);
 
-    //top shows available card pool
-    //if constructed, top shows all cards
-    //if sealed, top shows 5 booster packs
-    //if draft, top shows cards that were chosen
+    /**
+     * 
+     * Top shows available card pool.
+     * if constructed, top shows all cards
+     * if sealed, top shows 5 booster packs
+     * if draft, top shows cards that were chosen
+     * @return ItemPoolView<InventoryItem>
+     */
     ItemPoolView<InventoryItem> getTop();
 
-    //bottom shows cards that the user has chosen for his library
+    //
+    /**
+     * 
+     * Bottom shows cards that the user has chosen for his library.
+     * @return ItemPoolView<InventoryItem>
+     */
     ItemPoolView<InventoryItem> getBottom();
 
+    /**
+     * 
+     * Set title.
+     * @param message String
+     */
     void setTitle(String message);
 
+    /**
+     * 
+     * Get deck.
+     * @return Deck
+     */
     Deck getDeck();
+    /**
+     * 
+     * Get game type.
+     * @return GameType
+     */
     GameType getGameType();
 }
