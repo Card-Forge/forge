4ba72113182c76bd7256183935205237058f97b7
slapshot5 <slapshot5@cardforge.org>
2011-10-17 04:24:51 +0000

    add support for ChosenNumber in Number$ counting

--- src/main/java/forge/card/cardFactory/CardFactoryUtil.java
+++ src/main/java/forge/card/cardFactory/CardFactoryUtil.java
@@ -2862,7 +2862,12 @@ public class CardFactoryUtil {
         // accept straight numbers
         if (l[0].contains("Number$")) {
             String number = l[0].replace("Number$", "");
-            return doXMath(Integer.parseInt(number), m, c);
+            if (number.equals("ChosenNumber")) {
+                return doXMath(c.getChosenNumber(), m, c);
+            }
+            else {
+                return doXMath(Integer.parseInt(number), m, c);
+            }
         }
 
         // Manapool
