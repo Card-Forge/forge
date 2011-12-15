f0b0d0525be59615e21156953beef3e1efa58997
Doublestrike <doublestrike@cardforge.org>
2011-12-15 07:19:38 +0000

    Latest progress on Settings submenu.

--- src/main/java/forge/control/home/ControlSettings.java
+++ src/main/java/forge/control/home/ControlSettings.java
@@ -3,11 +3,15 @@ package forge.control.home;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JRadioButton;
+
 import forge.AllZone;
 import forge.Singletons;
 import forge.error.ErrorViewer;
 import forge.gui.ListChooser;
 import forge.properties.ForgePreferences;
+import forge.properties.ForgePreferences.CardSizeType;
+import forge.properties.ForgePreferences.StackOffsetType;
 import forge.view.home.ViewSettings;
 import forge.view.toolbox.FSkin;
 
@@ -36,12 +40,12 @@ public class ControlSettings {
 
     /** */
     public void addListeners() {
-        this.view.getBtnChooseSkin().addActionListener(new ActionListener() {
+        /*this.view.getLstChooseSkin().addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(final ActionEvent arg0) {
                 doChooseSkin();
             }
-        });
+        });*/
 
         //slapshot5 - work in progress, but I need to check this file in for other changes.
         /*
@@ -68,8 +72,72 @@ public class ControlSettings {
                         .setDeveloperMode(ControlSettings.this.view.getCbDevMode().isSelected());
             }
         });
+
+        this.view.getCbRemoveSmall().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setDeckGenRmvSmall(ControlSettings.this.view.getCbRemoveSmall().isSelected());
+            }
+        });
+
+        this.view.getCbRemoveArtifacts().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setDeckGenRmvArtifacts(ControlSettings.this.view.getCbRemoveArtifacts().isSelected());
+            }
+        });
+
+        this.view.getCbSingletons().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setDeckGenSingletons(ControlSettings.this.view.getCbSingletons().isSelected());
+            }
+        });
+
+        this.view.getCbOldUI().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setOldGui(ControlSettings.this.view.getCbOldUI().isSelected());
+            }
+        });
+
+        this.view.getCbUploadDraft().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setUploadDraftAI(ControlSettings.this.view.getCbUploadDraft().isSelected());
+            }
+        });
+
+        this.view.getCbStackLand().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setStackAiLand(ControlSettings.this.view.getCbStackLand().isSelected());
+            }
+        });
+
+        this.view.getCbRandomFoil().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setRandCFoil(ControlSettings.this.view.getCbRandomFoil().isSelected());
+            }
+        });
+
+        this.view.getCbTextMana().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent arg0) {
+                Singletons.getModel().getPreferences()
+                    .setCardOverlay(ControlSettings.this.view.getCbTextMana().isSelected());
+            }
+        });
     }
-    
+
     private void doChooseSkin() {
         final ListChooser<String> ch = new ListChooser<String>("Choose a skin", 0, 1, FSkin.getSkins());
         if (ch.show()) {
@@ -89,5 +157,26 @@ public class ControlSettings {
                 ErrorViewer.showError(ex);
             }
         }
+    }
+    
+    private void updateSkinList() {
+        
+    }
+
+    /** @param rad0 &emsp; JRadioButton */
+    public void updateStackOffset(JRadioButton rad0) {
+        StackOffsetType sot = StackOffsetType.valueOf(rad0.getText());
+        Singletons.getModel().getPreferences().setStackOffset(sot);
+    }
+
+    /** @param rad0 &emsp; JRadioButton */
+    public void updateCardSize(JRadioButton rad0) {
+        CardSizeType cst = CardSizeType.valueOf(rad0.getText());
+        Singletons.getModel().getPreferences().setCardSize(cst);
+    }
+
+    /** @param rad0 &emsp; JRadioButton */
+    public void updateStackSize(JRadioButton rad0) {
+        Singletons.getModel().getPreferences().setMaxStackSize(Integer.parseInt(rad0.getText()));
     }
 }
--- src/main/java/forge/view/home/ViewSettings.java
+++ src/main/java/forge/view/home/ViewSettings.java
@@ -1,12 +1,16 @@
 package forge.view.home;
 
 import java.awt.Font;
+import java.awt.event.ActionEvent;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
+import javax.swing.AbstractAction;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
 import javax.swing.border.MatteBorder;
 
@@ -26,9 +30,21 @@ public class ViewSettings extends JScrollPane {
     private ControlSettings control;
     private FSkin skin;
     private JPanel viewport;
-    
-    SubButton btnChooseSkin;
-    OptionsCheckBox cbAnte, cbScaleLarger, cbDevMode;
+
+    private JList lstChooseSkin;
+    private String spacer;
+
+    private JCheckBox cbAnte, cbScaleLarger, cbDevMode, cbRemoveSmall, cbRemoveArtifacts,
+        cbOldUI, cbUploadDraft, cbStackLand, cbRandomFoil, cbTextMana,
+        cbSingletons;
+
+    private JRadioButton radStack3, radStack4, radStack5, radStack6, radStack7,
+        radStack8, radStack9, radStack10, radStack11, radStack12;
+
+    private JRadioButton radOffsetTiny, radOffsetSmall, radOffsetMedium, radOffsetLarge;
+
+    private JRadioButton radCardTiny, radCardSmaller, radCardSmall,
+        radCardMedium, radCardLarge, radCardHuge;
     /**
      * 
      * TODO: Write javadoc for Constructor.
@@ -45,10 +61,13 @@ public class ViewSettings extends JScrollPane {
         this.getViewport().setOpaque(false);
         this.setBorder(null);
         this.setViewportView(viewport);
+        this.getVerticalScrollBar().setUnitIncrement(16);
         viewport.setLayout(new MigLayout("insets 0, gap 0, wrap"));
 
+        // Spacing between components is defined here.
+        spacer = ", gapbottom 1%";
         String constraints = "w 80%!, gapleft 10%";
-        String constraints2 = constraints + ", gapbottom 2%";
+        String constraints2 = constraints + spacer;
 
         // Deck Building Options
         JLabel lblTitleDecks = new JLabel("Deck Building Options");
@@ -57,18 +76,21 @@ public class ViewSettings extends JScrollPane {
         lblTitleDecks.setForeground(skin.getColor("text"));
         viewport.add(lblTitleDecks, constraints2 + ", gaptop 2%");
 
-        OptionsCheckBox cbRemoveSmall = new OptionsCheckBox("Remove Small Creatures");
-        NoteLabel lblRemoveSmall = new NoteLabel("Disables 1/1 and 0/X creatures in generated decks.");
+        cbRemoveSmall = new OptionsCheckBox("Remove Small Creatures");
+        cbRemoveSmall.setSelected(Singletons.getModel().getPreferences().isDeckGenRmvSmall());
+        JLabel lblRemoveSmall = new NoteLabel("Disables 1/1 and 0/X creatures in generated decks.");
         viewport.add(cbRemoveSmall, constraints);
         viewport.add(lblRemoveSmall, constraints2);
 
-        OptionsCheckBox cbSingletons = new OptionsCheckBox("Singleton Mode");
-        NoteLabel lblSingletons = new NoteLabel("Disables non-land duplicates in generated decks.");
+        cbSingletons = new OptionsCheckBox("Singleton Mode");
+        cbSingletons.setSelected(Singletons.getModel().getPreferences().isDeckGenSingletons());
+        JLabel lblSingletons = new NoteLabel("Disables non-land duplicates in generated decks.");
         viewport.add(cbSingletons, constraints);
         viewport.add(lblSingletons, constraints2);
 
-        OptionsCheckBox cbRemoveArtifacts = new OptionsCheckBox("Remove Artifacts");
-        NoteLabel lblRemoveArtifacts = new NoteLabel("Disables artifact cards in generated decks.");
+        cbRemoveArtifacts = new OptionsCheckBox("Remove Artifacts");
+        cbRemoveArtifacts.setSelected(Singletons.getModel().getPreferences().isDeckGenRmvArtifacts());
+        JLabel lblRemoveArtifacts = new NoteLabel("Disables artifact cards in generated decks.");
         viewport.add(cbRemoveArtifacts, constraints);
         viewport.add(lblRemoveArtifacts, constraints2);
 
@@ -79,8 +101,9 @@ public class ViewSettings extends JScrollPane {
         lblTitleUI.setForeground(skin.getColor("text"));
         viewport.add(lblTitleUI, constraints2);
 
-        OptionsCheckBox cbOldUI = new OptionsCheckBox("Old UI");
-        NoteLabel lblOldUI = new NoteLabel("Use the user interface from Beta 1.1.9.");
+        cbOldUI = new OptionsCheckBox("Old UI");
+        cbOldUI.setSelected(Singletons.getModel().getPreferences().isOldGui());
+        JLabel lblOldUI = new NoteLabel("Use the user interface from Beta 1.1.9.");
         viewport.add(cbOldUI, constraints);
         viewport.add(lblOldUI, constraints2);
 
@@ -93,31 +116,38 @@ public class ViewSettings extends JScrollPane {
         viewport.add(lblAnte, constraints2);
         */
 
-        OptionsCheckBox cbUploadDraft = new OptionsCheckBox("Upload Draft Pics");
-        NoteLabel lblUploadDraft = new NoteLabel("Sends draft picks to Forge servers for analysis, to improve draft AI.");
+        cbUploadDraft = new OptionsCheckBox("Upload Draft Pics");
+        cbUploadDraft.setSelected(Singletons.getModel().getPreferences().isUploadDraftAI());
+        JLabel lblUploadDraft = new NoteLabel("Sends draft picks to Forge servers for analysis, to improve draft AI.");
         viewport.add(cbUploadDraft, constraints);
         viewport.add(lblUploadDraft, constraints2);
 
-        OptionsCheckBox cbStackLand = new OptionsCheckBox("Stack AI Land");
-        NoteLabel lblStackLand = new NoteLabel("Minimizes mana lock in AI hands, giving a slight advantage to computer.");
+        cbStackLand = new OptionsCheckBox("Stack AI Land");
+        cbStackLand.setSelected(Singletons.getModel().getPreferences().isStackAiLand());
+        JLabel lblStackLand = new NoteLabel("Minimizes mana lock in AI hands, giving a slight advantage to computer.");
         viewport.add(cbStackLand, constraints);
         viewport.add(lblStackLand, constraints2);
 
         cbDevMode = new OptionsCheckBox("Developer Mode");
+        cbDevMode.setSelected(Singletons.getModel().getPreferences().isDeveloperMode());
         this.cbDevMode.setSelected(Singletons.getModel().getPreferences().isDeveloperMode());
-        NoteLabel lblDevMode = new NoteLabel("Enables menu with functions for testing during development.");
+        JLabel lblDevMode = new NoteLabel("Enables menu with functions for testing during development.");
         viewport.add(cbDevMode, constraints);
         viewport.add(lblDevMode, constraints2);
 
-        SubButton btnStackSize = new SubButton("Stack Size: 12");
-        NoteLabel lblStackSize = new NoteLabel("Specify maximum number of spells allowed on stack.");
-        viewport.add(btnStackSize, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
-        viewport.add(lblStackSize, constraints2);
+        JLabel lblTitleStack = new TitleLabel("Stack Size");
+        JLabel lblStackSize = new NoteLabel("Specify maximum number of spells allowed on stack.");
+        viewport.add(lblTitleStack, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
+        viewport.add(lblStackSize, constraints);
 
-        SubButton btnStackOffset = new SubButton("Stack Offset: Large");
-        NoteLabel lblStackOffset = new NoteLabel("Not sure what this does...can someone who does update this text");
-        viewport.add(btnStackOffset, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
-        viewport.add(lblStackOffset, constraints2);
+        populateStackRadios();
+
+        JLabel lblTitleOffset = new TitleLabel("Stack Offset");
+        JLabel lblStackOffset = new NoteLabel("Not sure what this does...can someone who does update this text");
+        viewport.add(lblTitleOffset, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
+        viewport.add(lblStackOffset, constraints);
+
+        populateOffsetRadios();
 
         // Graphic Options
         JLabel lblTitleGraphics = new JLabel("Graphic Options");
@@ -126,26 +156,101 @@ public class ViewSettings extends JScrollPane {
         lblTitleGraphics.setForeground(skin.getColor("text"));
         viewport.add(lblTitleGraphics, constraints2);
 
-        btnChooseSkin = new SubButton("Choose Skin");
-        viewport.add(btnChooseSkin, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
+        JLabel lblTitleSkin = new TitleLabel("Choose Skin");
+        JLabel lblNoteSkin = new NoteLabel("Various user-created themes for Forge backgrounds, fonts, and colors.");
+        viewport.add(lblTitleSkin, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
+        viewport.add(lblNoteSkin, constraints + ", gapbottom 2px");
 
-        OptionsCheckBox cbRandomFoil = new OptionsCheckBox("Random Foil");
-        NoteLabel lblRandomFoil = new NoteLabel("Adds foiled effects to random cards.");
+        lstChooseSkin = new JList();
+        viewport.add(new JScrollPane(lstChooseSkin), "gapleft 10%, h 60px!, w 150px!" + spacer);
+
+        JLabel lblTitleCardSize = new TitleLabel("Card Size");
+        JLabel lblCardSize = new NoteLabel("Size of cards in hand and playing field, when possible");
+        viewport.add(lblTitleCardSize, "gapleft 10%, h 25px!, w 150px!, gapbottom 2px");
+        viewport.add(lblCardSize, constraints);
+
+        populateCardSizeRadios();
+
+        cbRandomFoil = new OptionsCheckBox("Random Foil");
+        cbRandomFoil.setSelected(Singletons.getModel().getPreferences().isRandCFoil());
+        JLabel lblRandomFoil = new NoteLabel("Adds foiled effects to random cards.");
         viewport.add(cbRandomFoil, constraints);
         viewport.add(lblRandomFoil, constraints2);
 
         cbScaleLarger = new OptionsCheckBox("Scale image larger");
-        this.cbScaleLarger.setSelected(Singletons.getModel().getPreferences().isPlayForAnte());
-        NoteLabel lblScaleLarger = new NoteLabel("Not sure what this does...can someone who does update this text");
+        this.cbScaleLarger.setSelected(Singletons.getModel().getPreferences().isScaleLargerThanOriginal());
+        JLabel lblScaleLarger = new NoteLabel("Not sure what this does...can someone who does update this text");
         viewport.add(cbScaleLarger, constraints);
         viewport.add(lblScaleLarger, constraints2);
 
-        OptionsCheckBox cbTextMana = new OptionsCheckBox("Text / Mana Overlay");
-        NoteLabel lblTextMana = new NoteLabel("Overlays each card with basic card-specific information.");
+        cbTextMana = new OptionsCheckBox("Text / Mana Overlay");
+        cbTextMana.setSelected(Singletons.getModel().getPreferences().isCardOverlay());
+        JLabel lblTextMana = new NoteLabel("Overlays each card with basic card-specific information.");
         viewport.add(cbTextMana, constraints);
         viewport.add(lblTextMana, constraints2);
-        
+
         ViewSettings.this.control = new ControlSettings(this);
+    }
+
+    private void populateStackRadios() {
+        JPanel radioContainer = new JPanel();
+        radioContainer.setOpaque(false);
+        radioContainer.setLayout(new MigLayout("insets 0, gap 0, wrap 2"));
+
+        radStack3 = new StackSizeRadio("3");
+        radStack4 = new StackSizeRadio("4");
+        radStack5 = new StackSizeRadio("5");
+        radStack6 = new StackSizeRadio("6");
+        radStack7 = new StackSizeRadio("7");
+        radStack8 = new StackSizeRadio("8");
+        radStack9 = new StackSizeRadio("9");
+        radStack10 = new StackSizeRadio("10");
+        radStack11 = new StackSizeRadio("11");
+        radStack12 = new StackSizeRadio("12");
+
+        String constraints = "w 50px!, h 25px!";
+        radioContainer.add(radStack3, constraints);
+        radioContainer.add(radStack4, constraints);
+        radioContainer.add(radStack5, constraints);
+        radioContainer.add(radStack6, constraints);
+        radioContainer.add(radStack7, constraints);
+        radioContainer.add(radStack8, constraints);
+        radioContainer.add(radStack9, constraints);
+        radioContainer.add(radStack10, constraints);
+        radioContainer.add(radStack11, constraints);
+        radioContainer.add(radStack12, constraints);
+
+        viewport.add(radioContainer, "gapleft 10%" + spacer);
+    }
+
+    private void populateOffsetRadios() {
+        radOffsetTiny = new StackOffsetRadio("Tiny");
+        radOffsetSmall = new StackOffsetRadio("Small");
+        radOffsetMedium = new StackOffsetRadio("Medium");
+        radOffsetLarge = new StackOffsetRadio("Large");
+
+        String constraints = "gapleft 10%, wrap";
+        viewport.add(radOffsetTiny, constraints);
+        viewport.add(radOffsetSmall, constraints);
+        viewport.add(radOffsetMedium, constraints);
+        viewport.add(radOffsetLarge, constraints + spacer);
+    }
+
+    private void populateCardSizeRadios() {
+        radCardTiny = new CardSizeRadio("Tiny");
+        radCardSmaller = new CardSizeRadio("Smaller");
+        radCardSmall = new CardSizeRadio("Small");
+        radCardMedium = new CardSizeRadio("Medium");
+        radCardLarge = new CardSizeRadio("Large");
+        radCardHuge = new CardSizeRadio("Huge");
+
+        String constraints = "gapleft 10%, wrap";
+        viewport.add(radCardTiny, constraints);
+        viewport.add(radCardSmaller, constraints);
+        viewport.add(radCardSmall, constraints);
+        viewport.add(radCardMedium, constraints);
+        viewport.add(radCardLarge, constraints);
+        viewport.add(radCardHuge, constraints + spacer);
     }
 
     /** Consolidates checkbox styling in one place. */
@@ -172,6 +277,80 @@ public class ViewSettings extends JScrollPane {
         }
     }
 
+    /** Consolidates checkbox styling in one place. */
+    private class OptionsRadio extends JRadioButton {
+        public OptionsRadio(String txt0) {
+            super();
+            setText(txt0);
+            setFont(skin.getFont1().deriveFont(Font.BOLD, 12));
+            setForeground(skin.getColor("text"));
+            setBackground(skin.getColor("hover"));
+            setOpaque(false);
+
+            this.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseEntered(MouseEvent e) {
+                    setOpaque(true);
+                }
+
+                @Override
+                public void mouseExited(MouseEvent e) {
+                    setOpaque(false);
+                }
+            });
+        }
+    }
+
+    private class StackSizeRadio extends OptionsRadio {
+        public StackSizeRadio(String txt0) {
+            super(txt0);
+
+            if(Singletons.getModel().getPreferences().getMaxStackSize() == Integer.parseInt(txt0)) {
+                setSelected(true);
+            }
+
+            this.addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    control.updateStackSize(StackSizeRadio.this);
+                }
+            });
+        }
+    }
+
+    private class StackOffsetRadio extends OptionsRadio {
+        public StackOffsetRadio(String txt0) {
+            super(txt0);
+            if(Singletons.getModel().getPreferences().getStackOffset().toString() == txt0) {
+                setSelected(true);
+            }
+
+            this.addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    control.updateStackOffset(StackOffsetRadio.this);
+                }
+            });
+        }
+    }
+
+    private class CardSizeRadio extends OptionsRadio {
+        public CardSizeRadio(String txt0) {
+            super(txt0);
+            if(Singletons.getModel().getPreferences().getCardSize().toString() == txt0) {
+                setSelected(true);
+            }
+
+            this.addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent arg0) {
+                    control.updateCardSize(CardSizeRadio.this);
+                }
+            });
+        }
+    }
+
+    /** Consolidates notation label styling in one place. */
     private class NoteLabel extends JLabel {
         public NoteLabel(String txt0) {
             super(txt0);
@@ -179,23 +358,77 @@ public class ViewSettings extends JScrollPane {
             setForeground(skin.getColor("text"));
         }
     }
-    
-    public SubButton getBtnChooseSkin() {
-        return btnChooseSkin;
+
+    /** Consolidates title label styling in one place. */
+    private class TitleLabel extends JLabel {
+        public TitleLabel(String txt0) {
+            super(txt0);
+            setFont(skin.getFont1().deriveFont(Font.BOLD, 12));
+            setForeground(skin.getColor("text"));
+        }
     }
-    
+
+    /** @return JList */
+    public JList getLstChooseSkin() {
+        return lstChooseSkin;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbRemoveSmall() {
+        return cbRemoveSmall;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbSingletons() {
+        return cbSingletons;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbRemoveArtifacts() {
+        return cbRemoveArtifacts;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbOldUI() {
+        return cbOldUI;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbUploadDraft() {
+        return cbUploadDraft;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbStackLand() {
+        return cbStackLand;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbTextMana() {
+        return cbTextMana;
+    }
+
+    /** @return JCheckBox */
+    public JCheckBox getCbRandomFoil() {
+        return cbRandomFoil;
+    }
+
+    /** @return JCheckBox */
     public JCheckBox getCbAnte() {
         return cbAnte;
     }
-    
+
+    /** @return JCheckBox */
     public JCheckBox getCbScaleLarger() {
         return cbScaleLarger;
     }
-    
+
+    /** @return JCheckBox */
     public JCheckBox getCbDevMode() {
         return cbDevMode;
     }
-    
+
+    /** @return ControlSettings */
     public ControlSettings getController() {
         return ViewSettings.this.control;
     }
