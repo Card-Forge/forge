46e32c0ff8ba9aa0f90d7d0d90dc3a1429973758
Sol <zenchristo@gmail.com>
2011-12-17 13:48:00 +0000

    - Missed a file last night.

---
+++ src/main/java/forge/gui/input/InputMana.java
@@ -0,0 +1,18 @@
+package forge.gui.input;
+
+/** 
+ * TODO: Write javadoc for this type.
+ *
+ */
+public abstract class InputMana extends Input {
+
+    private static final long serialVersionUID = -9133423708688480255L;
+
+    /**
+     * <p>
+     * selectManaPool.
+     * </p>
+     * @param color a String that represents the Color the mana is coming from
+     */
+    public abstract void selectManaPool(String color);
+}
