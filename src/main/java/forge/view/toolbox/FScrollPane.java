34453c981a19ca5599a1ff6592faf5678493a6a4
Doublestrike <doublestrike@cardforge.org>
2012-02-22 08:27:01 +0000

    Minor bug fix.

--- src/main/java/forge/view/toolbox/FScrollPane.java
+++ src/main/java/forge/view/toolbox/FScrollPane.java
@@ -18,18 +18,18 @@ public class FScrollPane extends JScrollPane {
      * @param c0 {@link java.awt.Component}
      */
     public FScrollPane(final Component c0) {
-        super(c0, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        this(c0, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
     }
 
     /**
      * A very basic extension of JScrollPane to centralize common styling changes.
      * 
      * @param c0 &emsp; Viewport component.
-     * @param horizontal0 &emsp; Horizontal scroll bar policy
      * @param vertical0 &emsp; Vertical scroll bar policy
+     * @param horizontal0 &emsp; Horizontal scroll bar policy
      */
-    public FScrollPane(final Component c0, final int horizontal0, final int vertical0) {
-        super(c0, horizontal0, vertical0);
+    public FScrollPane(final Component c0, final int vertical0, final int horizontal0) {
+        super(c0, vertical0, horizontal0);
         getVerticalScrollBar().setUnitIncrement(16);
         getViewport().setOpaque(false);
         setBorder(new LineBorder(FSkin.getColor(FSkin.Colors.CLR_BORDERS), 1));
