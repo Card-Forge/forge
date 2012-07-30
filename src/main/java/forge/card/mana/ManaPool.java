b89592402498a3921cb494772bed4429cc641eea
Sloth <sloth@cardforge.org>
2012-07-30 05:51:21 +0000

    - Re-added UpdateObserver calls in ManaPool.

--- src/main/java/forge/card/mana/ManaPool.java
+++ src/main/java/forge/card/mana/ManaPool.java
@@ -151,6 +151,7 @@ public class ManaPool {
                 this.floatingTotals[i]++;
             }
         }
+        owner.updateObservers();
     }
 
     /**
@@ -181,7 +182,7 @@ public class ManaPool {
 
         if (this.floatingMana.size() == 0) {
             this.calculateManaTotals();
-            //this.owner.updateObservers();
+            this.owner.updateObservers();
             return numRemoved;
         }
 
@@ -201,7 +202,7 @@ public class ManaPool {
             this.floatingMana.clear();
         }
         this.calculateManaTotals();
-        //this.owner.updateObservers();
+        this.owner.updateObservers();
 
         return numRemoved;
     }
@@ -380,7 +381,7 @@ public class ManaPool {
                     this.floatingTotals[i]--;
                 }
             }
-            //owner.updateObservers();
+            owner.updateObservers();
         }
     }
 
