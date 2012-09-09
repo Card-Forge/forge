4c5bbff21290ed921c7aaa248f8f40dce397f35e
Sol <zenchristo@gmail.com>
2012-09-09 02:23:58 +0000

    - UnsortedListModel that was moved from DualListBox actually added.

---
+++ src/main/java/forge/gui/UnsortedListModel.java
@@ -0,0 +1,59 @@
+package forge.gui;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.swing.AbstractListModel;
+
+@SuppressWarnings("serial")
+class UnsortedListModel extends AbstractListModel {
+    List<Object> model;
+
+    public UnsortedListModel() {
+        model = new LinkedList<Object>();
+    }
+
+    public int getSize() {
+        return model.size();
+    }
+
+    public Object getElementAt(int index) {
+        return model.toArray()[index];
+    }
+
+    public void add(Object element) {
+        if (model.add(element)) {
+            fireContentsChanged(this, 0, getSize());
+        }
+    }
+
+    public void addAll(Object elements[]) {
+        Collection<Object> c = Arrays.asList(elements);
+        model.addAll(c);
+        fireContentsChanged(this, 0, getSize());
+    }
+
+    public void clear() {
+        model.clear();
+        fireContentsChanged(this, 0, getSize());
+    }
+
+    public boolean contains(Object element) {
+        return model.contains(element);
+    }
+
+    public Iterator iterator() {
+        return model.iterator();
+    }
+
+    public boolean removeElement(Object element) {
+        boolean removed = model.remove(element);
+        if (removed) {
+            fireContentsChanged(this, 0, getSize());
+        }
+        return removed;
+    }
+}
