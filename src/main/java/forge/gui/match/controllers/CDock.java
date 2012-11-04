774ffea38977c86589622bbfd5c3f901e8168c03
Agetian <stavdev@mail.ru>
2012-11-04 13:44:46 +0000

    Minor code style fix for the previous commit.

--- src/main/java/forge/gui/match/controllers/CDock.java
+++ src/main/java/forge/gui/match/controllers/CDock.java
@@ -213,10 +213,11 @@ public enum CDock implements ICDoc {
         //       (arcState == 1). If that mode is ever implemented correctly,
         //       the "if" block below may be removed and the code above (the
         //       original code which wraps at (arcState == 3) may be enabled.
-        if (arcState == 0)
+        if (arcState == 0) {
             arcState = 2;
-        else
+        } else {
             arcState = 0;
+        }
 
         refreshArcStateDisplay();
         FView.SINGLETON_INSTANCE.getFrame().repaint(); // repaint the match UI
