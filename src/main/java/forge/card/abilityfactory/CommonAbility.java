5863d7ab9fb2d4bc5243146e9f4e961b70ce86b3
Chris <chris.h@cardforge.org>
2012-11-10 14:53:59 +0000

    - CheckStyle.

--- src/main/java/forge/card/abilityfactory/CommonAbility.java
+++ src/main/java/forge/card/abilityfactory/CommonAbility.java
@@ -16,31 +16,32 @@ import forge.card.spellability.Target;
 public class CommonAbility extends AbilityActivated {
     private final SpellEffect effect;
     private final SpellAiLogic ai;
-    
+
     private static final long serialVersionUID = -4183793555528531978L;
 
-    public CommonAbility(ApiType api0, Card sourceCard, Cost abCost, Target tgt, Map<String,String> params0, SpellEffect effect0, SpellAiLogic ai0) {
+    public CommonAbility(ApiType api0, Card sourceCard, Cost abCost, Target tgt, Map<String, String> params0, SpellEffect effect0, SpellAiLogic ai0) {
         super(sourceCard, abCost, tgt);
         params = params0;
         api = api0;
         effect = effect0;
         ai = ai0;
-        
-        if ( effect0 instanceof ManaEffect || effect0 instanceof ManaReflectedEffect )
-        {
+
+        if (effect0 instanceof ManaEffect || effect0 instanceof ManaReflectedEffect) {
+
             this.setManaPart(new AbilityManaPart(sourceCard, params));
             this.setUndoable(true); // will try at least
         }
-        
-        if ( effect0 instanceof ChangeZoneEffect || effect0 instanceof ChangeZoneAllEffect )
+
+        if (effect0 instanceof ChangeZoneEffect || effect0 instanceof ChangeZoneAllEffect) {
             AbilityFactory.adjustChangeZoneTarget(params, this);
+        }
     }
-    
+
     @Override
     public String getStackDescription() {
         return effect.getStackDescriptionWithSubs(params, this);
     }
-    
+
     /* (non-Javadoc)
      * @see forge.card.spellability.AbilityActivated#getCopy()
      */
@@ -59,7 +60,7 @@ public class CommonAbility extends AbilityActivated {
     public void resolve() {
         effect.resolve(this);
     }
-    
+
     @Override
     public boolean canPlayAI() {
         return ai.canPlayAIWithSubs(getActivatingPlayer(), this);
