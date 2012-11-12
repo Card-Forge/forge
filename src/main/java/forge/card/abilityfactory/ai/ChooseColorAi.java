fb0b2f2471655be757149eae33702dfb756650ff
Chris <chris.h@cardforge.org>
2012-11-12 02:00:38 +0000

    - CheckStyle.

--- src/main/java/forge/card/abilityfactory/ai/ChooseColorAi.java
+++ src/main/java/forge/card/abilityfactory/ai/ChooseColorAi.java
@@ -6,8 +6,7 @@ import forge.game.player.Player;
 import forge.util.MyRandom;
 
 public class ChooseColorAi extends SpellAiLogic {
-    
-    
+
     @Override
     protected boolean canPlayAI(Player aiPlayer, SpellAbility sa) {
         if (!sa.hasParam("AILogic")) {
@@ -16,13 +15,12 @@ public class ChooseColorAi extends SpellAiLogic {
         boolean chance = MyRandom.getRandom().nextFloat() <= Math.pow(.6667, sa.getActivationsThisTurn());
         return chance;
     }
-    
+
     @Override
     public boolean chkAIDrawback(SpellAbility sa, Player aiPlayer) {
         return true;
     }
-    
-    
+
     @Override
     protected boolean doTriggerAINoCost(Player ai, SpellAbility sa, boolean mandatory) {
         return mandatory || canPlayAI(ai, sa);
