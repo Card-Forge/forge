9caf5bafdb7375bb8938486738744709ec181213
Chris <chris.h@cardforge.org>
2012-11-13 13:31:23 +0000

    - CheckStyle.

--- src/main/java/forge/card/abilityfactory/ai/DigAi.java
+++ src/main/java/forge/card/abilityfactory/ai/DigAi.java
@@ -12,7 +12,7 @@ import forge.game.player.Player;
 import forge.game.zone.ZoneType;
 import forge.util.MyRandom;
 
-    
+
 public class DigAi extends SpellAiLogic {
     /* (non-Javadoc)
      * @see forge.card.abilityfactory.SpellAiLogic#canPlayAI(forge.game.player.Player, java.util.Map, forge.card.spellability.SpellAbility)
