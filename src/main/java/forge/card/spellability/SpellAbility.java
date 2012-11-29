45b210ddc2e6556aee4ccf5855299cc0d8392a1f
ArsenalNut <arsenalnut@cardforge.org>
2012-11-29 15:13:34 +0000

    changed "getParentTargeting" methods to skip checking the "child" SpellAbility first

--- src/main/java/forge/card/spellability/SpellAbility.java
+++ src/main/java/forge/card/spellability/SpellAbility.java
@@ -1705,11 +1705,12 @@ public abstract class SpellAbility implements ISpellAbility {
 
         while (parent.getParent() != null) {
 
+            parent = parent.getParent();
             Target tgt = parent.getTarget();
             if (tgt != null && tgt.getTargetCards() != null && !tgt.getTargetCards().isEmpty()) {
                 break;
             }
-            parent = parent.getParent();
+
         }
         return parent;
     }
@@ -1725,11 +1726,12 @@ public abstract class SpellAbility implements ISpellAbility {
         SpellAbility parent = this;
         while (parent.getParent() != null) {
 
+            parent = parent.getParent();
             Target tgt = parent.getTarget();
             if (tgt != null && tgt.getTargetSAs() != null && !tgt.getTargetSAs().isEmpty()) {
                 break;
             }
-            parent = parent.getParent();
+
         }
         return parent;
     }
@@ -1745,11 +1747,12 @@ public abstract class SpellAbility implements ISpellAbility {
         SpellAbility parent = this;
         while (parent.getParent() != null) {
 
+            parent = parent.getParent();
             Target tgt = parent.getTarget();
             if (tgt != null && tgt.getTargetPlayers() != null && !tgt.getTargetPlayers().isEmpty()) {
                 break;
             }
-            parent = parent.getParent();
+
         }
         return parent;
     }
