95f6f7d4489c5b7e9b22fae3c737141266199fdc
Sol <zenchristo@gmail.com>
2013-01-03 02:51:34 +0000

    - Added FollowedBy Parameter in AddPhase which is common to many of these combat + main phase adders - Added Relentless Assault

--- src/main/java/forge/card/abilityfactory/effects/AddPhaseEffect.java
+++ src/main/java/forge/card/abilityfactory/effects/AddPhaseEffect.java
@@ -22,10 +22,21 @@ public class AddPhaseEffect extends SpellEffect {
             after = PhaseType.smartValueOf(sa.getParam("AfterPhase"));
         }
         else {
+            // If "AfterPhase" param is missing it means the added Phase comes after this Phase
             after = phaseHandler.getPhase();
         }
-
         phaseHandler.addExtraPhase(after, extra);
+        
+        if (sa.hasParam("FollowedBy")) {
+            String followedBy = sa.getParam("FollowedBy");
+            PhaseType followingExtra;
+            if ("ThisPhase".equals(followedBy)) {
+                followingExtra = phaseHandler.getPhase();
+            } else {
+                followingExtra = PhaseType.smartValueOf(followedBy);
+            }
+            PhaseType followingAfter = extra.equals(PhaseType.COMBAT_BEGIN) ? PhaseType.COMBAT_END : extra;
+            phaseHandler.addExtraPhase(followingAfter, followingExtra);
+        }
     }
-
 }
