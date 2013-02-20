e7b8254db9d054c8ace0f3efbaabea6ceb35e134
Maxmtg <maxmtg@cardforge.org>
2013-02-20 13:32:53 +0000

    restricted class visibility and performed some style adjustments on CardFaceRules

--- src/main/java/forge/card/CardFaceRules.java
+++ src/main/java/forge/card/CardFaceRules.java
@@ -9,7 +9,6 @@ import java.util.TreeMap;
 
 import org.apache.commons.lang3.StringUtils;
 
-
 import forge.card.mana.ManaCost;
 
 //
@@ -20,51 +19,49 @@ import forge.card.mana.ManaCost;
  * TODO: Write javadoc for this type.
  *
  */
-public class CardFaceRules implements ICardCharacteristics {
+final class CardFaceRules implements ICardCharacteristics {
 
     private final String name;
     private CardType type = null;
     private ManaCost manaCost = ManaCost.NO_COST;
     private ColorSet color = null;
-    
+
     private String oracleText = null;
     private int iPower = -1;
     private int iToughness = -1;
     private String power = null;
     private String toughness = null;
     private int initialLoyalty = 0;
-    
+
     private final List<String> keywords = new ArrayList<String>();
 
     // these implement ICardCharacteristics 
-    @Override public String getOracleText()         { return oracleText; }
-    @Override public int getIntPower()              { return iPower; }
-    @Override public int getIntToughness()          { return iToughness; }
-    @Override public String getPower()              { return power; }
-    @Override public String getToughness()          { return toughness; }
+    @Override public final String getOracleText()         { return oracleText; }
+    @Override public final int getIntPower()              { return iPower; }
+    @Override public final int getIntToughness()          { return iToughness; }
+    @Override public final String getPower()              { return power; }
+    @Override public final String getToughness()          { return toughness; }
     @Override public int getInitialLoyalty()        { return initialLoyalty; }
     @Override public final String getName()         { return this.name; }
     @Override public final CardType getType()       { return this.type; }
     @Override public final ManaCost getManaCost()   { return this.manaCost; }
     @Override public final ColorSet getColor()      { return this.color; }
-    @Override public Iterable<String> getKeywords() { return keywords; }
-    
-    
-        
-    // setters to allow parsers supply values here
-    public CardFaceRules(String name0) { this.name = name0; if ( StringUtils.isBlank(name0) ) throw new RuntimeException("Card name is empty"); }
-    
-    public void setType(CardType type0)             { this.type = type0; }
-    public void setManaCost(ManaCost manaCost0)     { this.manaCost = manaCost0; }
-    public void setColor(ColorSet color0)           { this.color = color0; }
-    public void setOracleText(String text)          { this.oracleText = text; }
-    public void addKeyword(String value)            { this.keywords.add(value); }
-    public void setInitialLoaylty(int value)        { this.initialLoyalty = value; }
-    public Map<String, CardInSet> getSetsData()     { return this.setsPrinted; } // reader will add sets here
+    @Override public final Iterable<String> getKeywords() { return keywords; }
 
-    
+
+    // Here come setters to allow parser supply values
+    public CardFaceRules(String name0) { this.name = name0; if ( StringUtils.isBlank(name0) ) throw new RuntimeException("Card name is empty"); }
+
+    public final void setType(CardType type0)             { this.type = type0; }
+    public final void setManaCost(ManaCost manaCost0)     { this.manaCost = manaCost0; }
+    public final void setColor(ColorSet color0)           { this.color = color0; }
+    public final void setOracleText(String text)          { this.oracleText = text; }
+    public final void addKeyword(String value)            { this.keywords.add(value); }
+    public final void setInitialLoaylty(int value)        { this.initialLoyalty = value; }
+    public final Map<String, CardInSet> getSetsData()     { return this.setsPrinted; } // reader will add sets here
+
     public void setPtText(String value) {
-        final int slashPos = value == null ? -1 : value.indexOf('/');
+        final int slashPos = value.indexOf('/');
         if (slashPos == -1) {
             throw new RuntimeException(String.format("Creature '%s' has bad p/t stats", this.getName()));
         }
@@ -79,9 +76,8 @@ public class CardFaceRules implements ICardCharacteristics {
         if ( manaCost == null && color == null ) System.err.println(name + " has neither ManaCost nor Color");
         if ( color == null ) color = ColorSet.fromManaCost(manaCost);
     }
-    
 
-    
+
     // This should not be here
     private final Map<String, CardInSet> setsPrinted = new TreeMap<String, CardInSet>(String.CASE_INSENSITIVE_ORDER);
     @Override public Set<Entry<String, CardInSet>> getSetsPrinted() { return this.setsPrinted.entrySet(); }
@@ -92,11 +88,5 @@ public class CardFaceRules implements ICardCharacteristics {
         }
         throw new RuntimeException(String.format("Card '%s' was never printed in set '%s'", this.getName(), setCode));
     }
-
-
-
-
-
-
 
 }
