84d64ef8ebf63ea701dee7b0e1d4eb1d5ed52acf
Maxmtg <maxmtg@cardforge.org>
2013-03-01 22:40:50 +0000

    IZone - adjusted parameter type or remove method thrown away get/setUpdate methods battlefield.getCards(false) will return R/O list of cards - might lead to exceptions if callers attempt to modify the method's result.

--- src/main/java/forge/game/zone/PlayerZoneBattlefield.java
+++ src/main/java/forge/game/zone/PlayerZoneBattlefield.java
@@ -17,7 +17,6 @@
  */
 package forge.game.zone;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import com.google.common.base.Predicate;
@@ -181,7 +180,7 @@ public class PlayerZoneBattlefield extends PlayerZone {
 
     /** {@inheritDoc} */
     @Override
-    public final void remove(final Object o) {
+    public final void remove(final Card o) {
 
         super.remove(o);
 
@@ -276,7 +275,7 @@ public class PlayerZoneBattlefield extends PlayerZone {
         // getCards(false) to get Phased Out cards
 
         if (!filter) {
-            return new ArrayList<Card>(cardList);
+            return super.getCards(false);
         }
         return Lists.newArrayList(Iterables.filter(cardList, isNotPhased));
 
