1728cf44402df5da5b7f263bdd17eab4011632ac
Sol <zenchristo@gmail.com>
2013-03-15 00:12:11 +0000

    - Phased out cards will cause spells targeting them to fizzle again

--- src/main/java/forge/Card.java
+++ src/main/java/forge/Card.java
@@ -8911,6 +8911,10 @@ public class Card extends GameEntity implements Comparable<Card> {
         if (this.hasProtectionFrom(sa.getSourceCard())) {
             return false;
         }
+        
+        if (this.isPhasedOut()) {
+            return false;
+        }
 
         if (this.getKeyword() != null) {
             final Card source = sa.getSourceCard();
