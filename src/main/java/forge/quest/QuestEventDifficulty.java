6d30afb166a34417153dd4072794fea891699de1
Maxmtg <maxmtg@cardforge.org>
2013-07-02 10:55:54 +0000

    fix npe parsing quest challenges without difficulty

--- src/main/java/forge/quest/QuestEventDifficulty.java
+++ src/main/java/forge/quest/QuestEventDifficulty.java
@@ -1,5 +1,7 @@
 package forge.quest;
 
+import org.apache.commons.lang3.StringUtils;
+
 /** 
  * TODO: Write javadoc for this type.
  *
@@ -21,6 +23,9 @@ public enum QuestEventDifficulty {
     }
     
     public static QuestEventDifficulty fromString(String src) {
+        if ( StringUtils.isBlank(src) )
+            return MEDIUM; // player have custom files, that didn't specify a valid difficulty
+
         for(QuestEventDifficulty qd : QuestEventDifficulty.values()) {
             if( src.equalsIgnoreCase(qd.inFile) || src.equalsIgnoreCase(qd.name()) )
                 return qd;
