c8ee4740257e1a31853d9d0b43b03b483e4e3665
drdev <drdev@cardforge.org>
2013-08-05 08:00:49 +0000

    Cleanup file header

--- src/main/java/forge/gui/toolbox/FSkin.java
+++ src/main/java/forge/gui/toolbox/FSkin.java
@@ -2,9 +2,7 @@
  * Forge: Play Magic: the Gathering.
  * Copyright (C) 2011  Forge Team
  *
- *
-import forge.view.SplashFrame;
- This program is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
