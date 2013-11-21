c4be1b86973052ad70f49dcf59dbdf8273abf089
Maxmtg <maxmtg@cardforge.org>
2013-11-21 21:27:27 +0000

    inlined type casts

--- forge-gui/src/main/java/forge/util/AwtUtil.java
+++ forge-gui/src/main/java/forge/util/AwtUtil.java
@@ -3,13 +3,6 @@ package forge.util;
 import java.awt.Component;
 import java.awt.Container;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class AwtUtil {
 
@@ -24,16 +17,15 @@ public class AwtUtil {
         return comps;
     }
 
+    @SuppressWarnings("unchecked")
     private static <T extends Component> void searchForComponents(Class<T> compType, Component searchComp, ArrayList<T> comps) {
-        T comp = ReflectionUtil.safeCast(searchComp, compType);
-        if (comp != null) {
-            comps.add(comp);
+        if (compType.isInstance(searchComp)) {
+            comps.add((T) searchComp);
         }
         
-        Container container = ReflectionUtil.safeCast(searchComp, Container.class);
-        if (container != null) {
+        if (searchComp instanceof Container) {
             //search child components
-            for (Component c : container.getComponents()) {
+            for (Component c : ((Container)searchComp).getComponents()) {
                 searchForComponents(compType, c, comps);
             }
         }
