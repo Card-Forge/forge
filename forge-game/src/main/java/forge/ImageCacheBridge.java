1dbc34fe629a2d0a610f3eab9ab0343deda8e1d2
Maxmtg <maxmtg@cardforge.org>
2014-01-21 06:16:10 +0000

    move command and constant, set up bridges for dependencies

---
+++ forge-game/src/main/java/forge/ImageCacheBridge.java
@@ -0,0 +1,14 @@
+package forge;
+
+import forge.item.InventoryItem;
+
+public class ImageCacheBridge {
+    
+    public static Methods instance;
+    
+    public interface Methods {
+        String getImageKey(InventoryItem cp, boolean altState);
+        String getTokenKey(String imageName);
+        String getMorphImage();
+    }
+}
---
+++ forge-gui/src/main/java/forge/PreferencesProvider.java
@@ -0,0 +1,45 @@
+package forge;
+
+import forge.PreferencesBridge.PreferencesSet;
+import forge.properties.ForgePreferences.FPref;
+
+public class PreferencesProvider implements PreferencesSet {
+    @Override
+    public boolean getEnableAiCheats() {
+        return Singletons.getModel().getPreferences().getPrefBoolean(FPref.UI_ENABLE_AI_CHEATS);
+    }
+
+
+    @Override
+    public boolean getCloneModeSource() {
+        return Singletons.getModel().getPreferences().getPrefBoolean(FPref.UI_CLONE_MODE_SOURCE);
+    }
+
+    @Override
+    public String getLogEntryType() {
+        return Singletons.getModel().getPreferences().getPref(FPref.DEV_LOG_ENTRY_TYPE);
+    }
+
+
+    @Override
+    public String getCurrentAiProfile() {
+        return Singletons.getModel().getPreferences().getPref(FPref.UI_CURRENT_AI_PROFILE);
+    }
+
+    @Override
+    public boolean canRandomFoil() {
+        return Singletons.getModel().getPreferences().getPrefBoolean(FPref.UI_RANDOM_FOIL);
+    }
+
+
+    @Override
+    public boolean isManaBurnEnabled() {
+        // TODO Auto-generated method stub
+        return Singletons.getModel().getPreferences().getPrefBoolean(FPref.UI_MANABURN);
+    }
+
+    @Override
+    public boolean areBlocksFree() {
+        return Singletons.getModel().getPreferences().getPrefBoolean(FPref.MATCHPREF_PROMPT_FREE_BLOCKS);
+    }
+}
