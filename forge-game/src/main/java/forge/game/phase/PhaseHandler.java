b0939aeed8704b1f5db1fe1b585222635c6ef983
Maxmtg <maxmtg@cardforge.org>
2014-02-08 21:10:23 +0000

    bug fix

--- forge-game/src/main/java/forge/game/phase/PhaseHandler.java
+++ forge-game/src/main/java/forge/game/phase/PhaseHandler.java
@@ -977,7 +977,7 @@ public class PhaseHandler implements java.io.Serializable {
                         do {
                             addedAnythingToStack |= game.getStack().chooseOrderOfSimultaneousStackEntry(whoAddsToStack);
                             whoAddsToStack = game.getNextPlayerAfter(whoAddsToStack);
-                        } while( whoAddsToStack != null && whoAddsToStack != pPlayerPriority);
+                        } while( whoAddsToStack != null && whoAddsToStack != playerTurn);
                     } while(!addedAnythingToStack);
 
                     chosenSa = pPlayerPriority.getController().chooseSpellAbilityToPlay();
