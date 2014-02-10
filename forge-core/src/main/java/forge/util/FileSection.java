b22ce18572529821b40b89cce410063e2d7e3a6a
Agetian <stavdev@mail.ru>
2014-02-10 08:34:09 +0000

    - Fix imports.

--- forge-core/src/main/java/forge/util/FileSection.java
+++ forge-core/src/main/java/forge/util/FileSection.java
@@ -24,8 +24,6 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import java.util.regex.Pattern;
 
 /**
