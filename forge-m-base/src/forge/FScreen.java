7d5d59343765ed12777e11b5f4351e8452871987
drdev <drdev@cardforge.org>
2014-02-23 07:51:12 +0000

    Add background behind header label

--- forge-m-base/src/forge/FScreen.java
+++ forge-m-base/src/forge/FScreen.java
@@ -12,6 +12,10 @@ import forge.utils.Utils;
 
 public abstract class FScreen extends FContainer {
     private static final FSkinColor clrTheme = FSkinColor.get(Colors.CLR_THEME);
+    private static final FSkinColor clr = clrTheme.stepColor(0);
+    private static final FSkinColor a100 = clr.alphaColor(100f / 255f);
+    private static final FSkinColor d40 = clr.stepColor(-40);
+    private static final FSkinColor d80 = clr.stepColor(-80);
 
     private final FLabel btnBack, lblHeader, btnMenu;
 
@@ -96,7 +100,19 @@ public abstract class FScreen extends FContainer {
 
     @Override
     protected void drawBackground(Graphics g) {
-        g.drawImage(FSkinImage.BG_TEXTURE, 0, 0, getWidth(), getHeight());
-        g.fillRect(clrTheme, 0, 0, getWidth(), getHeight());
+    	float w = getWidth();
+    	float h = getHeight();
+        g.drawImage(FSkinImage.BG_TEXTURE, 0, 0, w, h);
+        g.fillRect(clrTheme, 0, 0, w, h);
+
+        if (lblHeader != null) { //draw custom background behind header label
+        	float x = lblHeader.getLeft() + 6;
+        	float y = lblHeader.getTop() + 1;
+        	w -= x;
+        	h = lblHeader.getHeight() - 2;
+            g.fillRect(d80, x, y + 5, w, h - 5);
+            g.fillRect(a100, x + 5, y, w - 5, h - 5);
+            g.drawRect(d40, x + 5, y, w - 5, h - 5);
+        }
     }
 }
