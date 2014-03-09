5a821a7e3f5ad68f7eb8c0b4ba933b77d78749d9
drdev <drdev@cardforge.org>
2014-03-09 21:02:23 +0000

    Code cleanup

---
+++ forge-m-base/src/forge/toolbox/FFileChooser.java
@@ -0,0 +1,17 @@
+package forge.toolbox;
+
+import java.io.File;
+
+public class FFileChooser {
+
+    public boolean show(String title) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    public File getSelectedFile() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
