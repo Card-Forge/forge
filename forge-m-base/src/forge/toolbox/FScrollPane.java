0a2e6184dc5bd401708d9072a507b1bfc8d52566
drdev <drdev@cardforge.org>
2014-03-29 22:11:44 +0000

    Support panning scrollable regions

--- forge-m-base/src/forge/toolbox/FScrollPane.java
+++ forge-m-base/src/forge/toolbox/FScrollPane.java
@@ -26,11 +26,58 @@ public abstract class FScrollPane extends FContainer {
         return scrollBounds.height;
     }
 
+    public float getMaxScrollLeft() {
+        return getScrollWidth() - getWidth();
+    }
+
+    public float getMaxScrollTop() {
+        return getScrollHeight() - getHeight();
+    }
+
+    public void setScrollPositions(float scrollLeft0, float scrollTop0) {
+        if (scrollLeft0 < 0) {
+            scrollLeft0 = 0;
+        }
+        else {
+            float maxScrollLeft = getMaxScrollLeft();
+            if (scrollLeft0 > maxScrollLeft) {
+                scrollLeft0 = maxScrollLeft;
+            }
+        }
+        if (scrollTop0 < 0) {
+            scrollTop0 = 0;
+        }
+        else {
+            float maxScrollTop = getMaxScrollTop();
+            if (scrollTop0 > maxScrollTop) {
+                scrollTop0 = maxScrollTop;
+            }
+        }
+        float dx = scrollLeft - scrollLeft0;
+        float dy = scrollTop - scrollTop0;
+        if (dx == 0 && dy == 0) { return; } //do nothing if scroll didn't change
+
+        scrollLeft = scrollLeft0;
+        scrollTop = scrollTop0;
+
+        //shift position of all children based on change in scroll positions
+        for (FDisplayObject obj : getChildren()) {
+            obj.setPosition(obj.getLeft() + dx, obj.getTop() + dy);
+        }
+    }
+
     @Override
     protected final void doLayout(float width, float height) {
         scrollBounds = layoutAndGetScrollBounds(width, height);
         scrollBounds.increaseWidthTo(width); //ensure scroll bounds extends at least to visible bounds
         scrollBounds.increaseHeightTo(height);
+
+        //attempt to restore current scroll positions after reseting them in order to update positions of newly laid out stuff
+        float oldScrollLeft = scrollLeft;
+        float oldScrollTop = scrollTop;
+        scrollLeft = 0;
+        scrollTop = 0;
+        setScrollPositions(oldScrollLeft, oldScrollTop);
     }
 
     protected abstract ScrollBounds layoutAndGetScrollBounds(float visibleWidth, float visibleHeight);
@@ -67,6 +114,22 @@ public abstract class FScrollPane extends FContainer {
                 height = height0;
             }
         }
+    }
+
+    @Override
+    public boolean fling(float velocityX, float velocityY) {
+        return true;
+    }
+
+    @Override
+    public boolean pan(float x, float y, float deltaX, float deltaY) {
+        setScrollPositions(scrollLeft - deltaX, scrollTop - deltaY);
+        return true;
+    }
+
+    @Override
+    public boolean panStop(float x, float y) {
+        return true;
     }
 
     public final void draw(Graphics g) {
