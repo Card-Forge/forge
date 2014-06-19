fae0dfeb459c298b9cae2abdff8f1891393c3946
Krazy <mitchell@krazyweb.net>
2014-06-19 17:24:43 +0000

    Had hoped to find time to fix targeting overlay CPU usage this week, but was unable to. This adds the functional code back in with no fixes.

--- forge-gui-desktop/src/main/java/forge/screens/match/TargetingOverlay.java
+++ forge-gui-desktop/src/main/java/forge/screens/match/TargetingOverlay.java
@@ -25,6 +25,7 @@ import forge.screens.match.controllers.CDock;
 import forge.screens.match.views.VField;
 import forge.toolbox.FSkin;
 import forge.toolbox.FSkin.SkinnedPanel;
+import forge.view.FView;
 import forge.view.arcane.CardPanel;
 
 import javax.swing.*;
@@ -370,6 +371,8 @@ public enum TargetingOverlay {
             drawArcs(g2d, colorOther, arcsOther);
             drawArcs(g2d, colorCombat, arcsCombat);
 
+            FView.SINGLETON_INSTANCE.getFrame().repaint(); // repaint the match UI
+            
         }
 
     }
