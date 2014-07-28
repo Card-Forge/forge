5fcee5c3b05ba4bfd4d0a6f0a8b427935471acbd
drdev <drdev@cardforge.org>
2014-07-28 00:53:39 +0000

    Remove changes to home screen code

--- forge-gui-mobile/src/forge/screens/home/HomeScreen.java
+++ forge-gui-mobile/src/forge/screens/home/HomeScreen.java
@@ -2,8 +2,6 @@ package forge.screens.home;
 
 import java.util.ArrayList;
 
-import forge.game.card.Card;
-import forge.model.FModel;
 import forge.screens.FScreen;
 import forge.Forge;
 import forge.Graphics;
@@ -17,7 +15,6 @@ import forge.screens.sealed.SealedScreen;
 import forge.screens.settings.SettingsScreen;
 import forge.toolbox.FButton;
 import forge.toolbox.FEvent;
-import forge.toolbox.FOptionPane;
 import forge.toolbox.FEvent.FEventHandler;
 
 public class HomeScreen extends FScreen {
@@ -32,11 +29,7 @@ public class HomeScreen extends FScreen {
         addButton("Constructed", new FEventHandler() {
             @Override
             public void handleEvent(FEvent e) {
-                //String cardName = "LLanowar Elves";
-                String cardName = "Jace, the Mind Sculptor";
-                forge.item.PaperCard pc = FModel.getMagicDb().getCommonCards().getCard(cardName);
-                FOptionPane.showCardOptionDialog(Card.getCardForUi(pc), "Image for " + pc.getName() + ":", "Card Image Test", null, new String[] {"OK"}, 0, null);
-                //Forge.openScreen(new ConstructedScreen());
+                Forge.openScreen(new ConstructedScreen());
             }
         }, true);
         addButton("Booster Draft", new FEventHandler() {
