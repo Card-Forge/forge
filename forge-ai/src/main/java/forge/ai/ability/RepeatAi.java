545666f7c996100f3468465598d173c7b291d9e8
Sloth <sloth@cardforge.org>
2014-08-13 20:55:54 +0000

    - Added basic AI support for Helm of Obedience.

--- forge-ai/src/main/java/forge/ai/ability/RepeatAi.java
+++ forge-ai/src/main/java/forge/ai/ability/RepeatAi.java
@@ -2,9 +2,11 @@ package forge.ai.ability;
 
 
 import forge.ai.AiController;
+import forge.ai.ComputerUtilMana;
 import forge.ai.PlayerControllerAi;
 import forge.ai.SpellAbilityAi;
 import forge.game.ability.AbilityFactory;
+import forge.game.card.Card;
 import forge.game.player.Player;
 import forge.game.player.PlayerActionConfirmMode;
 import forge.game.spellability.AbilitySub;
@@ -15,14 +17,25 @@ public class RepeatAi extends SpellAbilityAi {
 
     @Override
     protected boolean canPlayAI(Player ai, SpellAbility sa) {
+    	final Card source = sa.getHostCard();
         final TargetRestrictions tgt = sa.getTargetRestrictions();
         final Player opp = ai.getOpponent();
+
         if (tgt != null) {
             if (!opp.canBeTargetedBy(sa)) {
                 return false;
             }
             sa.resetTargets();
             sa.getTargets().add(opp);
+        }
+        String logic = sa.getParam("AILogic");
+        if ("MaxX".equals(logic)) {
+            // Set PayX here to maximum value.
+            final int max = ComputerUtilMana.determineLeftoverMana(sa, ai);
+            source.setSVar("PayX", Integer.toString(max));
+            if (max <= 0) {
+                return false;
+            }
         }
         return true;
     }
