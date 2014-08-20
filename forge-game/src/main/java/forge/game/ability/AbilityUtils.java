Card-Forge/forge

commit 6ffb790523af8c795da46867aa8e6cb758f86451

Fix text changing costs, and fix displaying intrinsic SpellAbilities with changed text.
git-svn-id: http://svn.slightlymagic.net/forge/trunk@27099 269b9781-a132-4a9b-9d4e-f004f1b56b58

forge-game/src/main/java/forge/game/ability/AbilityUtils.java
@@ -1538,32 +1538,54 @@ public class AbilityUtils {
         return applyTextChangeEffects(def, ability.getHostCard(), true);
     }
 
+    /**
+     * Apply description-based text changes of a {@link Card} to a String. No
+     * checks are made on traits being intrinsic.
+     *
+     * @param def a String.
+     * @param card a {@link Card}.
+     * @return a new String, taking text changes into account.
+     */
+    public static final String applyDescriptionTextChangeEffects(final String def, final Card card) {
+        return applyTextChangeEffects(def, card, true);
+    }
+
     private static final String applyTextChangeEffects(final String def, final Card card, final boolean isDescriptive) {
+        if (StringUtils.isEmpty(def)) {
+            return def;
+        }
+
         String replaced = def;
         for (final Entry<String, String> e : card.getChangedTextColorWords().entrySet()) {
             final String key = e.getKey();
-            String value = e.getValue();
-            if (isDescriptive) {
-                value = "<strike>" + key + "</strike> " + value;
-            }
+            String value;
             if (key.equals("Any")) {
                 for (final byte c : MagicColor.WUBRG) {
-                    replaced = replaced.replace(MagicColor.toLongString(c).toLowerCase(), value.toLowerCase())
-                            .replace(StringUtils.capitalize(MagicColor.toLongString(c)), StringUtils.capitalize(value));
+                    final String colorLowerCase = MagicColor.toLongString(c).toLowerCase(),
+                            colorCaptCase = StringUtils.capitalize(MagicColor.toLongString(c));
+                    value = getReplacedText(colorLowerCase, e.getValue(), isDescriptive);
+                    replaced = replaced.replace(colorLowerCase, value.toLowerCase());
+                    value = getReplacedText(colorCaptCase, e.getValue(), isDescriptive);
+                    replaced.replace(colorCaptCase, StringUtils.capitalize(value));
                 }
             } else {
+                value = getReplacedText(key, e.getValue(), isDescriptive);
                 replaced = replaced.replace(key, value);
             }
         }
         for (final Entry<String, String> e : card.getChangedTextTypeWords().entrySet()) {
             final String key = e.getKey();
-            String value = e.getValue();
-            if (isDescriptive) {
-                value = "<strike>" + key + "</strike> " + value;
-            }
+            final String value = getReplacedText(key, e.getValue(), isDescriptive);
             replaced = replaced.replace(key, value);
         }
         return replaced;
+    }
+
+    private static final String getReplacedText(final String originalWord, final String newWord, final boolean isDescriptive) {
+        if (isDescriptive) {
+            return "<strike>" + originalWord + "</strike> " + newWord;
+        }
+        return newWord;
     }
 
     public static final String getSVar(final CardTraitBase ability, final String sVarName) {

forge-game/src/main/java/forge/game/card/Card.java
@@ -207,7 +207,7 @@ public class Card extends GameEntity implements Comparable<Card> {
     private long controllerTimestamp = 0;
     private TreeMap<Long, Player> tempControllers = new TreeMap<Long, Player>();
 
-    private String text = "";
+    private String originalText = "", text = "";
     private String echoCost = "";
     private Cost miracleCost = null;
     private String chosenType = "";
@@ -1881,7 +1881,8 @@ public class Card extends GameEntity implements Comparable<Card> {
      *            a {@link java.lang.String} object.
      */
     public final void setText(final String t) {
-        this.text = t;
+        this.originalText = t;
+        this.text = this.originalText;
     }
 
     // get the text that should be displayed
@@ -4598,16 +4599,18 @@ public class Card extends GameEntity implements Comparable<Card> {
      * Update the changed text of the intrinsic spell abilities and keywords.
      */
     private final void updateChangedText() {
-        final List<CardTraitBase> allAbs = Lists.newLinkedList();
-        allAbs.addAll(this.getSpellAbilities());
-        allAbs.addAll(this.getStaticAbilities());
-        allAbs.addAll(this.getReplacementEffects());
-        allAbs.addAll(this.getTriggers());
+        final List<CardTraitBase> allAbs = ImmutableList.<CardTraitBase>builder()
+            .addAll(this.getSpellAbilities())
+            .addAll(this.getStaticAbilities())
+            .addAll(this.getReplacementEffects())
+            .addAll(this.getTriggers())
+            .build();
         for (final CardTraitBase ctb : allAbs) {
             if (ctb.isIntrinsic()) {
                 ctb.changeText();
             }
         }
+        this.text = AbilityUtils.applyDescriptionTextChangeEffects(this.originalText, this);
     }
 
     public final ImmutableMap<String, String> getChangedTextColorWords() {

forge-game/src/main/java/forge/game/cost/Cost.java
@@ -19,6 +19,7 @@ package forge.game.cost;
 
 import forge.card.mana.ManaCost;
 import forge.card.mana.ManaCostParser;
+import forge.game.CardTraitBase;
 import forge.game.card.Card;
 import forge.game.card.CounterType;
 import forge.game.mana.ManaCostBeingPaid;
@@ -739,6 +740,12 @@ public class Cost {
             }
         }
         return this;
+    }
+
+    public final void applyTextChangeEffects(final CardTraitBase trait) {
+        for (final CostPart part : this.getCostParts()) {
+            part.applyTextChangeEffects(trait);
+        }
     }
 
     public static final Cost Zero = new Cost(0);

forge-game/src/main/java/forge/game/cost/CostPart.java
@@ -18,25 +18,28 @@
 package forge.game.cost;
 
 
+import forge.game.CardTraitBase;
+import forge.game.ability.AbilityUtils;
 import forge.game.card.Card;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
+
 import org.apache.commons.lang3.StringUtils;
 
 /**
  * The Class CostPart.
  */
 public abstract class CostPart {
-    private String amount = "1";
-    private final String type;
-    private final String typeDescription;
+    private String originalAmount;
+    private String amount;
+    private final String originalType, originalTypeDescription;
+    private String typeDescription, type;
 
     /**
      * Instantiates a new cost part.
      */
     public CostPart() {
-        type = "Card";
-        typeDescription = null;
+        this("1", "Card", null);
     }
 
     /**
@@ -51,8 +54,10 @@ public abstract class CostPart {
      */
     public CostPart(final String amount, final String type, final String description) {
         this.setAmount(amount);
-        this.type = type;
-        this.typeDescription = description;
+        this.originalType = type;
+        this.type = this.originalType;
+        this.originalTypeDescription = description;
+        this.typeDescription = originalTypeDescription;
     }
 
     /**
@@ -174,9 +179,15 @@ public abstract class CostPart {
      *            the amount to set
      */
     public void setAmount(final String amountIn) {
-        this.amount = amountIn;
+        this.originalAmount = amountIn;
+        this.amount = this.originalAmount;
     }
 
+    public final void applyTextChangeEffects(final CardTraitBase trait) {
+        this.amount = AbilityUtils.applyAbilityTextChangeEffects(this.originalAmount, trait);
+        this.type = AbilityUtils.applyAbilityTextChangeEffects(this.originalType, trait);
+        this.typeDescription = AbilityUtils.applyDescriptionTextChangeEffects(this.originalTypeDescription, trait);
+    }
 
     public abstract boolean payAsDecided(Player payer, PaymentDecision pd, SpellAbility sa);
 }

forge-game/src/main/java/forge/game/spellability/SpellAbility.java
@@ -65,8 +65,8 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
     }
     
     // choices for constructor isPermanent argument
-    private String description = "";
-    private String stackDescription = "";
+    private String originalDescription = "", description = "";
+    private String originalStackDescription = "", stackDescription = "";
     private ManaCost multiKickerManaCost = null;
     private Player activatingPlayer = null;
     private Player targetingPlayer = null;
@@ -774,9 +774,10 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
      *            a {@link java.lang.String} object.
      */
     public void setStackDescription(final String s) {
-        this.stackDescription = s;
-        if ((this.description == "") && this.hostCard.getText().equals("")) {
-            this.description = s;
+        this.originalStackDescription = s;
+        this.stackDescription = this.originalStackDescription;
+        if (StringUtils.isEmpty(this.description) && StringUtils.isEmpty(this.hostCard.getText())) {
+            this.setDescription(s);
         }
     }
 
@@ -806,7 +807,8 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
      *            a {@link java.lang.String} object.
      */
     public void setDescription(final String s) {
-        this.description = s;
+        this.originalDescription = s;
+        this.description = this.originalDescription;
     }
 
     /**
@@ -1780,6 +1782,13 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
         if (this.targetRestricions != null) {
             this.targetRestricions.applyTargetTextChanges(this);
         }
+
+        if (this.getPayCosts() != null) {
+            this.getPayCosts().applyTextChangeEffects(this);
+        }
+
+        this.stackDescription = AbilityUtils.applyDescriptionTextChangeEffects(this.originalStackDescription, this);
+        this.description = AbilityUtils.applyDescriptionTextChangeEffects(this.originalDescription, this);
 
         if (this.subAbility != null) {
             this.subAbility.changeText();

commit 1922fdde665aac195dd0578099de280b30df43a0

Fix bug in Swirl the Mists (accidentally introduced in r27099).

forge-game/src/main/java/forge/game/ability/AbilityUtils.java
@@ -1566,7 +1566,7 @@ public class AbilityUtils {
                     value = getReplacedText(colorLowerCase, e.getValue(), isDescriptive);
                     replaced = replaced.replace(colorLowerCase, value.toLowerCase());
                     value = getReplacedText(colorCaptCase, e.getValue(), isDescriptive);
-                    replaced.replace(colorCaptCase, StringUtils.capitalize(value));
+                    replaced = replaced.replace(colorCaptCase, StringUtils.capitalize(value));
                 }
             } else {
                 value = getReplacedText(key, e.getValue(), isDescriptive);