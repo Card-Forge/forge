34e2daaeb0640ac9ddb1d3507262aa10b08ade11
drdev <drdev@cardforge.org>
2014-08-23 15:49:10 +0000

    Make it easier to test downloading separate asset files

--- forge-gui-mobile-dev/src/forge/app/Main.java
+++ forge-gui-mobile-dev/src/forge/app/Main.java
@@ -5,12 +5,20 @@ import com.badlogic.gdx.backends.lwjgl.LwjglClipboard;
 
 import forge.Forge;
 import forge.interfaces.INetworkConnection;
+import forge.util.FileUtil;
 import forge.util.Utils;
 
 public class Main {
+    private static final boolean SHARE_DESKTOP_ASSETS = true;
+
     public static void main(String[] args) {
+        String assetsDir = SHARE_DESKTOP_ASSETS ? "../forge-gui/" : "testAssets/";
+        if (!SHARE_DESKTOP_ASSETS) {
+            FileUtil.ensureDirectoryExists(assetsDir);
+        }
+
         new LwjglApplication(Forge.getApp(new LwjglClipboard(), new DesktopNetworkConnection(),
-                "../forge-gui/", null), "Forge", Utils.DEV_SCREEN_WIDTH, Utils.DEV_SCREEN_HEIGHT);
+                assetsDir, null), "Forge", Utils.DEV_SCREEN_WIDTH, Utils.DEV_SCREEN_HEIGHT);
     }
 
     private static class DesktopNetworkConnection implements INetworkConnection {
