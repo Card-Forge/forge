0e6b4c204620b84377d9bd765c311eaa3ebfc91b
drdev <drdev@cardforge.org>
2014-08-30 20:57:53 +0000

    Support auto-targeting single target for triggered abilities

--- forge-gui/src/main/java/forge/player/TargetSelection.java
+++ forge-gui/src/main/java/forge/player/TargetSelection.java
@@ -125,6 +125,12 @@ public class TargetSelection {
                     return ability.getTargets().add(nonCardTargets.get(0));
                 }
             }
+            else if (validTargets.size() == 1 && ability.isTrigger() && !tgt.canTgtPlayer()) {
+                //if only one valid target card for triggered ability, auto-target that card
+                //only do this for triggered abilities to prevent auto-targeting when user chooses
+                //to play a spell or activat an ability
+                return ability.getTargets().add(validTargets.get(0));
+            }
             final Map<Player, Object> playersWithValidTargets = new HashMap<Player, Object>();
             for (Card card : validTargets) {
                 playersWithValidTargets.put(card.getController(), null);
