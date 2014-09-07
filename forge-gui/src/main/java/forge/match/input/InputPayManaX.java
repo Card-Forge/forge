221b08467927d7fdf9ba287592220f80eebccbd2
drdev <drdev@cardforge.org>
2014-09-07 15:24:14 +0000

    Add Auto support for paying X mana costs

--- forge-gui/src/main/java/forge/match/input/InputPayManaX.java
+++ forge-gui/src/main/java/forge/match/input/InputPayManaX.java
@@ -1,5 +1,6 @@
 package forge.match.input;
 
+import forge.ai.ComputerUtilMana;
 import forge.card.ColorSet;
 import forge.card.mana.ManaCost;
 import forge.card.mana.ManaCostParser;
@@ -7,7 +8,10 @@ import forge.game.card.Card;
 import forge.game.mana.Mana;
 import forge.game.mana.ManaCostBeingPaid;
 import forge.game.spellability.SpellAbility;
+import forge.util.Evaluator;
 import forge.util.ITriggerEvent;
+import forge.util.ThreadUtil;
+import forge.util.gui.SGuiChoose;
 
 import org.apache.commons.lang3.StringUtils;
 
@@ -19,14 +23,22 @@ public class InputPayManaX extends InputPayMana {
     private int xPaid = 0;
     private ArrayList<Mana> xPaidByColor = new ArrayList<>();
     private byte colorsPaid;
+    private final String xCostStr;
     private final ManaCost manaCostPerX;
     private final boolean xCanBe0;
     private boolean canceled = false;
+    private Integer max;
 
     public InputPayManaX(final SpellAbility sa0, final int amountX, final boolean xCanBe0) {
         super(sa0, sa0.getActivatingPlayer());
         xPaid = 0;
-        
+
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < amountX; i++) {
+            builder.append("{X}");
+        }
+        xCostStr = builder.toString();
+
         if (saPaidFor.hasParam("XColor")) {
             String xColor = saPaidFor.getParam("XColor");
             if (amountX == 1) {
@@ -47,6 +59,7 @@ public class InputPayManaX extends InputPayMana {
 
         this.xCanBe0 = xCanBe0;
         colorsPaid = saPaidFor.getHostCard().getColorsPaid(); // for effects like sunburst
+        canPayManaCost = true; //flag as true always since it doesn't need to be calculated using AI logic
     }
 
     /* (non-Javadoc)
@@ -61,7 +74,7 @@ public class InputPayManaX extends InputPayMana {
 
     @Override
     protected boolean supportAutoPay() {
-        return false;
+        return xPaid == 0;
     }
 
     @Override
@@ -73,17 +86,15 @@ public class InputPayManaX extends InputPayMana {
 
     @Override
     protected String getMessage() {
-        StringBuilder msg = new StringBuilder("Pay X Mana Cost for ");
-        msg.append(saPaidFor.getHostCard().getName()).append("\n").append(this.xPaid);
-        msg.append(" Paid so far.");
-        if (!xCanBe0) {
-            msg.append(" X Can't be 0.");
+        StringBuilder msg = new StringBuilder("Pay " + xCostStr + ". X=" + xPaid + ".");
+        if (xPaid > 0) {
+            // Enable just cancel is full X value hasn't been paid for multiple X values
+            // or X is 0, and x can't be 0
+            ButtonUtil.update(isPaid(), true, true);
         }
-
-        // Enable just cancel is full X value hasn't been paid for multiple X values
-        // or X is 0, and x can't be 0
-        ButtonUtil.update(isPaid(), true, true);
-
+        if (!xCanBe0) {
+            msg.append("\nX Can't be 0.");
+        }
         return msg.toString();
     }
 
@@ -112,8 +123,49 @@ public class InputPayManaX extends InputPayMana {
 
     @Override
     protected final void onOk() {
-        done();
-        this.stop();
+        if (supportAutoPay()) {
+            ThreadUtil.invokeInGameThread(new Runnable() {
+                @Override
+                public void run() {
+                    int min = xCanBe0 ? 0 : 1;
+                    if (max == null) {
+                        //use AI utility to determine maximum possible value for X if that hasn't been determined yet
+                        Evaluator<Integer> proc = new Evaluator<Integer>() {
+                            @Override
+                            public Integer evaluate() {
+                                ManaCostBeingPaid cost = new ManaCostBeingPaid(manaCostPerX);
+                                for (int i = 1; i < 100; i++) {
+                                    if (!ComputerUtilMana.canPayManaCost(cost, saPaidFor, player)) {
+                                        return i - 1;
+                                    }
+                                    cost.addManaCost(manaCostPerX);
+                                }
+                                return 99;
+                            }
+                        };
+                        runAsAi(proc);
+                        max = proc.getResult();
+                    }
+                    Integer value = SGuiChoose.getInteger("Choose a value for X", min, max, true);
+                    if (value != null) {
+                        xPaid = value;
+                        saPaidFor.getHostCard().setXManaCostPaid(xPaid);
+                        if (xPaid > 0) {
+                            ManaCostBeingPaid cost = new ManaCostBeingPaid(manaCostPerX);
+                            for (int i = 1; i < xPaid; i++) {
+                                cost.addManaCost(manaCostPerX);
+                            }
+                            ComputerUtilMana.payManaCost(cost, saPaidFor, player);
+                        }
+                        stop();
+                    }
+                }
+            });
+        }
+        else {
+            done();
+            stop();
+        }
     }
 
     @Override
