66973f5954a1449dc7275c5f46de4cd7f92f301e
Agetian <stavdev@mail.ru>
2014-09-14 06:15:32 +0000

    - Remember the actual moved equipment, not its target (fixes random "reconsideration" jump).

--- forge-ai/src/main/java/forge/ai/ability/AttachAi.java
+++ forge-ai/src/main/java/forge/ai/ability/AttachAi.java
@@ -1046,11 +1046,10 @@ public class AttachAi extends SpellAbilityAi {
             }
 
             // avoid randomly moving the equipment back and forth between several creatures in one turn
-            aic.getCardMemory().rememberCard(attachSource.getEquippingCard(), AiCardMemory.MemorySet.MOVED_EQUIPMENT);
-            if (aic.getCardMemory().isRememberedCard(c, AiCardMemory.MemorySet.MOVED_EQUIPMENT)) {
+            if (aic.getCardMemory().isRememberedCard(sa.getHostCard(), AiCardMemory.MemorySet.MOVED_EQUIPMENT)) {
                 return null;
             } else {
-                aic.getCardMemory().rememberCard(c, AiCardMemory.MemorySet.MOVED_EQUIPMENT);
+                aic.getCardMemory().rememberCard(sa.getHostCard(), AiCardMemory.MemorySet.MOVED_EQUIPMENT);
             }
         }
 
