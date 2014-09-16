9e566992206d3268dcab40548128dc5dde556bf0
drdev <drdev@cardforge.org>
2014-09-16 17:52:19 +0000

    Add achievements for each variant type

--- forge-gui/src/main/java/forge/achievement/ConstructedAchievements.java
+++ forge-gui/src/main/java/forge/achievement/ConstructedAchievements.java
@@ -1,5 +1,6 @@
 package forge.achievement;
 
+import forge.game.GameType;
 import forge.properties.ForgeConstants;
 
 public class ConstructedAchievements extends AchievementCollection {
@@ -15,5 +16,10 @@ public class ConstructedAchievements extends AchievementCollection {
     //add achievements that should appear at the bottom below core achievements for each game mode
     @Override
     protected void buildBottomShelf() {
+        add("Vanguard", new VariantWins(GameType.Vanguard, 25, 50));
+        add("Momir Basic", new VariantWins(GameType.MomirBasic, 25, 50));
+        add("Commander", new VariantWins(GameType.Commander, 25, 50));
+        add("Planechase", new VariantWins(GameType.Planechase, 25, 50));
+        add("Archenemy", new VariantWins(GameType.Archenemy, 25, 50));
     }
 }
---
+++ forge-gui/src/main/java/forge/achievement/VariantWins.java
@@ -0,0 +1,30 @@
+package forge.achievement;
+
+import forge.game.Game;
+import forge.game.GameType;
+import forge.game.player.Player;
+
+public class VariantWins extends Achievement {
+    private GameType variant;
+
+    public VariantWins(GameType variant0, int silver0, int gold0) {
+        super(variant0.toString(), false, true,
+            String.format("Win a %s game.", variant0.toString()), 1,
+            String.format("Win %d %s games.", silver0, variant0.toString()), silver0,
+            String.format("Win %d %s games.", gold0, variant0.toString()), gold0);
+        variant = variant0;
+    }
+
+    @Override
+    protected int evaluate(Player player, Game game, int current) {
+        if (player.getOutcome().hasWon()) {
+            if (game.getRules().hasAppliedVariant(variant)) {
+                return current + 1;
+            }
+            if (variant == GameType.Archenemy && game.getRules().hasAppliedVariant(GameType.ArchenemyRumble)) {
+                return current + 1; //lump Archenemy Rumble into same achievement as Archenemy 
+            }
+        }
+        return current;
+    }
+}
