0c09cdbbe0db4ea654c9467741f97cd743fc58ba
Agetian <stavdev@mail.ru>
2014-09-28 13:47:49 +0000

    - Better version of the previous fix.

--- forge-gui/src/main/java/forge/player/TargetSelection.java
+++ forge-gui/src/main/java/forge/player/TargetSelection.java
@@ -133,7 +133,9 @@ public class TargetSelection {
                 //if only one valid target card for triggered ability, auto-target that card
                 //only do this for triggered abilities to prevent auto-targeting when user chooses
                 //to play a spell or activat an ability
-                tgt.addDividedAllocation(validTargets.get(0), tgt.getStillToDivide());
+                if (tgt.isDividedAsYouChoose()) {
+                    tgt.addDividedAllocation(validTargets.get(0), tgt.getStillToDivide());
+                }
                 return ability.getTargets().add(validTargets.get(0));
             }
             final Map<PlayerView, Object> playersWithValidTargets = Maps.newHashMap();
