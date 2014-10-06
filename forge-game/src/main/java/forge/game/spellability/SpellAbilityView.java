255fc2ea5f2810e0188cb412a3562d49d7db96ba
drdev <drdev@cardforge.org>
2014-10-06 04:57:42 +0000

    Prevent crash when opening deck editor

--- forge-game/src/main/java/forge/game/spellability/SpellAbilityView.java
+++ forge-game/src/main/java/forge/game/spellability/SpellAbilityView.java
@@ -10,6 +10,10 @@ public class SpellAbilityView extends TrackableObject<SpellAbilityProp> {
         super(sa.getId(), SpellAbilityProp.class);
         updateHostCard(sa);
         updateDescription(sa);
+
+        //only update remaining properties if within Game context
+        if (sa.getHostCard().getGame() == null) { return; }
+
         updateCanPlay(sa);
         updatePromptIfOnlyPossibleAbility(sa);
     }
