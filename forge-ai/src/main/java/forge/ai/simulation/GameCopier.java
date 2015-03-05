8fd362240ca2d95da5671573dd6992daab427d89
Myrd <myrd@cardforge.org>
2015-03-05 02:44:45 +0000

    Fix simulation in commander games.

--- forge-ai/src/main/java/forge/ai/simulation/GameCopier.java
+++ forge-ai/src/main/java/forge/ai/simulation/GameCopier.java
@@ -71,6 +71,7 @@ public class GameCopier {
             newPlayer.setLifeLostLastTurn(origPlayer.getLifeLostLastTurn());
             newPlayer.setLifeLostThisTurn(origPlayer.getLifeLostThisTurn());
             newPlayer.setPreventNextDamage(origPlayer.getPreventNextDamage());
+            newPlayer.setCommander(origPlayer.getCommander()); // will be fixed up below
             playerMap.put(origPlayer, newPlayer);
         }
 
@@ -82,8 +83,9 @@ public class GameCopier {
         }
         
         copyGameState(newGame);
-        
+
         for (Player p : newGame.getPlayers()) {
+            p.setCommander(gameObjectMap.map(p.getCommander()));
             ((PlayerZoneBattlefield) p.getZone(ZoneType.Battlefield)).setTriggers(true);
         }
         newGame.getTriggerHandler().clearSuppression(TriggerType.ChangesZone);
