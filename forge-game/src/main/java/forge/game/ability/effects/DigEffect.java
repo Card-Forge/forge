fe9ac18f4411944c743cffd12ea790bed359b59c
Agetian <stavdev@mail.ru>
2015-09-23 06:40:35 +0000

    - Support CARDNAME generalization in the optional prompt for AB Dig.

--- forge-game/src/main/java/forge/game/ability/effects/DigEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/DigEffect.java
@@ -205,7 +205,7 @@ public class DigEffect extends SpellAbilityEffect {
                     // Optional abilities that use a dialog box to prompt the user to skip the ability (e.g. Explorer's Scope, Quest for Ula's Temple)
                     if (optional && mayBeSkipped && !valid.isEmpty()) {
                         String prompt = !optionalAbilityPrompt.isEmpty() ? optionalAbilityPrompt : "Would you like to proceed with the optional ability for " + sa.getHostCard() + "?\n\n(" + sa.getDescription() + ")";
-                        if (!p.getController().confirmAction(sa, null, prompt)) {
+                        if (!p.getController().confirmAction(sa, null, prompt.replace("CARDNAME", sa.getHostCard().getName()))) {
                             return;
                         }
                     }
