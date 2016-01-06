f5fd316c629b12611b9eec3760eaed0d2174c3f5
drdev <drdev@cardforge.org>
2016-01-06 05:37:04 +0000

    Use spellbook for card count

--- forge-gui-mobile/src/forge/screens/planarconquest/LoadConquestScreen.java
+++ forge-gui-mobile/src/forge/screens/planarconquest/LoadConquestScreen.java
@@ -297,7 +297,7 @@ public class LoadConquestScreen extends LaunchScreen {
                     float cardsWidth = font.getBounds(cards).width + iconSize + SettingsScreen.SETTING_PADDING;
                     float shardsWidth = font.getBounds(shards).width + iconSize + SettingsScreen.SETTING_PADDING;
                     g.drawText(value.getPlaneswalker().getName() + " - " + value.getCurrentPlane().getName(), font, SettingsScreen.DESC_COLOR, x, y, w - shardsWidth - cardsWidth, h, false, HAlignment.LEFT, false);
-                    g.drawImage(FSkinImage.HAND, x + w - shardsWidth - cardsWidth + iconOffset, y - SettingsScreen.SETTING_PADDING, iconSize, iconSize);
+                    g.drawImage(FSkinImage.SPELLBOOK, x + w - shardsWidth - cardsWidth + iconOffset, y - SettingsScreen.SETTING_PADDING, iconSize, iconSize);
                     g.drawText(cards, font, SettingsScreen.DESC_COLOR, x + w - shardsWidth - cardsWidth + iconSize + SettingsScreen.SETTING_PADDING, y, w, h, false, HAlignment.LEFT, false);
                     g.drawImage(FSkinImage.AETHER_SHARD, x + w - shardsWidth + iconOffset, y - SettingsScreen.SETTING_PADDING, iconSize, iconSize);
                     g.drawText(shards, font, SettingsScreen.DESC_COLOR, x + w - shardsWidth + iconSize + SettingsScreen.SETTING_PADDING, y, w, h, false, HAlignment.LEFT, false);
