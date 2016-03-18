d648b91c5f9d59987165fccc04cc8ab7feba6076
drdev <drdev@cardforge.org>
2016-03-18 03:35:48 +0000

    Version bump to 1.5.50.003

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -39,7 +39,7 @@ import forge.util.FileUtil;
 import forge.util.Utils;
 
 public class Forge implements ApplicationListener {
-    public static final String CURRENT_VERSION = "1.5.50.002";
+    public static final String CURRENT_VERSION = "1.5.50.003";
 
     private static final ApplicationListener app = new Forge();
     private static Clipboard clipboard;
