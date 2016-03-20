4ed97e796994bafa182f81e7774d85423068104f
drdev <drdev@cardforge.org>
2016-03-20 18:15:07 +0000

    Add visual distinction for exiled cards

--- forge-gui-mobile/src/forge/screens/planarconquest/ConquestCollectionScreen.java
+++ forge-gui-mobile/src/forge/screens/planarconquest/ConquestCollectionScreen.java
@@ -1,11 +1,17 @@
 package forge.screens.planarconquest;
 
+import java.util.Map.Entry;
+
 import com.google.common.base.Predicate;
 
 import forge.FThreads;
+import forge.Graphics;
+import forge.assets.FSkinColor;
 import forge.assets.FSkinFont;
 import forge.assets.FSkinImage;
 import forge.assets.TextRenderer;
+import forge.card.CardRenderer;
+import forge.card.CardZoom;
 import forge.deck.CardPool;
 import forge.item.PaperCard;
 import forge.itemmanager.CardManager;
@@ -26,9 +32,13 @@ import forge.planarconquest.ConquestPreferences.CQPref;
 import forge.screens.FScreen;
 import forge.toolbox.FEvent;
 import forge.toolbox.FLabel;
+import forge.toolbox.FList;
 import forge.toolbox.FEvent.FEventHandler;
+import forge.toolbox.FList.CompactModeHandler;
 
 public class ConquestCollectionScreen extends FScreen {
+    private static final float EXILED_ALPHA_COMPOSITE = 0.4f;
+
     private final FLabel lblShards = add(new FLabel.Builder().font(ConquestAEtherScreen.LABEL_FONT).parseSymbols().build());
     private final FLabel lblInfo = add(new FLabel.Builder().font(FSkinFont.get(11)).build());
     private final CollectionManager lstCollection = add(new CollectionManager());
@@ -144,6 +154,47 @@ public class ConquestCollectionScreen extends FScreen {
             addFilter(new CardColorFilter(this));
             addFilter(new CardOriginFilter(this));
             addFilter(new CardTypeFilter(this));
+        }
+
+        @Override
+        public ItemRenderer getListItemRenderer(final CompactModeHandler compactModeHandler) {
+            return new ItemRenderer() {
+                @Override
+                public float getItemHeight() {
+                    return CardRenderer.getCardListItemHeight(compactModeHandler.isCompactMode());
+                }
+
+                @Override
+                public void drawValue(Graphics g, Entry<PaperCard, Integer> value, FSkinFont font, FSkinColor foreColor, FSkinColor backColor, boolean pressed, float x, float y, float w, float h) {
+                    boolean exiled = FModel.getConquest().getModel().isInExile(value.getKey());
+                    if (exiled) { //fade out exiled cards
+                        g.setAlphaComposite(EXILED_ALPHA_COMPOSITE);
+                    }
+                    CardRenderer.drawCardListItem(g, font, foreColor, value.getKey(), isInfinite() ? 0 : value.getValue(), getItemSuffix(value), x, y, w, h, compactModeHandler.isCompactMode());
+                    if (exiled) {
+                        g.resetAlphaComposite();
+
+                        //also draw exile icon on top of card art
+                        float artX = x - FList.PADDING;
+                        float artY = y - FList.PADDING;
+                        float cardArtHeight = h + 2 * FList.PADDING;
+                        float cardArtWidth = cardArtHeight * CardRenderer.CARD_ART_RATIO;
+                        float iconSize = cardArtHeight / 2;
+                        g.drawImage(FSkinImage.EXILE, artX + (cardArtWidth - iconSize) / 2, artY + (cardArtHeight - iconSize) / 2, iconSize, iconSize);
+                    }
+                }
+
+                @Override
+                public boolean tap(Integer index, Entry<PaperCard, Integer> value, float x, float y, int count) {
+                    return CardRenderer.cardListItemTap(model.getOrderedList(), index, CollectionManager.this, x, y, count, compactModeHandler.isCompactMode());
+                }
+
+                @Override
+                public boolean longPress(Integer index, Entry<PaperCard, Integer> value, float x, float y) {
+                    CardZoom.show(model.getOrderedList(), index, CollectionManager.this);
+                    return true;
+                }
+            };
         }
     }
 
