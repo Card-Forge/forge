9bbd6dffb541c2e2dbe4214d9b72bb6d921a5461
Agetian <stavdev@mail.ru>
2016-03-27 19:49:12 +0000

    - Adding SOI planeswalker achievements.

--- forge-gui/src/main/java/forge/achievement/PlaneswalkerAchievements.java
+++ forge-gui/src/main/java/forge/achievement/PlaneswalkerAchievements.java
@@ -32,6 +32,7 @@ public class PlaneswalkerAchievements extends AchievementCollection {
         add("Ajani Vengeant", "Ajani's Tantrum", "Look Ma! No lands!");
         add("Ajani, Caller of the Pride", "Ajani's Menagerie", "You're a crazy cat lady now!");
         add("Ajani, Mentor of Heroes", "Ajani's Vitality", "Do sta let!");
+        add("Arlinn Kord", "Arlinn's Hunt", "Fair fight? What fair fight?");
         add("Ashiok, Nightmare Weaver", "Ashiok's Identity", "I subscribe to the theory of blank slate!");
         add("Chandra Ablaze", "Chandra's Bargain", "I feel like I've seen those before...");
         add("Chandra Nalaar", "Chandra's Rage", "Feel the power of my wrath!");
@@ -61,6 +62,7 @@ public class PlaneswalkerAchievements extends AchievementCollection {
         add("Jace, Telepath Unbound", "Jace's Erasure", "First empty your mind and then... nope, that's it.");
         add("Jace, the Living Guildpact", "Jace's Timetwister", "Symmetry? What symmetry?");
         add("Jace, the Mind Sculptor", "Jace's Lobotomy", "What do you mean, \"overpowered\"?");
+        add("Jace, Unraveler of Secrets", "Jace's Perfect Plan", "So, I've read about this neat guy named Erayo...");
         add("Karn Liberated", "Karn's Reset", "Let's do this again!");
         add("Kiora, Master of the Depths", "Kiora's Fight Club", "First rule is not to talk about the sucker punches");
         add("Kiora, the Crashing Wave", "Kiora's Kraken", "Say hello to Cthulhu for me!");
@@ -69,6 +71,7 @@ public class PlaneswalkerAchievements extends AchievementCollection {
         add("Liliana of the Dark Realms", "Liliana's Ritual", "What do you mean, \"out of character\"?");
         add("Liliana of the Veil", "Liliana's Choice", "Which one of your children do you love best?");
         add("Liliana, Defiant Necromancer", "Liliana's Necromastery", "Come join the Dark Side...");
+        add("Nahiri, the Harbinger", "Nahiri's Mystery Guest", "This should be your cue to run.");
         add("Nahiri, the Lithomancer", "Nahiri's Gift", "I pulled it out! Now I'm a king!");
         add("Narset Transcendent", "Narset's One Rule", "No spells for you!");
         add("Nicol Bolas, Planeswalker", "Bolas's Ultimatum", "Whatever you do, don't call him \"Nicol\"!");
@@ -84,6 +87,7 @@ public class PlaneswalkerAchievements extends AchievementCollection {
         add("Sarkhan, the Dragonspeaker", "Sarkhan's Voices", "Huh? What are you saying?)");
         add("Sarkhan Unbroken", "Sarkhan's Dragonstorm", "Skies full of dragons! Oh, what a glorious day!");
         add("Sorin Markov", "Sorin's Hypnosis", "You're getting sleepy... very sleepy...");
+        add("Sorin, Grim Nemesis", "Sorin's Vampire Army", "Taste the might of my blood relatives");
         add("Sorin, Lord of Innistrad", "Sorin's Recruitment", "My favorite game is Shogi!");
         add("Sorin, Solemn Visitor", "Sorin's Abyss", "Don't get so close to the edge!");
         add("Tamiyo, the Moon Sage", "Tamiyo's Recycling", "Wash, rinse, repeat!");
