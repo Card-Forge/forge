035b1f35d94caf55bd94f032b49bdaa15620992b
Hanmac <hanmac@cardforge.org>
2016-05-31 06:03:31 +0000

    TriggerHandler: fixed getCardState, should be done for all cards with alternate State, not just DoubleFaced

--- forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
+++ forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
@@ -481,7 +481,7 @@ public class TriggerHandler {
         else {
         	// get CardState does not work for transformed cards
         	// also its about LKI
-        	if (host.isInZone(ZoneType.Battlefield) || !host.isDoubleFaced()) {
+        	if (host.isInZone(ZoneType.Battlefield) || !host.hasAlternateState()) {
         		host = game.getCardState(host);
         	}
         }
