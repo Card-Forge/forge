b724f44ea64b69a509212b447e647256571cafa5
Agetian <stavdev@mail.ru>
2016-06-10 05:56:55 +0000

    - Android build publish 1.5.53.002 (forgot to update this)

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -39,7 +39,7 @@ import forge.util.FileUtil;
 import forge.util.Utils;
 
 public class Forge implements ApplicationListener {
-    public static final String CURRENT_VERSION = "1.5.53.001";
+    public static final String CURRENT_VERSION = "1.5.53.002";
 
     private static final ApplicationListener app = new Forge();
     private static Clipboard clipboard;
