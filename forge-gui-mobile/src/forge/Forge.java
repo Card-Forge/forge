76197d851d330b95d926942e4326eec723ef2c55
Agetian <stavdev@mail.ru>
2016-06-29 04:42:08 +0000

    - Preparing Mobile Forge release 1.5.54.001.

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -39,7 +39,7 @@ import forge.util.FileUtil;
 import forge.util.Utils;
 
 public class Forge implements ApplicationListener {
-    public static final String CURRENT_VERSION = "1.5.53.002";
+    public static final String CURRENT_VERSION = "1.5.54.001";
 
     private static final ApplicationListener app = new Forge();
     private static Clipboard clipboard;
