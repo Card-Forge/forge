28f7df093395e845c41b578d19968ca725716c27
Hanmac <hanmac@cardforge.org>
2016-07-19 19:05:20 +0000

    StaticAbility & StaticAbilityContinuous: "can't gain or have" is now applied in Ability1 Layer, instead of Rules later.

--- forge-game/src/main/java/forge/game/staticability/StaticAbilityContinuous.java
+++ forge-game/src/main/java/forge/game/staticability/StaticAbilityContinuous.java
@@ -26,6 +26,7 @@ import java.util.Set;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
 import forge.GameCommand;
 import forge.card.ColorSet;
@@ -122,7 +123,7 @@ public final class StaticAbilityContinuous {
         int keywordMultiplier = 1;
 
         String[] addKeywords = null;
-        String[] addHiddenKeywords = null;
+        List<String> addHiddenKeywords = Lists.newArrayList();
         String[] removeKeywords = null;
         String[] addAbilities = null;
         String[] addReplacements = null;
@@ -213,8 +214,12 @@ public final class StaticAbilityContinuous {
             }
         }
 
-        if (layer == StaticAbilityLayer.RULES && params.containsKey("AddHiddenKeyword")) {
-            addHiddenKeywords = params.get("AddHiddenKeyword").split(" & ");
+        if ((layer == StaticAbilityLayer.RULES || layer == StaticAbilityLayer.ABILITIES1) && params.containsKey("AddHiddenKeyword")) {
+            // can't have or gain, need to be applyed in ABILITIES1
+            for (String k : params.get("AddHiddenKeyword").split(" & ")) {
+                if ( (k.contains("can't have or gain")) == (layer == StaticAbilityLayer.ABILITIES1))
+                    addHiddenKeywords.add(k);
+            }
         }
 
         if (layer == StaticAbilityLayer.ABILITIES2 && params.containsKey("RemoveKeyword")) {
@@ -484,7 +489,7 @@ public final class StaticAbilityContinuous {
             }
 
             // add HIDDEN keywords
-            if (addHiddenKeywords != null) {
+            if (!addHiddenKeywords.isEmpty()) {
                 for (final String k : addHiddenKeywords) {
                     for (int j = 0; j < keywordMultiplier; j++) {
                         affectedCard.addHiddenExtrinsicKeyword(k);
