6cc0708a35db3a9b5fa9d51d33f4f5f8f7f7a9fd
Hanmac <hanmac@cardforge.org>
2016-07-20 06:23:27 +0000

    KeywordWithAmount: add parse, and improve reminderText for keywords with X

--- forge-game/src/main/java/forge/game/keyword/KeywordWithAmount.java
+++ forge-game/src/main/java/forge/game/keyword/KeywordWithAmount.java
@@ -2,6 +2,8 @@ package forge.game.keyword;
 
 public class KeywordWithAmount extends KeywordInstance<KeywordWithAmount> {
     private int amount;
+    private boolean withX;
+    private String extra = "";
 
     @Override
     public int getAmount() {
@@ -10,10 +12,28 @@ public class KeywordWithAmount extends KeywordInstance<KeywordWithAmount> {
 
     @Override
     protected void parse(String details) {
+        if (details.startsWith("X")) {
+            withX = true;
+            if (details.contains(":")) {
+                extra = details.split(":")[1];
+            }
+        } else  {
+            amount = Integer.parseInt(details);
+        }
     }
 
     @Override
     protected String formatReminderText(String reminderText) {
-        return String.format(reminderText, amount);
+        if (withX) {
+            StringBuilder result = new StringBuilder(
+                String.format(reminderText.replaceAll("%d", "%s"), "X")
+            );
+            if (!extra.isEmpty()) {
+                result.insert(result.length()-2, extra);
+            }
+            return result.toString();
+        } else {
+            return String.format(reminderText, amount);
+        }
     }
 }
