c9b5bba65b603bbfec99c7786f00c18f9afeb4f3
Hanmac <hanmac@cardforge.org>
2016-07-26 08:42:59 +0000

    CardFactoryUtil: do Escalate as Keyword

--- forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
+++ forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
@@ -2529,11 +2529,14 @@ public class CardFactoryUtil {
                 addReplacementEffect(keyword, card, null);
                 addStaticAbility(keyword, card, null);
             }
+            else if (keyword.startsWith("Escalate")) {
+                addStaticAbility(keyword, card, null);
+            }
             else if (keyword.startsWith("Rampage")) {
                 addTriggerAbility(keyword, card, null);
             }
             else if (keyword.startsWith("Strive")) {
-            	addStaticAbility(keyword, card, null);
+                addStaticAbility(keyword, card, null);
             }
             else if (keyword.startsWith("Bushido")) {
                 addTriggerAbility(keyword, card, null);
@@ -3277,10 +3280,23 @@ public class CardFactoryUtil {
 
     public static void addStaticAbility(final String keyword, final Card card, final KeywordsChange kws) {
         final boolean intrinsic = kws == null;
-        if (keyword.startsWith("Strive")) {
+
+        if (keyword.startsWith("Escalate")) {
             final String[] k = keyword.split(":");
             final String manacost = k[1];
-        
+            final Cost cost = new Cost(manacost, false);
+
+            final String effect = "Mode$ RaiseCost | ValidCard$ Card.Self | Type$ Spell | Amount$ Escalate | Cost$ "+ manacost +" | EffectZone$ All" +
+            		" | Description$ Escalate " + cost.toSimpleString() + " (" + Keyword.getInstance(keyword).getReminderText() + ")";
+            StaticAbility st = card.addStaticAbility(effect);
+            st.setIntrinsic(intrinsic);
+            if (!intrinsic) {
+                kws.addStaticAbility(st);
+            }
+        } else if (keyword.startsWith("Strive")) {
+            final String[] k = keyword.split(":");
+            final String manacost = k[1];
+
             final String effect = "Mode$ RaiseCost | ValidCard$ Card.Self | Type$ Spell | Amount$ Strive | Cost$ "+ manacost +" | EffectZone$ All" +
             		" | Description$ Strive - CARDNAME costs " + ManaCostParser.parse(manacost) + " more to cast for each target beyond the first.";
             StaticAbility st = card.addStaticAbility(effect);
