fd9ed11887d3003ac9053d4c3935d725c322f025
Hanmac <hanmac@cardforge.org>
2016-08-05 04:33:00 +0000

    CloneEffect: revert last changes with LKI, it breaks more than it has fixed

--- forge-game/src/main/java/forge/game/ability/effects/CloneEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/CloneEffect.java
@@ -77,11 +77,6 @@ public class CloneEffect extends SpellAbilityEffect {
             return;
         }
 
-        // if the target is gone, use LKI of it
-        if (!cardToCopy.getZone().is(ZoneType.Battlefield)) {
-            cardToCopy = game.getChangeZoneLKIInfo(cardToCopy);
-        }
-
         final boolean optional = sa.hasParam("Optional");
         if (optional && !host.getController().getController().confirmAction(sa, null, "Do you want to copy " + cardToCopy + "?")) {
             return;
