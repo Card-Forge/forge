26e9f880b4d5b093f28187c18223babb1634f1d4
Hanmac <hanmac@cardforge.org>
2016-08-12 07:18:35 +0000

    Spell: the check if Bestow has Flash should only happed if the card is not already Bestowed

--- forge-game/src/main/java/forge/game/spellability/Spell.java
+++ forge-game/src/main/java/forge/game/spellability/Spell.java
@@ -82,7 +82,7 @@ public abstract class Spell extends SpellAbility implements java.io.Serializable
 
         boolean flash = card.hasKeyword("Flash");
 
-        if (this.hasParam("Bestow")) {
+        if (this.hasParam("Bestow") && !card.isBestowed()) {
             // Rule 601.3: cast Bestow with Flash
             // for the check the card does need to be animated
             // otherwise the StaticAbility will not found them
