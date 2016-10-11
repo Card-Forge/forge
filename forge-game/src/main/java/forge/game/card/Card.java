0f4351952ddfd364b27f744b1ed2170208d3bac3
Hanmac <hanmac@cardforge.org>
2016-10-11 15:38:17 +0000

    Card: hide Cycling and TypeCycling in the Keyword list

--- forge-game/src/main/java/forge/game/card/Card.java
+++ forge-game/src/main/java/forge/game/card/Card.java
@@ -1412,6 +1412,7 @@ public class Card extends GameEntity implements Comparable<Card> {
             } else if (keyword.startsWith("Sunburst") && hasStartOfKeyword("Modular")) {
             } else if (keyword.startsWith("Modular") || keyword.startsWith("Bloodthirst")
             		|| keyword.startsWith("Strive") || keyword.startsWith("Escalate")
+            		|| keyword.startsWith("Cycling") || keyword.startsWith("TypeCycling")
                     || keyword.startsWith("ETBReplacement") || keyword.startsWith("MayEffectFromOpeningHand")) {
             } else if (keyword.startsWith("Provoke") || keyword.startsWith("Devour") || keyword.equals("Unleash")
                     || keyword.startsWith("Soulbond") || keyword.equals("Retrace")) {
