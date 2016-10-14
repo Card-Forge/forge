99bdf3377457655ec50556a09113efb3b8021e88
Hanmac <hanmac@cardforge.org>
2016-10-14 10:55:18 +0000

    SrcyAi: rewrote ScryAi to use new SpellAblityAi logic add logic for Brain in a Jar

--- forge-ai/src/main/java/forge/ai/ability/ScryAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ScryAi.java
@@ -1,13 +1,24 @@
 package forge.ai.ability;
 
 import forge.ai.SpellAbilityAi;
+import forge.game.card.Card;
+import forge.game.card.Card.SplitCMCMode;
+import forge.game.phase.PhaseHandler;
+import forge.game.phase.PhaseType;
+import forge.game.card.CardCollection;
+import forge.game.card.CardLists;
+import forge.game.card.CardPredicates;
+import forge.game.card.CounterType;
 import forge.game.player.Player;
 import forge.game.player.PlayerActionConfirmMode;
 import forge.game.spellability.SpellAbility;
 import forge.game.spellability.TargetRestrictions;
+import forge.game.zone.ZoneType;
 import forge.util.MyRandom;
 
 import java.util.Random;
+
+import com.google.common.base.Predicates;
 
 public class ScryAi extends SpellAbilityAi {
 
@@ -32,11 +43,91 @@ public class ScryAi extends SpellAbilityAi {
     public boolean chkAIDrawback(SpellAbility sa, Player ai) {
         return doTriggerAINoCost(ai, sa, false);
     }
-
-
+    
+    /**
+     * Checks if the AI will play a SpellAbility based on its phase restrictions
+     */
     @Override
-    protected boolean canPlayAI(Player ai, SpellAbility sa) {
-        // Card source = sa.getHostCard();
+    protected boolean checkPhaseRestrictions(final Player ai, final SpellAbility sa, final PhaseHandler ph) {
+        // in the playerturn Scry should only be done in Main1 or in upkeep if able
+        if (ph.isPlayerTurn(ai)) {
+            if (SpellAbilityAi.isSorcerySpeed(sa)) {
+                return ph.is(PhaseType.MAIN1);
+            } else {
+                return ph.is(PhaseType.UPKEEP);
+            }
+        }
+        return true;
+    }
+    
+    /**
+     * Checks if the AI will play a SpellAbility with the specified AiLogic
+     */
+    @Override
+    protected boolean checkAiLogic(final Player ai, final SpellAbility sa, final String aiLogic) {
+        if ("Never".equals(aiLogic)) {
+            return false;
+        } else if ("BrainJar".equals(aiLogic)) {
+            final Card source = sa.getHostCard();
+            
+            int counterNum = source.getCounters(CounterType.CHARGE);
+            // no need for logic
+            if (counterNum == 0) {
+                return false;
+            }
+            int libsize = ai.getCardsIn(ZoneType.Library).size();
+            
+            final CardCollection hand = CardLists.filter(ai.getCardsIn(ZoneType.Hand), Predicates.or( 
+                    CardPredicates.isType("Instant"), CardPredicates.isType("Sorcery")));
+            if (!hand.isEmpty()) {
+                // has spell that can be cast in hand with put ability
+                if (!CardLists.filter(hand, CardPredicates.hasCMC(counterNum + 1)).isEmpty()) {
+                    return false;
+                }
+                // has spell that can be cast if one counter is removed
+                if (!CardLists.filter(hand, CardPredicates.hasCMC(counterNum)).isEmpty()) {
+                    sa.setSVar("ChosenX", "Number$1");
+                    return true;
+                }
+            }
+            final CardCollection library = CardLists.filter(ai.getCardsIn(ZoneType.Library), Predicates.or( 
+                    CardPredicates.isType("Instant"), CardPredicates.isType("Sorcery")));
+            if (!library.isEmpty()) {
+                // get max cmc of instant or sorceries in the libary 
+                int maxCMC = 0;
+                for (final Card c : library) {
+                    int v = c.getCMC(); 
+                    if (c.isSplitCard()) {
+                        v = Math.max(c.getCMC(SplitCMCMode.LeftSplitCMC), c.getCMC(SplitCMCMode.RightSplitCMC));
+                    }
+                    if (v > maxCMC) {
+                        maxCMC = v;
+                    }
+                }
+                // there is a spell with more CMC, no need to remove counter
+                if (counterNum + 1 < maxCMC) {
+                    return false;
+                }
+                int maxToRemove = counterNum - maxCMC + 1;
+                // no Scry 0, even if its catched from later stuff 
+                if (maxToRemove <= 0) {
+                	return false;
+                }
+                sa.setSVar("ChosenX", "Number$" + Integer.toString(maxToRemove));
+            } else {
+                // no Instant or Sorceries anymore, just scry
+                sa.setSVar("ChosenX", "Number$" + Integer.toString(Math.min(counterNum, libsize)));
+            }
+        }
+        return true;
+    }
+    
+    @Override
+    protected boolean checkApiLogic(Player ai, SpellAbility sa) {
+        // does Scry make sense with no Library cards?
+        if (ai.getCardsIn(ZoneType.Library).isEmpty()) {
+            return false;
+        }
 
         double chance = .4; // 40 percent chance of milling with instant speed
                             // stuff
