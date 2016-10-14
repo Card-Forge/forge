8e220c866a07e622b4369062fa9984569c205374
Hanmac <hanmac@cardforge.org>
2016-10-14 15:20:00 +0000

    MillAi: rewrote AI to work with multiple opponents also try not to mill zero cards or itself that should fix some cards

--- forge-ai/src/main/java/forge/ai/ability/MillAi.java
+++ forge-ai/src/main/java/forge/ai/ability/MillAi.java
@@ -1,5 +1,13 @@
 package forge.ai.ability;
 
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Map;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+
 import forge.ai.ComputerUtil;
 import forge.ai.ComputerUtilMana;
 import forge.ai.SpellAbilityAi;
@@ -37,7 +45,7 @@ public class MillAi extends SpellAbilityAi {
     @Override
     protected boolean checkPhaseRestrictions(final Player ai, final SpellAbility sa, final PhaseHandler ph) {
         if ("You".equals(sa.getParam("Defined")) && !SpellAbilityAi.isSorcerySpeed(sa)
-                && !ph.is(PhaseType.END_OF_TURN, ai.getOpponent())) {
+                && !ph.is(PhaseType.END_OF_TURN) && !ph.getNextTurn().equals(ai)) {
             return false;   // only self-mill at opponent EOT
         }
         return true;
@@ -70,8 +78,7 @@ public class MillAi extends SpellAbilityAi {
         if ((sa.getParam("NumCards").equals("X") || sa.getParam("NumCards").equals("Z"))
                 && source.getSVar("X").startsWith("Count$xPaid")) {
             // Set PayX here to maximum value.
-            final int cardsToDiscard = Math.min(ComputerUtilMana.determineLeftoverMana(sa, ai),
-                    ai.getOpponent().getCardsIn(ZoneType.Library).size());
+            final int cardsToDiscard = getNumToDiscard(ai, sa);
             source.setSVar("PayX", Integer.toString(cardsToDiscard));
             if (cardsToDiscard <= 0) {
                 return false;
@@ -82,43 +89,66 @@ public class MillAi extends SpellAbilityAi {
 
     private boolean targetAI(final Player ai, final SpellAbility sa, final boolean mandatory) {
         final TargetRestrictions tgt = sa.getTargetRestrictions();
-        Player opp = ai.getOpponent();
+        final Card source = sa.getHostCard();
 
         if (tgt != null) {
             sa.resetTargets();
-            if (!sa.canTarget(opp)) {
+            final Map<Player, Integer> list = Maps.newHashMap();
+            for (final Player o : ai.getOpponents()) {
+                if (!sa.canTarget(o)) {
+                    continue;
+                }
+
+                // need to set as target for some calcuate
+                sa.getTargets().add(o);
+                final int numCards = AbilityUtils.calculateAmount(sa.getHostCard(), sa.getParam("NumCards"), sa);
+                sa.getTargets().remove(o);
+
+                // if it would mill none, try other one
+                if (numCards <= 0) {
+                    if ((sa.getParam("NumCards").equals("X") || sa.getParam("NumCards").equals("Z"))
+                            && source.getSVar("X").startsWith("Count$xPaid")) {
+                        // Spell is PayX based
+                    } else {
+                        continue;
+                    }
+                }
+
+                final CardCollectionView pLibrary = o.getCardsIn(ZoneType.Library);
+                if (pLibrary.isEmpty()) {
+                    continue;
+                }
+
+                // if that player can be miled, select this one.
+                if (numCards >= pLibrary.size()) {
+                    sa.getTargets().add(o);
+                    return true;
+                }
+
+                list.put(o, numCards);
+            }
+
+            // can't target opponent?
+            if (list.isEmpty()) {
                 if (mandatory && sa.canTarget(ai)) {
                     sa.getTargets().add(ai);
                     return true;
                 }
+                // TODO Obscure case when you know what your top card is so you might?
+                // want to mill yourself here
                 return false;
             }
 
-            final int numCards = AbilityUtils.calculateAmount(sa.getHostCard(), sa.getParam("NumCards"), sa);
-
-            final CardCollectionView pLibrary = opp.getCardsIn(ZoneType.Library);
-
-            if (pLibrary.isEmpty()) { // deck already empty, no need to mill
-                if (!mandatory) {
-                    return false;
+            // select Player which would cause the most damage
+            // JAVA 1.8 use Map.Entry.comparingByValue()
+            Map.Entry<Player, Integer> max = Collections.max(list.entrySet(), new Comparator<Map.Entry<Player,Integer>>(){
+                @Override
+                public int compare(Map.Entry<Player, Integer> o1, Map.Entry<Player, Integer> o2) {
+                    return o1.getValue() - o2.getValue();
                 }
+            });
 
-                sa.getTargets().add(opp);
-                return true;
-            }
-
-            if (numCards >= pLibrary.size()) {
-                // Can Mill out Human's deck? Do it!
-                sa.getTargets().add(opp);
-                return true;
-            }
-
-            // Obscure case when you know what your top card is so you might?
-            // want to mill yourself here
-            // if (AI wants to mill self)
-            // sa.getTargets().add(AllZone.getComputerPlayer());
-            // else
-            sa.getTargets().add(opp);
+            sa.getTargets().add(max.getKey());
         }
         return true;
     }
@@ -127,7 +157,6 @@ public class MillAi extends SpellAbilityAi {
     public boolean chkAIDrawback(SpellAbility sa, Player aiPlayer) {
         return targetAI(aiPlayer, sa, true);
     }
-
 
     @Override
     protected boolean doTriggerAINoCost(Player aiPlayer, SpellAbility sa, boolean mandatory) {
@@ -138,8 +167,7 @@ public class MillAi extends SpellAbilityAi {
         final Card source = sa.getHostCard();
         if (sa.getParam("NumCards").equals("X") && source.getSVar("X").equals("Count$xPaid")) {
             // Set PayX here to maximum value.
-            final int cardsToDiscard = Math.min(ComputerUtilMana.determineLeftoverMana(sa, aiPlayer), aiPlayer.getOpponent()
-                    .getCardsIn(ZoneType.Library).size());
+            final int cardsToDiscard = getNumToDiscard(aiPlayer, sa);
             source.setSVar("PayX", Integer.toString(cardsToDiscard));
         }
 
@@ -151,5 +179,36 @@ public class MillAi extends SpellAbilityAi {
     @Override
     public boolean confirmAction(Player player, SpellAbility sa, PlayerActionConfirmMode mode, String message) {
         return true;
+    }
+
+    /*
+     * return num of cards to discard
+     */
+    private int getNumToDiscard(final Player ai, final SpellAbility sa) {
+        // need list of affected players
+        List<Player> list = Lists.newArrayList();
+        if (sa.usesTargeting()) {
+            list.addAll(Lists.newArrayList(sa.getTargets().getTargetPlayers()));
+        } else {
+            list.addAll(AbilityUtils.getDefinedPlayers(sa.getHostCard(), sa.getParam("Defined"), sa));
+        }
+
+        // get targeted or defined Player with largest library 
+        // TODO in Java 8 find better way
+        final Player m = Collections.max(list, new Comparator<Player>() {
+            @Override
+            public int compare(Player arg0, Player arg1) {
+                return arg0.getCardsIn(ZoneType.Library).size() - arg1.getCardsIn(ZoneType.Library).size();
+            }
+        });
+
+        int cardsToDiscard =  m.getCardsIn(ZoneType.Library).size();
+
+        // if ai is in affected list too, try to not mill himself
+        if (list.contains(ai)) {
+            cardsToDiscard = Math.min(ai.getCardsIn(ZoneType.Library).size() - 5, cardsToDiscard);
+        }
+
+        return Math.min(ComputerUtilMana.determineLeftoverMana(sa, ai), cardsToDiscard);
     }
 }
