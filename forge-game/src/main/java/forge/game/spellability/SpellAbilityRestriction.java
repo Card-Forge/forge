6ba369d1db44f31698c0fac57413ef7d6e82bd11
Hanmac <hanmac@cardforge.org>
2016-10-19 07:41:03 +0000

    SpellAbilityRestriction:checkZoneRestrictions have it return true for no ZoneRestriction hopefully it does not break anything

--- forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
+++ forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
@@ -226,7 +226,7 @@ public class SpellAbilityRestriction extends SpellAbilityVariables {
             if (sa.isSpell()) {
                 final CardPlayOption o = c.mayPlay(sa.getMayPlay());
                 if (o == null) {
-                    return cardZone.is(this.getZone());
+                    return this.getZone() == null || cardZone.is(this.getZone());
                 } else if (o.getPlayer() == activator) {
                     Map<String,String> params = sa.getMayPlay().getMapParams();
 
