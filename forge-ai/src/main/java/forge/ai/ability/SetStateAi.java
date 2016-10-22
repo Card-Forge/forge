741bb9de02987ff70f83dcb637ce4e69d2208193
Hanmac <hanmac@cardforge.org>
2016-10-22 16:13:50 +0000

    SetStateAi: add logic for target spells, do it for Transform and for TurnFace

--- forge-ai/src/main/java/forge/ai/ability/SetStateAi.java
+++ forge-ai/src/main/java/forge/ai/ability/SetStateAi.java
@@ -1,16 +1,25 @@
 package forge.ai.ability;
 
+import com.google.common.base.Predicate;
+
 import forge.ai.ComputerUtilCard;
 import forge.ai.SpellAbilityAi;
+import forge.card.CardSplitType;
+import forge.game.Game;
 import forge.game.GlobalRuleChange;
+import forge.game.ability.AbilityUtils;
 import forge.game.card.Card;
+import forge.game.card.CardCollection;
+import forge.game.card.CardLists;
 import forge.game.card.CardState;
 import forge.game.card.CardUtil;
 import forge.game.card.CounterType;
+import forge.game.card.CardPredicates.Presets;
 import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
+import forge.game.spellability.TargetRestrictions;
 import forge.game.zone.ZoneType;
 
 public class SetStateAi extends SpellAbilityAi {
@@ -18,6 +27,11 @@ public class SetStateAi extends SpellAbilityAi {
     protected boolean checkApiLogic(final Player aiPlayer, final SpellAbility sa) {
         final Card source = sa.getHostCard();
         final String mode = sa.getParam("Mode");
+
+        // turning face is most likely okay
+        if("TurnFace".equals(mode)) {
+            return true;
+        }
 
         // Prevent transform into legendary creature if copy already exists
         // Check first if Legend Rule does still apply
@@ -47,16 +61,7 @@ public class SetStateAi extends SpellAbilityAi {
             }
         }
 
-        if (sa.usesTargeting()) {
-            // TODO add Logic for tgts
-            return false;
-        }
-
-        if("TurnFace".equals(mode)) {
-            return true;
-        } else if("Transform".equals(mode)) {
-            return !source.hasKeyword("CARDNAME can't transform");
-        } else if ("Flip".equals(mode)) {
+        if("Transform".equals(mode) || "Flip".equals(mode)) {
             return true;
         }
         return false;
@@ -71,59 +76,169 @@ public class SetStateAi extends SpellAbilityAi {
 
     @Override
     protected boolean checkPhaseRestrictions(Player ai, SpellAbility sa, PhaseHandler ph) {
+        final String mode = sa.getParam("Mode");
         final Card source = sa.getHostCard();
+        final Game game = source.getGame();
 
-        if("Transform".equals(sa.getParam("Mode"))) {
-            if (!source.hasAlternateState()) {
-                System.err.println("Warning: SetState without ALTERNATE on " + source.getName() + ".");
-                return false;
-            }
+        if("Transform".equals(mode)) {
+            if (!sa.usesTargeting()) {
+                // no Transform with Defined which is not Self
+                if (source.hasKeyword("CARDNAME can't transform")) {
+                    return false;
+                }
+                return shouldTransformCard(source, ai, ph);
+            } else {
+                final TargetRestrictions tgt = sa.getTargetRestrictions();
+                sa.resetTargets();
 
-            // need a copy for evaluation
-            Card transformed = CardUtil.getLKICopy(source);
-            transformed.getCurrentState().copyFrom(source, source.getAlternateState());
-            transformed.updateStateForView();
+                CardCollection list = CardLists.getValidCards(CardLists.filter(game.getCardsIn(ZoneType.Battlefield), Presets.CREATURES), tgt.getValidTgts(), ai, source, sa);
+                // select only cards with Transform as SplitType
+                list = CardLists.filter(list, new Predicate<Card>() {
+                    @Override
+                    public boolean apply(Card c) {
+                        return c.hasAlternateState() && c.getRules().getSplitType() == CardSplitType.Transform;
+                    }
+                });
+                // select only the ones that can transform
+                list = CardLists.getNotKeyword(list, "CARDNAME can't transform");
+                list = CardLists.getTargetableCards(list, sa);
 
-            int valueSource = ComputerUtilCard.evaluateCreature(source);
-            int valueTransformed = ComputerUtilCard.evaluateCreature(transformed);
+                if (list.isEmpty()) {
+                    return false;
+                }
 
-            // it would not survive being transformed
-            if (transformed.getNetToughness() < 1) {
-                return false;
-            }
-
-            // check which state would be better for attacking
-            if (ph.isPlayerTurn(ai) && ph.getPhase().isBefore(PhaseType.COMBAT_DECLARE_ATTACKERS)) {
-                boolean transformAttack = false;
-
-                // if an opponent can't block it, no need to transform (back)
-                for (Player opp : ai.getOpponents()) {
-                    
-                    boolean attackSource = !ComputerUtilCard.canBeBlockedProfitably(opp, source);
-                    boolean attackTransformed = !ComputerUtilCard.canBeBlockedProfitably(opp, transformed);
-
-                    // both forms can attack, try to use the one with better value 
-                    if (attackSource && attackTransformed) {
-                        return valueSource <= valueTransformed;
-                    } else if (attackTransformed) { // only transformed cam attack
-                        transformAttack = true;
+                for (final Card c : list) {
+                    if (shouldTransformCard(c, ai, ph)) {
+                        sa.getTargets().add(c);
+                        if (sa.getTargets().getNumTargeted() == tgt.getMaxTargets(source, sa)) {
+                            break;
+                        }
                     }
                 }
 
-                // can only attack in transformed form
-                if (transformAttack) {
-                    return true;
-                }
-            } else if (ph.isPlayerTurn(ai) && ph.getPhase().equals(PhaseType.COMBAT_DECLARE_BLOCKERS)) {
-                if (ph.inCombat() && ph.getCombat().isUnblocked(source)) {
-                    // if source is unblocked, check for the power
-                    return source.getNetPower() <= transformed.getNetPower();
+                if (sa.getTargets().getNumTargeted() < tgt.getMinTargets(source, sa)) {
+                    return false;
                 }
             }
-            // no clear way, alternate state is better,
-            // but for more cleaner way use Evaluate for check
-            return valueSource <= valueTransformed;
+        } else if ("TurnFace".equals(mode)) {
+            if (!sa.usesTargeting()) {
+                CardCollection list = AbilityUtils.getDefinedCards(sa.getHostCard(), sa.getParam("Defined"), sa);
+                if (list.isEmpty()) {
+                    return false;
+                }
+                return shouldTurnFace(list.get(0), ai, ph);
+            } else {
+                final TargetRestrictions tgt = sa.getTargetRestrictions();
+                sa.resetTargets();
+
+                CardCollection list = CardLists.getValidCards(game.getCardsIn(ZoneType.Battlefield), tgt.getValidTgts(), ai, source, sa);
+                list = CardLists.getTargetableCards(list, sa);
+
+                if (list.isEmpty()) {
+                    return false;
+                }
+
+                for (final Card c : list) {
+                    if (shouldTurnFace(c, ai, ph)) {
+                        sa.getTargets().add(c);
+                        if (sa.getTargets().getNumTargeted() == tgt.getMaxTargets(source, sa)) {
+                            break;
+                        }
+                    }
+                }
+
+                if (sa.getTargets().getNumTargeted() < tgt.getMinTargets(source, sa)) {
+                    return false;
+                }
+            }
         }
         return true;
+    }
+
+    private boolean shouldTransformCard(Card card, Player ai, PhaseHandler ph) {
+        if (!card.hasAlternateState()) {
+            System.err.println("Warning: SetState without ALTERNATE on " + card.getName() + ".");
+            return false;
+        }
+
+        // need a copy for evaluation
+        Card transformed = CardUtil.getLKICopy(card);
+        transformed.getCurrentState().copyFrom(card, card.getAlternateState());
+        transformed.updateStateForView();
+        
+        return compareCards(card, transformed, ai, ph);
+
+    }
+
+    private boolean shouldTurnFace(Card card, Player ai, PhaseHandler ph) {
+        if (card.isFaceDown()) {
+            // non-permanent facedown can't be turned face up
+            if (!card.getRules().getType().isPermanent()) {
+                return false;
+            }            
+        } else {
+            // doublefaced or meld cards can't be turned face down
+            if (card.isDoubleFaced() || card.isMeldable()) {
+                return false;
+            }
+        }
+
+        // need a copy for evaluation
+        Card transformed = CardUtil.getLKICopy(card);
+        if (!card.isFaceDown()) {
+            transformed.turnFaceDown(true);
+        } else {
+            transformed.turnFaceUp(false, false);
+        }
+        transformed.updateStateForView();
+        return compareCards(card, transformed, ai, ph);
+    }
+    
+    private boolean compareCards(Card original, Card copy, Player ai, PhaseHandler ph) {
+        int valueCard = ComputerUtilCard.evaluateCreature(original);
+        int valueTransformed = ComputerUtilCard.evaluateCreature(copy);
+
+        // card controlled by opponent, try to kill it or weak it
+        if (original.getController().isOpponentOf(ai)) {
+            return copy.getNetToughness() < 1 || valueCard > valueTransformed;
+        }
+
+        // it would not survive being transformed
+        if (copy.getNetToughness() < 1) {
+            return false;
+        }
+
+
+        // check which state would be better for attacking
+        if (ph.isPlayerTurn(ai) && ph.getPhase().isBefore(PhaseType.COMBAT_DECLARE_ATTACKERS)) {
+            boolean transformAttack = false;
+
+            // if an opponent can't block it, no need to transform (back)
+            for (Player opp : ai.getOpponents()) {
+                
+                boolean attackCard = !ComputerUtilCard.canBeBlockedProfitably(opp, original);
+                boolean attackTransformed = !ComputerUtilCard.canBeBlockedProfitably(opp, copy);
+
+                // both forms can attack, try to use the one with better value 
+                if (attackCard && attackTransformed) {
+                    return valueCard <= valueTransformed;
+                } else if (attackTransformed) { // only transformed cam attack
+                    transformAttack = true;
+                }
+            }
+
+            // can only attack in transformed form
+            if (transformAttack) {
+                return true;
+            }
+        } else if (ph.isPlayerTurn(ai) && ph.getPhase().equals(PhaseType.COMBAT_DECLARE_BLOCKERS)) {
+            if (ph.inCombat() && ph.getCombat().isUnblocked(original)) {
+                // if source is unblocked, check for the power
+                return original.getNetPower() <= copy.getNetPower();
+            }
+        }
+        // no clear way, alternate state is better,
+        // but for more cleaner way use Evaluate for check
+        return valueCard <= valueTransformed;
     }
 }
