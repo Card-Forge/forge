b21a9b36d079ce7917d91b15067488aae15d5fb3
Hanmac <hanmac@cardforge.org>
2016-10-30 15:09:20 +0000

    KeywordWithCostAndAmount: fixed Parsing for Keywords with X as Amount (and Cost)

--- forge-game/src/main/java/forge/game/keyword/KeywordWithCostAndAmount.java
+++ forge-game/src/main/java/forge/game/keyword/KeywordWithCostAndAmount.java
@@ -4,6 +4,7 @@ import forge.game.cost.Cost;
 
 public class KeywordWithCostAndAmount extends KeywordInstance<KeywordWithCostAndAmount> {
     private Cost cost;
+    private boolean withX;
     private int amount;
 
     @Override
@@ -14,12 +15,20 @@ public class KeywordWithCostAndAmount extends KeywordInstance<KeywordWithCostAnd
     @Override
     protected void parse(String details) {
         final String[] k = details.split(":");
-        amount = Integer.parseInt(k[0]);
+        if (k[0].startsWith("X")) {
+            withX = true;
+        } else {
+            amount = Integer.parseInt(k[0]);
+        }
         cost = new Cost(k[1].split("\\|", 2)[0].trim(), false);
     }
 
     @Override
     protected String formatReminderText(String reminderText) {
-        return String.format(reminderText, cost.toSimpleString(), amount);
+        String formatStr = reminderText;
+        if (withX) {
+            formatStr = reminderText.replaceAll("\\%(\\d+\\$)?d", "%$1s");
+        }
+        return String.format(formatStr, cost.toSimpleString(), withX ? "X" : amount);
     }
 }
