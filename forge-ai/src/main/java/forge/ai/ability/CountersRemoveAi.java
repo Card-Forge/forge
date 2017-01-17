20f354b331848a707880c4074bb8df11f8f57885
Hanmac <hanmac@cardforge.org>
2017-01-17 05:31:39 +0000

    CountersRemoveAi: refactor it for now, extend it later

--- forge-ai/src/main/java/forge/ai/ability/CountersRemoveAi.java
+++ forge-ai/src/main/java/forge/ai/ability/CountersRemoveAi.java
@@ -1,78 +1,62 @@
 package forge.ai.ability;
 
-import forge.ai.ComputerUtil;
-import forge.ai.ComputerUtilCost;
 import forge.ai.SpellAbilityAi;
-import forge.game.card.Card;
 import forge.game.card.CounterType;
-import forge.game.cost.Cost;
+import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
-import forge.game.spellability.TargetRestrictions;
 
 public class CountersRemoveAi extends SpellAbilityAi {
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * forge.ai.SpellAbilityAi#checkPhaseRestrictions(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility, forge.game.phase.PhaseHandler)
+     */
     @Override
-    protected boolean canPlayAI(Player ai, SpellAbility sa) {
-        // AI needs to be expanded, since this function can be pretty complex
-        // based on what
-        // the expected targets could be
-        final Cost abCost = sa.getPayCosts();
-        TargetRestrictions abTgt = sa.getTargetRestrictions();
-        final Card source = sa.getHostCard();
-        // List<Card> list;
-        // Card choice = null;
+    protected boolean checkPhaseRestrictions(Player ai, SpellAbility sa, PhaseHandler ph) {
+        final String type = sa.getParam("CounterType");
+
+        if (ph.getPhase().isBefore(PhaseType.MAIN2) && !sa.hasParam("ActivationPhases") && !type.equals("M1M1")) {
+            return false;
+        }
+        return super.checkPhaseRestrictions(ai, sa, ph);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see
+     * forge.ai.SpellAbilityAi#checkPhaseRestrictions(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility, forge.game.phase.PhaseHandler,
+     * java.lang.String)
+     */
+    @Override
+    protected boolean checkPhaseRestrictions(Player ai, SpellAbility sa, PhaseHandler ph, String logic) {
+        if ("EndOfOpponentsTurn".equals(logic)) {
+            if (!ph.is(PhaseType.END_OF_TURN) || !ph.getNextTurn().equals(ai)) {
+                return false;
+            }
+        }
+        return super.checkPhaseRestrictions(ai, sa, ph, logic);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see forge.ai.SpellAbilityAi#checkApiLogic(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility)
+     */
+    @Override
+    protected boolean checkApiLogic(Player ai, SpellAbility sa) {
 
         final String type = sa.getParam("CounterType");
-        // String amountStr = sa.get("CounterNum");
 
-        // TODO - currently, not targeted, only for Self
-
-        // Player player = af.isCurse() ? AllZone.getHumanPlayer() :
-        // AllZone.getComputerPlayer();
-
-        if (abCost != null) {
-            // AI currently disabled for these costs
-            if (!ComputerUtilCost.checkLifeCost(ai, abCost, source, 4, sa)) {
-                return false;
-            }
-
-            if (!ComputerUtilCost.checkDiscardCost(ai, abCost, source)) {
-                return false;
-            }
-
-            if (!ComputerUtilCost.checkSacrificeCost(ai, abCost, source)) {
-                return false;
-            }
-
-            if (!ComputerUtilCost.checkRemoveCounterCost(abCost, source)) {
-                return false;
-            }
-        }
-        
-
-        if ("EndOfOpponentsTurn".equals(sa.getParam("AILogic"))) {
-            if (!source.getGame().getPhaseHandler().is(PhaseType.END_OF_TURN) || source.getGame().getPhaseHandler().getNextTurn() != ai) {
-                return false;
-            }
-        }
-
-        // TODO handle proper calculation of X values based on Cost
-        // final int amount = calculateAmount(sa.getHostCard(), amountStr, sa);
-
-        if (ComputerUtil.preventRunAwayActivations(sa)) {
-            return false;
-        }
-
-        // currently, not targeted
-        if (abTgt != null) {
-            return false;
-        }
-
-        if (ai.getGame().getPhaseHandler().getPhase().isBefore(PhaseType.MAIN2)
-                && !sa.hasParam("ActivationPhases")
-                && !type.equals("M1M1")) {
+        // TODO currently, not targeted
+        if (sa.usesTargeting()) {
             return false;
         }
 
@@ -83,7 +67,7 @@ public class CountersRemoveAi extends SpellAbilityAi {
             }
         }
 
-        return true;
+        return super.checkApiLogic(ai, sa);
     }
 
     @Override
