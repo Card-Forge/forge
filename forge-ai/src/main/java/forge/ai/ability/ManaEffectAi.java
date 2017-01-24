5e0c6bbaeea966f91afa35b08fb2d9a589055b3b
Agetian <stavdev@mail.ru>
2017-01-24 11:16:25 +0000

    - Moved ManaRitual AI logic to ManaEffectAi (no longer card-specific).

--- forge-ai/src/main/java/forge/ai/ability/ManaEffectAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ManaEffectAi.java
@@ -1,13 +1,26 @@
 package forge.ai.ability;
 
-
+import com.google.common.base.Predicates;
+import forge.ai.AiPlayDecision;
 import forge.ai.ComputerUtil;
-import forge.ai.SpecialCardAi;
+import forge.ai.ComputerUtilAbility;
+import forge.ai.ComputerUtilMana;
+import forge.ai.PlayerControllerAi;
 import forge.ai.SpellAbilityAi;
+import forge.card.MagicColor;
+import forge.card.mana.ManaCost;
+import forge.game.ability.AbilityUtils;
+import forge.game.card.Card;
+import forge.game.card.CardCollection;
+import forge.game.card.CardLists;
+import forge.game.card.CardPredicates;
 import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
+import forge.game.zone.ZoneType;
+import java.util.Arrays;
+import java.util.List;
 
 public class ManaEffectAi extends SpellAbilityAi {
 
@@ -20,7 +33,7 @@ public class ManaEffectAi extends SpellAbilityAi {
     @Override
     protected boolean checkAiLogic(Player ai, SpellAbility sa, String aiLogic) {
         if ("ManaRitual".equals(aiLogic)) {
-            return SpecialCardAi.ManaRitual.consider(ai, sa);
+            return doManaRitual(ai, sa);
         }
         return super.checkAiLogic(ai, sa, aiLogic);
     }
@@ -77,7 +90,7 @@ public class ManaEffectAi extends SpellAbilityAi {
     }
 
     /**
-     * @param ai
+     * @param aiPlayer
      *            the AI player.
      * @param sa
      *            a {@link forge.game.spellability.SpellAbility} object.
@@ -90,5 +103,74 @@ public class ManaEffectAi extends SpellAbilityAi {
     protected boolean doTriggerAINoCost(Player aiPlayer, SpellAbility sa, boolean mandatory) {
         return true;
     }
+    
+    // Dark Ritual and other similar instants/sorceries that add mana to mana pool
+    private boolean doManaRitual(Player ai, SpellAbility sa) {
+        final Card host = sa.getHostCard();
+          
+        CardCollection manaSources = ComputerUtilMana.getAvailableMana(ai, true);
+        int numManaSrcs = manaSources.size();
+        int manaReceived = AbilityUtils.calculateAmount(host, sa.getParam("Amount"), sa);
+        int selfCost = sa.getPayCosts().getCostMana() != null ? sa.getPayCosts().getCostMana().getMana().getCMC() : 0;
+        byte producedColor = MagicColor.fromName(sa.getParam("Produced"));
+        int searchCMC = numManaSrcs - selfCost + manaReceived;
 
+        if ("X".equals(sa.getParam("Produced"))) {
+            String x = host.getSVar("X");
+            if ("Count$CardsInYourHand".equals(x) && host.isInZone(ZoneType.Hand)) {
+                searchCMC--; // the spell in hand will be used
+            } else if (x.startsWith("Count$NamedInAllYards") && host.isInZone(ZoneType.Graveyard)) {
+                searchCMC--; // the spell in graveyard will be used
+            }
+        }
+
+        if (searchCMC <= 0) {
+            return false;
+        }
+
+        String restrictValid = sa.hasParam("RestrictValid") ? sa.getParam("RestrictValid") : "Card";
+
+        CardCollection cardList = new CardCollection();
+        List<SpellAbility> all = ComputerUtilAbility.getSpellAbilities(ai.getCardsIn(ZoneType.Hand), ai);
+        for (final SpellAbility testSa : ComputerUtilAbility.getOriginalAndAltCostAbilities(all, ai)) {
+            ManaCost cost = testSa.getPayCosts().getTotalMana();
+            if (cost.getCMC() == 0 && cost.countX() == 0) {
+                // no mana cost, no need to activate this SA then (additional mana not needed)
+                continue;
+            }
+
+            if (testSa.getHostCard().getName().equals(host.getName())) {
+                // prevent infinitely recursing own ability when testing AI play decision
+                continue;
+            }
+
+            SpellAbility testSaNoCost = testSa.copyWithNoManaCost();
+            testSaNoCost.setActivatingPlayer(ai);
+            if (((PlayerControllerAi)ai.getController()).getAi().canPlaySa(testSaNoCost) == AiPlayDecision.WillPlay) {
+                if (testSa.getHostCard().isPermanent() && !testSa.getHostCard().hasKeyword("Haste") 
+                    && !ai.getGame().getPhaseHandler().is(PhaseType.MAIN2)) {
+                    // AI will waste a ritual in Main 1 unless the casted permanent is a haste creature
+                    continue;
+                }
+                if (testSa.getHostCard().isInstant()) {
+                    // AI is bad at choosing which instants are worth a Ritual
+                    continue;
+                }
+
+                // the AI is willing to play the spell
+                if (!cardList.contains(testSa.getHostCard())) {
+                    cardList.add(testSa.getHostCard());
+                }
+            }
+        }
+
+        CardCollection castableSpells = CardLists.filter(cardList,
+                Arrays.asList(
+                        CardPredicates.restriction(restrictValid.split(","), ai, host, sa),
+                        CardPredicates.lessCMC(searchCMC),
+                        Predicates.or(CardPredicates.isColorless(), CardPredicates.isColor(producedColor))));
+
+        // TODO: this will probably still waste the card from time to time. Somehow improve detection of castable material.
+        return castableSpells.size() > 0;
+    }
 }
