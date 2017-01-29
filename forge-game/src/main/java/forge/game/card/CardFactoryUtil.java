9470f60554d3fdcab10ba7a6a6fb9b1917b5154b
Agetian <stavdev@mail.ru>
2017-01-29 12:54:45 +0000

    - One more minor CardFactoryUtil mistype fix.

--- forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
+++ forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
@@ -2741,7 +2741,7 @@ public class CardFactoryUtil {
                     + " | TriggerDescription$ Fabricate " + n + " (" + Keyword.getInstance(keyword).getReminderText() + ")";
 
             final String choose = "DB$ GenericChoice | Choices$ DB" + name + "Counter,DB" + name + "Token | ConditionPresent$ Card.StrictlySelf | SubAbility$ DB" + name + "Token2 | AILogic$ " + name;
-            final String counter = "DB$ PutCounter | Defined$ Self | CounterType$ P1P1 | CounterNum$ " + n + " | SpellDescription$ put "
+            final String counter = "DB$ PutCounter | Defined$ Self | CounterType$ P1P1 | CounterNum$ " + n + " | SpellDescription$ Put "
                     + Lang.nounWithNumeral(n, "+1/+1 counter") + " on it.";
             final String token = "DB$ Token | TokenAmount$ " + n + " | TokenName$ Servo | TokenTypes$ Artifact,Creature,Servo "
                     + " | TokenOwner$ You | TokenColors$ Colorless | TokenPower$ 1 | TokenToughness$ 1 | TokenImage$ c 1 1 servo KLD | SpellDescription$ Create "
