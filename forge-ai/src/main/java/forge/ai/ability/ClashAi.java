9e136031225138b23e9576dbb4ec14738cc9d4a2
Hanmac <hanmac@cardforge.org>
2017-02-11 09:17:59 +0000

    ClashAi: rewrite it to use new SpellAbilityAi format

--- forge-ai/src/main/java/forge/ai/ability/ClashAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ClashAi.java
@@ -7,8 +7,9 @@ import forge.ai.SpellAbilityAi;
 import forge.game.card.Card;
 import forge.game.card.CardCollectionView;
 import forge.game.player.Player;
+import forge.game.player.PlayerCollection;
+import forge.game.player.PlayerPredicates;
 import forge.game.spellability.SpellAbility;
-import forge.game.spellability.TargetRestrictions;
 import forge.game.zone.ZoneType;
 
 public class ClashAi extends SpellAbilityAi {
@@ -21,43 +22,55 @@ public class ClashAi extends SpellAbilityAi {
         return true;
     }
 
-    /* (non-Javadoc)
-     * @see forge.card.abilityfactory.SpellAiLogic#canPlayAI(forge.game.player.Player, java.util.Map, forge.card.spellability.SpellAbility)
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see forge.ai.SpellAbilityAi#checkApiLogic(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility)
      */
     @Override
-    protected boolean canPlayAI(Player ai, SpellAbility sa) {
-        final TargetRestrictions tgt = sa.getTargetRestrictions();
-        final Player opp = ai.getOpponent();
-        if (tgt != null) {
-            if (!sa.canTarget(opp)) {
-                return false;
+    protected boolean checkApiLogic(Player ai, SpellAbility sa) {
+        if (sa.usesTargeting()) {
+            PlayerCollection players = ai.getOpponents().filter(PlayerPredicates.isTargetableBy(sa));
+            // use chooseSinglePlayer function to the select player
+            Player chosen = chooseSinglePlayer(ai, sa, players);
+            if (chosen != null) {
+                sa.resetTargets();
+                sa.getTargets().add(chosen);
+                return true;
             }
-            sa.resetTargets();
-            sa.getTargets().add(opp);
+            return false;
         }
         return true;
     }
 
+    /*
+     * (non-Javadoc)
+     * 
+     * @see forge.ai.SpellAbilityAi#chooseSinglePlayer(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility, java.lang.Iterable)
+     */
     @Override
-	protected Player chooseSinglePlayer(Player ai, SpellAbility sa, Iterable<Player> options) {
-    	for (Player p : options) {
-    		if (p.getCardsIn(ZoneType.Library).size() == 0)
-    			return p;
-    	}
+    protected Player chooseSinglePlayer(Player ai, SpellAbility sa, Iterable<Player> options) {
+        for (Player p : options) {
+            if (p.getCardsIn(ZoneType.Library).size() == 0)
+                return p;
+        }
 
     	CardCollectionView col = ai.getCardsIn(ZoneType.Library);
         if (!col.isEmpty() && col.getFirst().mayPlayerLook(ai)) {
-    		final Card top = col.get(0);
-    		for (Player p : options) {
-    			final Card oppTop = p.getCardsIn(ZoneType.Library).getFirst();
-    			// TODO add logic for SplitCards
-    			if (top.getCMC() > oppTop.getCMC()) {
-    				return p;
-    			}
-    		}
-    	}
+            final Card top = col.get(0);
+            for (Player p : options) {
+                final Card oppTop = p.getCardsIn(ZoneType.Library).getFirst();
+                // TODO add logic for SplitCards
+                if (top.getCMC() > oppTop.getCMC()) {
+                    return p;
+                }
+            }
+        }
 
-		return Iterables.getFirst(options, null);
-	}
+        return Iterables.getFirst(options, null);
+    }
 
 }
