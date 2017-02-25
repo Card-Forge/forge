571f69011b892eacb4765b15d5e24c70a5d14ab3
Hanmac <hanmac@cardforge.org>
2017-02-25 10:03:13 +0000

    Player: getAllies should not return the player itself

--- forge-game/src/main/java/forge/game/player/Player.java
+++ forge-game/src/main/java/forge/game/player/Player.java
@@ -333,7 +333,7 @@ public class Player extends GameEntity implements Comparable<Player> {
      * Should keep player relations somewhere in the match structure
      */
     public final PlayerCollection getAllies() {
-        return game.getPlayers().filter(Predicates.not(PlayerPredicates.isOpponentOf(this)));
+        return getAllOtherPlayers().filter(Predicates.not(PlayerPredicates.isOpponentOf(this)));
     }
 
     /**
