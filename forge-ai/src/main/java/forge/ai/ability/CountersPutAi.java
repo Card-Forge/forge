5025e090a50846eee7085b91aac57ba0b19a3537
Agetian <stavdev@mail.ru>
2017-05-24 06:40:19 +0000

    - Fixed the AI never playing */* cards that ETB with X +1/+1 counters on them due to a PayX miscalculation in CountersPutAi (e.g. Primordial Hydra, Altered Ego, Mikaeus, the Lunarch).

--- forge-ai/src/main/java/forge/ai/ability/CountersPutAi.java
+++ forge-ai/src/main/java/forge/ai/ability/CountersPutAi.java
@@ -595,17 +595,25 @@ public class CountersPutAi extends SpellAbilityAi {
                 // detect if there's more than one X in the cost (Hangarback Walker, Walking Ballista, etc.)
                 SpellAbility testSa = sa;
                 int countX = 0;
+                int nonXGlyphs = 0;
                 while (testSa != null && testSa.getPayCosts() != null && countX == 0) {
                     countX = testSa.getPayCosts().getTotalMana().countX();
+                    nonXGlyphs = testSa.getPayCosts().getTotalMana().getGlyphCount() - countX;
                     testSa = testSa.getSubAbility();
                 }
                 if (countX == 0) {
                     // try determining it from the card itself if neither SA has "X" in the cost
                     countX = source.getState(CardStateName.Original).getManaCost().countX();
+                    nonXGlyphs = source.getState(CardStateName.Original).getManaCost().getGlyphCount() - countX;
                 }
 
                 // Spend all remaining mana to add X counters (eg. Hero of Leina Tower)
                 int payX = ComputerUtilMana.determineLeftoverMana(sa, ai);
+
+                // Account for the possible presence of additional glyphs in cost (e.g. Mikaeus, the Lunarch; Primordial Hydra)
+                payX -= nonXGlyphs;
+
+                // Account for the multiple X in cost
                 if (countX > 1) { payX /= countX; }
 
                 source.setSVar("PayX", Integer.toString(payX));
