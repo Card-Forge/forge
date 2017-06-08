0a94d1978efdc76b369bae6fbd1718dd6225702c
Hanmac <hanmac@cardforge.org>
2017-06-08 18:35:17 +0000

    Card; hide Prowess Keyword, it is done by the Trigger

--- forge-game/src/main/java/forge/game/card/Card.java
+++ forge-game/src/main/java/forge/game/card/Card.java
@@ -1522,7 +1522,7 @@ public class Card extends GameEntity implements Comparable<Card> {
                     || keyword.startsWith("Evoke") || keyword.startsWith("Bestow") || keyword.startsWith("Dash")
                     || keyword.startsWith("Surge") || keyword.startsWith("Transmute") || keyword.startsWith("Suspend")
                     || keyword.equals("Undaunted") || keyword.startsWith("Monstrosity") || keyword.startsWith("Embalm")
-                    || keyword.startsWith("Level up")
+                    || keyword.startsWith("Level up") || keyword.equals("Prowess")
                     || keyword.startsWith("Cycling") || keyword.startsWith("TypeCycling")) {
                 // keyword parsing takes care of adding a proper description
             } else if (keyword.startsWith("CantBeBlockedBy")) {
