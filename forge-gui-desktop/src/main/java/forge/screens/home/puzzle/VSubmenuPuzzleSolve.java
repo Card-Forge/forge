597d3128f4c5b3ba6d2a7be473be0c2208916df5
Agetian <stavdev@mail.ru>
2017-06-29 16:09:44 +0000

    - Improving the Solve a Puzzle screen: it's now themed, and there is a vertical scrolling pane available.

--- forge-gui-desktop/src/main/java/forge/screens/home/puzzle/VSubmenuPuzzleSolve.java
+++ forge-gui-desktop/src/main/java/forge/screens/home/puzzle/VSubmenuPuzzleSolve.java
@@ -8,6 +8,8 @@ import forge.match.GameLobby;
 import forge.match.LocalLobby;
 import forge.net.event.UpdateLobbyPlayerEvent;
 import forge.screens.home.*;
+import forge.toolbox.FList;
+import forge.toolbox.FScrollPane;
 import net.miginfocom.swing.MigLayout;
 
 import javax.swing.*;
@@ -15,7 +17,9 @@ import javax.swing.*;
 public enum VSubmenuPuzzleSolve implements IVSubmenu<CSubmenuPuzzleSolve> {
     SINGLETON_INSTANCE;
 
-    private final JList puzzleList;
+    private final FList puzzleList;
+    private final FScrollPane puzzleListPane;
+
     final DefaultListModel model = new DefaultListModel();
 
     private final StartButton btnStart  = new StartButton();
@@ -27,7 +31,9 @@ public enum VSubmenuPuzzleSolve implements IVSubmenu<CSubmenuPuzzleSolve> {
     private final VLobby vLobby = new VLobby(lobby);
 
     VSubmenuPuzzleSolve() {
-        puzzleList = new JList<>();
+        puzzleList = new FList<>();
+        puzzleListPane = new FScrollPane(this.puzzleList, true);
+
         lobby.setListener(vLobby);
 
         vLobby.setPlayerChangeListener(new IPlayerChangeListener() {
@@ -100,7 +106,7 @@ public enum VSubmenuPuzzleSolve implements IVSubmenu<CSubmenuPuzzleSolve> {
         vLobby.getLblTitle().setText("Puzzle Mode: Solve");
         container.add(vLobby.getLblTitle(), "w 80%, h 40px!, gap 0 0 15px 15px, span 2, al right, pushx");
         puzzleList.setModel(model);
-        container.add(puzzleList, "w 80%, h 200px!, gap 0 0 0px 0px, span 2, al center");
+        container.add(puzzleListPane, "w 80%, h 200px!, gap 0 0 0px 0px, span 2, al center");
         container.add(btnStart, "w 98%!, ax center, gap 1% 0 20px 20px, span 2");
 
 
