7d489785a3baf2fcab68268245af8feaf6c045aa
Agetian <stavdev@mail.ru>
2017-07-12 04:53:40 +0000

    - Basic support for Puzzle Mode on mobile Forge.

--- forge-gui-mobile/src/forge/screens/home/puzzle/PuzzleScreen.java
+++ forge-gui-mobile/src/forge/screens/home/puzzle/PuzzleScreen.java
@@ -1,18 +1,35 @@
 package forge.screens.home.puzzle;
 
+import forge.GuiBase;
 import forge.assets.FSkinFont;
+import forge.deck.Deck;
+import forge.game.GameRules;
+import forge.game.GameType;
+import forge.game.player.RegisteredPlayer;
+import forge.match.HostedMatch;
+import forge.player.GamePlayerUtil;
+import forge.puzzle.Puzzle;
+import forge.puzzle.PuzzleIO;
 import forge.screens.LaunchScreen;
+import forge.screens.LoadingOverlay;
 import forge.screens.home.NewGameMenu;
 import forge.toolbox.FLabel;
 import forge.toolbox.FTextArea;
-import forge.util.ThreadUtil;
+import forge.toolbox.GuiChoose;
+import forge.util.Callback;
 import forge.util.Utils;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class PuzzleScreen extends LaunchScreen {
+
     private static final float PADDING = Utils.scale(10);
 
     private final FTextArea lblDesc = add(new FTextArea(false,
-            "Puzzle Mode loads in a puzzle that you have to win in a predetermined time/way."));
+            "Puzzle Mode loads in a puzzle that you have to win in a predetermined time/way.\n\n" +
+            "To begin, click the Start button below, then select a puzzle from a list.\n\n" +
+            "Your objective will be specified on a special effect card which will be placed in your command zone."));
 
     public PuzzleScreen() {
         super(null, NewGameMenu.getMenu());
@@ -32,11 +49,43 @@ public class PuzzleScreen extends LaunchScreen {
 
     @Override
     protected void startMatch() {
-        ThreadUtil.invokeInGameThread(new Runnable() { //must run in game thread to prevent blocking UI thread
+        final ArrayList<Puzzle> puzzles = PuzzleIO.loadPuzzles();
+        Collections.sort(puzzles);
+
+        GuiChoose.one("Choose a puzzle", puzzles, new Callback<Puzzle>() {
             @Override
-            public void run() {
-                // Load selected puzzle
+            public void run(Puzzle result) {
+                LoadingOverlay.show("Loading the puzzle...", new Runnable() {
+                    @Override
+                    public void run() {
+
+                        // Load selected puzzle
+                        final Puzzle selected = result;
+
+                        final HostedMatch hostedMatch = GuiBase.getInterface().hostMatch();
+                        hostedMatch.setStartGameHook(new Runnable() {
+                            @Override
+                            public final void run() {
+                                selected.applyToGame(hostedMatch.getGame());
+                            }
+                        });
+
+                        final List<RegisteredPlayer> players = new ArrayList<>();
+                        final RegisteredPlayer human = new RegisteredPlayer(new Deck()).setPlayer(GamePlayerUtil.getGuiPlayer());
+                        human.setStartingHand(0);
+                        players.add(human);
+
+                        final RegisteredPlayer ai = new RegisteredPlayer(new Deck()).setPlayer(GamePlayerUtil.createAiPlayer());
+                        ai.setStartingHand(0);
+                        players.add(ai);
+
+                        GameRules rules = new GameRules(GameType.Puzzle);
+                        rules.setGamesPerMatch(1);
+                        hostedMatch.startMatch(rules, null, players, human, GuiBase.getInterface().getNewGuiGame());
+                    }
+                });
             }
         });
+
     }
 }
