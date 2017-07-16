103ddb6f158457d508c7375c778392cc732a4b5b
Hanmac <hanmac@cardforge.org>
2017-07-16 08:17:21 +0000

    LifeGainAi: new Format and try to add more logic into it with using Lifegain as Damage Spell

--- forge-ai/src/main/java/forge/ai/ability/LifeGainAi.java
+++ forge-ai/src/main/java/forge/ai/ability/LifeGainAi.java
@@ -8,23 +8,104 @@ import forge.game.cost.Cost;
 import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
+import forge.game.player.PlayerCollection;
+import forge.game.player.PlayerPredicates;
 import forge.game.spellability.AbilitySub;
 import forge.game.spellability.SpellAbility;
-import forge.game.spellability.TargetRestrictions;
 import forge.util.MyRandom;
 
 public class LifeGainAi extends SpellAbilityAi {
 
-    /* (non-Javadoc)
-     * @see forge.card.abilityfactory.AbilityFactoryAlterLife.SpellAiLogic#canPlayAI(forge.game.player.Player, java.util.Map, forge.card.spellability.SpellAbility)
+    /*
+     * (non-Javadoc)
+     * 
+     * @see forge.ai.SpellAbilityAi#willPayCosts(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility, forge.game.cost.Cost,
+     * forge.game.card.Card)
      */
     @Override
-    protected boolean canPlayAI(Player ai, SpellAbility sa) {
-        final Cost abCost = sa.getPayCosts();
-        final Card source = sa.getHostCard();
-        final String sourceName = ComputerUtilAbility.getAbilitySourceName(sa);
+    protected boolean willPayCosts(Player ai, SpellAbility sa, Cost cost, Card source) {
         final Game game = source.getGame();
         final PhaseHandler ph = game.getPhaseHandler();
+        final int life = ai.getLife();
+
+        boolean lifeCritical = life <= 5;
+        lifeCritical |= ph.getPhase().isBefore(PhaseType.COMBAT_DAMAGE)
+                && ComputerUtilCombat.lifeInDanger(ai, game.getCombat());
+
+        if (!lifeCritical) {
+            // return super.willPayCosts(ai, sa, cost, source);
+            if (!ComputerUtilCost.checkSacrificeCost(ai, cost, source, false)) {
+                return false;
+            }
+            if (!ComputerUtilCost.checkLifeCost(ai, cost, source, 4, sa)) {
+                return false;
+            }
+
+            if (!ComputerUtilCost.checkDiscardCost(ai, cost, source)) {
+                return false;
+            }
+
+            if (!ComputerUtilCost.checkRemoveCounterCost(cost, source)) {
+                return false;
+            }
+        } else {
+            // don't sac possible blockers
+            if (!ph.getPhase().equals(PhaseType.COMBAT_DECLARE_BLOCKERS)
+                    || !game.getCombat().getDefenders().contains(ai)) {
+                boolean skipCheck = false;
+                // if it's a sac self cost and the effect source is not a
+                // creature, skip this check
+                // (e.g. Woodweaver's Puzzleknot)
+                skipCheck |= ComputerUtilCost.isSacrificeSelfCost(cost) && !source.isCreature();
+
+                if (!skipCheck) {
+                    if (!ComputerUtilCost.checkSacrificeCost(ai, cost, source, false)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return true;
+    }
+
+    @Override
+    protected boolean checkPhaseRestrictions(final Player ai, final SpellAbility sa, final PhaseHandler ph) {
+        final Game game = ai.getGame();
+        final int life = ai.getLife();
+        boolean activateForCost = ComputerUtil.activateForCost(sa, ai);
+
+        boolean lifeCritical = life <= 5;
+        lifeCritical |= ph.getPhase().isBefore(PhaseType.COMBAT_DAMAGE)
+                && ComputerUtilCombat.lifeInDanger(ai, game.getCombat());
+
+        // Don't use lifegain before main 2 if possible
+        if (!lifeCritical && ph.getPhase().isBefore(PhaseType.MAIN2) && !sa.hasParam("ActivationPhases")
+                && !ComputerUtil.castSpellInMain1(ai, sa)) {
+            return false;
+        }
+
+        if (!lifeCritical && !activateForCost
+                && (!ph.getNextTurn().equals(ai) || ph.getPhase().isBefore(PhaseType.END_OF_TURN))
+                && !sa.hasParam("PlayerTurn") && !SpellAbilityAi.isSorcerySpeed(sa)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see forge.ai.SpellAbilityAi#checkApiLogic(forge.game.player.Player,
+     * forge.game.spellability.SpellAbility)
+     */
+    @Override
+    protected boolean checkApiLogic(Player ai, SpellAbility sa) {
+        final Card source = sa.getHostCard();
+        final String sourceName = ComputerUtilAbility.getAbilitySourceName(sa);
+
         final int life = ai.getLife();
         final String amountStr = sa.getParam("LifeAmount");
         int lifeAmount = 0;
@@ -38,12 +119,12 @@ public class LifeGainAi extends SpellAbilityAi {
             lifeAmount = AbilityUtils.calculateAmount(sa.getHostCard(), amountStr, sa);
         }
 
-        //Ugin AI: always use ultimate
+        // Ugin AI: always use ultimate
         if (sourceName.equals("Ugin, the Spirit Dragon")) {
-            //TODO: somehow link with DamageDealAi for cases where +1 = win
+            // TODO: somehow link with DamageDealAi for cases where +1 = win
             return true;
         }
-        
+
         // don't use it if no life to gain
         if (!activateForCost && lifeAmount <= 0) {
             return false;
@@ -61,42 +142,6 @@ public class LifeGainAi extends SpellAbilityAi {
             }
         }
 
-        boolean lifeCritical = life <= 5;
-        lifeCritical |= ph.getPhase().isBefore(PhaseType.COMBAT_DAMAGE) && ComputerUtilCombat.lifeInDanger(ai, game.getCombat());
-
-        if (abCost != null) {
-            if (!lifeCritical) {
-	            if (!ComputerUtilCost.checkSacrificeCost(ai, abCost, source, false)) {
-	                return false;
-	            }
-	            if (!ComputerUtilCost.checkLifeCost(ai, abCost, source, 4, sa)) {
-	                return false;
-	            }
-	
-	            if (!ComputerUtilCost.checkDiscardCost(ai, abCost, source)) {
-	                return false;
-	            }
-	
-	            if (!ComputerUtilCost.checkRemoveCounterCost(abCost, source)) {
-	                return false;
-	            }
-            } else {
-            	// don't sac possible blockers
-            	if (!ph.getPhase().equals(PhaseType.COMBAT_DECLARE_BLOCKERS) || !game.getCombat().getDefenders().contains(ai)) {
-                    boolean skipCheck = false;
-                    // if it's a sac self cost and the effect source is not a creature, skip this check
-                    // (e.g. Woodweaver's Puzzleknot)
-                    skipCheck |= ComputerUtilCost.isSacrificeSelfCost(abCost) && !source.isCreature();
-
-                    if (!skipCheck) {
-                        if (!ComputerUtilCost.checkSacrificeCost(ai, abCost, source, false)) {
-                            return false;
-                        }
-                    }
-            	}
-            }
-        }
-
         if (!activateForCost && !ai.canGainLife()) {
             return false;
         }
@@ -106,41 +151,19 @@ public class LifeGainAi extends SpellAbilityAi {
             return false;
         }
 
-        if (ComputerUtil.playImmediately(ai, sa)) {
-            return true;
-        }
-        
-        // Don't use lifegain before main 2 if possible
-        if (!lifeCritical && ph.getPhase().isBefore(PhaseType.MAIN2)
-                && !sa.hasParam("ActivationPhases") && !ComputerUtil.castSpellInMain1(ai, sa)) {
-            return false;
-        }
-
-        if (!lifeCritical && !activateForCost && (!ph.getNextTurn().equals(ai)
-                || ph.getPhase().isBefore(PhaseType.END_OF_TURN))
-                && !sa.hasParam("PlayerTurn") && !SpellAbilityAi.isSorcerySpeed(sa)) {
-            return false;
-        }
-
-        // Don't tap creatures that may be able to block
-        if (ComputerUtil.waitForBlocking(sa)) {
-            return false;
-        }
-
-        final TargetRestrictions tgt = sa.getTargetRestrictions();
-        if (tgt != null) {
-            sa.resetTargets();
-            if (sa.canTarget(ai)) {
-                sa.getTargets().add(ai);
-            } else {
+        if (sa.usesTargeting()) {
+            if (!target(ai, sa, true)) {
                 return false;
             }
         }
-        
+
+        if (ComputerUtil.playImmediately(ai, sa)) {
+            return true;
+        }
+
         if (SpellAbilityAi.isSorcerySpeed(sa)
-        		|| sa.getSubAbility() != null
-        		|| SpellAbilityAi.playReusable(ai, sa)) {
-        	return true;
+                || sa.getSubAbility() != null || SpellAbilityAi.playReusable(ai, sa)) {
+            return true;
         }
         
         // Save instant-speed life-gain unless it is really worth it
@@ -150,7 +173,6 @@ public class LifeGainAi extends SpellAbilityAi {
         }
         return MyRandom.getRandom().nextFloat() < value;
     }
-
 
     /**
      * <p>
@@ -170,14 +192,8 @@ public class LifeGainAi extends SpellAbilityAi {
         // If the Target is gaining life, target self.
         // if the Target is modifying how much life is gained, this needs to be
         // handled better
-        final TargetRestrictions tgt = sa.getTargetRestrictions();
-        if (tgt != null) {
-            sa.resetTargets();
-            if (sa.canTarget(ai)) {
-                sa.getTargets().add(ai);
-            } else if (mandatory && sa.canTarget(ai.getOpponent())) {
-                sa.getTargets().add(ai.getOpponent());
-            } else {
+        if (sa.usesTargeting()) {
+            if (!target(ai, sa, mandatory)) {
                 return false;
             }
         }
@@ -196,5 +212,82 @@ public class LifeGainAi extends SpellAbilityAi {
     @Override
     public boolean chkAIDrawback(SpellAbility sa, Player ai) {
     	return doTriggerAINoCost(ai, sa, true);
+    }
+
+    private boolean target(Player ai, SpellAbility sa, boolean mandatory) {
+        Card source = sa.getHostCard();
+        sa.resetTargets();
+        // TODO : add add even more logic into it
+        // try to target opponents first if that would kill them
+
+        PlayerCollection opps = ai.getOpponents().filter(PlayerPredicates.isTargetableBy(sa));
+        PlayerCollection allies = ai.getAllies().filter(PlayerPredicates.isTargetableBy(sa));
+
+        if (sa.canTarget(ai) && ComputerUtil.lifegainPositive(ai, source)) {
+            sa.getTargets().add(ai);
+        } else {
+            boolean hasTgt = false;
+            // check for Lifegain negative on opponents
+            for (Player opp : opps) {
+                if (ComputerUtil.lifegainNegative(opp, source)) {
+                    sa.getTargets().add(opp);
+                    hasTgt = true;
+                    break;
+                }
+            }
+            if (!hasTgt) {
+                // lifegain on ally
+                for (Player ally : allies) {
+                    if (ComputerUtil.lifegainPositive(ally, source)) {
+                        sa.getTargets().add(ally);
+                        hasTgt = true;
+                        break;
+                    }
+                }
+            }
+            if (!hasTgt && mandatory) {
+                // need to target something but its neither negative against
+                // opponents,
+                // nor posive against allies
+
+                // hurting ally is probably better than healing opponent
+                // look for Lifegain not Negative (case of lifegain negated)
+                for (Player ally : allies) {
+                    if (!ComputerUtil.lifegainNegative(ally, source)) {
+                        sa.getTargets().add(ally);
+                        hasTgt = true;
+                        break;
+                    }
+                }
+                if (!hasTgt) {
+                    // same for opponent lifegain not positive
+                    for (Player opp : opps) {
+                        if (!ComputerUtil.lifegainPositive(opp, source)) {
+                            sa.getTargets().add(opp);
+                            hasTgt = true;
+                            break;
+                        }
+                    }
+                }
+
+                // still no luck, try to target ally with most life
+                if (!allies.isEmpty()) {
+                    Player ally = allies.max(PlayerPredicates.compareByLife());
+                    sa.getTargets().add(ally);
+                    hasTgt = true;
+                }
+                // better heal opponent which most life then the one with the
+                // lowest
+                if (!hasTgt) {
+                    Player opp = opps.max(PlayerPredicates.compareByLife());
+                    sa.getTargets().add(opp);
+                    hasTgt = true;
+                }
+            }
+            if (!hasTgt) {
+                return false;
+            }
+        }
+        return true;
     }
 }
