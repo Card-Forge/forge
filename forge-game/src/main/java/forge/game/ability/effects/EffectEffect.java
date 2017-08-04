f0af71d8c0e18ed886c37ccbcc47b35565946120
Agetian <stavdev@mail.ru>
2017-08-04 10:39:05 +0000

    - Removed an experimental test line. - Fixed Hazduhr the Abbot out of sight trigger definition.

--- forge-game/src/main/java/forge/game/ability/effects/EffectEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/EffectEffect.java
@@ -111,7 +111,6 @@ public class EffectEffect extends SpellAbilityEffect {
         }
 
         final Card eff = createEffect(hostCard, controller, name, image);
-        eff.setEffectSource(hostCard);
 
         // Grant SVars first in order to give references to granted abilities
         if (effectSVars != null) {
