6ed9a8b33671a91a9a3f266a173dd5e6674468d0
Agetian <stavdev@mail.ru>
2017-09-13 14:25:44 +0000

    - A little tweak to the previous commit.

--- forge-ai/src/main/java/forge/ai/AiBlockController.java
+++ forge-ai/src/main/java/forge/ai/AiBlockController.java
@@ -626,7 +626,7 @@ public class AiBlockController {
                     if (blocker.isFaceDown() && blocker.getState(CardStateName.Original).getType().isCreature()) {
                         // if the blocker is a face-down creature (e.g. cast via Morph, Manifest), evaluate it
                         // in relation to the original state, not to the Morph state
-                        evalBlk = ComputerUtilCard.evaluateCreature(Card.fromPaperCard(blocker.getPaperCard(), ai), true, false);
+                        evalBlk = ComputerUtilCard.evaluateCreature(Card.fromPaperCard(blocker.getPaperCard(), ai), false, true);
                     }
                     int chanceToSavePW = chanceToTradeDownToSaveWalker > 0 && evalAtk + 1 < evalBlk ? chanceToTradeDownToSaveWalker : chanceToTradeToSaveWalker;
                     boolean powerParityOrHigher = blocker.getNetPower() >= attacker.getNetPower();
