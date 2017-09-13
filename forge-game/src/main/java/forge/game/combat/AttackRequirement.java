a0abaf62b4da546dc6036de6248200523338158a
Agetian <stavdev@mail.ru>
2017-09-13 08:59:35 +0000

    - [XLN] Added Trove of Temptation. This card is ugly, so certain side effects in corner cases are probably still possible. Currently implemented as a keyword-like ability similar to "no more than X creatures can attack each turn" etc., but maybe is better as a global rule, I'm not sure (and not sure how to properly convert it to a global rule either...). Assistance and improvements are welcome.

--- forge-game/src/main/java/forge/game/combat/AttackRequirement.java
+++ forge-game/src/main/java/forge/game/combat/AttackRequirement.java
@@ -1,31 +1,37 @@
 package forge.game.combat;
 
-import java.util.List;
-import java.util.Map;
-
-import org.apache.commons.lang3.tuple.Pair;
-
 import com.google.common.base.Function;
 import com.google.common.collect.Lists;
-
 import forge.game.Game;
 import forge.game.GameEntity;
 import forge.game.ability.AbilityUtils;
 import forge.game.card.Card;
+import forge.game.card.CardLists;
+import forge.game.card.CardPredicates;
 import forge.game.player.Player;
 import forge.game.zone.ZoneType;
 import forge.util.collect.FCollectionView;
-import forge.util.maps.MapToAmountUtil;
 import forge.util.maps.LinkedHashMapToAmount;
 import forge.util.maps.MapToAmount;
+import forge.util.maps.MapToAmountUtil;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class AttackRequirement {
 
     private final MapToAmount<GameEntity> defenderSpecific;
+    private final MapToAmount<GameEntity> defenderOrPWSpecific;
+    private final Map<GameEntity, List<GameEntity>> defenderSpecificAlternatives;
     private final MapToAmount<Card> causesToAttack;
 
     public AttackRequirement(final Card attacker, final MapToAmount<Card> causesToAttack, final FCollectionView<GameEntity> possibleDefenders) {
         this.defenderSpecific = new LinkedHashMapToAmount<GameEntity>();
+        this.defenderOrPWSpecific = new LinkedHashMapToAmount<GameEntity>();
+        this.defenderSpecificAlternatives = new HashMap<GameEntity, List<GameEntity>>();
+
         this.causesToAttack = causesToAttack;
 
         final GameEntity mustAttack = attacker.getController().getMustAttackEntity();
@@ -64,19 +70,43 @@ public class AttackRequirement {
         }
 
         final Game game = attacker.getGame();
+
+        for (Card c : game.getCardsIn(ZoneType.Battlefield)) {
+            if (c.hasKeyword("Each opponent must attack you or a planeswalker you control with at least one creature each combat if able.")) {
+                if (attacker.getController().isOpponentOf(c.getController()) && !defenderOrPWSpecific.containsKey(c.getController())) {
+                    defenderOrPWSpecific.put(c.getController(), 1);
+                    for (Card pw : CardLists.filter(c.getController().getCardsIn(ZoneType.Battlefield), CardPredicates.Presets.PLANEWALKERS)) {
+                        // Add the attack alternatives that suffice (planeswalkers that can be attacked instead of the player)
+                        if (!defenderSpecificAlternatives.containsKey(c.getController())) {
+                            defenderSpecificAlternatives.put(c.getController(), Lists.<GameEntity>newArrayList());
+                        }
+                        defenderSpecificAlternatives.get(c.getController()).add(pw);
+                    }
+                }
+            }
+        }
+
         for (final GameEntity defender : possibleDefenders) {
             if (CombatUtil.getAttackCost(game, attacker, defender) == null) {
                 // use put here because we want to always put it, even if the value is 0
                 defenderSpecific.put(defender, Integer.valueOf(defenderSpecific.count(defender) + nAttackAnything));
+                if (defenderOrPWSpecific.containsKey(defender)) {
+                    defenderOrPWSpecific.put(defender, Integer.valueOf(defenderOrPWSpecific.count(defender) + nAttackAnything));
+                }
             } else {
                 defenderSpecific.remove(defender);
+                defenderOrPWSpecific.remove(defender);
             }
         }
 
         // Remove GameEntities that are no longer on the battlefield or are
         // related to Players who have lost the game
-        final List<GameEntity> toRemove = Lists.newArrayListWithCapacity(defenderSpecific.size());
-        for (final GameEntity entity : defenderSpecific.keySet()) {
+        final MapToAmount<GameEntity> combinedDefMap = new LinkedHashMapToAmount<>();
+        combinedDefMap.putAll(defenderSpecific);
+        combinedDefMap.putAll(defenderOrPWSpecific);
+
+        final List<GameEntity> toRemove = Lists.newArrayListWithCapacity(combinedDefMap.size());
+        for (final GameEntity entity : combinedDefMap.keySet()) {
             boolean removeThis = false;
             if (entity instanceof Player) {
                 if (((Player) entity).hasLost()) {
@@ -94,11 +124,12 @@ public class AttackRequirement {
         }
         for (final GameEntity entity : toRemove) {
             defenderSpecific.remove(entity);
+            defenderOrPWSpecific.remove(entity);
         }
     }
 
     public boolean hasRequirement() {
-        return !defenderSpecific.isEmpty() || !causesToAttack.isEmpty();
+        return !defenderSpecific.isEmpty() || !causesToAttack.isEmpty() || !defenderOrPWSpecific.isEmpty();
     }
 
     public final MapToAmount<Card> getCausesToAttack() {
@@ -111,7 +142,36 @@ public class AttackRequirement {
         }
 
         final boolean isAttacking = defender != null;
-        int violations = defenderSpecific.countAll() - (isAttacking ? defenderSpecific.count(defender) : 0);
+        int violations = 0;
+
+        // first. check to see if "must attack X or Y with at least one creature" requirements are satisfied
+        List<GameEntity> toRemoveFromDefSpecific = Lists.<GameEntity>newArrayList();
+        if (!defenderOrPWSpecific.isEmpty()) {
+            for (GameEntity def : defenderOrPWSpecific.keySet()) {
+                if (defenderSpecificAlternatives.containsKey(def)) {
+                    boolean isAttackingDefender = false;
+                    outer: for (Card atk : attackers.keySet()) {
+                        // is anyone attacking this defender or any of the alternative defenders?
+                        if (attackers.get(atk).equals(def)) {
+                            isAttackingDefender = true;
+                            break;
+                        }
+                        for (GameEntity altDef : defenderSpecificAlternatives.get(def)) {
+                            if (attackers.get(atk).equals(altDef)) {
+                                isAttackingDefender = true;
+                                break outer;
+                            }
+                        }
+                    }
+                    if (!isAttackingDefender) {
+                        violations++; // no one is attacking that defender or any of his PWs
+                    }
+                }
+            }
+        }
+
+        // now, count everything else
+        violations += defenderSpecific.countAll() - (isAttacking ? (defenderSpecific.count(defender)) : 0);
         if (isAttacking) {
             for (final Map.Entry<Card, Integer> mustAttack : causesToAttack.entrySet()) {
                 // only count violations if the forced creature can actually attack and has no cost incurred for doing so
@@ -126,6 +186,7 @@ public class AttackRequirement {
     public List<Pair<GameEntity, Integer>> getSortedRequirements() {
         final List<Pair<GameEntity, Integer>> result = Lists.newArrayListWithExpectedSize(defenderSpecific.size());
         result.addAll(MapToAmountUtil.sort(defenderSpecific));
+        result.addAll(MapToAmountUtil.sort(defenderOrPWSpecific));
 
         for (int i = 0; i < result.size(); i++) {
             final Pair<GameEntity, Integer> def = result.get(i);
--- forge-game/src/main/java/forge/game/combat/GlobalAttackRestrictions.java
+++ forge-game/src/main/java/forge/game/combat/GlobalAttackRestrictions.java
@@ -1,28 +1,31 @@
 package forge.game.combat;
 
-import java.util.Map;
-import java.util.Map.Entry;
-
 import com.google.common.primitives.Ints;
-
 import forge.game.Game;
 import forge.game.GameEntity;
 import forge.game.GlobalRuleChange;
 import forge.game.card.Card;
+import forge.game.card.CardCollection;
 import forge.game.player.Player;
+import forge.game.player.PlayerCollection;
 import forge.game.zone.ZoneType;
 import forge.util.collect.FCollectionView;
-import forge.util.maps.MapToAmountUtil;
 import forge.util.maps.LinkedHashMapToAmount;
 import forge.util.maps.MapToAmount;
+import forge.util.maps.MapToAmountUtil;
+
+import java.util.Map;
+import java.util.Map.Entry;
 
 public class GlobalAttackRestrictions {
 
     private final int max;
     private final MapToAmount<GameEntity> defenderMax;
-    private GlobalAttackRestrictions(final int max, final MapToAmount<GameEntity> defenderMax) {
+    private final PlayerCollection mustBeAttackedByEachOpp;
+    private GlobalAttackRestrictions(final int max, final MapToAmount<GameEntity> defenderMax, PlayerCollection mustBeAttackedByEachOpp) {
         this.max = max;
         this.defenderMax = defenderMax;
+        this.mustBeAttackedByEachOpp = mustBeAttackedByEachOpp;
     }
 
     public int getMax() {
@@ -32,17 +35,17 @@ public class GlobalAttackRestrictions {
         return defenderMax;
     }
 
-    public boolean isLegal(final Map<Card, GameEntity> attackers) {
-        return !getViolations(attackers, true).isViolated();
+    public boolean isLegal(final Map<Card, GameEntity> attackers, final CardCollection possibleAttackers) {
+        return !getViolations(attackers, possibleAttackers,true).isViolated();
     }
 
-    public GlobalAttackRestrictionViolations getViolations(final Map<Card, GameEntity> attackers) {
-        return getViolations(attackers, false);
+    public GlobalAttackRestrictionViolations getViolations(final Map<Card, GameEntity> attackers, final CardCollection possibleAttackers) {
+        return getViolations(attackers, possibleAttackers,false);
     }
-    private GlobalAttackRestrictionViolations getViolations(final Map<Card, GameEntity> attackers, final boolean returnQuickly) {
+    private GlobalAttackRestrictionViolations getViolations(final Map<Card, GameEntity> attackers, final CardCollection possibleAttackers, final boolean returnQuickly) {
         final int nTooMany = max < 0 ? 0 : attackers.size() - max;
         if (returnQuickly && nTooMany > 0) {
-            return new GlobalAttackRestrictionViolations(nTooMany, MapToAmountUtil.<GameEntity>emptyMap());
+            return new GlobalAttackRestrictionViolations(nTooMany, MapToAmountUtil.<GameEntity>emptyMap(), MapToAmountUtil.<GameEntity>emptyMap());
         }
 
         final MapToAmount<GameEntity> defenderTooMany = new LinkedHashMapToAmount<GameEntity>(defenderMax.size());
@@ -73,24 +76,58 @@ public class GlobalAttackRestrictions {
             }
         }
 
-        return new GlobalAttackRestrictionViolations(nTooMany, defenderTooMany);
+        final MapToAmount<GameEntity> defenderTooFew = new LinkedHashMapToAmount<GameEntity>(defenderMax.size());
+        for (final GameEntity mandatoryDef : mustBeAttackedByEachOpp) {
+            // check to ensure that this defender can even legally be attacked in the first place
+            boolean canAttackThisDef = false;
+            for (Card c : possibleAttackers) {
+                if (CombatUtil.canAttack(c, mandatoryDef) && null == CombatUtil.getAttackCost(c.getGame(), c, mandatoryDef)) {
+                    canAttackThisDef = true;
+                    break;
+                }
+            }
+            if (!canAttackThisDef) {
+                continue;
+            }
+
+            boolean isAttacked = false;
+            for (final GameEntity defender : attackers.values()) {
+                if (defender.equals(mandatoryDef)) {
+                    isAttacked = true;
+                    break;
+                } else if (defender instanceof Card && ((Card)defender).getController().equals(mandatoryDef)) {
+                    isAttacked = true;
+                    break;
+                }
+            }
+            if (!isAttacked) {
+                defenderTooFew.add(mandatoryDef);
+            }
+        }
+
+        return new GlobalAttackRestrictionViolations(nTooMany, defenderTooMany, defenderTooFew);
     }
 
     final class GlobalAttackRestrictionViolations {
         private final boolean isViolated;
         private final int globalTooMany;
         private final MapToAmount<GameEntity> defenderTooMany;
+        private final MapToAmount<GameEntity> defenderTooFew;
 
-        public GlobalAttackRestrictionViolations(final int globalTooMany, final MapToAmount<GameEntity> defenderTooMany) {
-            this.isViolated = globalTooMany > 0 || !defenderTooMany.isEmpty();
+        public GlobalAttackRestrictionViolations(final int globalTooMany, final MapToAmount<GameEntity> defenderTooMany, final MapToAmount<GameEntity> defenderTooFew) {
+            this.isViolated = globalTooMany > 0 || !defenderTooMany.isEmpty() || !defenderTooFew.isEmpty();
             this.globalTooMany = globalTooMany;
             this.defenderTooMany = defenderTooMany;
+            this.defenderTooFew = defenderTooFew;
         }
         public boolean isViolated() {
             return isViolated;
         }
         public int getGlobalTooMany() {
             return globalTooMany;
+        }
+        public MapToAmount<GameEntity> getDefenderTooFew() {
+            return defenderTooFew;
         }
         public MapToAmount<GameEntity> getDefenderTooMany() {
             return defenderTooMany;
@@ -102,13 +139,14 @@ public class GlobalAttackRestrictions {
      * Get all global restrictions (applying to all creatures).
      * </p>
      * 
-     * @param player
+     * @param attackingPlayer
      *            the {@link Player} declaring attack.
      * @return a {@link GlobalAttackRestrictions} object.
      */
     public static GlobalAttackRestrictions getGlobalRestrictions(final Player attackingPlayer, final FCollectionView<GameEntity> possibleDefenders) {
         int max = -1;
         final MapToAmount<GameEntity> defenderMax = new LinkedHashMapToAmount<GameEntity>(possibleDefenders.size());
+        final PlayerCollection mustBeAttacked = new PlayerCollection();
         final Game game = attackingPlayer.getGame();
 
         if (game.getStaticEffects().getGlobalRuleChange(GlobalRuleChange.onlyOneAttackerATurn)) {
@@ -132,6 +170,14 @@ public class GlobalAttackRestrictions {
             }
         }
 
+        for (final Card card : game.getCardsIn(ZoneType.Battlefield)) {
+            if (card.hasKeyword("Each opponent must attack you or a planeswalker you control with at least one creature each combat if able.")) {
+                if (attackingPlayer.isOpponentOf(card.getController())) {
+                    mustBeAttacked.add(card.getController());
+                }
+            }
+        }
+
         for (final GameEntity defender : possibleDefenders) {
             final int defMax = getMaxAttackTo(defender);
             if (defMax != -1) {
@@ -142,7 +188,8 @@ public class GlobalAttackRestrictions {
             // maximum on each defender, global maximum is sum of these
             max = Ints.min(max, defenderMax.countAll());
         }
-        return new GlobalAttackRestrictions(max, defenderMax);
+
+        return new GlobalAttackRestrictions(max, defenderMax, mustBeAttacked);
     }
 
     /**
