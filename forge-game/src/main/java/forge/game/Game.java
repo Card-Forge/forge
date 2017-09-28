9a6146e1ba81c5ede9b00a42b3c8db30d03f3fb7
Agetian <stavdev@mail.ru>
2017-09-28 18:22:17 +0000

    - One more tweak.

--- forge-game/src/main/java/forge/game/Game.java
+++ forge-game/src/main/java/forge/game/Game.java
@@ -884,7 +884,7 @@ public class Game {
         }
         for (Card c : p.getOpponents().getCardsIn(ZoneType.Battlefield)) {
             // Bone Dancer is important when an opponent has it active on the battlefield
-            if ("OPPONENT".equals(c.getSVar("NeedsOrderedGraveyard"))) {
+            if ("opponent".equalsIgnoreCase(c.getSVar("NeedsOrderedGraveyard"))) {
                 return true;
             }
         }
