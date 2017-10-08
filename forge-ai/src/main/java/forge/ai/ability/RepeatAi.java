e084f663544b4713e6872287c107b9e981cf6194
Agetian <stavdev@mail.ru>
2017-10-08 13:35:54 +0000

    - RemAIDeck update: next iteration (letter L finished, beginning of letter H).

--- forge-ai/src/main/java/forge/ai/ability/RepeatAi.java
+++ forge-ai/src/main/java/forge/ai/ability/RepeatAi.java
@@ -3,6 +3,7 @@ package forge.ai.ability;
 
 import forge.ai.*;
 import forge.game.card.Card;
+import forge.game.phase.PhaseType;
 import forge.game.player.Player;
 import forge.game.player.PlayerActionConfirmMode;
 import forge.game.spellability.SpellAbility;
@@ -24,7 +25,10 @@ public class RepeatAi extends SpellAbilityAi {
             sa.getTargets().add(opp);
         }
         String logic = sa.getParam("AILogic");
-        if ("MaxX".equals(logic)) {
+        if ("MaxX".equals(logic) || "MaxXAtOppEOT".equals(logic)) {
+            if ("MaxXAtOppEOT".equals(logic) && !(ai.getGame().getPhaseHandler().is(PhaseType.END_OF_TURN) && ai.getGame().getPhaseHandler().getNextTurn() == ai)) {
+                return false;
+            }
             // Set PayX here to maximum value.
             final int max = ComputerUtilMana.determineLeftoverMana(sa, ai);
             source.setSVar("PayX", Integer.toString(max));
