cd55c4077e68d07003870783992f2437474328f9
Hanmac <hanmac@cardforge.org>
2017-11-09 05:23:28 +0000

    Keyword: make Suspend extra class to make the special case if it doesn't have Cost or amount

---
+++ forge-game/src/main/java/forge/game/keyword/Suspend.java
@@ -0,0 +1,24 @@
+package forge.game.keyword;
+
+public class Suspend extends KeywordWithCostAndAmount {
+
+    boolean withoutCostAndAmount = false;
+    
+    @Override
+    protected void parse(String details) {
+        if ("".equals(details)) {
+            withoutCostAndAmount = true;
+        } else {
+            super.parse(details);
+        }
+    }
+
+    @Override
+    protected String formatReminderText(String reminderText) {
+        if (withoutCostAndAmount) {
+            return "At the beginning of its owner's upkeep, remove a time counter from that card. When the last is removed, the player plays it without paying its mana cost. If it's a creature, it has haste.";
+        } else {
+            return super.formatReminderText(reminderText);
+        }
+    }
+}
