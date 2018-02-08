36917c0d187fae290f874212c00de91ae51049f2
Seravy <Seravysensei@gmail.com>
2018-02-08 10:25:17 +0100

    Fixes compile/run bugs in IntelliJ

--- forge-gui-desktop/src/main/java/forge/view/Main.java
+++ forge-gui-desktop/src/main/java/forge/view/Main.java
@@ -54,7 +54,7 @@ public final class Main {
 
         // command line startup here
         String mode = args[0].toLowerCase();
-        
+
         switch(mode) {
             case "sim":
                 SimulateMatch.simulate(args);
