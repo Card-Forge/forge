3415d76b40b04779f0055b43630abc601193d44a
Jamin W. Collins <jamin.collins@gmail.com>
2018-02-28 18:40:57 -0700

    allow ALL cards to be (de)serialized
    This is needed by network play variants like:
    - Vanguard
    - Planechase
    
    Signed-off-by: Jamin W. Collins <jamin.collins@gmail.com>

--- forge-core/src/main/java/forge/StaticData.java
+++ forge-core/src/main/java/forge/StaticData.java
@@ -29,6 +29,7 @@ public class StaticData {
     private final String blockDataFolder;
     private final CardDb commonCards;
     private final CardDb variantCards;
+    private final CardDb allCards;
     private final TokenDb allTokens;
     private final CardEdition.Collection editions;
 
@@ -54,6 +55,7 @@ public class StaticData {
         lastInstance = this;
 
         {
+            final Map<String, CardRules> allCardsMap = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
             final Map<String, CardRules> regularCards = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
             final Map<String, CardRules> variantsCards = new TreeMap<>(String.CASE_INSENSITIVE_ORDER);
 
@@ -61,6 +63,7 @@ public class StaticData {
                 if (null == card) continue;
 
                 final String cardName = card.getName();
+                allCardsMap.put(cardName, card);
                 if (card.isVariant()) {
                     variantsCards.put(cardName, card);
                 } else {
@@ -68,10 +71,12 @@ public class StaticData {
                 }
             }
 
+            allCards = new CardDb(allCardsMap, editions);
             commonCards = new CardDb(regularCards, editions);
             variantCards = new CardDb(variantsCards, editions);
 
-            //muse initialize after establish field values for the sake of card image logic
+            //must initialize after establish field values for the sake of card image logic
+            allCards.initialize(false, false);
             commonCards.initialize(false, false);
             variantCards.initialize(false, false);
         }
@@ -183,6 +188,8 @@ public class StaticData {
     public CardDb getVariantCards() {
         return variantCards;
     }
+
+    public CardDb getAllCards() { return allCards; }
 
     public TokenDb getAllTokens() { return allTokens; }
 
