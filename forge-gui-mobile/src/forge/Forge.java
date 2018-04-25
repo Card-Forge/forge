83665d0992e9c5af304d49e060e883b27454045b
Agetian <stavdev@mail.ru>
2018-04-25 07:17:39 +0300

    - Preparing Forge for Android publish 1.6.9.004 [AI hotfix].

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -34,7 +34,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class Forge implements ApplicationListener {
-    public static final String CURRENT_VERSION = "1.6.9.003";
+    public static final String CURRENT_VERSION = "1.6.9.004";
 
     private static final ApplicationListener app = new Forge();
     private static Clipboard clipboard;
