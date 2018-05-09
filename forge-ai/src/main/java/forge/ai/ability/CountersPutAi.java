d13201fd47d8b7170a52565718047df91d36b906
Agetian <stavdev@mail.ru>
2018-05-09 15:12:30 +0300

    - Tetzimoc CountersPutAi: don't try to target creatures which can't legally be targeted (e.g. Hexproof).

--- forge-ai/src/main/java/forge/ai/ability/CountersPutAi.java
+++ forge-ai/src/main/java/forge/ai/ability/CountersPutAi.java
@@ -273,7 +273,8 @@ public class CountersPutAi extends SpellAbilityAi {
             }
 
             CardCollection oppCreats = CardLists.filter(ai.getOpponents().getCreaturesInPlay(),
-                    Predicates.not(CardPredicates.hasCounter(CounterType.getType(type))));
+                    Predicates.and(Predicates.not(CardPredicates.hasCounter(CounterType.getType(type))),
+                            CardPredicates.isTargetableBy(sa)));
 
             if (!oppCreats.isEmpty()) {
                 Card bestCreat = ComputerUtilCard.getBestCreatureAI(oppCreats);
