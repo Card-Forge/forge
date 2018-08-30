73b94a63a59ea49951cfa4b579ab25bdda8e58c6
Hanmac <hanmac@gmx.de>
2018-08-30 20:25:30 +0200

    Surge: don't use OptionalCost there

--- forge-game/src/main/java/forge/game/spellability/SpellAbility.java
+++ forge-game/src/main/java/forge/game/spellability/SpellAbility.java
@@ -97,6 +97,7 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
     private boolean cycling = false;
     private boolean dash = false;
     private boolean evoke = false;
+    private boolean surge = false;
     private boolean offering = false;
     private boolean emerge = false;
     private boolean morphup = false;
@@ -508,10 +509,6 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
     public boolean isKicked() {
         return isOptionalCostPaid(OptionalCost.Kicker1) || isOptionalCostPaid(OptionalCost.Kicker2) ||
             getMultiKickerManaCost() != null;
-    }
-
-    public boolean isSurged() {
-        return isOptionalCostPaid(OptionalCost.Surge);
     }
 
     public boolean isEntwine() {
@@ -1070,6 +1067,14 @@ public abstract class SpellAbility extends CardTraitBase implements ISpellAbilit
 
     public final void setEvoke(final boolean isEvoke) {
         evoke = isEvoke;
+    }
+
+    public final boolean isSurged() {
+        return surge;
+    }
+
+    public final void setSurged(final boolean isSurge) {
+        surge = isSurge;
     }
 
     public CardCollection getTappedForConvoke() {
