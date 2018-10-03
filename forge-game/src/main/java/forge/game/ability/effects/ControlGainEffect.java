ece38aed9df5d84512e3fac0d5d87299ff530b3e
swordshine <hjy1986@gmail.com>
2018-10-03 23:43:32 +0800

    - Added Treasure Nabber

--- forge-game/src/main/java/forge/game/ability/effects/ControlGainEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/ControlGainEffect.java
@@ -143,24 +143,31 @@ public class ControlGainEffect extends SpellAbilityEffect {
             }
 
             if (lose != null) {
+                final GameCommand loseControl = getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws);
                 if (lose.contains("LeavesPlay")) {
-                    sa.getHostCard().addLeavesPlayCommand(getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws));
+                    sa.getHostCard().addLeavesPlayCommand(loseControl);
                 }
                 if (lose.contains("Untap")) {
-                    sa.getHostCard().addUntapCommand(getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws));
+                    sa.getHostCard().addUntapCommand(loseControl);
                 }
                 if (lose.contains("LoseControl")) {
-                    sa.getHostCard().addChangeControllerCommand(getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws));
+                    sa.getHostCard().addChangeControllerCommand(loseControl);
                 }
                 if (lose.contains("EOT")) {
-                    game.getEndOfTurn().addUntil(getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws));
+                    game.getEndOfTurn().addUntil(loseControl);
                     tgtC.setSVar("SacMe", "6");
                 }
                 if (lose.contains("StaticCommandCheck")) {
                     String leftVar = sa.getSVar(sa.getParam("StaticCommandCheckSVar"));
                     String rightVar = sa.getParam("StaticCommandSVarCompare");
-                    sa.getHostCard().addStaticCommandList(new Object[]{leftVar, rightVar, tgtC,
-                            getLoseControlCommand(tgtC, tStamp, bTapOnLose, source, kws)});
+                    sa.getHostCard().addStaticCommandList(new Object[]{leftVar, rightVar, tgtC, loseControl});
+                }
+                if (lose.contains("UntilTheEndOfYourNextTurn")) {
+                    if (game.getPhaseHandler().isPlayerTurn(sa.getActivatingPlayer())) {
+                        game.getEndOfTurn().registerUntilEnd(sa.getActivatingPlayer(), loseControl);
+                    } else {
+                        game.getEndOfTurn().addUntilEnd(sa.getActivatingPlayer(), loseControl);
+                    }
                 }
             }
 
