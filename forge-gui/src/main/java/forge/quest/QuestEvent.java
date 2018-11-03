f4ba5258ead0ef30b979a008f2d4b20399d42a91
imakunee <jeremypelkala@gmail.com>
2018-11-03 00:36:26 +0000

    Revert "-CardPool"
    This reverts commit 37d9dc753f915853ecf1e17e1fea1b5feb7480bd

--- forge-gui/src/main/java/forge/quest/QuestEvent.java
+++ forge-gui/src/main/java/forge/quest/QuestEvent.java
@@ -48,7 +48,6 @@ public abstract class QuestEvent implements IQuestEvent {
     private String profile = "Default";
     // Opponent name if different from the challenge name
     private String opponentName = null;
-    private boolean isRandomMatch = false;
 
 
     public static final Function<QuestEvent, String> FN_GET_NAME = new Function<QuestEvent, String>() {
@@ -175,7 +174,4 @@ public abstract class QuestEvent implements IQuestEvent {
         this.showDifficulty = showDifficulty;
     }
 
-    public boolean getIsRandomMatch(){return isRandomMatch;}
-
-    public void setIsRandomMatch(boolean b){isRandomMatch = b;}
 }
