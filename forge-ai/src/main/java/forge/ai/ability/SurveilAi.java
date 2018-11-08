44de14d070c7965c7a7da0b18072383c485be914
Agetian <stavdev@mail.ru>
2018-11-08 08:47:19 +0300

    - Minor improvements in Surveil logic.

--- forge-ai/src/main/java/forge/ai/ability/SurveilAi.java
+++ forge-ai/src/main/java/forge/ai/ability/SurveilAi.java
@@ -1,6 +1,9 @@
 package forge.ai.ability;
 
-import forge.ai.SpellAbilityAi;
+import forge.ai.*;
+import forge.game.card.Card;
+import forge.game.cost.Cost;
+import forge.game.cost.CostPayLife;
 import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
@@ -68,8 +71,14 @@ public class SurveilAi extends SpellAbilityAi {
      */
     @Override
     protected boolean checkAiLogic(final Player ai, final SpellAbility sa, final String aiLogic) {
+        final Card source = sa.getHostCard();
+
         if ("Never".equals(aiLogic)) {
             return false;
+        } else if ("Once".equals(aiLogic)) {
+            if (AiCardMemory.isRememberedCard(ai, source, AiCardMemory.MemorySet.ACTIVATED_THIS_TURN)) {
+                return false;
+            }
         }
 
         // TODO: add card-specific Surveil AI logic here when/if necessary
@@ -82,6 +91,15 @@ public class SurveilAi extends SpellAbilityAi {
         // Makes no sense to do Surveil when there's nothing in the library
         if (ai.getCardsIn(ZoneType.Library).isEmpty()) {
             return false;
+        }
+
+        // Only Surveil for life when at decent amount of life remaining
+        final Cost cost = sa.getPayCosts();
+        if (cost != null && cost.hasSpecificCostType(CostPayLife.class)) {
+            final int maxLife = ((PlayerControllerAi)ai.getController()).getAi().getIntProperty(AiProps.SURVEIL_LIFEPERC_AFTER_PAYING_LIFE);
+            if (!ComputerUtilCost.checkLifeCost(ai, cost, sa.getHostCard(), ai.getStartingLife() * maxLife / 100, sa)) {
+                return false;
+            }
         }
 
         double chance = .4; // 40 percent chance for instant speed
@@ -99,6 +117,7 @@ public class SurveilAi extends SpellAbilityAi {
 
     @Override
     public boolean confirmAction(Player player, SpellAbility sa, PlayerActionConfirmMode mode, String message) {
+        AiCardMemory.rememberCard(player, sa.getHostCard(), AiCardMemory.MemorySet.ACTIVATED_THIS_TURN);
         return true;
     }
 }
