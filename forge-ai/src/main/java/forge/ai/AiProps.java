c36b1a4a6316a2fa424ffda7aced2af600d96186
Agetian <stavdev@mail.ru>
2018-11-22 19:54:04 +0300

    - Changed the defaults in AiProps.

--- forge-ai/src/main/java/forge/ai/AiProps.java
+++ forge-ai/src/main/java/forge/ai/AiProps.java
@@ -121,9 +121,9 @@ public enum AiProps { /** */
     FLASH_CHANCE_TO_RESPOND_TO_STACK_WITH_ETB("0"), /** */
     FLASH_CHANCE_TO_CAST_AS_VALUABLE_BLOCKER("100"),
     FLASH_USE_BUFF_AURAS_AS_COMBAT_TRICKS("true"),
-    FLASH_BUFF_AURA_CHANCE_TO_CAST_EARLY("0"),
-    FLASH_BUFF_AURA_CHANCE_CAST_AT_EOT("10"),
-    FLASH_BUFF_AURA_CHANCE_TO_RESPOND_TO_STACK("0"); /** */
+    FLASH_BUFF_AURA_CHANCE_TO_CAST_EARLY("1"),
+    FLASH_BUFF_AURA_CHANCE_CAST_AT_EOT("5"),
+    FLASH_BUFF_AURA_CHANCE_TO_RESPOND_TO_STACK("100"); /** */
     // Experimental features, must be promoted or removed after extensive testing and, ideally, defaulting
     // <-- There are no experimental options here -->
 
