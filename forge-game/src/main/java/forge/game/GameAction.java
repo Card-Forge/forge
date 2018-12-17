f703a903f5ce67ce491352be2f304cc26cf7bfa4
Agetian <stavdev@mail.ru>
2018-12-17 17:21:34 +0300

    - Transformed objects must be reset to Original before they go through CardFactory.copyCard, or they end up in a mixed state, at least visually (since the transformed state is set on the copied object).

--- forge-game/src/main/java/forge/game/GameAction.java
+++ forge-game/src/main/java/forge/game/GameAction.java
@@ -213,14 +213,18 @@ public class GameAction {
                     c.updateStateForView();
                 }
 
+                if (fromBattlefield && c.getCurrentStateName() != CardStateName.Original) {
+                    // when a card leaves the battlefield, ensure it's in its original state
+                    // (we need to do this on the object before copying it, or it won't work correctly e.g.
+                    // on Transformed objects)
+                    c.setState(CardStateName.Original, false);
+                }
+
                 copied = CardFactory.copyCard(c, false);
 
                 copied.setUnearthed(c.isUnearthed());
                 copied.setTapped(false);
-                if (fromBattlefield) {
-                    // when a card leaves the battlefield, ensure it's in its original state
-                    copied.setState(CardStateName.Original, false);
-                }
+
                 for (final Trigger trigger : copied.getTriggers()) {
                     trigger.setHostCard(copied);
                 }
