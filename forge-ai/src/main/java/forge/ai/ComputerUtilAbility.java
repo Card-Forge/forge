b29f390b625d8a8c18fc9b63bfd510de5f13fd15
Agetian <stavdev@mail.ru>
2019-01-31 16:39:49 +0300

    - Fix the AI activating player setup for additional costs. - Fix Thrilling Encore.

--- forge-ai/src/main/java/forge/ai/ComputerUtilAbility.java
+++ forge-ai/src/main/java/forge/ai/ComputerUtilAbility.java
@@ -99,12 +99,11 @@ public class ComputerUtilAbility {
         List<SpellAbility> originListWithAddCosts = Lists.newArrayList();
         for (SpellAbility sa : originList) {
             // If this spell has alternative additional costs, add them instead of the unmodified SA itself
+            sa.setActivatingPlayer(player);
             originListWithAddCosts.addAll(GameActionUtil.getAdditionalCostSpell(sa));
         }
 
         for (SpellAbility sa : originListWithAddCosts) {
-            sa.setActivatingPlayer(player);
-
             // determine which alternative costs are cheaper than the original and prioritize them
             List<SpellAbility> saAltCosts = GameActionUtil.getAlternativeCosts(sa, player);
             List<SpellAbility> priorityAltSa = Lists.newArrayList();
