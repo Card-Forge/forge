108abbe16c7ae72425a0954fdf05b28ce60ba8aa
Michael Kamensky <stavdev@mail.ru>
2019-05-28 12:08:19 +0000

    Vancouver mulligan only allows to scry if the mulligan actually happened

--- forge-game/src/main/java/forge/game/mulligan/VancouverMulligan.java
+++ forge-game/src/main/java/forge/game/mulligan/VancouverMulligan.java
@@ -2,6 +2,7 @@ package forge.game.mulligan;
 
 import com.google.common.collect.ImmutableList;
 import forge.game.player.Player;
+import forge.game.zone.ZoneType;
 
 public class VancouverMulligan extends ParisMulligan {
     public VancouverMulligan(Player p, boolean firstMullFree) {
@@ -10,6 +11,8 @@ public class VancouverMulligan extends ParisMulligan {
 
     public void afterMulligan() {
         super.afterMulligan();
-        player.getGame().getAction().scry(ImmutableList.of(player), 1, null);
+        if (player.getStartingHandSize() > player.getZone(ZoneType.Hand).size()) {
+            player.getGame().getAction().scry(ImmutableList.of(player), 1, null);
+        }
     }
 }
