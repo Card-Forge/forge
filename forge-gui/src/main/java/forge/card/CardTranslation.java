92bbda14acb95a6cd5b605a25fe0d993af33e7dd
Peter <pduran5@xtec.cat>
2019-07-28 07:52:45 +0200

    Revert "Changed normalized accents code"
    This reverts commit 86ce46eb

--- forge-gui/src/main/java/forge/card/CardTranslation.java
+++ forge-gui/src/main/java/forge/card/CardTranslation.java
@@ -7,7 +7,6 @@ import forge.util.LineReader;
 
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.Normalizer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,9 +17,17 @@ public class CardTranslation {
     public static Map <String, String> translatedoracles;
     public static Map <String, String> translatedflavors;
 
-    private static String removeDiacritics(String text) {
-        text = Normalizer.normalize(text, Normalizer.Form.NFD);
-        text = text.replaceAll("[\\p{InCombiningDiacriticalMarks}]", "");
+    private static String sanitize(String text) {
+        text = text.replace("á", "a");
+        text = text.replace("é", "e");
+        text = text.replace("í", "i");
+        text = text.replace("ó", "o");
+        text = text.replace("ú", "u");
+        text = text.replace("Á", "A");
+        text = text.replace("É", "E");
+        text = text.replace("Í", "I");
+        text = text.replace("Ó", "O");
+        text = text.replace("Ú", "U");
         text = text.replace("ñ", "ny");
         return text;
     }
@@ -28,25 +35,25 @@ public class CardTranslation {
     private static void readTranslationFile(String language) {
         String filename = "cardnames-" + language + ".txt";
 
-        try (LineReader translationFile = new LineReader(new FileInputStream(ForgeConstants.LANG_DIR + filename))) {
+        try (LineReader translationFile = new LineReader(new FileInputStream(ForgeConstants.LANG_DIR + filename));) {
 
             for (String line : translationFile.readLines()) {
                 String[] matches = line.split("#");
 
                 if(matches.length >= 2) {
-                    translatednames.put(matches[0], removeDiacritics(matches[1]));
+                    translatednames.put(matches[0], sanitize(matches[1]));
                 }
 
                 if(matches.length >= 3) {
-                    translatedtypes.put(matches[0], removeDiacritics(matches[2]));
+                    translatedtypes.put(matches[0], sanitize(matches[2]));
                 }
 
                 if(matches.length >= 4) {
-                    translatedoracles.put(matches[0], removeDiacritics(matches[3]).replace("\\n", "\n\n"));
+                    translatedoracles.put(matches[0], sanitize(matches[3]).replace("\\n", "\n\n"));
                 }
 
                 if(matches.length >= 5) {
-                    translatedflavors.put(matches[0], removeDiacritics(matches[4]));
+                    translatedflavors.put(matches[0], sanitize(matches[4]));
                 }
             }
         } catch (IOException e) {
@@ -60,10 +67,10 @@ public class CardTranslation {
 
     public static void preloadTranslation(String language) {
         if (needsTranslation()) {
-            translatednames = new HashMap<>();
-            translatedtypes = new HashMap<>();
-            translatedoracles = new HashMap<>();
-            translatedflavors = new HashMap<>();
+            translatednames = new HashMap<String, String>();
+            translatedtypes = new HashMap<String, String>();
+            translatedoracles = new HashMap<String, String>();
+            translatedflavors = new HashMap<String, String>();
             readTranslationFile(ForgePreferences.FPref.UI_LANGUAGE.toString());
         }
     }
