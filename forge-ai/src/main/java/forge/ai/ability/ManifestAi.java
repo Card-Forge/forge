15e5d68520eb37437ea53394bfeda7644e50e5eb
Hans Mackowiak <hanmac@gmx.de>
2019-09-25 08:03:32 +0000

    Update ManifestAi: unused import

--- forge-ai/src/main/java/forge/ai/ability/ManifestAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ManifestAi.java
@@ -2,7 +2,6 @@ package forge.ai.ability;
 
 import com.google.common.base.Predicate;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Maps;
 import forge.ai.ComputerUtil;
 import forge.ai.ComputerUtilCard;
 import forge.ai.ComputerUtilMana;
