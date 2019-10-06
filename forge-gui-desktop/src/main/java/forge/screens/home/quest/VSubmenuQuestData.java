bc859b3e52b0e768e26571e379fa7acfd4319aa4
Ryan1729 <ryan1729@gmail.com>
2019-10-06 00:56:12 -0600

    inline `wrap`

--- forge-gui-desktop/src/main/java/forge/screens/home/quest/VSubmenuQuestData.java
+++ forge-gui-desktop/src/main/java/forge/screens/home/quest/VSubmenuQuestData.java
@@ -19,7 +19,7 @@ import forge.screens.home.VHomeUI;
 import forge.toolbox.*;
 import forge.util.Localizer;
 import net.miginfocom.swing.MigLayout;
-import org.apache.commons.lang3.text.WordUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.swing.*;
 import javax.swing.plaf.basic.BasicComboBoxRenderer;
@@ -28,6 +28,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Assembles Swing components of quest data submenu singleton.
@@ -361,7 +363,80 @@ public enum VSubmenuQuestData implements IVSubmenu<CSubmenuQuestData> {
     }
 
     private static String wordWrapAsHTML(String str) {
-        return "<html>" + WordUtils.wrap(str, 40, "<br>", false, " ") + "</html>";
+        String result = null;
+        int wrapLength = 40;
+        String newLineStr = "<br>";
+        String wrapOn = " ";
+        if (str != null) {
+            if (newLineStr == null) {
+                newLineStr = System.lineSeparator();
+            }
+            if (wrapLength < 1) {
+                wrapLength = 1;
+            }
+            if (StringUtils.isBlank(wrapOn)) {
+                wrapOn = " ";
+            }
+            final Pattern patternToWrapOn = Pattern.compile(wrapOn);
+            final int inputLineLength = str.length();
+            int offset = 0;
+            final StringBuilder wrappedLine = new StringBuilder(inputLineLength + 32);
+            while (offset < inputLineLength) {
+                int spaceToWrapAt = -1;
+                Matcher matcher = patternToWrapOn.matcher(str.substring(offset, Math.min(offset + wrapLength + 1, inputLineLength)));
+                if (matcher.find()) {
+                    if (matcher.start() == 0) {
+                        offset += matcher.end();
+                        continue;
+                    }
+                    spaceToWrapAt = matcher.start() + offset;
+                }
+
+                // only last line without leading spaces is left
+                if (inputLineLength - offset <= wrapLength) {
+                    break;
+                }
+
+                while (matcher.find()) {
+                    spaceToWrapAt = matcher.start() + offset;
+                }
+
+                if (spaceToWrapAt >= offset) {
+                    // normal case
+                    wrappedLine.append(str, offset, spaceToWrapAt);
+                    wrappedLine.append(newLineStr);
+                    offset = spaceToWrapAt + 1;
+
+                } else {
+                    // really long word or URL
+                    if (false) {
+                        // wrap really long word one line at a time
+                        wrappedLine.append(str, offset, wrapLength + offset);
+                        wrappedLine.append(newLineStr);
+                        offset += wrapLength;
+                    } else {
+                        // do not wrap really long word, just extend beyond limit
+                        matcher = patternToWrapOn.matcher(str.substring(offset + wrapLength));
+                        if (matcher.find()) {
+                            spaceToWrapAt = matcher.start() + offset + wrapLength;
+                        }
+
+                        if (spaceToWrapAt >= 0) {
+                            wrappedLine.append(str, offset, spaceToWrapAt);
+                            wrappedLine.append(newLineStr);
+                            offset = spaceToWrapAt + 1;
+                        } else {
+                            wrappedLine.append(str, offset, str.length());
+                            offset = inputLineLength;
+                        }
+                    }
+                }
+            }// Whatever is left in line is short enough to just pass through
+            wrappedLine.append(str, offset, str.length());
+            result = wrappedLine.toString();
+        }
+
+        return "<html>" + result + "</html>";
     }
 
     /* (non-Javadoc)
