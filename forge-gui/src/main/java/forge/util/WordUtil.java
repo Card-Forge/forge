4969547938df0fbc426c60e90438f53016ef61de
Ryan1729 <ryan1729@gmail.com>
2019-10-06 01:36:37 -0600

    inline `capitalize`

--- forge-gui/src/main/java/forge/util/WordUtil.java
+++ forge-gui/src/main/java/forge/util/WordUtil.java
@@ -1,10 +1,24 @@
 package forge.util;
 
-import org.apache.commons.lang3.text.WordUtils;
+import org.apache.commons.lang3.StringUtils;
 
 public class WordUtil {
     public static String capitalize(String str) {
-        return WordUtils.capitalize(str);
+        if (StringUtils.isEmpty(str)) {
+            return str;
+        }
+        final char[] buffer = str.toCharArray();
+        boolean capitalizeNext = true;
+        for (int i = 0; i < buffer.length; i++) {
+            final char ch = buffer[i];
+            if (Character.isWhitespace(ch)) {
+                capitalizeNext = true;
+            } else if (capitalizeNext) {
+                buffer[i] = Character.toTitleCase(ch);
+                capitalizeNext = false;
+            }
+        }
+        return new String(buffer);
     }
 
     private WordUtil() {}
