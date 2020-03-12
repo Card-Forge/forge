9f35da4698db5cffc3aa0cf385b62bae2f9d16c2
Hans Mackowiak <hanmac@gmx.de>
2020-03-12 13:34:27 +0000

    ManaPart: use Effect for enter the battlefield when mana was spent

--- forge-game/src/main/java/forge/game/spellability/AbilityManaPart.java
+++ forge-game/src/main/java/forge/game/spellability/AbilityManaPart.java
@@ -6,23 +6,24 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package forge.game.spellability;
 
-import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
+
 import forge.card.ColorSet;
 import forge.card.MagicColor;
 import forge.card.mana.ManaAtom;
+import forge.game.Game;
 import forge.game.ability.AbilityFactory;
 import forge.game.ability.AbilityKey;
 import forge.game.card.Card;
@@ -35,6 +36,8 @@ import forge.game.replacement.*;
 import forge.game.trigger.Trigger;
 import forge.game.trigger.TriggerHandler;
 import forge.game.trigger.TriggerType;
+import forge.game.zone.ZoneType;
+import forge.util.Lang;
 import forge.util.TextUtil;
 import org.apache.commons.lang3.StringUtils;
 
@@ -47,7 +50,7 @@ import java.util.regex.Pattern;
  * <p>
  * Abstract AbilityMana class.
  * </p>
- * 
+ *
  * @author Forge
  * @version $Id$
  */
@@ -79,7 +82,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * Constructor for AbilityMana.
      * </p>
-     * 
+     *
      * @param sourceCard
      *            a {@link forge.game.card.Card} object.
      */
@@ -112,7 +115,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * produceMana.
      * </p>
-     * 
+     *
      * @param produced
      *            a {@link java.lang.String} object.
      * @param player
@@ -170,7 +173,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * cannotCounterPaidWith.
      * </p>
      * @param saBeingPaid
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public boolean cannotCounterPaidWith(SpellAbility saBeingPaid) {
@@ -187,7 +190,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * addKeywords.
      * </p>
      * @param saBeingPaid
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public boolean addKeywords(SpellAbility saBeingPaid) {
@@ -206,7 +209,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * getKeywords.
      * </p>
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public String getKeywords() {
@@ -218,7 +221,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * addsCounters.
      * </p>
      * @param saBeingPaid
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public boolean addsCounters(SpellAbility saBeingPaid) {
@@ -228,10 +231,26 @@ public class AbilityManaPart implements java.io.Serializable {
     /**
      * createETBCounters
      */
-    public void createETBCounters(Card c) {
+    public void createETBCounters(Card c, Player controller) {
         String[] parse = this.addsCounters.split("_");
         // Convert random SVars if there are other cards with this effect
         if (c.isValid(parse[0], c.getController(), c, null)) {
+            final Game game = this.sourceCard.getGame();
+            final Card eff = new Card(game.nextCardId(), game);
+            eff.setTimestamp(game.getNextTimestamp());
+            eff.setName(sourceCard.getName() + "'s Effect");
+            eff.addType("Effect");
+            eff.setToken(true); // Set token to true, so when leaving play it gets nuked
+            eff.setOwner(controller);
+
+            eff.setImageKey(sourceCard.getImageKey());
+            eff.setColor(MagicColor.COLORLESS);
+            eff.setImmutable(true);
+            // try to get the SpellAbility from the mana ability
+            //eff.setEffectSource((SpellAbility)null);
+
+            eff.addRemembered(c);
+
             String abStr = "DB$ PutCounter | Defined$ ReplacedCard | CounterType$ " + parse[1]
                     + " | ETB$ True | CounterNum$ " + parse[2];
 
@@ -241,15 +260,37 @@ public class AbilityManaPart implements java.io.Serializable {
             }
             CardFactoryUtil.setupETBReplacementAbility(sa);
 
-            String repeffstr = "Event$ Moved | ValidCard$ Card.Self | Destination$ Battlefield "
-                    + " | Secondary$ True | Description$ CARDNAME"
-                    + " enters the battlefield with " + CounterType.valueOf(parse[1]).getName() + " counters.";
+            String desc = "It enters the battlefield with ";
+            desc += Lang.nounWithNumeral(parse[2], CounterType.valueOf(parse[1]).getName() + " counter");
+            desc += " on it.";
 
-            ReplacementEffect re = ReplacementHandler.parseReplacement(repeffstr, c, false);
+            String repeffstr = "Event$ Moved | ValidCard$ Card.IsRemembered | Destination$ Battlefield | Description$ " + desc;
+
+            ReplacementEffect re = ReplacementHandler.parseReplacement(repeffstr, eff, true);
             re.setLayer(ReplacementLayer.Other);
             re.setOverridingAbility(sa);
 
-            c.addChangedCardTraits(null, null, null, ImmutableList.of(re), null, false, false, false, sa.getHostCard().getGame().getNextTimestamp());
+            eff.addReplacementEffect(re);
+
+            // Forgot Trigger
+            String trig = "Mode$ ChangesZone | ValidCard$ Card.IsRemembered | Origin$ Stack | Destination$ Any | TriggerZones$ Command | Static$ True";
+            String forgetEffect = "DB$ Pump | ForgetObjects$ TriggeredCard";
+            String exileEffect = "DB$ ChangeZone | Defined$ Self | Origin$ Command | Destination$ Exile"
+                    + " | ConditionDefined$ Remembered | ConditionPresent$ Card | ConditionCompare$ EQ0";
+
+            SpellAbility saForget = AbilityFactory.getAbility(forgetEffect, eff);
+            AbilitySub saExile = (AbilitySub) AbilityFactory.getAbility(exileEffect, eff);
+            saForget.setSubAbility(saExile);
+
+            final Trigger parsedTrigger = TriggerHandler.parseTrigger(trig, eff, true);
+            parsedTrigger.setOverridingAbility(saForget);
+            eff.addTrigger(parsedTrigger);
+            eff.updateStateForView();
+
+            // TODO: Add targeting to the effect so it knows who it's dealing with
+            game.getTriggerHandler().suppressMode(TriggerType.ChangesZone);
+            game.getAction().moveTo(ZoneType.Command, eff, null);
+            game.getTriggerHandler().clearSuppression(TriggerType.ChangesZone);
         }
     }
 
@@ -270,7 +311,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * getManaRestrictions.
      * </p>
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public String getManaRestrictions() {
@@ -281,7 +322,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * meetsManaRestrictions.
      * </p>
-     * 
+     *
      * @param sa
      *            a {@link forge.game.spellability.SpellAbility} object.
      * @return a boolean.
@@ -297,7 +338,7 @@ public class AbilityManaPart implements java.io.Serializable {
             if (restriction.equals("nonSpell")) {
                 return !sa.isSpell();
             }
-            
+
             if (restriction.equals("CumulativeUpkeep")) {
                 if (sa.isCumulativeupkeep()) {
                     return true;
@@ -350,7 +391,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * mana.
      * </p>
-     * 
+     *
      * @return a {@link java.lang.String} object.
      */
     public final String mana() {
@@ -439,7 +480,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * canProduce.
      * </p>
-     * 
+     *
      * @param s
      *            a {@link java.lang.String} object.
      * @return a boolean.
@@ -469,7 +510,7 @@ public class AbilityManaPart implements java.io.Serializable {
      * <p>
      * isBasic.
      * </p>
-     * 
+     *
      * @return a boolean.
      */
     public final boolean isBasic() {
@@ -542,7 +583,7 @@ public class AbilityManaPart implements java.io.Serializable {
     public Card getSourceCard() {
         return sourceCard;
     }
-    
+
     public void setSourceCard(final Card host) {
         sourceCard = host;
     }
