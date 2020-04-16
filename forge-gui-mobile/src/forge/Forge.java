29c4d6d4d138da3726f0ea02e1a8e0a396a7868a
Anthony Calosa <anthonycalosa@gmail.com>
2020-04-17 06:54:14 +0800

    limit rotate transform to overlay and child objects

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -436,13 +436,13 @@ public class Forge implements ApplicationListener {
 
             graphics.begin(screenWidth, screenHeight);
             screen.screenPos.setSize(screenWidth, screenHeight);
-            if (screen.getRotate180()) {
+            /*if (screen.getRotate180()) {
                 graphics.startRotateTransform(screenWidth / 2, screenHeight / 2, 180);
-            }
+            }*/
             screen.draw(graphics);
-            if (screen.getRotate180()) {
+            /*if (screen.getRotate180()) {
                 graphics.endTransform();
-            }
+            }*/
             for (FOverlay overlay : FOverlay.getOverlays()) {
                 if (overlay.isVisibleOnScreen(currentScreen)) {
                     overlay.screenPos.setSize(screenWidth, screenHeight);
