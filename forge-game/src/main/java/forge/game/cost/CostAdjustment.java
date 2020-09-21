07fe0d150189486553ec80a2ca1eb8f9385fca73
Hans Mackowiak <hanmac@gmx.de>
2020-09-21 08:32:54 +0200

    ZNR: add vine_gecko with OnlyFirstSpell

--- forge-game/src/main/java/forge/game/cost/CostAdjustment.java
+++ forge-game/src/main/java/forge/game/cost/CostAdjustment.java
@@ -14,6 +14,7 @@ import forge.game.mana.ManaCostBeingPaid;
 import forge.game.player.Player;
 import forge.game.spellability.AbilityActivated;
 import forge.game.spellability.SpellAbility;
+import forge.game.spellability.SpellAbilityPredicates;
 import forge.game.spellability.TargetChoices;
 import forge.game.staticability.StaticAbility;
 import forge.game.zone.Zone;
@@ -423,6 +424,7 @@ public class CostAdjustment {
         final Player controller = hostCard.getController();
         final Player activator = sa.getActivatingPlayer();
         final Card card = sa.getHostCard();
+        final Game game = hostCard.getGame();
 
         if (st.hasParam("ValidCard")
                 && !card.isValid(st.getParam("ValidCard").split(","), controller, hostCard, sa)) {
@@ -452,7 +454,19 @@ public class CostAdjustment {
                     if (activator == null ) {
                         return false;
                     }
-                    List<Card> list = CardUtil.getThisTurnCast(st.getParam("ValidCard"), hostCard);
+                    List<Card> list;
+                    if (st.hasParam("ValidCard")) {
+                        list = CardUtil.getThisTurnCast(st.getParam("ValidCard"), hostCard);
+                    } else {
+                        list = game.getStack().getSpellsCastThisTurn();
+                    }
+
+                    if (st.hasParam("ValidSpell")) {
+                        list = CardLists.filterAsList(list, CardPredicates.castSA(
+                            SpellAbilityPredicates.isValid(st.getParam("ValidSpell").split(","), controller, hostCard, sa))
+                        );
+                    }
+
                     if (CardLists.filterControlledBy(list, activator).size() > 0) {
                         return false;
                     }
