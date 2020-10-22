2ef07f7a9c75a65bd39442acd2199d1296b1e29b
Anthony Calosa <anthonycalosa@gmail.com>
2020-10-22 18:58:28 +0800

    [Mobile] Update Stack Description -adds Optional Costs payment to Stack Description text.

--- forge-game/src/main/java/forge/game/spellability/StackItemView.java
+++ forge-game/src/main/java/forge/game/spellability/StackItemView.java
@@ -38,6 +38,7 @@ public class StackItemView extends TrackableObject implements IHasCardView {
         updateAbility(si);
         updateOptionalTrigger(si);
         updateSubInstance(si);
+        updateOptionalCost(si);
     }
 
     public String getKey() {
@@ -45,6 +46,55 @@ public class StackItemView extends TrackableObject implements IHasCardView {
     }
     void updateKey(SpellAbilityStackInstance si) {
     	set(TrackableProperty.Key, si.getSpellAbility(false).yieldKey());
+    }
+
+    public String getOptionalCostString() {
+        return get(TrackableProperty.OptionalCosts);
+    }
+    void updateOptionalCost(SpellAbilityStackInstance si) {
+        String OptionalCostString = "";
+        boolean kicked = false;
+        boolean entwined = false;
+        boolean buyback = false;
+        boolean retraced = false;
+        boolean jumpstart = false;
+        boolean additional = false;
+        boolean alternate = false;
+        boolean generic = false;
+
+        for (OptionalCost cost : si.getSpellAbility(false).getOptionalCosts()) {
+            if (cost == OptionalCost.Kicker1 || cost == OptionalCost.Kicker2)
+                kicked = true;
+            if (cost == OptionalCost.Entwine)
+                entwined = true;
+            if (cost == OptionalCost.Buyback)
+                buyback = true;
+            if (cost == OptionalCost.Retrace)
+                retraced = true;
+            if (cost == OptionalCost.Jumpstart)
+                jumpstart = true;
+            if (cost == OptionalCost.Flash)
+                additional = true;
+            if (cost == OptionalCost.Generic)
+                generic = true;
+            if (cost == OptionalCost.AltCost)
+                alternate = true;
+        }
+        if (!alternate) {
+            if (kicked && !generic)
+                OptionalCostString += "Kicked";
+            if (entwined)
+                OptionalCostString += OptionalCostString.equals("") ? "Entwined" : ", Entwined";
+            if (buyback)
+                OptionalCostString += OptionalCostString.equals("") ? "Buyback" : ", Buyback";
+            if (retraced)
+                OptionalCostString += OptionalCostString.equals("") ? "Retraced" : ", Retraced";
+            if (jumpstart)
+                OptionalCostString += OptionalCostString.equals("") ? "Jumpstart" : ", Jumpstart";
+            if (additional || generic)
+                OptionalCostString += OptionalCostString.equals("") ? "Additional" : ", Additional";
+        }
+        set(TrackableProperty.OptionalCosts, OptionalCostString);
     }
 
     public int getSourceTrigger() {
