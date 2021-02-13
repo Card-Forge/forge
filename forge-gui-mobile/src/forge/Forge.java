c813ca396eb5e622b4854ec873a1ea1ddf34515b
Michael Kamensky <stavdev@mail.ru>
2021-02-13 18:17:39 +0300

    - Prepare Forge for Android release 1.6.38.001 [incremental]

--- forge-gui-mobile/src/forge/Forge.java
+++ forge-gui-mobile/src/forge/Forge.java
@@ -43,7 +43,7 @@ import java.util.Deque;
 import java.util.List;
 
 public class Forge implements ApplicationListener {
-    public static final String CURRENT_VERSION = "1.6.37.001";
+    public static final String CURRENT_VERSION = "1.6.38.001";
 
     private static final ApplicationListener app = new Forge();
     private static Clipboard clipboard;
