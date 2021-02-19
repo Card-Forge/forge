c519600629d3a85cf6915fcda7621d917c66ab3d
Michael Kamensky <stavdev@mail.ru>
2021-02-19 14:35:37 +0300

    - A somewhat more generally applicable logic.

--- forge-ai/src/main/java/forge/ai/ability/AnimateAllAi.java
+++ forge-ai/src/main/java/forge/ai/ability/AnimateAllAi.java
@@ -1,6 +1,9 @@
 package forge.ai.ability;
 
+import forge.ai.ComputerUtilCard;
+import forge.ai.ComputerUtilCombat;
 import forge.ai.SpellAbilityAi;
+import forge.game.card.Card;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
 
@@ -10,8 +13,13 @@ public class AnimateAllAi extends SpellAbilityAi {
     protected boolean canPlayAI(Player aiPlayer, SpellAbility sa) {
         String logic = sa.getParamOrDefault("AILogic", "");
 
-        if ("NeedsCreature".equals(logic)) {
-            return !aiPlayer.getCreaturesInPlay().isEmpty();
+        if ("CreatureAdvantage".equals(logic) && !aiPlayer.getCreaturesInPlay().isEmpty()) {
+            // TODO: improve this or implement a better logic for abilities like Oko, the Trickster ultimate
+            for (Card c : aiPlayer.getCreaturesInPlay()) {
+                if (ComputerUtilCard.doesCreatureAttackAI(aiPlayer, c)) {
+                    return true;
+                }
+            }
         }
 
         return "Always".equals(logic);
