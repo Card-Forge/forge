cffef2e479d923db4ed0d0aed987caccbe7ead17
Michael Kamensky <stavdev@mail.ru>
2021-02-19 14:23:28 +0300

    - A simple logic to enable Ultimate for Oko, the Trickster for the AI (to be improved).

--- forge-ai/src/main/java/forge/ai/ability/AnimateAllAi.java
+++ forge-ai/src/main/java/forge/ai/ability/AnimateAllAi.java
@@ -8,12 +8,18 @@ public class AnimateAllAi extends SpellAbilityAi {
 
     @Override
     protected boolean canPlayAI(Player aiPlayer, SpellAbility sa) {
-        return "Always".equals(sa.getParam("AILogic"));
+        String logic = sa.getParamOrDefault("AILogic", "");
+
+        if ("NeedsCreature".equals(logic)) {
+            return !aiPlayer.getCreaturesInPlay().isEmpty();
+        }
+
+        return "Always".equals(logic);
     } // end animateAllCanPlayAI()
 
     @Override
     protected boolean doTriggerAINoCost(Player aiPlayer, SpellAbility sa, boolean mandatory) {
-        return mandatory;
+        return mandatory || canPlayAI(aiPlayer, sa);
     }
 
 } // end class AbilityFactoryAnimate
