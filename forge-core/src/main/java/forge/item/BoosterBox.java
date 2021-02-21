d06ba8231cc20f11b9c3def05190b76c1d2eefd8
Michael Kamensky <stavdev@mail.ru>
2021-02-21 13:28:19 +0300

    - Fix compile (unused imports)

--- forge-core/src/main/java/forge/item/BoosterBox.java
+++ forge-core/src/main/java/forge/item/BoosterBox.java
@@ -18,19 +18,13 @@
 
 package forge.item;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
-
 import com.google.common.base.Function;
-
 import forge.ImageKeys;
 import forge.StaticData;
 import forge.card.CardEdition;
-import forge.util.TextUtil;
-import forge.util.storage.StorageReaderFile;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.ArrayList;
 
 public class BoosterBox extends BoxedProduct {
     public static final Function<CardEdition, BoosterBox> FN_FROM_SET = new Function<CardEdition, BoosterBox>() {
