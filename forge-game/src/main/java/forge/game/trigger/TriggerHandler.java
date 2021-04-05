5445ec1b8d87d7a3cfe85883d64cd61232855af2
Northmoc <timothy.s.mocny@gmail.com>
2021-04-05 08:44:40 -0400

    RememberTriggerCards param support for Conspiracy Theorist

--- forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
+++ forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
@@ -573,6 +573,13 @@ public class TriggerHandler {
             host.addRemembered(triggeredCard);
         }
 
+        if (regtrig.hasParam("RememberTriggeringCards")) {
+            CardCollection triggeringCards = ((CardCollection) sa.getTriggeringObject(AbilityKey.Cards));
+            for (Card c : triggeringCards) {
+                host.addRemembered(c);
+            }
+        }
+
         if (regtrig.hasParam("RememberKey")) {
             host.addRemembered(runParams.get(AbilityKey.fromString(regtrig.getParam("RememberKey"))));
         }
