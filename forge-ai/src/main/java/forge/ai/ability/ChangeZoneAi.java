a5aeb4330500376b323857356f6955f2f912a3de
tool4EvEr <tool4EvEr@192.168.0.59>
2021-05-03 18:57:42 +0200

    Partner with support

--- forge-ai/src/main/java/forge/ai/ability/ChangeZoneAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ChangeZoneAi.java
@@ -216,6 +216,10 @@ public class ChangeZoneAi extends SpellAbilityAi {
         }
 
         if ("Always".equals(sa.getParam("AILogic"))) {
+            if (sa.getTrigger().getParam("TriggerDescription") != null && sa.getTrigger().getParam("TriggerDescription").contains("Partner with")) {
+                sa.resetTargets();
+                sa.getTargets().add(aiPlayer);
+            }
             return true;
         } else if ("IfNotBuffed".equals(sa.getParam("AILogic"))) {
             if (ComputerUtilCard.isUselessCreature(aiPlayer, sa.getHostCard())) {
