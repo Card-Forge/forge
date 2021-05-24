70b24f9b8647adcfbf9b0f22bbee4b60384e6341
tool4EvEr <tool4EvEr@192.168.0.59>
2021-05-24 10:55:51 +0200

    Fix Sen Triplets interaction

--- forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
+++ forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
@@ -244,7 +244,7 @@ public class SpellAbilityRestriction extends SpellAbilityVariables {
                     // NOTE: this assumes that it's always possible to cast cards from hand and you don't
                     // need special permissions for that. If WotC ever prints a card that forbids casting
                     // cards from hand, this may become relevant.
-                    if (!o.grantsZonePermissions() && cardZone != null && !cardZone.is(ZoneType.Hand)) {
+                    if (!o.grantsZonePermissions() && cardZone != null && (!cardZone.is(ZoneType.Hand) || activator != c.getOwner())) {
                         final List<CardPlayOption> opts = c.mayPlay(activator);
                         boolean hasOtherGrantor = false;
                         for (CardPlayOption opt : opts) {
