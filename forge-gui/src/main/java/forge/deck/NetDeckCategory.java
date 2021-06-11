45f598e0b967a3022635636ca0612393fd867709
Churrufli <churruflis@gmail.com>
2021-06-11 09:07:41 +0200

    Oathbreaker Net Decks implementation

--- forge-gui/src/main/java/forge/deck/NetDeckCategory.java
+++ forge-gui/src/main/java/forge/deck/NetDeckCategory.java
@@ -21,7 +21,7 @@ import forge.util.storage.StorageBase;
 
 public class NetDeckCategory extends StorageBase<Deck> {
     public static final String PREFIX = "NET_DECK_";
-    private static Map<String, NetDeckCategory> constructed, commander, brawl;
+    private static Map<String, NetDeckCategory> constructed, commander, brawl,oathbreaker;
 
     private static Map<String, NetDeckCategory> loadCategories(String filename) {
         Map<String, NetDeckCategory> categories = new TreeMap<>();
@@ -63,6 +63,12 @@ public class NetDeckCategory extends StorageBase<Deck> {
                     brawl = loadCategories(ForgeConstants.NET_DECKS_BRAWL_LIST_FILE);
                 }
                 categories = brawl;
+                break;
+            case Oathbreaker:
+                if (oathbreaker == null) {
+                    oathbreaker = loadCategories(ForgeConstants.NET_DECKS_OATHBREAKER_LIST_FILE);
+                }
+                categories = oathbreaker;
                 break;
         default:
             return null;
