b3fe2a55db7a0b212ed0bca7ca0f86d9fd0cb20d
tool4EvEr <tool4EvEr@192.168.0.59>
2021-06-15 18:15:16 +0200

    Improve AI

--- forge-ai/src/main/java/forge/ai/ability/SkipPhaseAi.java
+++ forge-ai/src/main/java/forge/ai/ability/SkipPhaseAi.java
@@ -9,27 +9,33 @@ import forge.game.spellability.SpellAbility;
 public class SkipPhaseAi extends SpellAbilityAi {
     @Override
     protected boolean canPlayAI(Player aiPlayer, SpellAbility sa) {
+        return targetPlayer(aiPlayer, sa, false);
+    }
+
+    @Override
+    protected boolean doTriggerAINoCost(Player aiPlayer, SpellAbility sa, boolean mandatory) {
+        return targetPlayer(aiPlayer, sa, mandatory);
+    }
+
+    @Override
+    public boolean confirmAction(Player player, SpellAbility sa, PlayerActionConfirmMode mode, String message) {
+        return true;
+    }
+    
+    public boolean targetPlayer(Player ai, SpellAbility sa, boolean mandatory) {
         if (sa.usesTargeting()) {
-            final Player opp = AiAttackController.choosePreferredDefenderPlayer(aiPlayer);
+            final Player opp = AiAttackController.choosePreferredDefenderPlayer(ai);
+            sa.resetTargets();
             if (sa.canTarget(opp)) {
-                sa.resetTargets();
                 sa.getTargets().add(opp);
+            }
+            else if (mandatory && sa.canTarget(ai)) {
+                sa.getTargets().add(ai); 
             }
             else {
                 return false;
             }
         }
-
-        return true;
-    }
-
-    @Override
-    protected boolean doTriggerAINoCost(Player aiPlayer, SpellAbility sa, boolean mandatory) {
-        return canPlayAI(aiPlayer, sa) || mandatory;
-    }
-
-    @Override
-    public boolean confirmAction(Player player, SpellAbility sa, PlayerActionConfirmMode mode, String message) {
         return true;
     }
 }
