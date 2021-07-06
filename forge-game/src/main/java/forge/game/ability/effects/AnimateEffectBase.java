9543534ddac3e6f3b10278a68f92de4cedc57599
Northmoc <timothy.s.mocny@gmail.com>
2021-07-06 19:58:34 -0400

    AnimateEffectBase - support "GainsTriggeredAbilitiesOf"

--- forge-game/src/main/java/forge/game/ability/effects/AnimateEffectBase.java
+++ forge-game/src/main/java/forge/game/ability/effects/AnimateEffectBase.java
@@ -164,6 +164,14 @@ public abstract class AnimateEffectBase extends SpellAbilityEffect {
             final Trigger parsedTrigger = TriggerHandler.parseTrigger(AbilityUtils.getSVar(sa, s), c, false, sa);
             addedTriggers.add(parsedTrigger);
         }
+        if (sa.hasParam("GainsTriggeredAbilitiesOf")) {
+            final List<Card> cards = AbilityUtils.getDefinedCards(sa.getHostCard(), sa.getParam("GainsTriggeredAbilitiesOf"), sa);
+            for (final Card card : cards) {
+                for (Trigger t : card.getTriggers()) {
+                    addedTriggers.add(t.copy(c, false));
+                }
+            }
+        }
 
         // give replacement effects
         final List<ReplacementEffect> addedReplacements = Lists.newArrayList();
