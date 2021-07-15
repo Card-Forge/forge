a9c92da5ddac5f2e940e1cb17133eb603b3d766b
tool4EvEr <tool4EvEr@192.168.0.59>
2021-07-15 17:08:50 +0200

    FightEffect: Fix missing trigger

--- forge-game/src/main/java/forge/game/ability/effects/FightEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/FightEffect.java
@@ -38,7 +38,6 @@ public class FightEffect extends DamageBaseEffect {
         return sb.toString();
     }
 
-
     /* (non-Javadoc)
      * @see forge.game.ability.SpellAbilityEffect#resolve(forge.game.spellability.SpellAbility)
      */
@@ -77,6 +76,7 @@ public class FightEffect extends DamageBaseEffect {
 
         final Map<AbilityKey, Object> runParams = AbilityKey.newMap();
         runParams.put(AbilityKey.Fighters, fighters);
+        game.getTriggerHandler().runTrigger(TriggerType.FightOnce, runParams, false);
     }
 
     private static List<Card> getFighters(SpellAbility sa) {
