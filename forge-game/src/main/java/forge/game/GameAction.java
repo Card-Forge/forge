94da5f3c3ae959d9f55cbd14b734243f655c358e
Northmoc <timothy.s.mocny@gmail.com>
2021-07-23 10:43:23 -0400

    add Remember objects from Damage effects to GameAction before the maps are cleared

--- forge-game/src/main/java/forge/game/GameAction.java
+++ forge-game/src/main/java/forge/game/GameAction.java
@@ -2196,6 +2196,20 @@ public class GameAction {
             }
         }
 
+        // Remember objects as needed
+        final Card sourceLKI = cause.getHostCard().getGame().getChangeZoneLKIInfo(cause.getHostCard());
+        final boolean rememberCard = cause.hasParam("RememberDamaged") || cause.hasParam("RememberDamagedCreature");
+        final boolean rememberPlayer = cause.hasParam("RememberDamaged") || cause.hasParam("RememberDamagedPlayer");
+        if (rememberCard || rememberPlayer) {
+            for (GameEntity e : damageMap.row(sourceLKI).keySet()) {
+                if (e instanceof Card && rememberCard) {
+                    cause.getHostCard().addRemembered(e);
+                } else if (e instanceof Player && rememberPlayer) {
+                    cause.getHostCard().addRemembered(e);
+                }
+            }
+        }
+
         preventMap.triggerPreventDamage(isCombat);
         preventMap.clear();
 
