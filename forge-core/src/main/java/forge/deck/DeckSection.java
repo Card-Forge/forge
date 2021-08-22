057213342c279176775e9aef688c34acb3454060
leriomaggio <valerio.maggio@gmail.com>
2021-08-22 23:27:52 +0100

    DeckSection have been extended in order to support validation
    Each section requiring Validation, now can incorporate a method for sanity check. This will be useful when the deck importer will import cards that are placed in the wrong section in the list.

--- forge-core/src/main/java/forge/deck/DeckSection.java
+++ forge-core/src/main/java/forge/deck/DeckSection.java
@@ -1,33 +1,111 @@
 package forge.deck;
 
+import forge.card.CardType;
+import forge.item.PaperCard;
+
+import java.util.function.Function;
+
 public enum DeckSection {
-    Avatar(1),
-    Commander(1),
-    Main(60),
-    Sideboard(15),
-    Planes(10),
-    Schemes(20),
-    Conspiracy(0);
+    Avatar(1, Validators.AVATAR_VALIDATOR),
+    Commander(1, Validators.COMMANDER_VALIDATOR),
+    Main(60, Validators.DECK_VALIDATOR),
+    Sideboard(15, Validators.DECK_VALIDATOR),
+    Planes(10, Validators.PLANES_VALIDATOR),
+    Schemes(20, Validators.SCHEME_VALIDATOR),
+    Conspiracy(0, Validators.CONSPIRACY_VALIDATOR);
 
     private final int typicalSize; // Rules enforcement is done in DeckFormat class, this is for reference only
-    DeckSection(int commonSize) {
-        typicalSize = commonSize;
+    private Function<PaperCard, Boolean> fnValidator;
+
+    DeckSection(int commonSize, Function<PaperCard, Boolean> validator){
+        this.typicalSize = commonSize;
+        fnValidator = validator;
     }
     
     public boolean isSingleCard() { return typicalSize == 1; }
-    
+
+    public boolean validate(PaperCard card){
+        return fnValidator.apply(card);
+    }
+
+    // Returns the matching section for "special"/supplementary core types.
+    public static DeckSection matchingSection(PaperCard card){
+        CardType t = card.getRules().getType();
+        if (t.isConspiracy())
+            return Conspiracy;
+        if (t.isScheme())
+            return Schemes;
+        if (t.isVanguard())
+            return Avatar;
+        if (t.isPlane() || t.isPhenomenon() | t.isDungeon())
+            return Planes;
+        return Main;  // default
+    }
+
     public static DeckSection smartValueOf(String value) {
-        if (value == null) {
+        if (value == null)
             return null;
-        }
-        
         final String valToCompare = value.trim();
         for (final DeckSection v : DeckSection.values()) {
             if (v.name().compareToIgnoreCase(valToCompare) == 0) {
                 return v;
             }
         }
-        
         return null;
+    }
+
+    private static class Validators {
+        static final Function<PaperCard, Boolean> DECK_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                // NOTE: Same rules applies to both Deck and Side, despite "Conspiracy cards" are allowed
+                // in the SideBoard (see Rule 313.2)
+                // Those will be matched later, in case (see `Deck::loadDeferredSection`)
+                return (!t.isConspiracy() && !t.isDungeon() && !t.isPhenomenon() && !t.isPlane()
+                        && !t.isScheme() && !t.isVanguard());
+            }
+        };
+
+        static final Function<PaperCard, Boolean> COMMANDER_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                return (t.isPlaneswalker() || (t.isCreature() && t.isLegendary()));
+            }
+        };
+
+        static final Function<PaperCard, Boolean> PLANES_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                return (t.isPlane() || t.isPhenomenon());
+            }
+        };
+
+        static final Function<PaperCard, Boolean> SCHEME_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                return (t.isScheme());
+            }
+        };
+
+        static final Function<PaperCard, Boolean> CONSPIRACY_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                return (t.isConspiracy());
+            }
+        };
+
+        static final Function<PaperCard, Boolean> AVATAR_VALIDATOR = new Function<PaperCard, Boolean>() {
+            @Override
+            public Boolean apply(PaperCard card) {
+                CardType t = card.getRules().getType();
+                return ((t.isCreature() && t.hasSubtype("Avatar")) || t.isVanguard());
+            }
+        };
+
     }
 }
