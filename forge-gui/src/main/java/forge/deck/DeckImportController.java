dc845b1f671568394052271acfb075fae99a26dc
leriomaggio <valerio.maggio@gmail.com>
2021-08-22 23:58:56 +0100

    Improved controller to incorporate restrictions from game and deck formats
    The new controller has been extended with new attributes to enable the validation to cards recognised from card list performed by DeckRecognizer.
    in particular, the controller will keep track of current game format (i.e. allowedSetCodes), as well as current deck format.
    Lastly, it extends support for import/replace deck

--- forge-gui/src/main/java/forge/deck/DeckImportController.java
+++ forge-gui/src/main/java/forge/deck/DeckImportController.java
@@ -5,7 +5,6 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
-import org.apache.commons.lang3.NotImplementedException;
 import org.apache.commons.lang3.StringUtils;
 
 import forge.gui.interfaces.ICheckBox;
@@ -16,21 +15,51 @@ import forge.model.FModel;
 import forge.util.Localizer;
 
 public class DeckImportController {
-    private final boolean replacingDeck;
-    private final ICheckBox newEditionCheck, dateTimeCheck, onlyCoreExpCheck;
+    private boolean replacingDeck;
+    private final ICheckBox dateTimeCheck;
     private final IComboBox<String> monthDropdown;
     private final IComboBox<Integer> yearDropdown;
     private final List<DeckRecognizer.Token> tokens = new ArrayList<>();
+    private final boolean currentDeckNotEmpty;
+    private List<String> allowedSetCodes;
+    private DeckFormat currentDeckFormat;
 
-    public DeckImportController(boolean replacingDeck0, ICheckBox newEditionCheck0, ICheckBox dateTimeCheck0, ICheckBox onlyCoreExpCheck0, IComboBox<String> monthDropdown0, IComboBox<Integer> yearDropdown0) {
-        replacingDeck = replacingDeck0;
-        newEditionCheck = newEditionCheck0;
+    public DeckImportController(ICheckBox dateTimeCheck0,
+                                IComboBox<String> monthDropdown0, IComboBox<Integer> yearDropdown0,
+                                boolean currentDeckNotEmpty) {
+        this(dateTimeCheck0, monthDropdown0, yearDropdown0, currentDeckNotEmpty, null, null);
+    }
+
+    public DeckImportController (ICheckBox dateTimeCheck0,
+                                IComboBox<String> monthDropdown0, IComboBox<Integer> yearDropdown0,
+                                boolean currentDeckNotEmpty, List<String> setCodes, DeckFormat deckFormat) {
         dateTimeCheck = dateTimeCheck0;
-        onlyCoreExpCheck = onlyCoreExpCheck0;
         monthDropdown = monthDropdown0;
         yearDropdown = yearDropdown0;
 
+        /* This keeps track whether the current deck in editor is **not empty**.
+           If that is the case, and the "Replace" option won't be checked, this will
+           allow to ask for confirmation whether the *Merge* action is what
+           really intended by the user!
+         */
+        this.currentDeckNotEmpty = currentDeckNotEmpty;
+        // this option will control the "replace" action controlled by UI widget.
+        replacingDeck = false; // by default is false, as we DO NOT want to replace by default.
+
+        if (setCodes != null && setCodes.size() == 0)
+            this.allowedSetCodes = null;
+        else
+            this.allowedSetCodes = setCodes;
+        this.currentDeckFormat = deckFormat;
         fillDateDropdowns();
+    }
+
+    public void setReplacingDeck(boolean replaceDeck){
+        this.replacingDeck = replaceDeck;
+    }
+
+    public boolean getReplacingDeck(){
+        return this.replacingDeck;
     }
 
     private void fillDateDropdowns() {
@@ -50,71 +79,74 @@ public class DeckImportController {
 
     public List<DeckRecognizer.Token> parseInput(String input) {
         tokens.clear();
-
-        DeckRecognizer recognizer = new DeckRecognizer(newEditionCheck.isSelected(),  onlyCoreExpCheck.isSelected(), FModel.getMagicDb().getCommonCards());
+        DeckRecognizer recognizer = new DeckRecognizer(FModel.getMagicDb().getCommonCards(),
+                                                       FModel.getMagicDb().getVariantCards());
         if (dateTimeCheck.isSelected()) {
             recognizer.setDateConstraint(monthDropdown.getSelectedIndex(), yearDropdown.getSelectedItem());
         }
+        if (this.allowedSetCodes != null && this.allowedSetCodes.size() > 0)
+            recognizer.setCardEditionConstrain(this.allowedSetCodes);
+        if (this.currentDeckFormat != null)
+            recognizer.setDeckFormatConstraint(this.currentDeckFormat);
+
         String[] lines = input.split("\n");
         for (String line : lines) {
-            tokens.add(recognizer.recognizeLine(line));
+            DeckRecognizer.Token token = recognizer.recognizeLine(line);
+            if (token != null)
+                tokens.add(token);
         }
         return tokens;
     }
 
-    public Deck accept() {
+    public Deck accept(){
+        return this.accept("");
+    }
+
+    public Deck accept(String currentDeckName) {
         final Localizer localizer = Localizer.getInstance();
         if (tokens.isEmpty()) { return null; }
 
+        String deckName = "";
+        if (currentDeckName != null && currentDeckName.length() > 0)
+            deckName = String.format("\"%s\"", currentDeckName);
+
         if (replacingDeck) {
-            final String warning = localizer.getMessage("lblReplaceCurrentDeckConfirm");
-            if (!SOptionPane.showConfirmDialog(warning, localizer.getMessage("lblReplaceCurrentDeck"), localizer.getMessage("lblReplace"), localizer.getMessage("lblCancel"))) {
+            final String warning = localizer.getMessage("lblConfirmReplaceDeck", deckName);
+            if (!SOptionPane.showConfirmDialog(warning, localizer.getMessage("lblReplaceCurrentDeck"),
+                    localizer.getMessage("lblYes"), localizer.getMessage("lblNo"))) {
                 return null;
             }
+        }  else if (this.currentDeckNotEmpty){
+            final String warning = localizer.getMessage("lblConfirmCardImport", deckName);
+            if (!SOptionPane.showConfirmDialog(warning,
+                    localizer.getMessage("lblBulkImport"),
+                    localizer.getMessage("lblYes"), localizer.getMessage("lblNo")))
+                return null;
         }
-
-        final Deck result = new Deck();
+        final Deck resultDeck = new Deck();
         DeckSection deckSection = DeckSection.Main;
-        String section = "";
         for (final DeckRecognizer.Token t : tokens) {
             final DeckRecognizer.TokenType type = t.getType();
-            if (type == DeckRecognizer.TokenType.SectionName) {
-                section = t.getText().toLowerCase();
-                if (section.startsWith("//")) {
-                    continue;
-                }
-                // can't use wildcards in switch/case, so if/else it is
-                if (section.startsWith("main")) {
-                    deckSection = DeckSection.Main;
-                }
-                else if (section.startsWith("side")) {
-                    deckSection = DeckSection.Sideboard;
-                }
-                else if (section.startsWith("commander")) {
-                    deckSection = DeckSection.Commander;
-                }
-                else if (section.startsWith("avatar")) {
-                    deckSection = DeckSection.Avatar;
-                }
-                else if (section.startsWith("planes")) {
-                    deckSection = DeckSection.Planes;
-                }
-                else if (section.startsWith("scheme")) {
-                    deckSection = DeckSection.Schemes;
-                }
-                else if (section.startsWith("conspiracy")) {
-                    deckSection = DeckSection.Conspiracy;
-                }
-                else {
-                    throw new NotImplementedException("Unexpected section: " + t.getText());
-                }
+
+            if (type == DeckRecognizer.TokenType.DeckName) {
+                resultDeck.setName(t.getText());
             }
+            if (type == DeckRecognizer.TokenType.DeckSectionName) {
+                deckSection = DeckSection.smartValueOf(t.getText());
+            }
+            // all other tokens will be discarded.
             if (type != DeckRecognizer.TokenType.KnownCard) {
                 continue;
             }
             final PaperCard crd = t.getCard();
-            result.getOrCreate(deckSection).add(crd, t.getNumber());
+            // Leverage on the DeckSection validation mechanism to match cards to the corresponding deck section!
+            if (deckSection.validate(crd))
+                resultDeck.getOrCreate(deckSection).add(crd, t.getNumber());
+            else {
+                DeckSection matchingSec = DeckSection.matchingSection(crd);
+                resultDeck.getOrCreate(matchingSec).add(crd, t.getNumber());
+            }
         }
-        return result;
+        return resultDeck;
     }
 }
