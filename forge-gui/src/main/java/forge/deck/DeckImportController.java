812946a889bf5ca80c1b4277888ac4627b6f692d
leriomaggio <valeriomaggio@gmail.com>
2021-09-18 14:08:53 +0100

    Latest additions with Art Preference and Block Filter
    Signed-off-by: leriomaggio <valeriomaggio@gmail.com>

--- forge-gui/src/main/java/forge/deck/DeckImportController.java
+++ forge-gui/src/main/java/forge/deck/DeckImportController.java
@@ -5,6 +5,11 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+import forge.StaticData;
+import forge.card.CardDb;
+import forge.game.GameFormat;
+import forge.localinstance.properties.ForgePreferences;
+import forge.model.FModel;
 import org.apache.commons.lang3.StringUtils;
 
 import forge.gui.interfaces.ICheckBox;
@@ -15,9 +20,17 @@ import forge.util.Localizer;
 
 public class DeckImportController {
     private boolean createNewDeck;
+    // Date filter
     private final ICheckBox dateTimeCheck;
     private final IComboBox<String> monthDropdown;
     private final IComboBox<Integer> yearDropdown;
+    // CardArt Preference Filter
+    private CardDb.CardArtPreference artPreference;
+    // Block Preference Filter
+    private ICheckBox blockCheck = null;
+    private IComboBox<GameFormat> blocksDropdown = null;
+    private boolean isAnyBlockFormatSupported = false;
+
     private final List<DeckRecognizer.Token> tokens = new ArrayList<>();
     private final boolean currentDeckNotEmpty;
     private final List<String> allowedSetCodes;
@@ -26,15 +39,22 @@ public class DeckImportController {
     public DeckImportController(ICheckBox dateTimeCheck0,
                                 IComboBox<String> monthDropdown0, IComboBox<Integer> yearDropdown0,
                                 boolean currentDeckNotEmpty) {
-        this(dateTimeCheck0, monthDropdown0, yearDropdown0, currentDeckNotEmpty, null, null);
+        this(dateTimeCheck0, monthDropdown0, yearDropdown0, currentDeckNotEmpty, null, null,
+                null, null);
     }
 
-    public DeckImportController (ICheckBox dateTimeCheck0,
+    public DeckImportController(ICheckBox dateTimeCheck0,
                                 IComboBox<String> monthDropdown0, IComboBox<Integer> yearDropdown0,
-                                boolean currentDeckNotEmpty, List<String> setCodes, DeckFormat deckFormat) {
-        dateTimeCheck = dateTimeCheck0;
-        monthDropdown = monthDropdown0;
-        yearDropdown = yearDropdown0;
+                                boolean currentDeckNotEmpty, List<String> setCodes, DeckFormat deckFormat,
+                                ICheckBox blockCheck0, IComboBox<GameFormat> blocksDropdown) {
+        this.dateTimeCheck = dateTimeCheck0;
+        this.monthDropdown = monthDropdown0;
+        this.yearDropdown = yearDropdown0;
+        this.artPreference = StaticData.instance().getCardArtPreference();  // default
+        if (blockCheck0 != null && blocksDropdown != null){
+            this.blockCheck = blockCheck0;
+            this.blocksDropdown = blocksDropdown;
+        }
 
         /* This keeps track whether the current deck in editor is **not empty**.
            If that is the case, and the "Replace" option won't be checked, this will
@@ -44,13 +64,17 @@ public class DeckImportController {
         this.currentDeckNotEmpty = currentDeckNotEmpty;
         // this option will control the "new deck" action controlled by UI widget
         createNewDeck = false;
-
         if (setCodes != null && setCodes.size() == 0)
             this.allowedSetCodes = null;
         else
             this.allowedSetCodes = setCodes;
         this.currentDeckFormat = deckFormat;
+
         fillDateDropdowns();
+    }
+
+    public void setCardArtPreference(boolean isLatest, boolean coreFilterEnabled){
+        this.artPreference = StaticData.instance().getCardArtPreference(isLatest, coreFilterEnabled);
     }
 
     public void setCreateNewDeck(boolean createNewDeck){
@@ -72,16 +96,55 @@ public class DeckImportController {
         for (int i = yearNow; i >= 1993; i--) {
             yearDropdown.addItem(i);
         }
+
+        if (this.blocksDropdown != null &&
+                FModel.getPreferences().getPrefBoolean(ForgePreferences.FPref.LOAD_HISTORIC_FORMATS)){
+            this.blocksDropdown.removeAllItems();
+            final List<GameFormat> blockFormats = FModel.getBlockFormats();
+            for (final GameFormat f : blockFormats) {
+                if (isBlockFormatCompliantWithCurrentGameType(f)) {
+                    this.blocksDropdown.addItem(f);
+                    this.isAnyBlockFormatSupported = true;
+                }
+            }
+        }
+    }
+
+    private boolean isBlockFormatCompliantWithCurrentGameType(GameFormat f) {
+        if (this.allowedSetCodes == null)
+            return true;
+        List<String> formatSetCodes = f.getAllowedSetCodes();
+        boolean blockFormatCompliant = true;
+        for (String setCode : formatSetCodes){
+            if (!allowedSetCodes.contains(setCode)){
+                blockFormatCompliant = false;
+                break;
+            }
+        }
+        return blockFormatCompliant;
+    }
+
+    public boolean isBlockFormatsSupported() {
+        return this.isAnyBlockFormatSupported;
     }
 
     public List<DeckRecognizer.Token> parseInput(String input) {
         tokens.clear();
         DeckRecognizer recognizer = new DeckRecognizer();
-        if (dateTimeCheck.isSelected()) {
+        // Set Art Preference first thing
+        recognizer.setArtPreference(this.artPreference);
+
+        if (dateTimeCheck.isSelected())
             recognizer.setDateConstraint(yearDropdown.getSelectedItem(), monthDropdown.getSelectedIndex());
+
+        if (this.allowedSetCodes != null && this.allowedSetCodes.size() > 0){
+            if (this.blockCheck.isSelected() && this.isAnyBlockFormatSupported) {
+                GameFormat gameFormat = this.blocksDropdown.getSelectedItem();
+                recognizer.setGameFormatConstraint(gameFormat.getAllowedSetCodes());
+            } else
+                recognizer.setGameFormatConstraint(this.allowedSetCodes);
         }
-        if (this.allowedSetCodes != null && this.allowedSetCodes.size() > 0)
-            recognizer.setGameFormatConstraint(this.allowedSetCodes);
+
         if (this.currentDeckFormat != null)
             recognizer.setDeckFormatConstraint(this.currentDeckFormat);
 
