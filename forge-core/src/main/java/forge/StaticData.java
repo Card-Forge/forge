79923806c81ab5b771de155afcf3030aba1a64bc
leriomaggio <valeriomaggio@gmail.com>
2021-11-02 17:51:06 +0000

    Few typos in Doc + added a new parameter for alternative card art to consider also a list of allowed set codes.
    This change relies on the robust cardDb methods to retrieve a card also under the circumstances of a filter in place.
    This change has been introduced as this is going to be leveraged by Card Art optimisation applied to DeckList in DeckImport.

--- forge-core/src/main/java/forge/StaticData.java
+++ forge-core/src/main/java/forge/StaticData.java
@@ -354,7 +354,7 @@ public class StaticData {
     }
 
     /**
-     * Determins whether the input String corresponds to an MTG Card Name (in any available card database)
+     * Determines whether the input String corresponds to an MTG Card Name (in any available card database)
      * @param cardName Name of the Card to verify (CASE SENSITIVE)
      * @return True if a card with the given input string can be found. False otherwise.
      */
@@ -455,7 +455,6 @@ public class StaticData {
      *
      * Note: if input card is Foil, and an alternative card art is found, it will be returned foil too!
      *
-     * @see StaticData#getAlternativeCardPrint(forge.item.PaperCard, java.util.Date)
      * @param card Input Reference Card
      * @param setReleaseDate reference set release date
      * @return Alternative Card Art (from a different edition) of input card, or null if not found.
@@ -464,7 +463,7 @@ public class StaticData {
         boolean isCardArtPreferenceLatestArt = this.cardArtPreferenceIsLatest();
         boolean cardArtPreferenceHasFilter = this.isCoreExpansionOnlyFilterSet();
         return this.getAlternativeCardPrint(card, setReleaseDate, isCardArtPreferenceLatestArt,
-                                            cardArtPreferenceHasFilter);
+                                            cardArtPreferenceHasFilter, null);
     }
 
     /**
@@ -486,25 +485,25 @@ public class StaticData {
      * @param setReleaseDate  The reference release date used to control the search for alternative card print.
      *                        The chose candidate will be gathered from an edition printed before (upper bound) or
      *                        after (lower bound) the reference set release date.
-     * @param isCardArtPreferenceLatestArt  Determines whether or not "Latest Art" Card Art preference should be used
+     * @param isCardArtPreferenceLatestArt  Determines whether "Latest Art" Card Art preference should be used
      *                                      when looking for an alternative candidate print.
-     * @param cardArtPreferenceHasFilter    Determines whether or not the search should only consider
+     * @param cardArtPreferenceHasFilter    Determines whether the search should only consider
      *                                      Core, Expansions, or Reprints sets when looking for alternative candidates.
      * @return  an instance of <code>PaperCard</code> that is the selected alternative candidate, or <code>null</code>
      * if None could be found.
      */
     public PaperCard getAlternativeCardPrint(PaperCard card, Date setReleaseDate,
                                              boolean isCardArtPreferenceLatestArt,
-                                             boolean cardArtPreferenceHasFilter) {
+                                             boolean cardArtPreferenceHasFilter, List<String> allowedSetCodes) {
         Date searchReferenceDate = getReferenceDate(setReleaseDate, isCardArtPreferenceLatestArt);
         CardDb.CardArtPreference searchCardArtStrategy = getSearchStrategyForAlternativeCardArt(isCardArtPreferenceLatestArt,
                                                                           cardArtPreferenceHasFilter);
         return searchAlternativeCardCandidate(card, isCardArtPreferenceLatestArt, searchReferenceDate,
-                                              searchCardArtStrategy);
+                                              searchCardArtStrategy, allowedSetCodes);
     }
 
     /**
-     * This method extends the defatult <code>getAlternativeCardPrint</code> with extra settings to be used for
+     * This method extends the default <code>getAlternativeCardPrint</code> with extra settings to be used for
      * alternative card print.
      *
      * <p>
@@ -531,18 +530,54 @@ public class StaticData {
     public PaperCard getAlternativeCardPrint(PaperCard card, Date setReleaseDate, boolean isCardArtPreferenceLatestArt,
                                              boolean cardArtPreferenceHasFilter,
                                              boolean preferCandidatesFromExpansionSets, boolean preferModernFrame) {
+        return getAlternativeCardPrint(card, setReleaseDate, isCardArtPreferenceLatestArt, cardArtPreferenceHasFilter,
+                                        preferCandidatesFromExpansionSets, preferModernFrame, null);
+    }
+
+    /**
+     * This method extends the default <code>getAlternativeCardPrint</code> with extra settings to be used for
+     * alternative card print.
+     *
+     * <p>
+     * These options for Alternative Card Print make sense as part of the harmonisation/theme-matching process for
+     * cards in Deck Sections (i.e. CardPool). In fact, the values of the provided flags for alternative print
+     * for a single card will be determined according to whole card pool (Deck section) the card appears in.
+     *
+     * @param card  The instance of <code>PaperCard</code> to look for an alternative print
+     * @param setReleaseDate  The reference release date used to control the search for alternative card print.
+     *                        The chose candidate will be gathered from an edition printed before (upper bound) or
+     *                        after (lower bound) the reference set release date.
+     * @param isCardArtPreferenceLatestArt  Determines whether or not "Latest Art" Card Art preference should be used
+     *                                      when looking for an alternative candidate print.
+     * @param cardArtPreferenceHasFilter    Determines whether or not the search should only consider
+     *                                      Core, Expansions, or Reprints sets when looking for alternative candidates.
+     * @param preferCandidatesFromExpansionSets Whenever the selected Card Art Preference has filter, try to get
+     *                                          prefer candidates from Expansion Sets over those in Core or Reprint
+     *                                          Editions (whenever possible)
+     *                                          e.g. Necropotence from Ice Age rather than 5th Edition (w/ Latest=false)
+     * @param preferModernFrame  If True, Modern Card Frame will be preferred over Old Frames.
+     * @param allowedSetCodes The list of the allowed set codes to consider when looking for alternative card art
+     *                        candidates. If the list is not null and not empty, will be used in combination with the
+     *                        <code>isLegal</code> predicate.
+     * @see CardDb#isLegal(List<String>)
+     * @return an instance of <code>PaperCard</code> that is the selected alternative candidate, or <code>null</code>
+     *          if None could be found.
+     */
+    public PaperCard getAlternativeCardPrint(PaperCard card, Date setReleaseDate, boolean isCardArtPreferenceLatestArt,
+                                             boolean cardArtPreferenceHasFilter,
+                                             boolean preferCandidatesFromExpansionSets, boolean preferModernFrame,
+                                             List<String> allowedSetCodes){
         PaperCard altCard = this.getAlternativeCardPrint(card, setReleaseDate, isCardArtPreferenceLatestArt,
-                                                         cardArtPreferenceHasFilter);
+                                                          cardArtPreferenceHasFilter, allowedSetCodes);
         if (altCard == null)
             return altCard;
         // from here on, we're sure we do have a candidate already!
 
         /* Try to refine selection by getting one candidate with frame matching current
            Card Art Preference (that is NOT the lookup strategy!)*/
-        PaperCard refinedAltCandidate = this.tryToGetCardPrintWithMatchingFrame(altCard,
-                isCardArtPreferenceLatestArt,
-                cardArtPreferenceHasFilter,
-                preferModernFrame);
+        PaperCard refinedAltCandidate = this.tryToGetCardPrintWithMatchingFrame(altCard, isCardArtPreferenceLatestArt,
+                                                                                cardArtPreferenceHasFilter,
+                                                                                preferModernFrame, allowedSetCodes);
         if (refinedAltCandidate != null)
             altCard = refinedAltCandidate;
 
@@ -551,7 +586,7 @@ public class StaticData {
                NOTE: At this stage, any future selection should be already compliant with previous filter on
                Card Frame (if applied) given that we'll be moving either UP or DOWN the timeline of Card Edition */
             refinedAltCandidate = this.tryToGetCardPrintFromExpansionSet(altCard, isCardArtPreferenceLatestArt,
-                                                                         preferModernFrame);
+                                                                            preferModernFrame, allowedSetCodes);
             if (refinedAltCandidate != null)
                 altCard = refinedAltCandidate;
         }
@@ -560,21 +595,29 @@ public class StaticData {
 
     private PaperCard searchAlternativeCardCandidate(PaperCard card, boolean isCardArtPreferenceLatestArt,
                                                      Date searchReferenceDate,
-                                                     CardDb.CardArtPreference searchCardArtStrategy) {
+                                                     CardDb.CardArtPreference searchCardArtStrategy,
+                                                     List<String> allowedSetCodes) {
         // Note: this won't apply to Custom Nor Variant Cards, so won't bother including it!
         CardDb cardDb = this.commonCards;
         String cardName = card.getName();
         int artIndex = card.getArtIndex();
         PaperCard altCard = null;
+        Predicate<PaperCard> filter = null;
+        if (allowedSetCodes != null && !allowedSetCodes.isEmpty())
+            filter = (Predicate<PaperCard>) cardDb.isLegal(allowedSetCodes);
 
         if (isCardArtPreferenceLatestArt) {  // RELEASED AFTER REFERENCE DATE
-            altCard = cardDb.getCardFromEditionsReleasedAfter(cardName, searchCardArtStrategy, artIndex, searchReferenceDate);
+            altCard = cardDb.getCardFromEditionsReleasedAfter(cardName, searchCardArtStrategy, artIndex,
+                                                                searchReferenceDate, filter);
             if (altCard == null)  // relax artIndex condition
-                altCard = cardDb.getCardFromEditionsReleasedAfter(cardName, searchCardArtStrategy, searchReferenceDate);
+                altCard = cardDb.getCardFromEditionsReleasedAfter(cardName, searchCardArtStrategy,
+                                                                    searchReferenceDate, filter);
         } else {  // RELEASED BEFORE REFERENCE DATE
-            altCard = cardDb.getCardFromEditionsReleasedBefore(cardName, searchCardArtStrategy, artIndex, searchReferenceDate);
+            altCard = cardDb.getCardFromEditionsReleasedBefore(cardName, searchCardArtStrategy, artIndex,
+                                                                searchReferenceDate, filter);
             if (altCard == null)  // relax artIndex constraint
-                altCard = cardDb.getCardFromEditionsReleasedBefore(cardName, searchCardArtStrategy, searchReferenceDate);
+                altCard = cardDb.getCardFromEditionsReleasedBefore(cardName, searchCardArtStrategy,
+                                                                    searchReferenceDate, filter);
         }
         if (altCard == null)
             return null;
@@ -611,7 +654,8 @@ public class StaticData {
 
     private PaperCard tryToGetCardPrintFromExpansionSet(PaperCard altCard,
                                                         boolean isCardArtPreferenceLatestArt,
-                                                        boolean preferModernFrame) {
+                                                        boolean preferModernFrame,
+                                                        List<String> allowedSetCodes) {
         CardEdition altCardEdition = editions.get(altCard.getEdition());
         if (altCardEdition.getType() == CardEdition.Type.EXPANSION)
             return null;  // Nothing to do here!
@@ -624,7 +668,7 @@ public class StaticData {
         while (altCandidate != null) {
             Date referenceDate = editions.get(altCandidate.getEdition()).getDate();
             altCandidate = this.searchAlternativeCardCandidate(altCandidate, preferModernFrame,
-                                                                referenceDate, searchStrategy);
+                                                                referenceDate, searchStrategy, allowedSetCodes);
             if (altCandidate != null) {
                 CardEdition altCandidateEdition = editions.get(altCandidate.getEdition());
                 if (altCandidateEdition.getType() == CardEdition.Type.EXPANSION)
@@ -638,7 +682,7 @@ public class StaticData {
     private PaperCard tryToGetCardPrintWithMatchingFrame(PaperCard altCard,
                                                          boolean isCardArtPreferenceLatestArt,
                                                          boolean cardArtHasFilter,
-                                                         boolean preferModernFrame) {
+                                                         boolean preferModernFrame, List<String> allowedSetCodes) {
         CardEdition altCardEdition = editions.get(altCard.getEdition());
         boolean frameIsCompliantAlready = (altCardEdition.isModern() == preferModernFrame);
         if (frameIsCompliantAlready)
@@ -650,7 +694,7 @@ public class StaticData {
         while (altCandidate != null) {
             Date referenceDate = editions.get(altCandidate.getEdition()).getDate();
             altCandidate = this.searchAlternativeCardCandidate(altCandidate, preferModernFrame,
-                                                               referenceDate, searchStrategy);
+                                                               referenceDate, searchStrategy, allowedSetCodes);
             if (altCandidate != null) {
                 CardEdition altCandidateEdition = editions.get(altCandidate.getEdition());
                 if (altCandidateEdition.isModern() == preferModernFrame)
