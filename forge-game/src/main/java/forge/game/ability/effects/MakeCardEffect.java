7a265a0d4a5cb8ec4645b3d36ac5f739739bd1ad
Northmoc <timothy.s.mocny@gmail.com>
2021-12-07 15:38:30 -0500

    expand MakeCardEffect

--- forge-game/src/main/java/forge/game/ability/effects/MakeCardEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/MakeCardEffect.java
@@ -12,34 +12,47 @@ import forge.game.zone.ZoneType;
 public class MakeCardEffect extends SpellAbilityEffect {
     @Override
     public void resolve(SpellAbility sa) {
-        final Player player = sa.getActivatingPlayer();
-        final Game game = player.getGame();
+        for (final Player player : getTargetPlayers(sa)) {
+            final Game game = player.getGame();
 
-        String name = sa.getParamOrDefault("Name", sa.getHostCard().getName());
-        if (name.equals("ChosenName")) {
-            name = sa.getHostCard().getChosenName();
-        }
-        final ZoneType zone = ZoneType.smartValueOf(sa.getParamOrDefault("Zone", "Library"));
-        int amount = sa.hasParam("Amount") ? Integer.parseInt(sa.getParam("Amount")) : 1;
+            String name = sa.getParamOrDefault("Name", "");
+            if (name.equals("ChosenName")) {
+                if (sa.getHostCard().hasChosenName()) {
+                    name = sa.getHostCard().getChosenName();
+                } else {
+                    continue;
+                }
 
-        CardCollection cards = new CardCollection();
-
-        while (amount > 0) {
-            Card card = Card.fromPaperCard(StaticData.instance().getCommonCards().getUniqueByName(name), player);
-            if (!sa.hasParam("NotToken")) { card.setTokenCard(true); }
-            game.getAction().moveTo(ZoneType.None, card, sa);
-            cards.add(card);
-            amount--;
-        }
-
-        for (final Card c : cards) {
-            game.getAction().moveTo(zone, c, sa);
-            if (sa.hasParam("RememberMade")) {
-                sa.getHostCard().addRemembered(c);
             }
-        }
-        if (zone.equals(ZoneType.Library)) {
-            player.shuffle(sa);
+            final ZoneType zone = ZoneType.smartValueOf(sa.getParamOrDefault("Zone", "Library"));
+            int amount = sa.hasParam("Amount") ? Integer.parseInt(sa.getParam("Amount")) : 1;
+
+            CardCollection cards = new CardCollection();
+
+            if (!name.equals("")) {
+                while (amount > 0) {
+                    Card card = Card.fromPaperCard(StaticData.instance().getCommonCards().getUniqueByName(name), player);
+                    if (!sa.hasParam("NotToken")) {
+                        card.setTokenCard(true);
+                    }
+                    game.getAction().moveTo(ZoneType.None, card, sa);
+                    cards.add(card);
+                    amount--;
+                }
+
+                for (final Card c : cards) {
+                    game.getAction().moveTo(zone, c, sa);
+                    if (sa.hasParam("RememberMade")) {
+                        sa.getHostCard().addRemembered(c);
+                    }
+                    if (sa.hasParam("ImprintMade")) {
+                        sa.getHostCard().addImprintedCard(c);
+                    }
+                }
+                if (zone.equals(ZoneType.Library)) {
+                    player.shuffle(sa);
+                }
+            }
         }
     }
 }
