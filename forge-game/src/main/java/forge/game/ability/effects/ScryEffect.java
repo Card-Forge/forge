edab767b1927ba6fc013f0cda68dde780299dfa8
Tim Mocny <timothy.s.mocny@gmail.com>
2022-02-05 19:11:15 +0000

    improve getStackDescription for some more Effects

--- forge-game/src/main/java/forge/game/ability/effects/ScryEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/ScryEffect.java
@@ -8,6 +8,7 @@ import forge.game.ability.AbilityUtils;
 import forge.game.ability.SpellAbilityEffect;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
+import forge.util.Lang;
 import forge.util.Localizer;
 
 
@@ -16,16 +17,16 @@ public class ScryEffect extends SpellAbilityEffect {
     protected String getStackDescription(SpellAbility sa) {
         final StringBuilder sb = new StringBuilder();
 
-        for (final Player p : getTargetPlayers(sa)) {
-            sb.append(p.toString()).append(" ");
-        }
+        final List<Player> players = Lists.newArrayList(); // players really affected
+        players.addAll(getTargetPlayers(sa));
+        sb.append(Lang.joinHomogenous(players)).append(" ");
 
         int num = 1;
         if (sa.hasParam("ScryNum")) {
             num = AbilityUtils.calculateAmount(sa.getHostCard(), sa.getParam("ScryNum"), sa);
         }
 
-        sb.append("scrys (").append(num).append(").");
+        sb.append(players.size() == 1 ? "scries " : "scry ").append(num).append(".");
         return sb.toString();
     }
 
