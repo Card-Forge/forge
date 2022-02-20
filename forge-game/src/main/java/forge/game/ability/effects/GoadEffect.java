95d8faf41d4ea50274a3ae6e8a5607f37f700bb8
Tim Mocny <timothy.s.mocny@gmail.com>
2022-02-20 04:20:12 +0000

    NEC: Kaima, the Fractured Calm and support

--- forge-game/src/main/java/forge/game/ability/effects/GoadEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/GoadEffect.java
@@ -15,6 +15,7 @@ public class GoadEffect extends SpellAbilityEffect {
         final Player player = sa.getActivatingPlayer();
         final Game game = player.getGame();
         final long timestamp = game.getNextTimestamp();
+        final boolean remember = sa.hasParam("RememberGoaded");
 
         for (final Card tgtC : getDefinedCardsOrTargeted(sa)) {
             // only pump things in PumpZone
@@ -40,6 +41,10 @@ public class GoadEffect extends SpellAbilityEffect {
             };
 
             game.getCleanup().addUntil(player, untilEOT);
+
+            if (remember && tgtC.isGoaded()) {
+                sa.getHostCard().addRemembered(tgtC);
+            }
         }
     }
 
