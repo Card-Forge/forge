26f27bdd768fe76a31c42459458a7f2292786a7b
Anthony Calosa <anthonycalosa@gmail.com>
2022-02-27 11:00:28 +0800

    [Adventure] Try to load old save file - override mismatch

--- forge-gui-mobile/src/forge/adventure/util/SaveFileData.java
+++ forge-gui-mobile/src/forge/adventure/util/SaveFileData.java
@@ -9,8 +9,10 @@ import forge.Forge;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.io.ObjectStreamClass;
 import java.util.HashMap;
 
 public class SaveFileData extends HashMap<String,byte[]>
@@ -181,7 +183,7 @@ public class SaveFileData extends HashMap<String,byte[]>
         try {
 
             ByteArrayInputStream stream=new ByteArrayInputStream(get(key));
-            ObjectInputStream objStream=new ObjectInputStream(stream);
+            ObjectInputStream objStream=new DecompressibleInputStream(stream);
             return objStream.readObject();
 
         } catch (IOException | ClassNotFoundException e) {
@@ -327,5 +329,40 @@ public class SaveFileData extends HashMap<String,byte[]>
         return false;
     }
 
+    class DecompressibleInputStream extends ObjectInputStream {
 
+        //private static Logger logger = LoggerFactory.getLogger(DecompressibleInputStream.class);
+
+        public DecompressibleInputStream(InputStream in) throws IOException {
+            super(in);
+        }
+
+        @Override
+        protected ObjectStreamClass readClassDescriptor() throws IOException, ClassNotFoundException {
+            ObjectStreamClass resultClassDescriptor = super.readClassDescriptor(); // initially streams descriptor
+            Class localClass; // the class in the local JVM that this descriptor represents.
+            try {
+                localClass = Class.forName(resultClassDescriptor.getName());
+            } catch (ClassNotFoundException e) {
+                //logger.error("No local class for " + resultClassDescriptor.getName(), e);
+                System.err.println("[Class Not Found Exception]\nNo local class for " + resultClassDescriptor.getName());
+                return resultClassDescriptor;
+            }
+            ObjectStreamClass localClassDescriptor = ObjectStreamClass.lookup(localClass);
+            if (localClassDescriptor != null) { // only if class implements serializable
+                final long localSUID = localClassDescriptor.getSerialVersionUID();
+                final long streamSUID = resultClassDescriptor.getSerialVersionUID();
+                if (streamSUID != localSUID) { // check for serialVersionUID mismatch.
+                    final StringBuffer s = new StringBuffer("Overriding serialized class version mismatch: ");
+                    s.append("local serialVersionUID = ").append(localSUID);
+                    s.append(" stream serialVersionUID = ").append(streamSUID);
+                    //Exception e = new InvalidClassException(s.toString());
+                    //logger.error("Potentially Fatal Deserialization Operation.", e);
+                    System.err.println("[Invalid Class Exception\n]"+s);
+                    resultClassDescriptor = localClassDescriptor; // Use local class descriptor for deserialization
+                }
+            }
+            return resultClassDescriptor;
+        }
+    }
 }
