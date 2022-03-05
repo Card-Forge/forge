8a1d787b54b61cac6362fa7756375efb6341e7ae
Tim Mocny <timothy.s.mocny@gmail.com>
2022-03-05 05:13:16 +0000

    Streets of New Capenna cards (and THB Demigod / various Ajani tweaks)

--- forge-game/src/main/java/forge/game/ability/effects/CountersPutAllEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/CountersPutAllEffect.java
@@ -44,11 +44,10 @@ public class CountersPutAllEffect extends SpellAbilityEffect  {
     public void resolve(SpellAbility sa) {
         final Card host = sa.getHostCard();
         final Player activator = sa.getActivatingPlayer();
-        final String type = sa.getParam("CounterType");
+        final CounterType type = CounterType.getType(sa.getParam("CounterType"));
         final int counterAmount = AbilityUtils.calculateAmount(host, sa.getParam("CounterNum"), sa);
         final String valid = sa.getParam("ValidCards");
         final ZoneType zone = sa.hasParam("ValidZone") ? ZoneType.smartValueOf(sa.getParam("ValidZone")) : ZoneType.Battlefield;
-        final boolean etbcounter = sa.hasParam("ETB");
         final Game game = activator.getGame();
 
         if (counterAmount <= 0) {
@@ -71,12 +70,29 @@ public class CountersPutAllEffect extends SpellAbilityEffect  {
 
         GameEntityCounterTable table = new GameEntityCounterTable();
         for (final Card tgtCard : cards) {
-            if (etbcounter) {
-                tgtCard.addEtbCounter(CounterType.getType(type), counterAmount, placer);
-            } else {
-                tgtCard.addCounter(CounterType.getType(type), counterAmount, placer, table);
+            tgtCard.addCounter(type, counterAmount, placer, table);
+        }
+
+        if (sa.hasParam("ValidCards2") || sa.hasParam("CounterType2") || sa.hasParam("CounterNum2")) {
+            final CounterType type2 = sa.hasParam("CounterType2") ?
+                    CounterType.getType(sa.getParam("CounterType2")) : type;
+            final ZoneType zone2 = sa.hasParam("ValidZone2") ?
+                    ZoneType.smartValueOf(sa.getParam("ValidZone2")) : zone;
+            if (sa.hasParam("ValidCards2")) {
+                cards = CardLists.getValidCards(game.getCardsIn(zone2), sa.getParam("ValidCards2"),
+                        host.getController(), host, sa);
+                if (sa.usesTargeting()) {
+                    cards = CardLists.filterControlledBy(cards, sa.getTargets().getFirstTargetedPlayer());
+                }
+            }
+            final int counterAmount2 = sa.hasParam("CounterNum2") ?
+                    AbilityUtils.calculateAmount(host, sa.getParam("CounterNum2"), sa) : counterAmount;
+
+            for (final Card tgtCard : cards) {
+                tgtCard.addCounter(type2, counterAmount2, placer, table);
             }
         }
+
         table.replaceCounterEffect(game, sa, true);
     }
 
