5315c90d24a4cd20880bd5d68b6b762d3dc0c2a0
Anthony Calosa <anthonycalosa@gmail.com>
2022-03-11 04:55:00 +0000

    unused imports

--- forge-gui-mobile/src/forge/screens/SplashScreen.java
+++ forge-gui-mobile/src/forge/screens/SplashScreen.java
@@ -1,7 +1,5 @@
 package forge.screens;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Align;
