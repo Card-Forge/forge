c3b5e2e270234b12730ac981928b5cf8b699d195
Tim Mocny <timothy.s.mocny@gmail.com>
2022-04-06 04:37:45 +0000

    SNC: Riveteers Charm and support

--- forge-game/src/main/java/forge/game/ability/effects/EffectEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/EffectEffect.java
@@ -310,6 +310,8 @@ public class EffectEffect extends SpellAbilityEffect {
                     game.getUpkeep().addUntil(controller, endEffect);
                 } else if (duration.equals("UntilEndOfCombat")) {
                     game.getEndOfCombat().addUntil(endEffect);
+                } else if (duration.equals("UntilYourNextEndStep")) {
+                    game.getEndOfTurn().addUntil(controller, endEffect);
                 } else if (duration.equals("UntilTheEndOfYourNextTurn")) {
                     if (game.getPhaseHandler().isPlayerTurn(controller)) {
                         game.getEndOfTurn().registerUntilEnd(controller, endEffect);
