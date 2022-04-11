a5bd40d7affd29d43ac6ca789510098f15c478b9
Magpie <magpie514@users.noreply.github.com>
2022-04-11 09:25:36 +0200

    Adventure mode PoI refactor. Renamed, re-set and cherry-picked a few of the various PoIs and reverted placement behavior.

--- forge-gui-mobile/src/forge/adventure/data/PointOfInterestData.java
+++ forge-gui-mobile/src/forge/adventure/data/PointOfInterestData.java
@@ -1,13 +1,10 @@
 package forge.adventure.data;
 
 import com.badlogic.gdx.files.FileHandle;
-import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.Json;
 import forge.adventure.util.Config;
 import forge.adventure.util.Paths;
-
-import java.util.ArrayList;
 
 /**
  * Data class that will be used to read Json configuration files
@@ -39,13 +36,9 @@ public class PointOfInterestData {
         }
         return pointOfInterestList;
     }
-    public static PointOfInterestData getPointOfInterest(String name, Vector2 index) {
-        ArrayList<PointOfInterestData> candidates = new ArrayList<PointOfInterestData>();
+    public static PointOfInterestData getPointOfInterest(String name) {
         for(PointOfInterestData data: new Array.ArrayIterator<>(getAllPointOfInterest())){
-            if(data.name.equals(name)) candidates.add(data); //Populate candidates with specified name.
-        }
-        if(candidates.size() > 0){ //If we got any candidates, return a random one.
-            return candidates.get(((int)(index.x*6719+index.y)%candidates.size()));//6719 is just a big prime to avoid collisions
+            if(data.name.equals(name)) return data;
         }
         return null;
     }
