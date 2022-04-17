ac4917a0931a67768bf8714210e5d57ba0e7513a
Leandro Doctors <ldoctors@gmail.com>
2022-04-17 02:21:27 -0300

    forge-game: tests: migrate from JUnit3 to TestNG

--- forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
+++ forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
@@ -1,24 +1,26 @@
 package forge.game.ability;
 
+import org.testng.annotations.Test;
+import org.testng.AssertJUnit;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
 
-import junit.framework.TestCase;
+public class AbilityKeyTest {
+	@Test
+	public void testFromStringWorksForAllKeys() {
+		for (AbilityKey key : AbilityKey.values()) {
+			AssertJUnit.assertEquals(key, AbilityKey.fromString(key.toString()));
+		}
+	}
 
-public class AbilityKeyTest extends TestCase {
-    public void testFromStringWorksForAllKeys() {
-        for (AbilityKey key : AbilityKey.values()) {
-            assertEquals(key, AbilityKey.fromString(key.toString()));
-        }
-    }
+	@Test
+	public void testCopyingEmptyMapWorks() {
+		Map<AbilityKey, Object> map = Maps.newHashMap();
 
-    public void testCopyingEmptyMapWorks() {
-        Map<AbilityKey, Object> map = Maps.newHashMap();
+		Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
 
-        Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
-
-        // An actual copy should be made.
-        assertNotSame(map, newMap);
-    }
+		// An actual copy should be made.
+		AssertJUnit.assertNotSame(map, newMap);
+	}
 }
