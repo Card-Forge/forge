e570222e31f48b47d10a97ddb06b2fc230be0608
Leandro Doctors <ldoctors@gmail.com>
2022-04-18 15:27:25 -0300

    format: convert tabs to spaces

--- forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
+++ forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
@@ -8,19 +8,19 @@ import com.google.common.collect.Maps;
 
 public class AbilityKeyTest {
 
-	@Test
-	public void testFromStringWorksForAllKeys() {
-		for (AbilityKey key : AbilityKey.values()) {
-			AssertJUnit.assertEquals(key, AbilityKey.fromString(key.toString()));
-		}
-	}
+    @Test
+    public void testFromStringWorksForAllKeys() {
+        for (AbilityKey key : AbilityKey.values()) {
+            AssertJUnit.assertEquals(key, AbilityKey.fromString(key.toString()));
+        }
+    }
 
-	@Test
-	public void testCopyingEmptyMapWorks() {
-		Map<AbilityKey, Object> map = Maps.newHashMap();
-		Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
+    @Test
+    public void testCopyingEmptyMapWorks() {
+        Map<AbilityKey, Object> map = Maps.newHashMap();
+        Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
 
-		// An actual copy should be made.
-		AssertJUnit.assertNotSame(map, newMap);
-	}
+        // An actual copy should be made.
+        AssertJUnit.assertNotSame(map, newMap);
+    }
 }
--- forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
+++ forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
@@ -12,30 +12,30 @@ import forge.card.mana.ManaCostParser;
 
 public class ManaCostBeingPaidTest {
 
-	@Test
-	public void testPayManaViaConvoke() {
-		runConvokeTest("1 W W", new byte[] { WHITE, COLORLESS, WHITE }, new String[] { "{1}{W}{W}", "{1}{W}", "{W}" });
-		runConvokeTest("1 W W", new byte[] { COLORLESS, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
-		runConvokeTest("1 W W", new byte[] { GREEN, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
-		runConvokeTest("1 W G", new byte[] { GREEN, RED, WHITE }, new String[] { "{1}{W}{G}", "{1}{W}", "{W}" });
-	}
+    @Test
+    public void testPayManaViaConvoke() {
+        runConvokeTest("1 W W", new byte[] { WHITE, COLORLESS, WHITE }, new String[] { "{1}{W}{W}", "{1}{W}", "{W}" });
+        runConvokeTest("1 W W", new byte[] { COLORLESS, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
+        runConvokeTest("1 W W", new byte[] { GREEN, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
+        runConvokeTest("1 W G", new byte[] { GREEN, RED, WHITE }, new String[] { "{1}{W}{G}", "{1}{W}", "{W}" });
+    }
 
-	private void runConvokeTest(String initialCost, byte[] colorsToPay, String[] expectedRemainder) {
+    private void runConvokeTest(String initialCost, byte[] colorsToPay, String[] expectedRemainder) {
 
-		ManaCostBeingPaid costBeingPaid = createManaCostBeingPaid(initialCost);
+        ManaCostBeingPaid costBeingPaid = createManaCostBeingPaid(initialCost);
 
-		for (int i = 0; i < colorsToPay.length; i++) {
-			AssertJUnit.assertEquals(expectedRemainder[i], costBeingPaid.toString());
-			costBeingPaid.payManaViaConvoke(colorsToPay[i]);
-		}
+        for (int i = 0; i < colorsToPay.length; i++) {
+            AssertJUnit.assertEquals(expectedRemainder[i], costBeingPaid.toString());
+            costBeingPaid.payManaViaConvoke(colorsToPay[i]);
+        }
 
-		AssertJUnit.assertEquals("0", costBeingPaid.toString());
-	}
+        AssertJUnit.assertEquals("0", costBeingPaid.toString());
+    }
 
-	private ManaCostBeingPaid createManaCostBeingPaid(String costString) {
-		ManaCostParser parsedCostString = new ManaCostParser(costString);
-		ManaCost manaCost = new ManaCost(parsedCostString);
+    private ManaCostBeingPaid createManaCostBeingPaid(String costString) {
+        ManaCostParser parsedCostString = new ManaCostParser(costString);
+        ManaCost manaCost = new ManaCost(parsedCostString);
 
-		return new ManaCostBeingPaid(manaCost);
-	}
+        return new ManaCostBeingPaid(manaCost);
+    }
 }
