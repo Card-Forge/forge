67054f48a1c32577a85db2605fe0aea92232f4ac
tool4EvEr <tool4EvEr@192.168.0.59>
2022-04-29 23:09:47 +0200

    Always change zone of cards when they move

--- forge-game/src/main/java/forge/game/zone/Zone.java
+++ forge-game/src/main/java/forge/game/zone/Zone.java
@@ -121,11 +121,11 @@ public class Zone implements java.io.Serializable, Iterable<Card> {
             c.setTapped(false);
         }
 
+        c.setZone(this);
+
         // Do not add Tokens to other zones than the battlefield. (unless it's a copy of a card 707.12)
         // But Effects/Emblems count as Tokens too, so allow Command too.
         if ((zoneType == ZoneType.Battlefield || !c.isToken()) || (zoneType == ZoneType.Stack && c.getCopiedPermanent() != null)) {
-            c.setZone(this);
-
             if (index == null) {
                 cardList.add(c);
             } else {
