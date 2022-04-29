4877c24e8e9357b052d90d74960198f3a1cff9cf
Northmoc <timothy.s.mocny@gmail.com>
2022-04-29 11:00:01 -0400

    restore import IntelliJ said was unneeded :(

--- forge-game/src/main/java/forge/game/cost/CostExile.java
+++ forge-game/src/main/java/forge/game/cost/CostExile.java
@@ -28,6 +28,7 @@ import forge.game.spellability.SpellAbility;
 import forge.game.zone.ZoneType;
 import forge.util.Lang;
 import forge.util.TextUtil;
+import forge.util.collect.FCollectionView;
 
 /**
  * The Class CostExile.
