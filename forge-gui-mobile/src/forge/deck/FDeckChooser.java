f2fb22328a9c71b8d0f6b9c5f6396b5681fdaa07
Anthony Calosa <anthonycalosa@gmail.com>
2022-05-07 07:22:20 +0800

    unused import

--- forge-gui-mobile/src/forge/deck/FDeckChooser.java
+++ forge-gui-mobile/src/forge/deck/FDeckChooser.java
@@ -7,7 +7,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 import forge.util.MyRandom;
