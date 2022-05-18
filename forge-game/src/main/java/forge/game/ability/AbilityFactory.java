60d8cda45f0962910ed88ec45c07b039ebacaec0
Northmoc <timothy.s.mocny@gmail.com>
2022-05-17 20:47:44 -0400

    add "Card" to commonStuff

--- forge-game/src/main/java/forge/game/ability/AbilityFactory.java
+++ forge-game/src/main/java/forge/game/ability/AbilityFactory.java
@@ -334,7 +334,7 @@ public final class AbilityFactory {
         String tgtWhat = mapParams.get("ValidTgts");
         final String[] commonStuff = new String[] {
                 //list of common one word non-core type ValidTgts that should be lowercase in the target prompt
-                "Player", "Opponent"
+                "Player", "Opponent", "Card"
         };
         if (Arrays.asList(commonStuff).contains(tgtWhat) || CardType.CoreType.isValidEnum(tgtWhat)) {
             tgtWhat = tgtWhat.toLowerCase();
