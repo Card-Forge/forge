59ff2280881bfe4bc3cf27b80bbc654f4465998d
Northmoc <timothy.s.mocny@gmail.com>
2022-05-22 22:26:07 -0400

    AbilityUtils: support "OrController" suffix in getDefinedPlayers()

--- forge-game/src/main/java/forge/game/ability/AbilityUtils.java
+++ forge-game/src/main/java/forge/game/ability/AbilityUtils.java
@@ -1059,7 +1059,24 @@ public class AbilityUtils {
             }
             final SpellAbility root = ((SpellAbility)sa).getRootAbility();
             Object o = null;
-            if (defParsed.endsWith("Controller")) {
+            if (defParsed.endsWith("OrController")) {
+                String triggeringType = defParsed.substring(9, defParsed.length() - 12);
+                final Object c = root.getTriggeringObject(AbilityKey.fromString(triggeringType));
+                if (c instanceof Player) {
+                    o = c;
+                }
+                if (c instanceof Card) {
+                    o = ((Card) c).getController();
+                }
+                if (c instanceof SpellAbility) {
+                    o = ((SpellAbility) c).getActivatingPlayer();
+                }
+                // For merged permanent
+                if (c instanceof CardCollection) {
+                    o = ((CardCollection) c).get(0).getController();
+                }
+
+            } else if (defParsed.endsWith("Controller")) {
                 String triggeringType = defParsed.substring(9);
                 triggeringType = triggeringType.substring(0, triggeringType.length() - 10);
                 final Object c = root.getTriggeringObject(AbilityKey.fromString(triggeringType));
