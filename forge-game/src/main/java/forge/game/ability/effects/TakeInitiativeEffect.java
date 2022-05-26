e527c2f9a687ae5b829892b756c5e6f37d16584d
Northmoc <timothy.s.mocny@gmail.com>
2022-05-25 22:18:13 -0400

    TakeInitiativeEffect remove unneeded imports

--- forge-game/src/main/java/forge/game/ability/effects/TakeInitiativeEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/TakeInitiativeEffect.java
@@ -1,15 +1,11 @@
 package forge.game.ability.effects;
 
 import java.util.List;
-import java.util.Map;
 
 import forge.game.Game;
-import forge.game.ability.AbilityKey;
 import forge.game.ability.SpellAbilityEffect;
-import forge.game.card.Card;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
-import forge.game.trigger.TriggerType;
 import org.apache.commons.lang3.StringUtils;
 
 public class TakeInitiativeEffect extends SpellAbilityEffect {
