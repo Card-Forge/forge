dd5a2f16b4f1c0c7f813912ba45225782694c7ca
Northmoc <timothy.s.mocny@gmail.com>
2022-05-25 22:01:01 -0400

    TakeInitiativeEffect.java

---
+++ forge-game/src/main/java/forge/game/ability/effects/TakeInitiativeEffect.java
@@ -0,0 +1,41 @@
+package forge.game.ability.effects;
+
+import java.util.List;
+import java.util.Map;
+
+import forge.game.Game;
+import forge.game.ability.AbilityKey;
+import forge.game.ability.SpellAbilityEffect;
+import forge.game.card.Card;
+import forge.game.player.Player;
+import forge.game.spellability.SpellAbility;
+import forge.game.trigger.TriggerType;
+import org.apache.commons.lang3.StringUtils;
+
+public class TakeInitiativeEffect extends SpellAbilityEffect {
+
+    @Override
+    protected String getStackDescription(SpellAbility sa) {
+        final StringBuilder sb = new StringBuilder();
+
+        final List<Player> tgtPlayers = getTargetPlayers(sa);
+
+        sb.append(StringUtils.join(tgtPlayers, ", "));
+        sb.append(" takes the initiative.");
+
+        return sb.toString();
+    }
+
+    @Override
+    public void resolve(SpellAbility sa) {
+        // TODO: improve ai and fix corner cases
+        final String set = sa.getHostCard().getSetCode();
+
+        for (final Player p : getTargetPlayers(sa)) {
+            final Game game = p.getGame();
+            if (!sa.usesTargeting() || p.canBeTargetedBy(sa)) {
+                game.getAction().takeInitiative(p, set);
+            }
+        }
+    }
+}
