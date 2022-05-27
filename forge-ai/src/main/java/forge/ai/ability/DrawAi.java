496ab01599dd76676817d060b7e9b8327f997e25
tool4EvEr <tool4EvEr@192.168.0.59>
2022-05-27 10:26:43 +0200

    Fix Tap trigger decisions

--- forge-ai/src/main/java/forge/ai/ability/DrawAi.java
+++ forge-ai/src/main/java/forge/ai/ability/DrawAi.java
@@ -102,7 +102,7 @@ public class DrawAi extends SpellAbilityAi {
             return false;
         }
 
-        if (!ComputerUtilCost.checkDiscardCost(ai, cost, source,sa)) {
+        if (!ComputerUtilCost.checkDiscardCost(ai, cost, source, sa)) {
             AiCostDecision aiDecisions = new AiCostDecision(ai, sa, false);
             for (final CostPart part : cost.getCostParts()) {
                 if (part instanceof CostDiscard) {
