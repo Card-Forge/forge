852142d767ce3d1cce9938e1f22f28aad4840727
Northmoc <timothy.s.mocny@gmail.com>
2022-05-27 12:58:44 -0400

    Card > keywordsToText() for Choose a Background

--- forge-game/src/main/java/forge/game/card/Card.java
+++ forge-game/src/main/java/forge/game/card/Card.java
@@ -2091,7 +2091,7 @@ public class Card extends GameEntity implements Comparable<Card>, IHasSVars {
                         || keyword.equals("Ascend") || keyword.equals("Totem armor")
                         || keyword.equals("Battle cry") || keyword.equals("Devoid") || keyword.equals("Riot")
                         || keyword.equals("Daybound") || keyword.equals("Nightbound")
-                        || keyword.equals("Friends forever")) {
+                        || keyword.equals("Friends forever") || keyword.equals("Choose a Background")) {
                     sbLong.append(keyword).append(" (").append(inst.getReminderText()).append(")");
                 } else if (keyword.startsWith("Partner:")) {
                     final String[] k = keyword.split(":");
