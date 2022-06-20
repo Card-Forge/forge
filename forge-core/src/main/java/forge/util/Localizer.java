10f1b0c62d495ebcffb037c0120f6cee596847f0
Anthony Calosa <anthonycalosa@gmail.com>
2022-06-21 02:55:44 +0800

    update Localizer

--- forge-core/src/main/java/forge/util/Localizer.java
+++ forge-core/src/main/java/forge/util/Localizer.java
@@ -77,57 +77,18 @@ public class Localizer {
         }
     }
     public String getEnglishMessage(final String key, final Object... messageArguments) {
-        MessageFormat formatter = null;
-
-        try {
-            //formatter = new MessageFormat(resourceBundle.getString(key.toLowerCase()), locale);
-            formatter = new MessageFormat(englishBundle.getString(key), Locale.ENGLISH);
-        } catch (final IllegalArgumentException | MissingResourceException e) {
-            if (!silent)
-                e.printStackTrace();
-        }
-
-        if (formatter == null && !silent) {
-            System.err.println("INVALID PROPERTY: '" + key + "' -- Translation Needed?");
-            return "INVALID PROPERTY: '" + key + "' -- Translation Needed?";
-        }
-
-        silent = false;
-
-        formatter.setLocale(Locale.ENGLISH);
-
-        String formattedMessage = "CHAR ENCODING ERROR";
-        final String[] charsets = { "ISO-8859-1", "UTF-8" };
-        //Support non-English-standard characters
-        String detectedCharset = charset(englishBundle.getString(key), charsets);
-
-        final int argLength = messageArguments.length;
-        Object[] syncEncodingMessageArguments = new Object[argLength];
-        //when messageArguments encoding not equal resourceBundle.getString(key),convert to equal
-        //avoid convert to a have two encoding content formattedMessage string.
-        for (int i = 0; i < argLength; i++) {
-            String objCharset = charset(messageArguments[i].toString(), charsets);
-            try {
-                syncEncodingMessageArguments[i] = convert(messageArguments[i].toString(), objCharset, detectedCharset);
-            } catch (UnsupportedEncodingException ignored) {
-                System.err.println("Cannot Convert '" + messageArguments[i].toString() + "' from '" + objCharset + "' To '" + detectedCharset + "'");
-                return "encoding '" + key + "' translate string failure";
-            }
-        }
-
-        try {
-            formattedMessage = new String(formatter.format(syncEncodingMessageArguments).getBytes(detectedCharset), StandardCharsets.UTF_8);
-        } catch(UnsupportedEncodingException ignored) {}
-
-        return formattedMessage;
+        return getMessage(true, key, messageArguments);
     }
     //FIXME: localizer should return default value from english locale or it will crash some GUI element like the NewGameMenu->NewGameScreen Popup when returned null...
     public String getMessage(final String key, final Object... messageArguments) {
+        return getMessage(false, key, messageArguments);
+    }
+    public String getMessage(final boolean forcedEnglish, final String key, final Object... messageArguments) {
         MessageFormat formatter = null;
 
         try {
             //formatter = new MessageFormat(resourceBundle.getString(key.toLowerCase()), locale);
-            formatter = new MessageFormat(english ? englishBundle.getString(key) : resourceBundle.getString(key), english ? Locale.ENGLISH : locale);
+            formatter = new MessageFormat(english || forcedEnglish ? englishBundle.getString(key) : resourceBundle.getString(key), english || forcedEnglish ? Locale.ENGLISH : locale);
         } catch (final IllegalArgumentException | MissingResourceException e) {
             if (!silent)
                 e.printStackTrace();
@@ -140,12 +101,12 @@ public class Localizer {
 
         silent = false;
 
-        formatter.setLocale(english ? Locale.ENGLISH : locale);
+        formatter.setLocale(english || forcedEnglish ? Locale.ENGLISH : locale);
 
         String formattedMessage = "CHAR ENCODING ERROR";
         final String[] charsets = { "ISO-8859-1", "UTF-8" };
         //Support non-English-standard characters
-        String detectedCharset = charset(english ? englishBundle.getString(key) : resourceBundle.getString(key), charsets);
+        String detectedCharset = charset(english || forcedEnglish ? englishBundle.getString(key) : resourceBundle.getString(key), charsets);
 
         final int argLength = messageArguments.length;
         Object[] syncEncodingMessageArguments = new Object[argLength];
@@ -194,7 +155,7 @@ public class Localizer {
                 englishBundle = ResourceBundle.getBundle("en-US", new Locale("en", "US"), loader);
             } catch (NullPointerException | MissingResourceException e) {
                 //If the language can't be loaded, default to US English
-                resourceBundle = ResourceBundle.getBundle("en-US", new Locale("en", "US"), loader);
+                resourceBundle = ResourceBundle.getBundle("en-US", new Locale("en_US"), loader);
                 e.printStackTrace();
             }
 
