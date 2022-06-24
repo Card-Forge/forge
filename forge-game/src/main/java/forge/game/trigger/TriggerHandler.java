8c537c2d1f79e13fab242025280880f436330be4
tool4ever <therealtoolkit@hotmail.com>
2022-06-24 05:30:00 +0200

    Fix Oni-Cult Anvil not triggering when sac itself (#774)
    Co-authored-by: TRT <>

--- forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
+++ forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
@@ -243,7 +243,8 @@ public class TriggerHandler {
                     TriggerType.Exploited.equals(t.getMode()) ||
                     TriggerType.Sacrificed.equals(t.getMode()) ||
                     TriggerType.Destroyed.equals(t.getMode()) ||
-                    (TriggerType.ChangesZone.equals(t.getMode()) && "Battlefield".equals(t.getParam("Origin")))) { // TODO needs additional logic in case origin=Any
+                    ((TriggerType.ChangesZone.equals(t.getMode()) || TriggerType.ChangesZoneAll.equals(t.getMode()))
+                            && "Battlefield".equals(t.getParam("Origin")))) { // TODO needs additional logic in case origin=Any
                 registerOneTrigger(t);
             }
         }
