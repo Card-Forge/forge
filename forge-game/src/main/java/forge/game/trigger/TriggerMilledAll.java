8cd7fda014def2e5a1a6326dfd29c0494b30490d
Northmoc <timothy.s.mocny@gmail.com>
2022-07-07 15:05:58 -0400

    TriggerMilledAll.java

---
+++ forge-game/src/main/java/forge/game/trigger/TriggerMilledAll.java
@@ -0,0 +1,71 @@
+package forge.game.trigger;
+
+import forge.game.ability.AbilityKey;
+import forge.game.card.Card;
+import forge.game.card.CardCollection;
+import forge.game.card.CardLists;
+import forge.game.spellability.SpellAbility;
+import forge.util.Localizer;
+
+import java.util.Map;
+
+/**
+ * <p>
+ * TriggerMilledAll class.
+ * </p>
+ */
+public class TriggerMilledAll extends Trigger {
+
+    /**
+     * <p>
+     * Constructor for TriggerMilledAll
+     * </p>
+     *
+     * @param params
+     *            a {@link java.util.HashMap} object.
+     * @param host
+     *            a {@link forge.game.card.Card} object.
+     * @param intrinsic
+     *            the intrinsic
+     */
+    public TriggerMilledAll (final Map<String, String> params, final Card host, final boolean intrinsic) {
+        super(params, host, intrinsic);
+    }
+
+    /** {@inheritDoc}
+     * @param runParams*/
+    @Override
+    public final boolean performTest(final Map<AbilityKey, Object> runParams) {
+
+        if (!matchesValidParam("ValidPlayer", runParams.get(AbilityKey.Player))) {
+            return false;
+        }
+        if (!matchesValidParam("ValidCard", runParams.get(AbilityKey.Cards))) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void setTriggeringObjects(final SpellAbility sa, Map<AbilityKey, Object> runParams) {
+        CardCollection cards = (CardCollection) runParams.get(AbilityKey.Cards);
+
+        if (hasParam("ValidCard")) {
+            cards = CardLists.getValidCards(cards, getParam("ValidCard"), getHostCard().getController(),
+                    getHostCard(), this);
+        }
+
+        sa.setTriggeringObject(AbilityKey.Cards, cards);
+        sa.setTriggeringObjectsFrom(runParams, AbilityKey.Player);
+    }
+
+    @Override
+    public String getImportantStackObjects(SpellAbility sa) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Localizer.getInstance().getMessage("lblPlayer")).append(": ");
+        sb.append(sa.getTriggeringObject(AbilityKey.Player));
+        return sb.toString();
+    }
+}
