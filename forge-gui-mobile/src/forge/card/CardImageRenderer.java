9b37b25a74bfd8f2c8decc8145e306a756d0056f
Anthony Calosa <anthonycalosa@gmail.com>
2022-07-26 08:18:52 +0800

    Update CardImageRenderer.java

--- forge-gui-mobile/src/forge/card/CardImageRenderer.java
+++ forge-gui-mobile/src/forge/card/CardImageRenderer.java
@@ -724,7 +724,7 @@ public class CardImageRenderer {
             return;
         }
 
-        if (image == ImageCache.defaultImage || Forge.enableUIMask.equals("Art")) { //support drawing card image manually if card image not found
+        if (image == ImageCache.getDefaultImage() || Forge.enableUIMask.equals("Art")) { //support drawing card image manually if card image not found
             drawCardImage(g, card, altState, x, y, w, h, CardStackPosition.Top, true, true);
         } else {
             float radius = (h - w)/8;
