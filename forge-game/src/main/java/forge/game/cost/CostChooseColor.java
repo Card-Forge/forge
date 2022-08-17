8a691a511422e60a916ea58df47bed08ea9c0f3e
Northmoc <timothy.s.mocny@gmail.com>
2022-08-17 15:28:20 -0400

    CostChooseColor.java

---
+++ forge-game/src/main/java/forge/game/cost/CostChooseColor.java
@@ -0,0 +1,83 @@
+/*
+ * Forge: Play Magic: the Gathering.
+ * Copyright (C) 2011  Forge Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package forge.game.cost;
+
+import forge.game.player.Player;
+import forge.game.spellability.SpellAbility;
+
+/**
+ * the class CostChooseColor
+ */
+public class CostChooseColor extends CostPart {
+
+    /**
+     * Serializables need a version ID.
+     */
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Instantiates a new cost choose color.
+     *
+     * @param amount
+     *            the amount
+     */
+    public CostChooseColor(final String amount) {
+        this.setAmount(amount);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * forge.card.cost.CostPart#canPay(forge.card.spellability.SpellAbility,
+     * forge.Card, forge.Player, forge.card.cost.Cost)
+     */
+    @Override
+    public final boolean canPay(final SpellAbility ability, final Player payer, final boolean effect) {
+        return true;
+    }
+
+    @Override
+    public boolean payAsDecided(Player payer, PaymentDecision pd, SpellAbility sa, final boolean effect) {
+        sa.getHostCard().setChosenColors(pd.colors);
+        return true;
+    }
+
+    @Override
+    public int paymentOrder() { return 8; }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see forge.card.cost.CostPart#toString()
+     */
+    @Override
+    public final String toString() {
+        final StringBuilder sb = new StringBuilder();
+        final Integer i = this.convertAmount();
+        sb.append("Choose ");
+        sb.append(Cost.convertAmountTypeToWords(i, this.getAmount(), "color"));
+        return sb.toString();
+    }
+
+    @Override
+    public <T> T accept(ICostVisitor<T> visitor) {
+        return visitor.visit(this);
+    }
+
+}
