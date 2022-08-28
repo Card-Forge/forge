a35ffbcb0fccac5a7eea6ff513a52a518fe23f3d
tool4ever <therealtoolkit@hotmail.com>
2022-08-28 05:40:44 +0200

    Rith, Liberated Primeval and support (#1409)
    * Rith, Liberated Primeval and support
    
    * Add AI hints
    
    Co-authored-by: TRT <>

--- forge-game/src/main/java/forge/game/GameAction.java
+++ forge-game/src/main/java/forge/game/GameAction.java
@@ -2419,7 +2419,7 @@ public class GameAction {
         }
 
         // for Zangief do this before runWaitingTriggers DamageDone
-        damageMap.triggerExcessDamage(isCombat, lethalDamage, game);
+        damageMap.triggerExcessDamage(isCombat, lethalDamage, game, lkiCache);
 
         // lose life simultaneously
         if (isCombat) {
