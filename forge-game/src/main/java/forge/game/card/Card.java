3c31d95710b25df8c678176fe7cc08569df32c11
Simisays <67333662+Simisays@users.noreply.github.com>
2022-09-02 23:11:35 +0200

    Update Card.java

--- forge-game/src/main/java/forge/game/card/Card.java
+++ forge-game/src/main/java/forge/game/card/Card.java
@@ -5312,15 +5312,12 @@ public class Card extends GameEntity implements Comparable<Card>, IHasSVars {
         return getType().sharesCreaturetypeWith(c1.getType());
     }
     public final boolean sharesPowerWith(final int n) {
-        //need to get GameState for Discarded Cards
         final Card source = game.getCardState(this);
 
-        //do not check for SplitCard anymore
         return source.getNetPower() == n;
     }
 
     public final boolean sharesPowerWith(final Card c1) {
-        //need to get GameState for Discarded Cards
         final Card source = game.getCardState(this);
         final Card other = game.getCardState(c1);
 
