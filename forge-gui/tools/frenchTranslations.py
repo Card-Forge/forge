2e2eb77a72304e8543763b2346d57d67f6d58115
add-le <add.lelann@pm.me>
2022-09-04 16:01:12 +0200

    add manual scrap on the script

--- forge-gui/tools/frenchTranslations.py
+++ forge-gui/tools/frenchTranslations.py
@@ -3,6 +3,8 @@
 
 __author__ = 'add-le'
 
+import sys
+
 import requests
 import re
 
@@ -23,11 +25,41 @@ def convertMana(cardInfo: str) -> str:
 	cardInfo = cardInfo.replace('<br />', '\\n')
 
 	#Replace all correspondances of [0-9A-Z]
-	symbol = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20']
+	symbol = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '100', '(', ')']
 	for sym in symbol:
 		cardInfo = cardInfo.replace("<img src='/img/symbole/mana/" + sym + ".png' />", '{' + sym + '}')
 
 	return cardInfo
+
+#Manuel scrap
+if sys.argv[1] == '-u' or sys.argv[1] == '-url':
+	if len(sys.argv) <= 2:
+		print('Missing second arg : url to scrap')
+		exit(1)
+
+	output = open('cardnames-fr-FR-missing.txt', 'a')
+
+	response = requests.get(sys.argv[2])
+
+	#Founded
+	if response.status_code == SUCCESS_STATUS:
+		#Get brut chaos the data from web page
+		try:
+			match = re.findall(r'<div class="text_card text_fr txt_fr_right"><div class="type">.+?<\/div><div class="clear"><\/div><div class="cout (1|hide)">.+?<\/div><div class="clear"><\/div><div class="txt">.+?<\/div>(<div class="forc_end">|<\/div>)', response.content.decode('utf-8').replace('\n', ''))[0]
+
+			#Split in different tags, interesting data
+			translatedName: str = re.findall(r'<title>.+?<\/title>', response.content.decode('utf-8').replace('\n', ''))[0].split('<title>')[1].split(' -')[0]
+			orignalName: str = re.findall(r'<title>.+?<\/title>', response.content.decode('utf-8').replace('\n', ''))[0].split('<title>')[1].split(' -')[1].split(' -')[0]
+			translatedType: str = match.split('<div class="text_card text_fr txt_fr_right"><div class="type">')[1].split('</div>')[0]
+			translatedInfo: str = convertMana(match.split('<div class="txt">')[1].split('</div>')[0])
+
+			#print(orignalName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo)
+			output.writelines(orignalName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo + '\n')
+		except IndexError:
+			#Request probably gives multiple value
+			print(response.url)
+	output.close()
+	exit(0)
 
 output = open('cardnames-fr-FR-missing.txt', 'w')
 
@@ -40,23 +72,22 @@ for line in lines:
 
 		#Founded
 		if response.url.endswith(engName) == False and response.status_code == SUCCESS_STATUS:
-			print(counter, end='\r', flush=True)
+			#print(counter, end='\r', flush=True)
 
 			#Get brut chaos the data from web page
 			try:
-				match = re.findall(r'<div class="text_card text_fr txt_fr_right"><div class="type">.+?<\/div><div class="clear"><\/div><div class="cout 1">.+?<\/div><div class="clear"><\/div><div class="txt">.+?<\/div>[<div class="forc_end">|<\/div>]', response.content.decode('utf-8').replace('\n', ''))[0]
+				match = re.findall(r'<div class="text_card text_fr txt_fr_right"><div class="type">.+?<\/div><div class="clear"><\/div><div class="cout (1|hide)">.+?<\/div><div class="clear"><\/div><div class="txt">.+?<\/div>(<div class="forc_end">|<\/div>)', response.content.decode('utf-8').replace('\n', ''))[0]
+
+				#Split in different tags, interesting data
+				translatedName: str = re.findall(r'<title>.+?<\/title>', response.content.decode('utf-8').replace('\n', ''))[0].split('<title>')[1].split(' -')[0]
+				translatedType: str = match.split('<div class="text_card text_fr txt_fr_right"><div class="type">')[1].split('</div>')[0]
+				translatedInfo: str = convertMana(match.split('<div class="txt">')[1].split('</div>')[0])
+
+				#print(engName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo)
+				output.writelines(engName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo + '\n')
 			except IndexError:
 				#Request probably gives multiple value
 				print(response.url)
-
-			#Split in different tags, interesting data
-			translatedName: str = re.findall(r'<title>.+?<\/title>', response.content.decode('utf-8').replace('\n', ''))[0].split('<title>')[1].split(' -')[0]
-			translatedType: str = match.split('<div class="text_card text_fr txt_fr_right"><div class="type">')[1].split('</div>')[0]
-			translatedInfo: str = convertMana(match.split('<div class="txt">')[1].split('</div>')[0])
-
-			#print(engName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo)
-			output.writelines(engName + '|' + translatedName + '|' + translatedType.replace('&nbsp;', '') + '|' + translatedInfo + '\n')
-
 		counter += 1
 
 
