9904f4d18d8b01bfac6f8ca3bb3b7a3efbf39923
Northmoc <timothy.s.mocny@gmail.com>
2022-09-17 22:40:02 -0500

    TriggerManaAdded.java

---
+++ forge-game/src/main/java/forge/game/trigger/TriggerManaAdded.java
@@ -0,0 +1,96 @@
+/*
+ * Forge: Play Magic: the Gathering.
+ * Copyright (C) 2011  Forge Team
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package forge.game.trigger;
+
+import forge.card.MagicColor;
+import forge.game.ability.AbilityKey;
+import forge.game.card.Card;
+import forge.game.spellability.SpellAbility;
+import forge.util.Localizer;
+
+import java.util.Map;
+
+import static forge.util.TextUtil.toManaString;
+
+/**
+ * <p>
+ * Trigger_ManaAdded class.
+ * </p>
+ *
+ * @author Forge
+ * @version $Id$
+ */
+public class TriggerManaAdded extends Trigger {
+
+    /**
+     * <p>
+     * Constructor for Trigger_TapsForMana.
+     * </p>
+     *
+     * @param params
+     *            a {@link java.util.HashMap} object.
+     * @param host
+     *            a {@link forge.game.card.Card} object.
+     * @param intrinsic
+     *            the intrinsic
+     */
+    public TriggerManaAdded(final Map<String, String> params, final Card host, final boolean intrinsic) {
+        super(params, host, intrinsic);
+    }
+
+    /** {@inheritDoc}
+     * @param runParams*/
+    @Override
+    public final boolean performTest(final Map<AbilityKey, Object> runParams) {
+
+        if (!matchesValidParam("ValidSource", runParams.get(AbilityKey.Card))) {
+            return false;
+        }
+        if (!matchesValidParam("Player", runParams.get(AbilityKey.Player))) {
+            return false;
+        }
+
+        if (hasParam("Produced")) {
+            Object prod = runParams.get(AbilityKey.Produced);
+            if (!(prod instanceof String)) {
+                return false;
+            }
+            String produced = (String) prod;
+            if ("ChosenColor".equals(getParam("Produced"))) {
+                if (!this.getHostCard().hasChosenColor() || !produced.contains(MagicColor.toShortString(this.getHostCard().getChosenColor()))) {
+                    return false;
+                }
+            } else if (!produced.contains(MagicColor.toShortString(this.getParam("Produced")))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /** {@inheritDoc} */
+    @Override
+    public final void setTriggeringObjects(final SpellAbility sa, Map<AbilityKey, Object> runParams) {
+        sa.setTriggeringObjectsFrom(runParams, AbilityKey.Card, AbilityKey.Player, AbilityKey.Produced);
+    }
+
+    @Override
+    public String getImportantStackObjects(SpellAbility sa) {
+        return Localizer.getInstance().getMessage("lblProduced") + ": " +
+                toManaString(sa.getTriggeringObject(AbilityKey.Produced).toString());
+    }
+}
