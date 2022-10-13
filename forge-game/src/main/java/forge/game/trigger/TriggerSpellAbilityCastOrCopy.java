03e4892692a9ed154192f036913525b1062ef75d
Northmoc <timothy.s.mocny@gmail.com>
2022-10-13 12:19:25 -0400

    arcee_sharpshooter_arcee_acrobatic_coupe.txt

--- forge-game/src/main/java/forge/game/trigger/TriggerSpellAbilityCastOrCopy.java
+++ forge-game/src/main/java/forge/game/trigger/TriggerSpellAbilityCastOrCopy.java
@@ -284,6 +284,7 @@ public class TriggerSpellAbilityCastOrCopy extends Trigger {
             sa.setTriggeringObject(AbilityKey.SpellAbilityTarget, saForTargets.getTargets().get(0));
         }
         sa.setTriggeringObject(AbilityKey.SpellAbilityTargetingCards, saForTargets.getTargets().getTargetCards());
+        sa.setTriggeringObject(AbilityKey.SpellAbilityNumTargets, saForTargets.getTargets().size());
         sa.setTriggeringObjectsFrom(
                 runParams,
             AbilityKey.Player,
