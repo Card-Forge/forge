bd1b31a904c60899af82eea56337452cdc246ec3
asvitkine <alexei.svitkine@gmail.com>
2022-11-02 11:07:06 -0400

    Improve AI logic for cavern of souls.
    Improve AI logic for cavern of souls.
    
    - Don't count token creature types when looking for most prominent creature type, since cavern of souls doesn't help with them.
    - Weigh cards in hand and commanders double, as they are more likely to be cast.
    
    I tested with a Tiny Leaders deck running Geist of Saint Traft that didn't have any creature type synergy and confirmed that the changes make the AI choose Spirit for Geist, rather than Human as before.

--- forge-ai/src/main/java/forge/ai/ComputerUtil.java
+++ forge-ai/src/main/java/forge/ai/ComputerUtil.java
@@ -2378,8 +2378,9 @@ public class ComputerUtil {
                         chosen = ComputerUtilCard.getMostProminentType(list, valid);
                     }
                 }
-                else if (logic.equals("MostProminentInComputerDeck")) {
-                    chosen = ComputerUtilCard.getMostProminentType(ai.getAllCards(), valid);
+                else if (logic.equals("MostProminentInComputerDeck") || logic.equals("MostProminentInComputerDeckNonToken")) {
+                    boolean includeTokens = logic.equals("MostProminentInComputerDeck");
+                    chosen = ComputerUtilCard.getMostProminentType(ai.getAllCards(), valid, includeTokens);
                 }
                 else if (logic.equals("MostProminentInComputerGraveyard")) {
                     chosen = ComputerUtilCard.getMostProminentType(ai.getCardsIn(ZoneType.Graveyard), valid);
