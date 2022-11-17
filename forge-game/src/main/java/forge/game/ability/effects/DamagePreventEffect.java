d0249a96072c0a918f9c9f72a1164974ddc27a71
Northmoc <timothy.s.mocny@gmail.com>
2022-11-17 10:38:51 -0500

    remove unneeded import

--- forge-game/src/main/java/forge/game/ability/effects/DamagePreventEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/DamagePreventEffect.java
@@ -13,7 +13,6 @@ import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
 import forge.game.zone.ZoneType;
 import forge.util.Aggregates;
-import forge.util.Localizer;
 import forge.util.collect.FCollection;
 
 public class DamagePreventEffect extends DamagePreventEffectBase {
