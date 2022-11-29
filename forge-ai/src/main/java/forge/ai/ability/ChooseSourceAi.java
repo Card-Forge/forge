24b1e9de314fd237b0e25dcc21cedb2d85cfecff
TRT <>
2022-11-29 20:19:31 +0100

    Fix imports

--- forge-ai/src/main/java/forge/ai/ability/ChooseSourceAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ChooseSourceAi.java
@@ -6,7 +6,6 @@ import java.util.Map;
 import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 
 import forge.ai.AiAttackController;
 import forge.ai.ComputerUtilCard;
