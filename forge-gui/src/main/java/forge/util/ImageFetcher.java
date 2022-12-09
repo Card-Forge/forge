b48da28f7f44e9d85898483d3b34bf72dcd5bb71
paulsnoops <paulhammerton1@hotmail.co.uk>
2022-12-09 15:36:56 +0000

    return_comment

--- forge-gui/src/main/java/forge/util/ImageFetcher.java
+++ forge-gui/src/main/java/forge/util/ImageFetcher.java
@@ -67,6 +67,7 @@ public abstract class ImageFetcher {
             }
             return null;
         } else {
+            // 1. Try MCI code first, as it original.
             String setCode = edition.getScryfallCode();
             String langCode = edition.getCardsLangCode();
             return ForgeConstants.URL_PIC_SCRYFALL_DOWNLOAD +
