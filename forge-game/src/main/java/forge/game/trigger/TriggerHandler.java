3d53786f491dcff54b07ba5ce5dc340e92261314
tool4EvEr <tool4EvEr@192.168.0.59>
2023-02-12 23:21:20 +0100

    fixes phasing affecting delayed triggers

--- forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
+++ forge-game/src/main/java/forge/game/trigger/TriggerHandler.java
@@ -394,7 +394,8 @@ public class TriggerHandler {
             return false; // Trigger removed by effect
         }
 
-        if (!regtrig.zonesCheck(game.getZoneOf(regtrig.getHostCard()))) {
+        // do not check delayed
+        if (regtrig.getSpawningAbility() == null && !regtrig.zonesCheck(game.getZoneOf(regtrig.getHostCard()))) {
             return false; // Host card isn't where it needs to be.
         }
 
