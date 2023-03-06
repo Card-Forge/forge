6bca3c1d2d904707ea9a6ee61cf1288ce9114ca3
pfirpfel <michaelkuenzli@gmail.com>
2023-03-06 16:50:01 +0100

    Add new rarity slot used by Alchemy sets

--- forge-core/src/main/java/forge/item/generation/BoosterSlots.java
+++ forge-core/src/main/java/forge/item/generation/BoosterSlots.java
@@ -6,6 +6,7 @@ public class BoosterSlots {
     public static final String COMMON = "Common";
     public static final String UNCOMMON = "Uncommon";
     public static final String UNCOMMON_RARE = "UncommonRare";
+    public static final String UNCOMMON_RARE_MYTHIC = "UncommonRareMythic";
     public static final String RARE = "Rare";
     public static final String RARE_MYTHIC = "RareMythic";
     public static final String MYTHIC = "Mythic";
