23734b2f79492eb09e97c1be921958684668c7e8
Alwayssnarky <yannifirewind@gmail.com>
2023-03-18 16:21:31 -0400

    Tweaks to Tiles
    Modifies phyrexiantiles.png to standardize tile grid, edits phyreixan_w1 and tiles.tsx to accommodate.

--- forge-gui/res/adventure/Shandalar/maps/tileset/phyrexiantiles.tsx
+++ forge-gui/res/adventure/Shandalar/maps/tileset/phyrexiantiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="phyrexiantiles" tilewidth="16" tileheight="16" tilecount="54" columns="9">
- <image source="../tileset/phyrexiantiles.png" width="144" height="96"/>
+<tileset version="1.9" tiledversion="1.9.2" name="phyrexiantiles" tilewidth="16" tileheight="16" tilecount="66" columns="11">
+ <image source="phyrexiantiles.png" width="176" height="96"/>
 </tileset>
