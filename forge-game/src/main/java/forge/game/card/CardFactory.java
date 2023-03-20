af7179f73e1bd7448f96e73a34fd881e00053fdb
Hans Mackowiak <hanmac@gmx.de>
2023-03-20 12:24:48 +0100

    Update CardFactory.java
    Set PT should also be done for vehicle

--- forge-game/src/main/java/forge/game/card/CardFactory.java
+++ forge-game/src/main/java/forge/game/card/CardFactory.java
@@ -764,7 +764,7 @@ public class CardFactory {
                 state.removeIntrinsicKeyword(kw);
             }
 
-            if (state.getType().isCreature()) {
+            if (state.getType().isCreature() || state.getType().hasSubtype("Vehicle")) {
                 if (sa.hasParam("SetPower")) {
                     state.setBasePower(AbilityUtils.calculateAmount(sa.getHostCard(), sa.getParam("SetPower"), sa));
                 }
