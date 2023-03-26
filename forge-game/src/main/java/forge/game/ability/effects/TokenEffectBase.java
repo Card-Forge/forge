2a0c36eaa13533356f2021a03cda4f81b1fafc39
Anthony Calosa <anthonycalosa@gmail.com>
2023-03-26 10:45:13 +0800

    Update TokenEffectBase.java
    fix ConcurrentModificationException (anointed procession and divine visitation in play, then cast benevolent offering, then choose replacement effect divine visitation, hit ok)

--- forge-game/src/main/java/forge/game/ability/effects/TokenEffectBase.java
+++ forge-game/src/main/java/forge/game/ability/effects/TokenEffectBase.java
@@ -78,7 +78,7 @@ public abstract class TokenEffectBase extends SpellAbilityEffect {
 
         // support PlayerCollection for affected
         Set<Player> toRemove = Sets.newHashSet();
-        for (Player p : tokenTable.rowKeySet()) {
+        for (Player p : Sets.newHashSet(tokenTable.rowKeySet())) {
             final Map<AbilityKey, Object> repParams = AbilityKey.mapFromAffected(p);
             repParams.put(AbilityKey.Token, tokenTable);
             repParams.put(AbilityKey.EffectOnly, true); // currently only effects can create tokens?
