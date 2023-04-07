242b4abc723fc5c7cbe1d1dcf3108913f7c5d739
Benjamin Perry <git@gapid.com>
2023-04-07 19:26:35 -0400

    Dynamically-updating search post fix

--- forge-gui-mobile/src/forge/adventure/stage/MapSprite.java
+++ forge-gui-mobile/src/forge/adventure/stage/MapSprite.java
@@ -8,6 +8,7 @@ import com.badlogic.gdx.utils.Array;
 import com.github.tommyettinger.textra.TextraLabel;
 import forge.adventure.data.BiomeSpriteData;
 import forge.adventure.pointofintrest.PointOfInterest;
+import forge.adventure.pointofintrest.PointOfInterestChanges;
 import forge.adventure.util.Controls;
 import forge.adventure.world.WorldSave;
 import org.apache.commons.lang3.tuple.Pair;
@@ -23,23 +24,19 @@ public class MapSprite extends Actor {
     static public int SpriteLayer = 0;
     TextureRegion texture;
     TextraLabel searchPost = Controls.newTextraLabel("[%80][+SearchPost]");
-    boolean isCaveDungeon, isOldorVisited;
+    boolean isCaveDungeon;
+
+    PointOfInterest point;
 
     public MapSprite(Vector2 pos, TextureRegion sprite, PointOfInterest point) {
         if (point != null) {
             isCaveDungeon = "cave".equalsIgnoreCase(point.getData().type) || "dungeon".equalsIgnoreCase(point.getData().type);
-            if (point.getData().map != null && point.getID() != null) {
-                isOldorVisited = WorldSave.getCurrentSave().getPointOfInterestChanges(point.getID() + point.getData().map).hasDeletedObjects();
-            }
         }
+        this.point = point;
         texture = sprite;
         setPosition(pos.x, pos.y);
         setHeight(texture.getRegionHeight());
         setWidth(texture.getRegionWidth());
-    }
-
-    public void checkOut() {
-        isOldorVisited = true;
     }
 
     public static Array<Actor> GetMapSprites(int chunkX, int chunkY) {
@@ -53,13 +50,7 @@ public class MapSprite extends Actor {
 
         List<Pair<Vector2, Integer>> objects = WorldSave.getCurrentSave().getWorld().GetMapObjects(chunkX, chunkY);
 
-        for (Pair<Vector2, Integer> entry : objects) {
-            BiomeSpriteData data = WorldSave.getCurrentSave().getWorld().getObject(entry.getValue());
-            if (data.layer != SpriteLayer)
-                continue;
-            Actor sprite = new MapSprite(entry.getKey(), WorldSave.getCurrentSave().getWorld().getData().GetBiomeSprites().getSprite(data.name, (int) entry.getKey().x + (int) entry.getKey().y * 11483), null);
-            actorGroup.add(sprite);
-        }
+        actorGroup.addAll(GetMapSpritesBackground(chunkX, chunkY));
         return actorGroup;
     }
 
@@ -82,9 +73,12 @@ public class MapSprite extends Actor {
         if (texture == null)
             return;
         batch.draw(texture, getX(), getY());
-        if (isCaveDungeon && !isOldorVisited) {
-            searchPost.setPosition(getX() - 7, getY() + 7);
-            searchPost.draw(batch, parentAlpha);
+        if (isCaveDungeon && point.getData().map != null && point.getID() != null){
+            PointOfInterestChanges changes = WorldSave.getCurrentSave().getPointOfInterestChanges(point.getID() + point.getData().map);
+            if (!changes.hasDeletedObjects()) {
+                searchPost.setPosition(getX() - 7, getY() + 7);
+                searchPost.draw(batch, parentAlpha);
+            }
         }
         //font.draw(batch,String.valueOf(getZIndex()),getX()-(getWidth()/2),getY());
     }
