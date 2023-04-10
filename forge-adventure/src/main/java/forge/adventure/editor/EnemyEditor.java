2e7b663bd5c1ed37cc7fbcdd16e2f527739ae609
jjayers99 <56438137+jjayers99@users.noreply.github.com>
2023-04-10 06:56:57 -0400

    More Adventure import fixes

--- forge-adventure/src/main/java/forge/adventure/editor/EnemyEditor.java
+++ forge-adventure/src/main/java/forge/adventure/editor/EnemyEditor.java
@@ -9,10 +9,7 @@ import forge.adventure.util.Config;
 import forge.adventure.util.Paths;
 
 import javax.swing.*;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
--- forge-adventure/src/main/java/forge/adventure/editor/TextListEdit.java
+++ forge-adventure/src/main/java/forge/adventure/editor/TextListEdit.java
@@ -2,8 +2,6 @@ package forge.adventure.editor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
 /**
