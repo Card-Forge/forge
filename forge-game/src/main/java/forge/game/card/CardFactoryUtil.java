a1051b3f3ec9c2e2946cd57a1a2e312e2df9e2a7
Northmoc <timothy.s.mocny@gmail.com>
2023-04-11 19:49:19 -0400

    more battle stuff

--- forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
+++ forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
@@ -3932,7 +3932,7 @@ public class CardFactoryUtil {
         StringBuilder chooseSB = new StringBuilder();
         chooseSB.append("Event$ Moved | ValidCard$ Card.Self | Destination$ Battlefield | ReplacementResult$ Updated");
         chooseSB.append(" | Description$ (As a Siege enters the battlefield, choose an opponent to protect it. You and others can attack it. When it's defeated, exile it, then cast it transformed.)");
-        String chooseProtector = "DB$ ChoosePlayer | Defined$ You | Choices$ Player.Opponent | Protect$ True | ChoiceTitle$ Choose an opponent to protect this battle | AILogic$ Curse";
+        String chooseProtector = "DB$ ChoosePlayer | Defined$ You | Choices$ Opponent | Protect$ True | ChoiceTitle$ Choose an opponent to protect this battle | AILogic$ Curse";
 
         ReplacementEffect re = ReplacementHandler.parseReplacement(chooseSB.toString(), card, true);
         re.setOverridingAbility(AbilityFactory.getAbility(chooseProtector, card));
@@ -3944,7 +3944,7 @@ public class CardFactoryUtil {
         triggerDefeated.append(" TriggerDescription$ When CARDNAME is defeated, exile it, then cast it transformed.");
 
         String castExileBattle = "DB$ ChangeZone | Defined$ Self | Origin$ Battlefield | Destination$ Exile | RememberChanged$ True";
-        String castDefeatedBattle = "DB$ Play | Defined$ Remembered | WithoutManaCost$ True | CastTransformed$ True";
+        String castDefeatedBattle = "DB$ Play | Defined$ Remembered | CastTransformed$ True";
 
         Trigger defeatedTrigger = TriggerHandler.parseTrigger(triggerDefeated.toString(), card, true);
         SpellAbility exileAbility = AbilityFactory.getAbility(castExileBattle, card);
