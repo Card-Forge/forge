8b359d8b5ca40c52872c32fb0920dd9740a9a378
Anthony Calosa <anthonycalosa@gmail.com>
2023-04-12 18:10:52 +0800

    update navigation arrow

--- forge-gui-mobile/src/forge/adventure/util/NavArrowActor.java
+++ forge-gui-mobile/src/forge/adventure/util/NavArrowActor.java
@@ -1,31 +1,43 @@
 package forge.adventure.util;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.utils.Array;
 
 public class NavArrowActor extends Actor {
 
-    Sprite texture;
     public float navTargetAngle = 0.0f;
+    private Animation<TextureRegion> currentAnimation;
+    float timer;
 
     public NavArrowActor() {
         //TODO: Expand compass sprite to have color coded arrows, swap sprites based on distance to target
-        Array<Sprite> textureAtlas = Config.instance().getAtlas("maps/tileset/compass.atlas").createSprites("compass");
+        Array<Sprite> textureAtlas = Config.instance().getAtlas("maps/tileset/compass.atlas").createSprites();
         if (textureAtlas.isEmpty()) {
             System.out.print("NavArrow sprite not found");
         }
-        texture = textureAtlas.get(0);
-        setHeight(texture.getRegionHeight());
-        setWidth(texture.getRegionWidth());
+        currentAnimation = new Animation<>(0.6f, textureAtlas);
+    }
+
+    @Override
+    public void act(float delta) {
+        timer += delta;
+        super.act(delta);
     }
 
     @Override
     public void draw(Batch batch, float parentAlpha) {
-        if (texture == null)
+        if (currentAnimation == null)
             return;
+        TextureRegion currentFrame = currentAnimation.getKeyFrame(timer, true);
+        setHeight(currentFrame.getRegionHeight());
+        setWidth(currentFrame.getRegionWidth());
+
+
         //TODO: Simplify params somehow for readability? All this does is spin the image around the player.
-        batch.draw(texture, getX()-texture.getWidth()/2, getY()-texture.getHeight()/2 ,(texture.getWidth()*texture.getScaleX()/2),(texture.getHeight()*texture.getScaleY()/2), texture.getWidth(), texture.getHeight(), texture.getScaleX(), texture.getScaleY(), navTargetAngle);
+        batch.draw(currentFrame, getX()-currentFrame.getRegionWidth()/2, getY()-currentFrame.getRegionHeight()/2 ,(currentFrame.getRegionWidth()*0.5f),(currentFrame.getRegionHeight()*0.5f), currentFrame.getRegionWidth(), currentFrame.getRegionHeight(), 1, 1, navTargetAngle);
     }
 }
