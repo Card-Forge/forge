8f1100dff3d496772b03fa32037cd9c1876f1b29
Hans Mackowiak <hanmac@gmx.de>
2023-06-09 11:56:46 +0200

    Amass type (#3222)
    * AmassEffect: add Type parameter
    
    * fix AmassAi
    
    * update editions tokenscripts
    
    * Update AmassEffect.java
    
    early return if no Army could be created

--- forge-game/src/main/java/forge/game/ability/effects/AmassEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/AmassEffect.java
@@ -5,8 +5,10 @@ import java.util.Map;
 import org.apache.commons.lang3.mutable.MutableBoolean;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
+import forge.card.CardType;
 import forge.game.Game;
 import forge.game.GameEntityCounterTable;
 import forge.game.ability.AbilityUtils;
@@ -17,6 +19,7 @@ import forge.game.card.CardPredicates;
 import forge.game.card.CardZoneTable;
 import forge.game.card.CounterEnumType;
 import forge.game.card.CounterType;
+import forge.game.card.token.TokenInfo;
 import forge.game.event.GameEventCombatChanged;
 import forge.game.event.GameEventTokenCreated;
 import forge.game.player.Player;
@@ -33,12 +36,14 @@ public class AmassEffect extends TokenEffectBase {
         final StringBuilder sb = new StringBuilder("Amass ");
         final Card card = sa.getHostCard();
         final int amount = AbilityUtils.calculateAmount(card, sa.getParamOrDefault("Num", "1"), sa);
+        final String type = sa.getParam("Type");
 
-        sb.append(amount).append(" (Put ");
+        sb.append(CardType.getPluralType(type)).append(" ").append(amount).append(" (Put ");
 
         sb.append(Lang.nounWithNumeral(amount, "+1/+1 counter"));
 
-        sb.append("on an Army you control. If you don't control one, create a 0/0 black Zombie Army creature token first.)");
+        // TODO fix reminder after CR
+        sb.append("on an Army you control. If you don't control one, create a 0/0 black " + type + " Army creature token first.)");
 
         return sb.toString();
     }
@@ -52,13 +57,20 @@ public class AmassEffect extends TokenEffectBase {
 
         final int amount = AbilityUtils.calculateAmount(card, sa.getParamOrDefault("Num", "1"), sa);
         final boolean remember = sa.hasParam("RememberAmass");
+        final String type = sa.getParam("Type");
 
         // create army token if needed
         if (!Iterables.any(activator.getCardsIn(ZoneType.Battlefield), CardPredicates.isType("Army"))) {
             CardZoneTable triggerList = new CardZoneTable();
             MutableBoolean combatChanged = new MutableBoolean(false);
 
-            makeTokenTable(makeTokenTableInternal(activator, "b_0_0_zombie_army", 1, sa), false, triggerList, combatChanged, sa);
+            final Card result = TokenInfo.getProtoType("b_0_0_army", sa, activator, false);
+            // need to alter the token to add the Type from the Parameter
+            result.setCreatureTypes(Lists.newArrayList(type, "Army"));
+            result.setName(type + " Army Token");
+            result.setTokenSpawningAbility(sa);
+
+            makeTokenTable(makeTokenTableInternal(activator, result, 1), false, triggerList, combatChanged, sa);
 
             triggerList.triggerChangesZoneAll(game, sa);
             triggerList.clear();
@@ -78,15 +90,22 @@ public class AmassEffect extends TokenEffectBase {
         CardCollectionView tgtCards = CardLists.getType(activator.getCardsIn(ZoneType.Battlefield), "Army");
         tgtCards = pc.chooseCardsForEffect(tgtCards, sa, Localizer.getInstance().getMessage("lblChooseAnArmy"), 1, 1, false, params);
 
+        if (tgtCards.isEmpty()) {
+            return;
+        }
         GameEntityCounterTable table = new GameEntityCounterTable();
         for (final Card tgtCard : tgtCards) {
             tgtCard.addCounter(CounterEnumType.P1P1, amount, activator, table);
-
             if (remember) {
                 card.addRemembered(tgtCard);
             }
         }
         table.replaceCounterEffect(game, sa, true);
+        // change type after counters
+        long ts = game.getNextTimestamp();
+        for (final Card tgtCard : tgtCards) {
+            tgtCard.addChangedCardTypes(CardType.parse(type, true), null, false, false, false, false, false, false, false, false, ts, 0, true, false);
+        }
     }
 
 }
