8fa3878430e1d0ff51ff0c1005dca9a81fc73f46
Agetian <stavdev@mail.ru>
2023-06-11 22:14:41 +0300

    - Fix imports.

--- forge-ai/src/main/java/forge/ai/ability/PeekAndRevealAi.java
+++ forge-ai/src/main/java/forge/ai/ability/PeekAndRevealAi.java
@@ -1,10 +1,6 @@
 package forge.ai.ability;
 
-import java.util.Map;
-import java.util.Set;
-
 import forge.ai.AiAttackController;
-import forge.ai.AiCardMemory;
 import forge.ai.SpellAbilityAi;
 import forge.ai.SpellApiToAi;
 import forge.game.card.Card;
@@ -17,6 +13,8 @@ import forge.game.spellability.AbilityStatic;
 import forge.game.spellability.AbilitySub;
 import forge.game.spellability.SpellAbility;
 import forge.game.zone.ZoneType;
+
+import java.util.Map;
 
 /** 
  * TODO: Write javadoc for this type.
