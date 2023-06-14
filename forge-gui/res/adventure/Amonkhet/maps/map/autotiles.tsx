c911150ee3ba655062c1383cca60a8c301a63b18
Rob Schnautz <schnautzr@yahoo.com>
2023-06-14 11:29:42 -0500

    Adding Amonkhet to the project. To access, you'll need to change \adventure\settings.json to say "Amonkhet" instead of "Shandalar". When finished, remember to change it back again.

---
+++ forge-gui/res/adventure/Amonkhet/maps/map/autotiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="autotiles" tilewidth="16" tileheight="16" tilecount="432" columns="12">
+ <image source="../../../Amonkhet/world/terrain/autotiles.png" width="192" height="576"/>
+</tileset>
