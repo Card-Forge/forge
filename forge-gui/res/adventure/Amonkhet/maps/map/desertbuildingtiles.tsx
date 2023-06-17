cfaa3cab505c0cc67729a55fce889e974b179c7a
Rob Schnautz <schnautzr@yahoo.com>
2023-06-16 23:14:25 -0500

    Add warrior, started work on Amonkhet gym. 8x8 desert building tiles need remade as 16x16.

---
+++ forge-gui/res/adventure/Amonkhet/maps/map/desertbuildingtiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="desertbuildingtiles" tilewidth="8" tileheight="8" tilecount="1036" columns="28" tilerendersize="grid">
+ <image source="../tileset/desertbuildingtiles.png" width="224" height="296"/>
+</tileset>
