49507b9b3b11902e6d28103c29118411cdd58794
Anthony Calosa <anthonycalosa@gmail.com>
2023-06-29 19:51:25 +0800

    add quest button on MapViewScene -to scroll available quests

--- forge-gui-mobile/src/forge/adventure/scene/MapViewScene.java
+++ forge-gui-mobile/src/forge/adventure/scene/MapViewScene.java
@@ -6,7 +6,9 @@ import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.github.tommyettinger.textra.TextraButton;
 import com.github.tommyettinger.textra.TypingLabel;
+import com.google.common.collect.Lists;
 import forge.Forge;
 import forge.adventure.data.AdventureQuestData;
 import forge.adventure.pointofintrest.PointOfInterest;
@@ -15,6 +17,8 @@ import forge.adventure.stage.WorldStage;
 import forge.adventure.util.Controls;
 import forge.adventure.util.Current;
 import forge.adventure.world.WorldSave;
+
+import java.util.List;
 
 /**
  * Displays the rewards of a fight or a treasure
@@ -26,6 +30,9 @@ public class MapViewScene extends UIScene {
     private Texture miniMapTexture;
     private final Image miniMapPlayer;
     private final Group table;
+    private final List<TypingLabel> labels;
+    private int index = -1;
+    private float avatarX = 0, avatarY = 0;
 
     public static MapViewScene instance() {
         if (object == null)
@@ -36,7 +43,9 @@ public class MapViewScene extends UIScene {
     private MapViewScene() {
         super(Forge.isLandscapeMode() ? "ui/map.json" : "ui/map_portrait.json");
         ui.onButtonPress("done", this::done);
+        ui.onButtonPress("quest", this::scroll);
         scroll = ui.findActor("map");
+        labels = Lists.newArrayList();
         table = new Group();
         scroll.setActor(table);
         img = new Image();
@@ -52,7 +61,22 @@ public class MapViewScene extends UIScene {
             if (a instanceof TypingLabel)
                 a.remove();
         }
+        labels.clear();
         Forge.switchToLast();
+        return true;
+    }
+
+    public boolean scroll() {
+        if (!labels.isEmpty()) {
+            index++;
+            if (index >= labels.size()) {
+                index = -1;
+                scroll.scrollTo(avatarX, avatarY, miniMapPlayer.getWidth(), miniMapPlayer.getHeight(), true, true);
+                return true;
+            }
+            TypingLabel label = labels.get(index);
+            scroll.scrollTo(label.getX(), label.getY(), miniMapPlayer.getWidth(), miniMapPlayer.getHeight(), true, true);
+        }
         return true;
     }
 
@@ -66,21 +90,24 @@ public class MapViewScene extends UIScene {
         img.setDrawable(new TextureRegionDrawable(miniMapTexture));
         miniMapPlayer.setDrawable(new TextureRegionDrawable(Current.player().avatar()));
         miniMapPlayer.setSize(Current.player().avatar().getRegionWidth(), Current.player().avatar().getRegionHeight());
-        float avatarX = getMapX(WorldStage.getInstance().getPlayerSprite().getX()) - miniMapPlayer.getWidth() / 2;
-        float avatarY = getMapY(WorldStage.getInstance().getPlayerSprite().getY()) - miniMapPlayer.getHeight() / 2;
+        avatarX = getMapX(WorldStage.getInstance().getPlayerSprite().getX()) - miniMapPlayer.getWidth() / 2;
+        avatarY = getMapY(WorldStage.getInstance().getPlayerSprite().getY()) - miniMapPlayer.getHeight() / 2;
         miniMapPlayer.setPosition(avatarX, avatarY);
         miniMapPlayer.layout();
         scroll.scrollTo(avatarX, avatarY, miniMapPlayer.getWidth(), miniMapPlayer.getHeight(), true, true);
         for (AdventureQuestData adq : Current.player().getQuests()) {
-            if (adq.isTracked) {
-                PointOfInterest poi = adq.getTargetPOI();
-                if (poi != null) {
-                    TypingLabel label = Controls.newTypingLabel("[%80][+GPS]{GRADIENT=RED;WHITE;1;1}-" + poi.getData().name + "{ENDGRADIENT}");
-                    table.addActor(label);
-                    label.setPosition(getMapX(poi.getPosition().x) - label.getWidth() / 2, getMapY(poi.getPosition().y) - label.getHeight() / 2);
-                    label.skipToTheEnd();
-                }
+            PointOfInterest poi = adq.getTargetPOI();
+            if (poi != null) {
+                TypingLabel label = Controls.newTypingLabel("[%?BLACKEN][+GPS]{GRADIENT=RED;WHITE;1;1}>" + adq.name + "{ENDGRADIENT}");
+                labels.add(label);
+                table.addActor(label);
+                label.setPosition(getMapX(poi.getPosition().x) - label.getWidth() / 2, getMapY(poi.getPosition().y) - label.getHeight() / 2);
+                label.skipToTheEnd();
             }
+        }
+        TextraButton questButton = ui.findActor("quest");
+        if (questButton != null) {
+            questButton.setDisabled(labels.isEmpty());
         }
         super.enter();
     }
