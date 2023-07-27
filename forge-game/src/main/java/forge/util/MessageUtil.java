dba4fbd99db2b58e24a43e4adb88997142e54d12
Northmoc <timothy.s.mocny@gmail.com>
2023-07-27 11:57:08 -0400

    Secret Council + support

--- forge-game/src/main/java/forge/util/MessageUtil.java
+++ forge-game/src/main/java/forge/util/MessageUtil.java
@@ -72,8 +72,12 @@ public class MessageUtil {
             case PutCounter:// For Clay Golem cost text
                 return value;
             case Vote:
-                String chooser = StringUtils.capitalize(mayBeYou(player, target));
-                return Localizer.getInstance().getMessage("lblPlayerVoteValue", chooser, value);
+                if (sa.hasParam("Secret")) {
+                    return value;
+                } else {
+                    String chooser = StringUtils.capitalize(mayBeYou(player, target));
+                    return Localizer.getInstance().getMessage("lblPlayerVoteValue", chooser, value);
+                }
             default:
                 String tgt = mayBeYou(player, target);
                 if (tgt.equals("(null)")) {
