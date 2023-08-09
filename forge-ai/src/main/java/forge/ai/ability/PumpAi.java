4577b3361d51927fa0da5fab8d7c73fbe0d3e6ee
Hans Mackowiak <hanmac@gmx.de>
2023-08-09 07:17:44 +0200

    PumpAi: check if creature would die through damage and check for regeneration

--- forge-ai/src/main/java/forge/ai/ability/PumpAi.java
+++ forge-ai/src/main/java/forge/ai/ability/PumpAi.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import forge.ai.*;
 import forge.game.Game;
+import forge.game.ability.AbilityKey;
 import forge.game.ability.AbilityUtils;
 import forge.game.ability.ApiType;
 import forge.game.card.*;
@@ -14,6 +15,9 @@ import forge.game.phase.PhaseHandler;
 import forge.game.phase.PhaseType;
 import forge.game.player.Player;
 import forge.game.player.PlayerActionConfirmMode;
+import forge.game.replacement.ReplacementEffect;
+import forge.game.replacement.ReplacementLayer;
+import forge.game.replacement.ReplacementType;
 import forge.game.spellability.SpellAbility;
 import forge.game.spellability.TargetRestrictions;
 import forge.game.zone.ZoneType;
@@ -473,35 +477,47 @@ public class PumpAi extends PumpAiBase {
             // Logic to kill opponent creatures
             CardCollection oppCreatures = CardLists.getTargetableCards(ai.getOpponents().getCreaturesInPlay(), sa);
             if (!oppCreatures.isEmpty()) {
-                CardCollection oppCreaturesFiltered = CardLists.filter(oppCreatures, new Predicate<Card>() {
+                CardCollection oppCreaturesFiltered = CardLists.filter(oppCreatures, card -> {
+                    // don't care about useless creatures
+                    if (ComputerUtilCard.isUselessCreature(ai, card)
+                            || card.hasSVar("EndOfTurnLeavePlay")) {
+                        return false;
+                    }
+                    // dies by target
+                    if (card.getSVar("Targeting").equals("Dies")) {
+                        return true;
+                    }
+                    // dies by state based action
+                    if (card.getNetPower() <= 0) {
+                        return true;
+                    }
+                    if (card.hasKeyword(Keyword.INDESTRUCTIBLE)) {
+                        return false;
+                    }
+                    // check if switching PT causes it to be lethal
+                    Card lki = CardCopyService.getLKICopy(card);
+                    lki.addSwitchPT(-1, 0);
 
-                    @Override
-                    public boolean apply(Card input) {
-                        // don't care about useless creatures
-                        if (ComputerUtilCard.isUselessCreature(ai, input)
-                                || input.hasSVar("EndOfTurnLeavePlay")) {
-                            return false;
-                        }
-                        // dies by target
-                        if (input.getSVar("Targeting").equals("Dies")) {
-                            return true;
-                        }
-                        // dies by state based action
-                        if (input.getNetPower() <= 0) {
-                            return true;
-                        }
-                        if (input.hasKeyword(Keyword.INDESTRUCTIBLE)) {
-                            return false;
-                        }
-                        // check if switching PT causes it to be lethal
-                        Card lki = CardUtil.getLKICopy(input);
-                        lki.addSwitchPT(-1, 0);
-                        if (input.getLethal() - input.getDamage() <= 0) {
-                            return true;
-                        }
+                    // check if creature could regenerate
+                    Map<AbilityKey, Object> runParams = AbilityKey.mapFromAffected(card);
+                    runParams.put(AbilityKey.Regeneration, true);
+                    List<ReplacementEffect> repDestoryList = game.getReplacementHandler().getReplacementList(ReplacementType.Destroy, runParams, ReplacementLayer.Other);
+                    // non-Regeneration one like Totem-Armor
+                    // should do it anyway to destroy the aura?
+                    if (repDestoryList.stream().anyMatch(r -> !r.hasParam("Regeneration"))) {
+                        return false;
+                    }
+                    // TODO make it force to use regen?
+                    // should check phase and make it before combat damage or better before blocker?
+                    if (repDestoryList.stream().anyMatch(r -> r.hasParam("Regeneration")) && card.canBeShielded()) {
                         return false;
                     }
 
+                    // maybe do it anyway to reduce its power?
+                    if (card.getLethal() - card.getDamage() > 0) {
+                        return false;
+                    }
+                    return true;
                 });
                 // the ones that die by switching PT
                 if (!oppCreaturesFiltered.isEmpty()) {
