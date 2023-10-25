77a41240e12d982e7869453294b5359509b1f513
tool4EvEr <tool4EvEr@192.168.0.60>
2023-10-25 10:24:24 +0200

    Support for Ojer Taq

--- forge-game/src/main/java/forge/game/ability/effects/ReplaceTokenEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/ReplaceTokenEffect.java
@@ -47,14 +47,14 @@ public class ReplaceTokenEffect extends SpellAbilityEffect {
         Map<AbilityKey, Object> originalParams =
                 (Map<AbilityKey, Object>) repSA.getReplacingObject(AbilityKey.OriginalParams);
 
-        // currently the only ones that changes the amount does double it
         if ("Amount".equals(sa.getParam("Type"))) {
+            final String mod = sa.getParamOrDefault("Amount", "Twice");
             for (Map.Entry<Card, Integer> e : table.row(affected).entrySet()) {
                 if (!sa.matchesValidParam("ValidCard", e.getKey())) {
                     continue;
                 }
-                // currently the amount is only doubled
-                table.put(affected, e.getKey(), e.getValue() * 2);
+                int newAmt = AbilityUtils.doXMath(e.getValue(), mod, card, sa);
+                table.put(affected, e.getKey(), newAmt);
             }
         } else if ("AddToken".equals(sa.getParam("Type"))) {
             long timestamp = game.getNextTimestamp();
