86be8bc07a53e97bd0e047cebdf2d966f7f64554
Agetian <stavdev@mail.ru>
2023-10-31 19:09:49 +0300

    - Some more minor cleanup.

--- forge-ai/src/main/java/forge/ai/ComputerUtilCost.java
+++ forge-ai/src/main/java/forge/ai/ComputerUtilCost.java
@@ -360,10 +360,11 @@ public class ComputerUtilCost {
                         // e.g. Sakura Tribe-Elder
                         final Combat combat = ai.getGame().getCombat();
                         final boolean beforeNextTurn = ai.getGame().getPhaseHandler().is(PhaseType.END_OF_TURN) && ai.getGame().getPhaseHandler().getNextTurn().equals(ai);
-                        final boolean creatureInDanger = ComputerUtil.predictCreatureWillDieThisTurn(ai, source, sourceAbility, false);
+                        final boolean creatureInDanger = ComputerUtil.predictCreatureWillDieThisTurn(ai, source, sourceAbility, false)
+                                && (combat == null || !ComputerUtilCombat.willOpposingCreatureDieInCombat(ai, source, combat));
                         final int lifeThreshold = (((PlayerControllerAi) ai.getController()).getAi().getIntProperty(AiProps.AI_IN_DANGER_THRESHOLD));
                         final boolean aiInDanger = ai.getLife() <= lifeThreshold && ai.canLoseLife() && !ai.cantLoseForZeroOrLessLife();
-                        if (creatureInDanger && (combat == null || !ComputerUtilCombat.willOpposingCreatureDieInCombat(ai, source, combat))) {
+                        if (creatureInDanger) {
                             return true;
                         } else if (aiInDanger || !beforeNextTurn) {
                             return false;
