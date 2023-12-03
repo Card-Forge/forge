3f1a002a328008ab96f13f6d432d348505f630bc
Agetian <stavdev@mail.ru>
2023-12-03 13:26:06 +0300

    - Remove some unused code.

--- forge-ai/src/main/java/forge/ai/ability/DiscoverAi.java
+++ forge-ai/src/main/java/forge/ai/ability/DiscoverAi.java
@@ -1,6 +1,5 @@
 package forge.ai.ability;
 
-import com.google.common.collect.Iterables;
 import forge.ai.AiPlayDecision;
 import forge.ai.ComputerUtil;
 import forge.ai.PlayerControllerAi;
@@ -8,18 +7,12 @@ import forge.ai.SpellAbilityAi;
 import forge.card.CardStateName;
 import forge.game.ability.AbilityUtils;
 import forge.game.card.Card;
-import forge.game.card.CardCollection;
-import forge.game.card.CardUtil;
 import forge.game.player.Player;
 import forge.game.player.PlayerActionConfirmMode;
 import forge.game.spellability.LandAbility;
 import forge.game.spellability.Spell;
 import forge.game.spellability.SpellAbility;
-import forge.game.spellability.SpellAbilityPredicates;
-import forge.game.zone.ZoneType;
 
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 
 public class DiscoverAi extends SpellAbilityAi {
@@ -28,11 +21,6 @@ public class DiscoverAi extends SpellAbilityAi {
     protected boolean checkApiLogic(final Player ai, final SpellAbility sa) {
         if (ComputerUtil.preventRunAwayActivations(sa)) {
             return false; // prevent infinite loop
-        }
-
-        List<Card> cards = getPlayableCards(sa, ai);
-        if (cards.isEmpty()) {
-            return false;
         }
 
         return true;
@@ -57,7 +45,7 @@ public class DiscoverAi extends SpellAbilityAi {
     @Override
     public boolean confirmAction(Player ai, SpellAbility sa, PlayerActionConfirmMode mode, String message, Map<String, Object> params) {
         Card c = (Card)params.get("Card");
-        for (SpellAbility s : AbilityUtils.getBasicSpellsFromPlayEffect(c, ai, CardStateName.Original)) {
+        for (SpellAbility s : AbilityUtils.getBasicSpellsFromPlayEffect(c, ai, CardStateName.Original)) { // TODO: other states for split cards and MDFC?
             if (!sa.matchesValidParam("ValidSA", s)) {
                 continue;
             }
@@ -79,36 +67,4 @@ public class DiscoverAi extends SpellAbilityAi {
         }
         return false;
     }
-
-    private static List<Card> getPlayableCards(SpellAbility sa, Player ai) {
-        List<Card> cards = null;
-        final Card source = sa.getHostCard();
-
-        if (sa.usesTargeting()) {
-            cards = CardUtil.getValidCardsToTarget(sa);
-        } else if (!sa.hasParam("Valid")) {
-            cards = AbilityUtils.getDefinedCards(source, sa.getParam("Defined"), sa);
-        }
-
-        if (cards != null & sa.hasParam("ValidSA")) {
-            final String valid[] = sa.getParam("ValidSA").split(",");
-            final Iterator<Card> itr = cards.iterator();
-            while (itr.hasNext()) {
-                final Card c = itr.next();
-                if (!Iterables.any(AbilityUtils.getBasicSpellsFromPlayEffect(c, ai), SpellAbilityPredicates.isValid(valid, ai , source, sa))) {
-                    itr.remove();
-                }
-            }
-        }
-
-        // Ensure that if a ValidZone is specified, there's at least something to choose from in that zone.
-        if (sa.hasParam("ValidZone")) {
-            cards = new CardCollection(AbilityUtils.filterListByType(ai.getGame().getCardsIn(ZoneType.listValueOf(sa.getParam("ValidZone"))),
-                    sa.getParam("Valid"), sa));
-        }
-        // exclude own card
-        cards.remove(source);
-        return cards;
-    }
-
 }
