30902fa4e492246e32a47d011970ea9aa0191bd5
Paul Egan <paullygon@gmail.com>
2024-01-19 09:18:10 +1100

    handle tracking when creature removed from combat early

--- forge-game/src/main/java/forge/game/combat/Combat.java
+++ forge-game/src/main/java/forge/game/combat/Combat.java
@@ -31,6 +31,7 @@ import forge.game.spellability.SpellAbility;
 import forge.game.spellability.SpellAbilityStackInstance;
 import forge.game.staticability.StaticAbilityAssignCombatDamageAsUnblocked;
 import forge.game.trigger.TriggerType;
+import forge.game.zone.ZoneType;
 import forge.util.CardTranslation;
 import forge.util.Localizer;
 import forge.util.collect.FCollection;
@@ -155,10 +156,8 @@ public class Combat {
         combatantsThatDealtFirstStrikeDamage.clear();
 
         //clear tracking for cards that care about "this combat"
-        for (Card c : attackers) {
-            c.getDamageHistory().endCombat();
-        }
-        for (Card c : blockers) {
+        Game game = playerWhoAttacks.getGame();
+        for (Card c : game.getCardsIncludePhasingIn(ZoneType.Battlefield)) {
             c.getDamageHistory().endCombat();
         }
         playerWhoAttacks.clearAttackedPlayersMyCombat();
