5fe761f16710fbd12f51b7e048706d8a7226666e
friarsol <zenchristo@gmail.com>
2024-01-20 15:13:59 -0500

    Add Crimestopper Sprite (although the optional cost doesn't work yet)

--- forge-ai/src/main/java/forge/ai/AiCostDecision.java
+++ forge-ai/src/main/java/forge/ai/AiCostDecision.java
@@ -59,9 +59,6 @@ public class AiCostDecision extends CostDecisionMakerBase {
     @Override
     public PaymentDecision visit(CostCollectEvidence cost) {
         int c = cost.getAbilityAmount(ability);
-
-        // TODO figure out how to select Total CMC >= to c
-
         CardCollectionView chosen = ComputerUtil.chooseCollectEvidence(player, cost, source, c, ability);
 
         return null == chosen ? null : PaymentDecision.card(chosen);
