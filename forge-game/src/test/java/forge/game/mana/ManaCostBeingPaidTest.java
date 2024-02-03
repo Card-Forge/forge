6984d035794c6276034b3b006fe911d97fe0025e
Leandro Doctors <ldoctors@gmail.com>
2024-02-04 00:00:08 +0100

    tests: migrate from TestNG to JUnit 3 (forge-game)
    Since our final objective is to migrate to JUnit 5,
    the JAR is JUnit4 (wich is compatible with JUnit 3).

--- forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
+++ forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
@@ -1,7 +1,5 @@
 package forge.game.mana;
 
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import static forge.card.MagicColor.COLORLESS;
 import static forge.card.MagicColor.GREEN;
 import static forge.card.MagicColor.RED;
@@ -9,32 +7,27 @@ import static forge.card.MagicColor.WHITE;
 
 import forge.card.mana.ManaCost;
 import forge.card.mana.ManaCostParser;
+import junit.framework.TestCase;
 
-public class ManaCostBeingPaidTest {
-
-    @Test
+public class ManaCostBeingPaidTest extends TestCase {
     public void testPayManaViaConvoke() {
-        runConvokeTest("1 W W", new byte[] { WHITE, COLORLESS, WHITE }, new String[] { "{1}{W}{W}", "{1}{W}", "{W}" });
-        runConvokeTest("1 W W", new byte[] { COLORLESS, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
-        runConvokeTest("1 W W", new byte[] { GREEN, WHITE, WHITE }, new String[] { "{1}{W}{W}", "{W}{W}", "{W}" });
-        runConvokeTest("1 W G", new byte[] { GREEN, RED, WHITE }, new String[] { "{1}{W}{G}", "{1}{W}", "{W}" });
+        runConvokeTest("1 W W", new byte[]{WHITE, COLORLESS, WHITE}, new String[]{"{1}{W}{W}", "{1}{W}", "{W}"});
+        runConvokeTest("1 W W", new byte[]{COLORLESS, WHITE, WHITE}, new String[]{"{1}{W}{W}", "{W}{W}", "{W}"});
+        runConvokeTest("1 W W", new byte[]{GREEN, WHITE, WHITE}, new String[]{"{1}{W}{W}", "{W}{W}", "{W}"});
+        runConvokeTest("1 W G", new byte[]{GREEN, RED, WHITE}, new String[]{"{1}{W}{G}", "{1}{W}", "{W}"});
     }
 
     private void runConvokeTest(String initialCost, byte[] colorsToPay, String[] expectedRemainder) {
-        ManaCostBeingPaid costBeingPaid = createManaCostBeingPaid(initialCost);
-
+        ManaCostBeingPaid cost = createManaCostBeingPaid(initialCost);
         for (int i = 0; i < colorsToPay.length; i++) {
-            AssertJUnit.assertEquals(expectedRemainder[i], costBeingPaid.toString());
-            costBeingPaid.payManaViaConvoke(colorsToPay[i]);
+            assertEquals(expectedRemainder[i], cost.toString());
+            cost.payManaViaConvoke(colorsToPay[i]);
         }
-
-        AssertJUnit.assertEquals("0", costBeingPaid.toString());
+        assertEquals("0", cost.toString());
     }
 
-    private ManaCostBeingPaid createManaCostBeingPaid(String costString) {
-        ManaCostParser parsedCostString = new ManaCostParser(costString);
-        ManaCost manaCost = new ManaCost(parsedCostString);
-
-        return new ManaCostBeingPaid(manaCost);
+    private ManaCostBeingPaid createManaCostBeingPaid(String cost) {
+        ManaCostParser parser = new ManaCostParser(cost);
+        return new ManaCostBeingPaid(new ManaCost(parser));
     }
 }
