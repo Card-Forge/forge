0f336dd2915fc375b6ea272a5dcba05dd1db6dbd
Leandro Doctors <ldoctors@gmail.com>
2024-02-04 00:11:12 +0100

    tests: migrate from JUnit 3 to JUnit 4 (forge-game)
    We are now ready to migrate to JUnit 5.

--- forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
+++ forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
@@ -2,23 +2,27 @@ package forge.game.ability;
 
 import java.util.Map;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import com.google.common.collect.Maps;
 
-import junit.framework.TestCase;
 
-public class AbilityKeyTest extends TestCase {
+public class AbilityKeyTest {
+
+    @Test
     public void testFromStringWorksForAllKeys() {
         for (AbilityKey key : AbilityKey.values()) {
-            assertEquals(key, AbilityKey.fromString(key.toString()));
+            Assert.assertEquals(key, AbilityKey.fromString(key.toString()));
         }
     }
 
+    @Test
     public void testCopyingEmptyMapWorks() {
         Map<AbilityKey, Object> map = Maps.newHashMap();
-
         Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
 
         // An actual copy should be made.
-        assertNotSame(map, newMap);
+        Assert.assertNotSame(map, newMap);
     }
 }
--- forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
+++ forge-game/src/test/java/forge/game/mana/ManaCostBeingPaidTest.java
@@ -5,11 +5,15 @@ import static forge.card.MagicColor.GREEN;
 import static forge.card.MagicColor.RED;
 import static forge.card.MagicColor.WHITE;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 import forge.card.mana.ManaCost;
 import forge.card.mana.ManaCostParser;
-import junit.framework.TestCase;
 
-public class ManaCostBeingPaidTest extends TestCase {
+public class ManaCostBeingPaidTest {
+
+    @Test
     public void testPayManaViaConvoke() {
         runConvokeTest("1 W W", new byte[]{WHITE, COLORLESS, WHITE}, new String[]{"{1}{W}{W}", "{1}{W}", "{W}"});
         runConvokeTest("1 W W", new byte[]{COLORLESS, WHITE, WHITE}, new String[]{"{1}{W}{W}", "{W}{W}", "{W}"});
@@ -20,10 +24,10 @@ public class ManaCostBeingPaidTest extends TestCase {
     private void runConvokeTest(String initialCost, byte[] colorsToPay, String[] expectedRemainder) {
         ManaCostBeingPaid cost = createManaCostBeingPaid(initialCost);
         for (int i = 0; i < colorsToPay.length; i++) {
-            assertEquals(expectedRemainder[i], cost.toString());
+            Assert.assertEquals(expectedRemainder[i], cost.toString());
             cost.payManaViaConvoke(colorsToPay[i]);
         }
-        assertEquals("0", cost.toString());
+        Assert.assertEquals("0", cost.toString());
     }
 
     private ManaCostBeingPaid createManaCostBeingPaid(String cost) {
