9d114c09863f9235b7a8a3115f7270a371d15ba9
Leandro Doctors <ldoctors@gmail.com>
2024-02-04 00:00:08 +0100

    tests: migrate from TestNG to JUnit 3 (forge-game)
    Since our final objective is to migrate to JUnit 5,
    the JAR is JUnit4 (wich is compatible with JUnit 3).

--- forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
+++ forge-game/src/test/java/forge/game/ability/AbilityKeyTest.java
@@ -1,26 +1,24 @@
 package forge.game.ability;
 
-import org.testng.annotations.Test;
-import org.testng.AssertJUnit;
 import java.util.Map;
 
 import com.google.common.collect.Maps;
 
-public class AbilityKeyTest {
+import junit.framework.TestCase;
 
-    @Test
+public class AbilityKeyTest extends TestCase {
     public void testFromStringWorksForAllKeys() {
         for (AbilityKey key : AbilityKey.values()) {
-            AssertJUnit.assertEquals(key, AbilityKey.fromString(key.toString()));
+            assertEquals(key, AbilityKey.fromString(key.toString()));
         }
     }
 
-    @Test
     public void testCopyingEmptyMapWorks() {
         Map<AbilityKey, Object> map = Maps.newHashMap();
+
         Map<AbilityKey, Object> newMap = AbilityKey.newMap(map);
 
         // An actual copy should be made.
-        AssertJUnit.assertNotSame(map, newMap);
+        assertNotSame(map, newMap);
     }
 }
