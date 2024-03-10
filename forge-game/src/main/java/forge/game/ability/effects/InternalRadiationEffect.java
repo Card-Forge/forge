1872cc6ab75a9a5c9bf3fe89795825d988081bb9
Hans Mackowiak <hanmac@gmx.de>
2024-03-10 09:05:08 +0100

    InternalRadiationEffect: use GameAction::mill

--- forge-game/src/main/java/forge/game/ability/effects/InternalRadiationEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/InternalRadiationEffect.java
@@ -6,7 +6,6 @@ import com.google.common.base.Predicates;
 import com.google.common.collect.Maps;
 
 import forge.game.Game;
-import forge.game.GameLogEntryType;
 import forge.game.ability.AbilityKey;
 import forge.game.ability.SpellAbilityEffect;
 import forge.game.card.CardCollectionView;
@@ -15,12 +14,11 @@ import forge.game.card.CardPredicates;
 import forge.game.card.CardZoneTable;
 import forge.game.card.CounterEnumType;
 import forge.game.player.Player;
+import forge.game.player.PlayerCollection;
 import forge.game.spellability.SpellAbility;
 import forge.game.staticability.StaticAbilityGainLifeRadiation;
 import forge.game.trigger.TriggerType;
 import forge.game.zone.ZoneType;
-import forge.util.Lang;
-import forge.util.Localizer;
 
 public class InternalRadiationEffect extends SpellAbilityEffect {
 
@@ -28,18 +26,13 @@ public class InternalRadiationEffect extends SpellAbilityEffect {
     public void resolve(SpellAbility sa) {
         final Player p = sa.getActivatingPlayer();
         final Game game = p.getGame();
-        
-        int numRad = p.getCounters(CounterEnumType.RAD);
-        
-        // TODO move mill part to GameAction
-        final CardZoneTable table = new CardZoneTable(game.getLastStateBattlefield(), game.getLastStateGraveyard());
-        Map<AbilityKey, Object> moveParams = AbilityKey.newMap();
-        AbilityKey.addCardZoneTableParams(moveParams, table);
 
-        final CardCollectionView milled = p.mill(numRad, ZoneType.Graveyard, sa, moveParams);
-        game.getAction().reveal(milled, p, false, Localizer.getInstance().getMessage("lblMilledCards", p), false);
-        game.getGameLog().add(GameLogEntryType.ZONE_CHANGE, p + " milled " +
-                Lang.joinHomogenous(milled) + ".");
+        int numRad = p.getCounters(CounterEnumType.RAD);
+
+        Map<AbilityKey, Object> moveParams = AbilityKey.newMap();
+        final CardZoneTable table = AbilityKey.addCardZoneTableParams(moveParams, sa);
+
+        final CardCollectionView milled = game.getAction().mill(new PlayerCollection(p), numRad, ZoneType.Graveyard, sa, moveParams);
         table.triggerChangesZoneAll(game, sa);
         int n = CardLists.count(milled, Predicates.not(CardPredicates.Presets.LANDS));
         
