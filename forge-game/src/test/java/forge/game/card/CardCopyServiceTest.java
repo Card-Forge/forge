bd5448825acba72e4209fe604c2cd43d41803c7c
Chris H <zenchristo@gmail.com>
2024-03-10 16:20:04 -0400

    Setup a test bed for the future

---
+++ forge-game/src/test/java/forge/game/card/CardCopyServiceTest.java
@@ -0,0 +1,26 @@
+package forge.game.card;
+
+import org.testng.annotations.Test;
+
+public class CardCopyServiceTest {
+
+    @Test
+    public void testCopyCard() {
+
+    }
+
+    @Test
+    public void testCopyState() {
+
+    }
+
+    @Test
+    public void testCopyStats() {
+
+    }
+
+    @Test
+    public void testGetLKICopy() {
+
+    }
+}
