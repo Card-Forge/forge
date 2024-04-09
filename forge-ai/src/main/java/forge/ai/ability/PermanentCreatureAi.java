8e053bbbfee9c0c6554d70f1d0f1d352ceacb19b
Agetian <stavdev@mail.ru>
2024-04-09 12:30:08 +0300

    Fix advanced Flash logic messing with PlayEffectAi (#4989)
    * - Add puzzle PS_PIP1.
    
    * - Fix advanced Flash logic messing with PlayEffect AI (e.g. Discover X or transformed Battle)
    
    * - Better tweak to Flash logic conditions

--- forge-ai/src/main/java/forge/ai/ability/PermanentCreatureAi.java
+++ forge-ai/src/main/java/forge/ai/ability/PermanentCreatureAi.java
@@ -90,7 +90,7 @@ public class PermanentCreatureAi extends PermanentAi {
         if (ai.getController().isAI()) {
             advancedFlash = ((PlayerControllerAi)ai.getController()).getAi().getBooleanProperty(AiProps.FLASH_ENABLE_ADVANCED_LOGIC);
         }
-        if (card.hasKeyword(Keyword.FLASH) || (!ai.canCastSorcery() && sa.canCastTiming(ai))) {
+        if (card.hasKeyword(Keyword.FLASH) || (!ai.canCastSorcery() && sa.canCastTiming(ai) && !sa.isCastFromPlayEffect())) {
             if (advancedFlash) {
                 return doAdvancedFlashLogic(card, ai, sa);
             } else {
