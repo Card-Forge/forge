e49dd53b2060b621c3d8dcfd5b3b037590d6e2fb
tool4ever <therealtoolkit@hotmail.com>
2024-06-06 06:35:05 +0200

    Include TokenScripts when checking for DeckHints (#5379)
    * Include TokenScripts when checking for DeckHints
    
    * Fix test
    
    * Add field for extra logic
    
    ---------
    
    Co-authored-by: tool4EvEr <tool4EvEr@192.168.0.60>

--- forge-core/src/main/java/forge/card/CardRules.java
+++ forge-core/src/main/java/forge/card/CardRules.java
@@ -22,6 +22,7 @@ import java.util.*;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import forge.card.mana.IParserManaCost;
@@ -385,6 +386,12 @@ public final class CardRules implements ICardCharacteristics {
     private int deltaHand;
     private int deltaLife;
 
+    private List<String> tokens;
+
+    public List<String> getTokens() {
+        return tokens;
+    }
+
     public int getHand() { return deltaHand; }
     public int getLife() { return deltaLife; }
     public void setVanguardProperties(String pt) {
@@ -420,6 +427,8 @@ public final class CardRules implements ICardCharacteristics {
         private String handLife = null;
         private String normalizedName = "";
 
+        private List<String> tokens = Lists.newArrayList();
+
         // fields to build CardAiHints
         private boolean removedFromAIDecks = false;
         private boolean removedFromRandomDecks = false;
@@ -453,6 +462,7 @@ public final class CardRules implements ICardCharacteristics {
             this.meldWith = "";
             this.partnerWith = "";
             this.normalizedName = "";
+            this.tokens = Lists.newArrayList();
         }
 
         /**
@@ -474,6 +484,7 @@ public final class CardRules implements ICardCharacteristics {
             result.setNormalizedName(this.normalizedName);
             result.meldWith = this.meldWith;
             result.partnerWith = this.partnerWith;
+            result.tokens = tokens.isEmpty() ? Collections.emptyList() : tokens;
             if (StringUtils.isNotBlank(handLife))
                 result.setVanguardProperties(handLife);
             return result;
@@ -505,6 +516,18 @@ public final class CardRules implements ICardCharacteristics {
             int colonPos = line.indexOf(':');
             String key = colonPos > 0 ? line.substring(0, colonPos) : line;
             String value = colonPos > 0 ? line.substring(1+colonPos).trim() : null;
+
+            if (value != null) {
+                int tokIdx = value.indexOf("TokenScript$");
+                if (tokIdx > 0) {
+                    String tokenParam = value.substring(tokIdx + 12).trim();
+                    int endIdx = tokenParam.indexOf("|");
+                    if (endIdx > 0) {
+                        tokenParam = tokenParam.substring(0, endIdx).trim();
+                    }
+                    this.tokens.addAll(Arrays.asList(tokenParam.split(",")));
+                }
+            }
 
             switch (key.charAt(0)) {
                 case 'A':
