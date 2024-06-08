1e5e1ba6c0e80f1cc87c8c50b7d6b524a41dde3d
Jetz <Jetz722@gmail.com>
2024-06-08 12:36:20 -0400

    Redundant import

--- forge-core/src/main/java/forge/card/CardRules.java
+++ forge-core/src/main/java/forge/card/CardRules.java
@@ -23,7 +23,6 @@ import com.google.common.collect.Lists;
 import org.apache.commons.lang3.StringUtils;
 
 import com.google.common.collect.Iterables;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
 import forge.card.mana.IParserManaCost;
