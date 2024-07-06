d0b3de2afb8eec8e32402fbe5481688e36b2f920
Renato Filipe Vidal Santos <45150760+dracontes@users.noreply.github.com>
2024-07-06 17:32:51 +0100

    Update ChooseGenericEffectAi.java

--- forge-ai/src/main/java/forge/ai/ability/ChooseGenericEffectAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ChooseGenericEffectAi.java
@@ -206,7 +206,7 @@ public class ChooseGenericEffectAi extends SpellAbilityAi {
             }
 
             // milling against Tamiyo is pointless
-            if (owner.isCardInCommand("Emblem - Tamiyo, the Moon Sage")) {
+            if (owner.isCardInCommand("Emblem — Tamiyo, the Moon Sage")) {
                 return allow;
             }
 
