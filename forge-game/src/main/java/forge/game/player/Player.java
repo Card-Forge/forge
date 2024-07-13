64cf5a946c6be08ab97b1b4731136255965432e1
tool4ever <therealtoolkit@hotmail.com>
2024-07-13 20:42:12 +0000

    Moonstone Harbinger and support (#5591)

--- forge-game/src/main/java/forge/game/player/Player.java
+++ forge-game/src/main/java/forge/game/player/Player.java
@@ -491,6 +491,8 @@ public class Player extends GameEntity implements Comparable<Player> {
             runParams.put(AbilityKey.FirstTime, firstGain);
             game.getTriggerHandler().runTrigger(TriggerType.LifeGained, runParams, false);
 
+            game.getTriggerHandler().runTrigger(TriggerType.LifeChanged, runParams, false);
+
             game.fireEvent(new GameEventPlayerLivesChanged(this, oldLife, life));
             return true;
         }
@@ -561,6 +563,8 @@ public class Player extends GameEntity implements Comparable<Player> {
         runParams.put(AbilityKey.LifeAmount, toLose);
         runParams.put(AbilityKey.FirstTime, firstLost);
         game.getTriggerHandler().runTrigger(TriggerType.LifeLost, runParams, false);
+
+        game.getTriggerHandler().runTrigger(TriggerType.LifeChanged, runParams, false);
 
         return lifeLost;
     }
