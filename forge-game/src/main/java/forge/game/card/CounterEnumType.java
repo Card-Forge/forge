3b29804568255b246522ca67380ac0bc28bdc790
Glorax <114269106+Glorax@users.noreply.github.com>
2024-08-02 01:19:02 -0700

    Rottenmouth Viper (#5704)

--- forge-game/src/main/java/forge/game/card/CounterEnumType.java
+++ forge-game/src/main/java/forge/game/card/CounterEnumType.java
@@ -35,7 +35,7 @@ public enum CounterEnumType {
 
     LOYALTY("LOYAL", 198, 198, 198),
 
-    ACORN("ACORN", 139,69,19),
+    ACORN("ACORN", 139, 69, 19),
 
     AEGIS("AEGIS", 207, 207, 207),
 
@@ -50,19 +50,21 @@ public enum CounterEnumType {
     AWAKENING("AWAKE", 0, 231, 79),
 
     BLAZE("BLAZE", 255, 124, 82),
-    
+
     BLESSING("BLESS", 251, 0, 94),
+
+    BLIGHT("BLGHT", 130, 115, 160),
 
     BLOOD("BLOOD", 255, 108, 111),
 
     BLOODLINE("BLDLN", 224, 44, 44),
-    
+
     BLOODSTAIN("BLDST", 224, 44, 44),
 
     BORE("BORE", 98, 47, 34),
 
     BOUNTY("BOUNT", 255, 158, 0),
-    
+
     BRAIN("BRAIN", 197, 62, 212),
 
     BRIBERY("BRIBE", 172, 201, 235),
