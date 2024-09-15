4ae7c91ecf0b15312f3d123c34a025f94246d2b8
Hans Mackowiak <hanmac@gmx.de>
2024-09-15 21:03:32 +0200

    ManifestDread as separate Effect

---
+++ forge-game/src/main/java/forge/game/ability/effects/ManifestDreadEffect.java
@@ -0,0 +1,56 @@
+package forge.game.ability.effects;
+
+import java.util.Map;
+
+import forge.game.Game;
+import forge.game.ability.AbilityKey;
+import forge.game.card.Card;
+import forge.game.card.CardCollection;
+import forge.game.card.CardZoneTable;
+import forge.game.player.Player;
+import forge.game.spellability.SpellAbility;
+import forge.game.trigger.TriggerType;
+import forge.util.Localizer;
+
+public class ManifestDreadEffect extends ManifestBaseEffect {
+    @Override
+    protected String getDefaultMessage() {
+        return Localizer.getInstance().getMessage("lblChooseCardToManifest");
+    }
+    @Override
+    protected Card internalEffect(Card c, Player p, SpellAbility sa, Map<AbilityKey, Object> moveParams) {
+        final Card source = sa.getHostCard();
+        Card rem = c.manifest(p, sa, moveParams);
+        if (rem != null && sa.hasParam("RememberManifested") && rem.isManifested()) {
+            source.addRemembered(rem);
+        }
+        return rem;
+    }
+
+    @Override
+    protected void manifestLoop(SpellAbility sa, Player p, final int amount) {
+        final Game game = p.getGame();
+        for (int i = 0; i < amount; i++) {
+            CardCollection tgtCards = p.getTopXCardsFromLibrary(2);
+            Card manifest = null;
+            Card toGrave = null;
+            if (!tgtCards.isEmpty()) {
+                manifest = p.getController().chooseSingleEntityForEffect(tgtCards, sa, getDefaultMessage(), null);
+                tgtCards.remove(manifest);
+                toGrave = tgtCards.isEmpty() ? null : tgtCards.getFirst();
+
+                // CR 701.34d If an effect instructs a player to manifest multiple cards from their library, those cards are manifested one at a time.
+                Map<AbilityKey, Object> moveParams = AbilityKey.newMap();
+                CardZoneTable triggerList = AbilityKey.addCardZoneTableParams(moveParams, sa);
+                internalEffect(manifest, p, sa, moveParams);
+                if (toGrave != null) {
+                    toGrave = game.getAction().moveToGraveyard(toGrave, sa, moveParams);
+                }
+                triggerList.triggerChangesZoneAll(game, sa);
+            }
+            Map<AbilityKey, Object> runParams = AbilityKey.mapFromPlayer(p);
+            runParams.put(AbilityKey.Card, toGrave);
+            game.getTriggerHandler().runTrigger(TriggerType.ManifestDread, runParams, true);
+        }
+    }
+}
---
+++ forge-game/src/main/java/forge/game/trigger/TriggerManifestDread.java
@@ -0,0 +1,35 @@
+package forge.game.trigger;
+
+import java.util.Map;
+
+import forge.game.ability.AbilityKey;
+import forge.game.card.Card;
+import forge.game.spellability.SpellAbility;
+
+public class TriggerManifestDread extends Trigger {
+
+    public TriggerManifestDread(Map<String, String> params, Card host, boolean intrinsic) {
+        super(params, host, intrinsic);
+    }
+
+    @Override
+    public boolean performTest(Map<AbilityKey, Object> runParams) {
+        if (!matchesValidParam("ValidPlayer", runParams.get(AbilityKey.Player))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void setTriggeringObjects(SpellAbility sa, Map<AbilityKey, Object> runParams) {
+        sa.setTriggeringObject(AbilityKey.NewCard, runParams.get(AbilityKey.Card));
+
+    }
+
+    @Override
+    public String getImportantStackObjects(SpellAbility sa) {
+        // TODO Auto-generated method stub
+        return "";
+    }
+
+}
