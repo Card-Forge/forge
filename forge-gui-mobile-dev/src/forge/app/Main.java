07d2d0a023c98af77a039cde32d5fd02cb66e83f
Anthony Calosa <anthonycalosa@gmail.com>
2024-10-24 07:30:16 +0800

    update splash closing

--- forge-gui-mobile-dev/src/forge/app/Main.java
+++ forge-gui-mobile-dev/src/forge/app/Main.java
@@ -17,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Optional;
 
 public class Main {
     private static final String versionString = BuildInfo.getVersionString();
@@ -79,9 +80,11 @@ public class Main {
 
         @Override
         public void closeSplashScreen() {
-            SplashScreen splash = SplashScreen.getSplashScreen();
-            if (splash != null) {
-                splash.close();
+            //could throw exception..
+            try {
+                Optional.ofNullable(SplashScreen.getSplashScreen()).ifPresent(SplashScreen::close);
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
 
