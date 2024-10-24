42fce6031389dd353f532fdb2bf0f99bf7441b31
Agetian <stavdev@mail.ru>
2024-10-24 08:13:18 +0300

    - Fix imports

--- forge-ai/src/main/java/forge/ai/ability/ChooseCardAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ChooseCardAi.java
@@ -29,7 +29,6 @@ import forge.game.player.PlayerPredicates;
 import forge.game.spellability.SpellAbility;
 import forge.game.zone.ZoneType;
 import forge.util.Aggregates;
-import forge.util.TextUtil;
 
 public class ChooseCardAi extends SpellAbilityAi {
 
