9a5aaa9e61e0e86c64905a74e01527327bf2f542
Northmoc <tnorthmoc@gmail.com>
2024-11-26 18:30:37 -0500

    Chosen letter support (Unsets)

--- forge-ai/src/main/java/forge/ai/PlayerControllerAi.java
+++ forge-ai/src/main/java/forge/ai/PlayerControllerAi.java
@@ -734,6 +734,17 @@ public class PlayerControllerAi extends PlayerController {
     }
 
     @Override
+    public List<String> chooseLetter(int n, String ai, List<String> letters) {
+        List<String> chosen = Lists.newArrayList();
+        for (int i = 0; i <=n; i++) {
+            String choice = Aggregates.random(letters);
+            letters.remove(choice);
+            chosen.add(choice);
+        }
+        return chosen;
+    }
+
+    @Override
     public PlanarDice choosePDRollToIgnore(List<PlanarDice> rolls) {
         //TODO create AI logic for this
         return Aggregates.random(rolls);
