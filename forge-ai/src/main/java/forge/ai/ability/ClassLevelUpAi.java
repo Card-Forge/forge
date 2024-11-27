3f1203df7a3b7b3c4c9ffbfd3f5aff0ac47ace20
Hans Mackowiak <hanmac@gmx.de>
2024-11-27 09:55:15 +0100

    Update ClassLevelUpAi.java
    ~ fix import

--- forge-ai/src/main/java/forge/ai/ability/ClassLevelUpAi.java
+++ forge-ai/src/main/java/forge/ai/ability/ClassLevelUpAi.java
@@ -8,6 +8,7 @@ import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
 import forge.game.staticability.StaticAbility;
 import forge.game.trigger.Trigger;
+import forge.game.trigger.TriggerType;
 
 public class ClassLevelUpAi extends SpellAbilityAi {
     @Override
