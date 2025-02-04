2a5d0e4db9c8d892c4a4cc42a4d3b5900aa2e52a
Northmoc <tnorthmoc@gmail.com>
2025-02-04 15:27:17 -0500

    better handle SpeedUp event

--- forge-game/src/main/java/forge/game/event/GameEventSpeedUp.java
+++ forge-game/src/main/java/forge/game/event/GameEventSpeedUp.java
@@ -1,6 +1,14 @@
 package forge.game.event;
 
+import forge.game.player.Player;
+
 public class GameEventSpeedUp extends GameEvent {
+
+    public final Player player;
+
+    public GameEventSpeedUp(Player affected) {
+        player = affected;
+    }
 
     @Override
     public <T> T visit(IGameEventVisitor<T> visitor) {
