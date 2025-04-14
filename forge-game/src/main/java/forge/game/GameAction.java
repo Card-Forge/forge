dfe5e98ffd44fb2cb14e8ad3c6d209597da5a83b
Hans Mackowiak <hanmac@gmx.de>
2025-04-14 22:54:16 +0200

    AttachAI: remove getFirstAttachSpell

--- forge-game/src/main/java/forge/game/GameAction.java
+++ forge-game/src/main/java/forge/game/GameAction.java
@@ -2777,19 +2777,15 @@ public class GameAction {
             return false;
         }
 
-        SpellAbility aura = source.getFirstAttachSpell();
-        if (aura == null) {
-            // if it's not normally an aura
-            aura = new SpellAbility.EmptySa(ApiType.Attach, source);
-            if (source.hasSVar("AttachAITgts")) {
-                aura.putParam("AITgts", source.getSVar("AttachAITgts"));
-            }
-            if (source.hasSVar("AttachAILogic")) {
-                aura.putParam("AILogic", source.getSVar("AttachAILogic"));
-            }
-            if (source.hasSVar("AttachAIValid")) {
-                aura.putParam("AIValid", source.getSVar("AttachAIValid"));
-            }
+        SpellAbility aura = new SpellAbility.EmptySa(ApiType.Attach, source);
+        if (source.hasSVar("AttachAITgts")) {
+            aura.putParam("AITgts", source.getSVar("AttachAITgts"));
+        }
+        if (source.hasSVar("AttachAILogic")) {
+            aura.putParam("AILogic", source.getSVar("AttachAILogic"));
+        }
+        if (source.hasSVar("AttachAIValid")) {
+            aura.putParam("AIValid", source.getSVar("AttachAIValid"));
         }
         aura.setActivatingPlayer(source.getController());
 
@@ -2810,7 +2806,7 @@ public class GameAction {
         }
         Player p = source.getController();
         if (canTargetPlayer) {
-            final FCollection<Player> players = game.getPlayers().filter(PlayerPredicates.canBeAttached(source, aura));
+            final FCollection<Player> players = game.getPlayers().filter(PlayerPredicates.canBeAttached(source, null));
 
             final Player pa = p.getController().chooseSingleEntityForEffect(players, aura,
                     Localizer.getInstance().getMessage("lblSelectAPlayerAttachSourceTo", CardTranslation.getTranslatedName(source.getName())), null);
@@ -2832,7 +2828,7 @@ public class GameAction {
             }
             list.addAll(game.getCardsIn(zones));
 
-            list = CardLists.filter(list, CardPredicates.canBeAttached(source, aura));
+            list = CardLists.filter(list, CardPredicates.canBeAttached(source, null));
             if (list.isEmpty()) {
                 return false;
             }
