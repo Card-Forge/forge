e32b1f68047a17977205b8486dacd46631bb36d6
Cees Timmerman <CTimmerman@users.noreply.github.com>
2025-07-07 10:07:49 +0200

    Read copied game log from the top as well

--- forge-gui-desktop/src/main/java/forge/screens/match/ViewWinLose.java
+++ forge-gui-desktop/src/main/java/forge/screens/match/ViewWinLose.java
@@ -6,6 +6,7 @@ import java.awt.Font;
 import java.awt.Point;
 import java.awt.Toolkit;
 import java.awt.datatransfer.StringSelection;
+import java.util.Collections;
 import java.util.List;
 
 import javax.swing.JPanel;
@@ -129,7 +130,9 @@ public class ViewWinLose implements IWinLoseView<FButton> {
 
         // Assemble game log scroller.
         final FTextArea txtLog = new FTextArea();
-        txtLog.setText(StringUtils.join(game.getGameLog().getLogEntries(null), "\r\n").replace("[COMPUTER]", "[AI]"));
+        List<GameLogEntry> entries = game.getGameLog().getLogEntries(null);
+        Collections.reverse(entries);
+        txtLog.setText(StringUtils.join(entries, "\r\n").replace("[COMPUTER]", "[AI]"));
         txtLog.setFont(FSkin.getRelativeFont(14));
         txtLog.setFocusable(true); // allow highlighting and copying of log
 
