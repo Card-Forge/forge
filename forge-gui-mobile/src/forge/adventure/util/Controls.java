ff39f820990ac225995c76adc852bdfd7881b14d
Anthony Calosa <anthonycalosa@gmail.com>
2025-07-13 12:52:53 +0800

    minor cleanup

--- forge-gui-mobile/src/forge/adventure/util/Controls.java
+++ forge-gui-mobile/src/forge/adventure/util/Controls.java
@@ -31,9 +31,26 @@ import java.util.function.Function;
  * Class to create ui elements in the correct style
  */
 public class Controls {
+
+    static public Styles.LabelStyle getLabelStyle() {
+        return getLabelStyle("default");
+    }
+
+    static public Styles.LabelStyle getLabelStyle(String name) {
+        return new Styles.LabelStyle(getSkin().get(name, Label.LabelStyle.class));
+    }
+
+    static public Styles.TextButtonStyle getTextButtonStyle() {
+        return getTextButtonStyle("default");
+    }
+
+    static public Styles.TextButtonStyle getTextButtonStyle(String name) {
+        return new Styles.TextButtonStyle(getSkin().get(name, TextButton.TextButtonStyle.class));
+    }
+
     static class LabelFix extends TextraLabel {
         public LabelFix(String text, Font font) {
-            super(text, new Styles.LabelStyle(getSkin().get(Label.LabelStyle.class)), font);
+            super(text, getLabelStyle(), font);
         }
 
         @Override
@@ -54,7 +71,7 @@ public class Controls {
 
     static class TextButtonFix extends TextraButton {
         public TextButtonFix(@Null String text) {
-            super(text == null ? "NULL" : text, new Styles.TextButtonStyle(Controls.getSkin().get(TextButton.TextButtonStyle.class)), Controls.getTextraFont());
+            super(text == null ? "NULL" : text, getTextButtonStyle(), getTextraFont());
             addListener(new ClickListener(){
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
@@ -89,7 +106,7 @@ public class Controls {
 
     static class TypingButtonFix extends TypingButton {
         public TypingButtonFix(@Null String text) {
-            super(text == null ? "NULL" : text, new Styles.TextButtonStyle(Controls.getSkin().get(TextButton.TextButtonStyle.class)), Controls.getTextraFont());
+            super(text == null ? "NULL" : text, getTextButtonStyle(), getTextraFont());
             addListener(new ClickListener(){
                 @Override
                 public void clicked(InputEvent event, float x, float y) {
@@ -472,7 +489,7 @@ public class Controls {
     }
 
     public static TypingLabel newTypingLabel(String name) {
-        TypingLabel ret = new TypingLabel(name == null ? "" : name, new Styles.LabelStyle(getSkin().get(Label.LabelStyle.class)), getTextraFont());
+        TypingLabel ret = new TypingLabel(name == null ? "" : name, getLabelStyle(), getTextraFont());
         String pn = Current.player().getName();
         if (pn != null) // this variable is used for dialogs
             ret.setVariable("player_name", pn);
@@ -518,7 +535,7 @@ public class Controls {
         public AccountingLabel(TextraLabel target, boolean isShards) {
             target.setVisible(false);
             placeholder = target;
-            label = Controls.newTextraLabel(target.getName() + "Replacement");
+            label = newTextraLabel(target.getName() + "Replacement");
             currencyAmount = isShards ? Current.player().getShards() : Current.player().getGold();
             this.isShards = isShards;
 
@@ -597,13 +614,13 @@ public class Controls {
         }
 
         private TextraLabel getDefaultLabel() {
-            return Controls.newTextraLabel(getLabelText(currencyAmount));
+            return newTextraLabel(getLabelText(currencyAmount));
         }
 
         private TextraLabel getUpdateLabel(int newAmount) {
             int delta = newAmount - currencyAmount;
             String updateText = delta == 0 ? "" : (delta < 0 ? NEGDECOR + delta * -1 : POSDECOR + delta);
-            return Controls.newTextraLabel(getLabelText(newAmount, updateText));
+            return newTextraLabel(getLabelText(newAmount, updateText));
         }
 
         private String getLabelText(int amount) {
