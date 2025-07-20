514d40aefdaca62d47b32b5a7fa1024203d06047
tool4ever <therealtoolkit@hotmail.com>
2025-07-20 10:35:03 +0000

    Consolidate some PlayEffect logic (#8133)
    * Fix potential GUI lock if game ends during RepeatEffect
    
    * Fix casting Feast of Blood from PlayEffect without creatures in play

--- forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
+++ forge-game/src/main/java/forge/game/spellability/SpellAbilityRestriction.java
@@ -353,6 +353,10 @@ public class SpellAbilityRestriction extends SpellAbilityVariables {
     public final boolean checkActivatorRestrictions(final Card c, final SpellAbility sa) {
         Player activator = sa.getActivatingPlayer();
 
+        if (sa.isCastFromPlayEffect()) {
+            return true;
+        }
+
         if (sa.isSpell()) {
             // Spells should always default to "controller" but use mayPlay check.
             final CardPlayOption o = c.mayPlay(sa.getMayPlay());
@@ -615,14 +619,12 @@ public class SpellAbilityRestriction extends SpellAbilityVariables {
             return false;
         }
 
-        if (!sa.isCastFromPlayEffect()) {
-            if (!checkTimingRestrictions(c, sa)) {
-                return false;
-            }
+        if (!checkActivatorRestrictions(c, sa)) {
+            return false;
+        }
 
-            if (!checkActivatorRestrictions(c, sa)) {
-                return false;
-            }
+        if (!checkTimingRestrictions(c, sa)) {
+            return false;
         }
 
         if (!checkZoneRestrictions(c, sa)) {
