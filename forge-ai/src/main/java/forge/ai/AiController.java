1f3871d5dc99d0a239de3bc2a3a7b2758fe6e48b
tool4EvEr <tool4EvEr@>
2025-08-04 22:09:44 +0200

    Thread.stop fallback

--- forge-ai/src/main/java/forge/ai/AiController.java
+++ forge-ai/src/main/java/forge/ai/AiController.java
@@ -1748,7 +1748,12 @@ public class AiController {
             // instead of computing all available concurrently just add a simple timeout depending on the user prefs
             return future.get(game.getAITimeout(), TimeUnit.SECONDS);
         } catch (InterruptedException | ExecutionException | TimeoutException e) {
-            t.stop();
+            try {
+                t.stop();
+            } catch (UnsupportedOperationException ex) {
+                // Android and Java 20 dropped support to stop so sadly thread will keep running
+                future.cancel(true);
+            }
             return null;
         }
     }
