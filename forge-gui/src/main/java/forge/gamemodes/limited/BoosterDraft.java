64897f1ab44ad7f6c503a8f2880d2a29e619076c
Chris H <zenchristo@gmail.com>
2025-09-14 10:36:17 -0400

    Simplify logic

--- forge-gui/src/main/java/forge/gamemodes/limited/BoosterDraft.java
+++ forge-gui/src/main/java/forge/gamemodes/limited/BoosterDraft.java
@@ -279,17 +279,15 @@ public class BoosterDraft implements IBoosterDraft {
     }
 
     public static BoosterDraft createDraft(final LimitedPoolType draftType, final CardBlock block, final String[] boosters, Integer numPlayers) {
-        // quest draft
         final BoosterDraft draft = new BoosterDraft(draftType);
 
         String setCode = boosters[0];
         CardEdition edition = FModel.getMagicDb().getEditions().get(setCode);
         // If this is metaset, edtion will be null
         if (edition != null) {
-            if (numPlayers != null) {
-                draft.setPodSize(numPlayers);
-            } else if (draft.getPodSize() != edition.getDraftOptions().getRecommendedPodSize()) {
-                // Auto choosing recommended pod size. In the future we may want to allow user to choose
+            // Auto choosing recommended pod size. If we've chosen the podsize it should be passed in via numPlayers
+            int newPodSize = Objects.requireNonNullElseGet(numPlayers, () -> edition.getDraftOptions().getRecommendedPodSize());
+            if (newPodSize != draft.getPodSize()) {
                 draft.setPodSize(edition.getDraftOptions().getRecommendedPodSize());
             }
             draft.doublePickDuringDraft = edition.getDraftOptions().isDoublePick(draft.getPodSize());
