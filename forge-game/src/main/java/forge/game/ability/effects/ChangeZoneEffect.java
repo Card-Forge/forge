cdbf10f1f7acf8c7855573161994279ab0ac181a
Hans Mackowiak <hanmac@gmx.de>
2025-09-30 20:43:42 +0200

    Change type desc (#8809)
    * add ChangeTypeDesc for basic land
    
    * Search N is up to
    
    * add some more ChangeTypeDesc
    
    * add some more ChangeTypeDesc
    
    * add some more ChangeTypeDesc
    
    * Update panorama
    
    * update landscape
    
    * Update Clan Monuments
    
    * Small Monument to Perfection change
    
    * update capenna fetchers
    
    * remove ChangeNum from basic Land searchers
    
    * better desc for fetch lands
    
    * remove uneeded ChangeTypeDesc

--- forge-game/src/main/java/forge/game/ability/effects/ChangeZoneEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/ChangeZoneEffect.java
@@ -31,6 +31,7 @@ import org.apache.commons.lang3.tuple.Pair;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ChangeZoneEffect extends SpellAbilityEffect {
 
@@ -103,6 +104,7 @@ public class ChangeZoneEffect extends SpellAbilityEffect {
         }
         final String destination = sa.getParam("Destination");
 
+        final int num = sa.hasParam("ChangeNum") ? AbilityUtils.calculateAmount(host, sa.getParam("ChangeNum"), sa) : 1;
         String type = "card";
         boolean defined = false;
         if (sa.hasParam("ChangeTypeDesc")) {
@@ -117,12 +119,11 @@ public class ChangeZoneEffect extends SpellAbilityEffect {
             type = Lang.joinHomogenous(tgts);
             defined = true;
         } else if (sa.hasParam("ChangeType") && !sa.getParam("ChangeType").equals("Card")) {
-            final String ct = sa.getParam("ChangeType");
-            type = CardType.CoreType.isValidEnum(ct) ? ct.toLowerCase() : ct;
+            List<String> typeList = Arrays.stream(sa.getParam("ChangeType").split(",")).map(ct -> CardType.isACardType(ct) ? ct.toLowerCase() : ct).collect(Collectors.toList());
+            type = Lang.joinHomogenous(typeList, null, num == 1 ? "or" : "and/or");
         }
         final String cardTag = type.contains("card") ? "" : " card";
 
-        final int num = sa.hasParam("ChangeNum") ? AbilityUtils.calculateAmount(host, sa.getParam("ChangeNum"), sa) : 1;
         boolean tapped = sa.hasParam("Tapped");
         boolean attacking = sa.hasParam("Attacking");
         if (sa.isNinjutsu()) {
@@ -151,6 +152,9 @@ public class ChangeZoneEffect extends SpellAbilityEffect {
                                 " and/or graveyard for ");
             } else {
                 sb.append(" for ");
+            }
+            if (num != 1) {
+                sb.append(" up to ");
             }
             sb.append(Lang.nounWithNumeralExceptOne(num, type + cardTag)).append(", ");
             if (!sa.hasParam("NoReveal") && ZoneType.smartValueOf(destination) != null && ZoneType.smartValueOf(destination).isHidden()) {
