d11631c26230121c02584082744f0ea6e4a8384c
Eradev <admin@eradev.com>
2025-11-23 01:10:56 -0500

    Add new predicate

--- forge-gui-mobile/src/forge/adventure/data/RewardData.java
+++ forge-gui-mobile/src/forge/adventure/data/RewardData.java
@@ -106,11 +106,10 @@ public class RewardData implements Serializable {
         if (configData.allowedEditions != null && configData.allowedEditions.length > 0) {
             allCards = IterableUtil.filter(allCards, PaperCardPredicates.printedInAnyEditions(configData.allowedEditions));
         } else if (configData.restrictedEditions != null && configData.restrictedEditions.length > 0) {
-            allCards = IterableUtil.filter(allCards, PaperCardPredicates.onlyPrintedInEditions(configData.restrictedEditions).negate());
+            allCards = IterableUtil.filter(allCards, PaperCardPredicates.isObtainableNotRestricted(configData.restrictedEditions));
+        } else {
+            allCards = IterableUtil.filter(allCards, PaperCardPredicates.isObtainableAnyEdition());
         }
-
-        // Remove unobtainable cards (ie. Conjure-only)
-        allCards = IterableUtil.filter(allCards, PaperCardPredicates.isObtainableAnyEdition());
 
         Set<String> restrictedCards = new HashSet<>(Arrays.asList(configData.restrictedCards));
 
