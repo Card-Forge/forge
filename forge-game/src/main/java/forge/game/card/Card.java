1202e991cc83a61475cd42a7806901cc3f08216b
Trevor Hayes <trevor.hayes@gmail.com>
2025-12-13 07:35:32 -1000

    Fix perpetual static mana cost changes updating the base mana cost.

--- forge-game/src/main/java/forge/game/card/Card.java
+++ forge-game/src/main/java/forge/game/card/Card.java
@@ -437,8 +437,8 @@ public class Card extends GameEntity implements Comparable<Card>, IHasSVars, ITr
     }
 
     public void updateManaCostForView() {
-        currentState.setManaCost(this.getManaCost());
         currentState.getView().updateManaCost(this);
+        currentState.calculatePerpetualAdjustedManaCost();
 
         // TODO re-factor Spell ManaCost fallback to CardState ManaCost
         if (getFirstSpellAbility() != null) {
