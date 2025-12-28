ae7cbdeea2e3d89a36a76df356a7c379847860c5
earno12 <earnold2010@gmail.com>
2025-12-28 13:25:34 -0500

    Refactored CostBlight class to reduce redundancy with counters variable.

--- forge-game/src/main/java/forge/game/cost/CostBlight.java
+++ forge-game/src/main/java/forge/game/cost/CostBlight.java
@@ -4,8 +4,7 @@ import forge.game.card.CounterEnumType;
 
 public class CostBlight extends CostPutCounter {
     public CostBlight(final String counters) {
-        super("1", CounterEnumType.M1M1, "Creature.YouCtrl", "a creature you control");
-        this.setAmount(counters);
+        super(counters, CounterEnumType.M1M1, "Creature.YouCtrl", "a creature you control");
     }
 
     @Override
