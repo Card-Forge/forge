d5203a4c25d17e10b4acf86805ef07fb104fa433
Jeff T <jtomse@gmail.com>
2026-01-05 16:45:17 -0500

    Added flying animation for flying creatures

--- forge-gui-desktop/src/main/java/forge/screens/home/settings/VSubmenuPreferences.java
+++ forge-gui-desktop/src/main/java/forge/screens/home/settings/VSubmenuPreferences.java
@@ -109,6 +109,7 @@ public enum VSubmenuPreferences implements IVSubmenu<CSubmenuPreferences> {
     private final JCheckBox cbOpenPacksIndiv = new OptionsCheckBox(localizer.getMessage("cbOpenPacksIndiv"));
     private final JCheckBox cbTokensInSeparateRow = new OptionsCheckBox(localizer.getMessage("cbTokensInSeparateRow"));
     private final JCheckBox cbStackCreatures = new OptionsCheckBox(localizer.getMessage("cbStackCreatures"));
+    private final JCheckBox cbAnimateFlying = new OptionsCheckBox(localizer.getMessage("cbAnimateFlying"));
     private final JCheckBox cbFilterLandsByColorId = new OptionsCheckBox(localizer.getMessage("cbFilterLandsByColorId"));
     private final JCheckBox cbShowStormCount = new OptionsCheckBox(localizer.getMessage("cbShowStormCount"));
     private final JCheckBox cbRemindOnPriority = new OptionsCheckBox(localizer.getMessage("cbRemindOnPriority"));
@@ -422,6 +423,9 @@ public enum VSubmenuPreferences implements IVSubmenu<CSubmenuPreferences> {
 
         pnlPrefs.add(cbStackCreatures, titleConstraints);
         pnlPrefs.add(new NoteLabel(localizer.getMessage("nlStackCreatures")), descriptionConstraints);
+
+        pnlPrefs.add(cbAnimateFlying, titleConstraints);
+        pnlPrefs.add(new NoteLabel(localizer.getMessage("nlAnimateFlying")), descriptionConstraints);
 
         pnlPrefs.add(cbTimedTargOverlay, titleConstraints);
         pnlPrefs.add(new NoteLabel(localizer.getMessage("nlTimedTargOverlay")), descriptionConstraints);
@@ -971,6 +975,10 @@ public enum VSubmenuPreferences implements IVSubmenu<CSubmenuPreferences> {
 
     public final JCheckBox getCbStackCreatures() {
         return cbStackCreatures;
+    }
+
+    public final JCheckBox getCbAnimateFlying() {
+        return cbAnimateFlying;
     }
 
     public final JCheckBox getCbManaLostPrompt() {
