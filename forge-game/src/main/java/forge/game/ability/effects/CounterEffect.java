5441a4032f8dbb240a9270546d9e6178865de254
tool4EvEr <tool4EvEr@>
2026-01-07 22:02:04 +0100

    Support Glen Elendra's Answer

--- forge-game/src/main/java/forge/game/ability/effects/CounterEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/CounterEffect.java
@@ -11,7 +11,6 @@ import forge.game.replacement.ReplacementResult;
 import forge.game.replacement.ReplacementType;
 import forge.game.spellability.SpellAbility;
 import forge.game.spellability.SpellAbilityStackInstance;
-import forge.game.spellability.SpellPermanent;
 import forge.game.trigger.TriggerType;
 import forge.game.zone.ZoneType;
 import forge.util.Localizer;
