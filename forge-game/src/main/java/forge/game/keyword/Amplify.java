441dc544df75473d7930d0fe225679c3cae6a6cf
Hans Mackowiak <hanmac@gmx.de>
2026-01-11 17:49:18 +0100

    Compleated: move Desc to extra Keyword (#9490)

--- forge-game/src/main/java/forge/game/keyword/Amplify.java
+++ forge-game/src/main/java/forge/game/keyword/Amplify.java
@@ -1,12 +1,17 @@
 package forge.game.keyword;
 
+import forge.game.card.Card;
 import forge.util.Lang;
 
 public class Amplify extends KeywordWithAmount {
 
     @Override
     protected String formatReminderText(String reminderText) {
-        String type = Lang.getInstance().buildValidDesc(getHostCard().getType().getCreatureTypes(), true);
+        Card card = getHostCard();
+        String type = "creature";
+        if (card != null) {
+            type = Lang.getInstance().buildValidDesc(card.getType().getCreatureTypes(), true);
+        }
         return String.format(reminderText, amount, type);
     }
 }
