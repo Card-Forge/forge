ef3330139e4b1004b51aef03aa83986c6d43187c
Bram Teurlings <bram.teurlings@live.nl>
2026-01-14 22:13:25 +0100

    Added text that tells us if the player has won

--- forge-gui-mobile/src/forge/adventure/stage/IAfterMatch.java
+++ forge-gui-mobile/src/forge/adventure/stage/IAfterMatch.java
@@ -2,7 +2,10 @@ package forge.adventure.stage;
 
 public interface IAfterMatch {
     default void setWinner(boolean winner, boolean isArena) {
-        // Todo: Print a message on the screen to show that we know the player won
-        System.out.println("FORGE_ARCHIPELAGO DETECTED GAME CONCLUSION.");
+        String playerResultText = "LOST!";
+        if (winner) {
+            playerResultText = "WON!";
+        }
+        System.out.println("FORGE_ARCHIPELAGO DETECTED GAME CONCLUSION. THE PLAYER HAS " + playerResultText);
     }
 }
