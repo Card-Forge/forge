013c47d10f9a72d4884432bf354b65c5ac8c4416
Hans Mackowiak <hanmac@gmx.de>
2026-01-20 14:18:55 +0000

    fix

--- forge-core/src/main/java/forge/card/StateChangedType.java
+++ forge-core/src/main/java/forge/card/StateChangedType.java
@@ -1,6 +1,6 @@
 package forge.card;
 
-public record StateChangedType(CardType type) implements ICardChangedType {
+public record StateChangedType(CardTypeView type) implements ICardChangedType {
 
     @Override
     public CardType applyChanges(CardType newType) {
