a265c395a65b22220e3383108a777920f81c0546
Tomas Beneda <Tomas.Beneda@gmail.com>
2026-01-20 23:25:47 +0100

    SacrificeAllAi considers Shape anew

--- forge-ai/src/main/java/forge/ai/ability/SacrificeAllAi.java
+++ forge-ai/src/main/java/forge/ai/ability/SacrificeAllAi.java
@@ -1,8 +1,14 @@
 package forge.ai.ability;
 
 import forge.ai.*;
+import forge.game.card.Card;
+import forge.game.card.CardCollectionView;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
+import forge.game.zone.ZoneType;
+
+import java.util.OptionalInt;
+import java.util.function.Function;
 
 public class SacrificeAllAi extends SpellAbilityAi {
 
@@ -18,6 +24,8 @@ public class SacrificeAllAi extends SpellAbilityAi {
             }
         } else if (logic.equals("MadSarkhanDragon")) {
             return SpecialCardAi.SarkhanTheMad.considerMakeDragon(ai, sa);
+        } else if (logic.equals("ShapeAnew")) {
+            return considerShapeAnew(ai, sa);
         }
 
         AiAbilityDecision decision = DestroyAllAi.doMassRemovalLogic(ai, sa);
@@ -33,5 +41,18 @@ public class SacrificeAllAi extends SpellAbilityAi {
         //TODO: Add checks for bad outcome
         return new AiAbilityDecision(100, AiPlayDecision.WillPlay);
 
+    }
+
+    private AiAbilityDecision considerShapeAnew(Player ai, SpellAbility sa) {
+        Function<CardCollectionView, OptionalInt> minArtifactCmc = cards ->
+            cards.stream().filter(Card::isArtifact).mapToInt(Card::getCMC).min();
+
+        int minToSacrifice = minArtifactCmc.apply(ai.getCardsIn(ZoneType.Battlefield)).orElse(100);
+        int minToGain = minArtifactCmc.apply(ai.getCardsIn(ZoneType.Library)).orElse(0);
+        if (minToGain > minToSacrifice + sa.getHostCard().getCMC()) {
+            return new AiAbilityDecision(100, AiPlayDecision.WillPlay);
+        } else {
+            return new AiAbilityDecision(0, AiPlayDecision.CantAfford);
+        }
     }
 }
