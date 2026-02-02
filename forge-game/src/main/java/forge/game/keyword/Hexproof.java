aa87a1e8dd4932b5d3378463a5ae4eab83809d0a
Hans Mackowiak <hanmac@gmx.de>
2026-02-02 12:35:44 +0100

    KeywordWithType: add getTitle (#9652)
    * KeywordWithType: add getTitle

--- forge-game/src/main/java/forge/game/keyword/Hexproof.java
+++ forge-game/src/main/java/forge/game/keyword/Hexproof.java
@@ -3,10 +3,18 @@ package forge.game.keyword;
 public class Hexproof extends KeywordWithType {
 
     @Override
+    public String getTitle() {
+        if (type.isEmpty()) {
+            return "Hexproof";
+        }
+        return "Hexproof from " + this.getTypeDescription();
+    }
+
+    @Override
     protected String formatReminderText(String reminderText) {
         if (type.isEmpty()) {
             return "This can't be the target of spells or abilities your opponents control.";
         }
-        return String.format(reminderText, type);
+        return String.format(reminderText, descType);
     }
 }
