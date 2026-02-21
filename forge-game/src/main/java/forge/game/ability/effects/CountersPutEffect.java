d754d48f399be123a4d808e013e20c3526524d22
Hans Mackowiak <hanmac@gmx.de>
2026-02-21 11:49:10 +0100

    Adapt: fix not setting OriginalParams

--- forge-game/src/main/java/forge/game/ability/effects/CountersPutEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/CountersPutEffect.java
@@ -744,9 +744,10 @@ public class CountersPutEffect extends SpellAbilityEffect {
         super.buildSpellAbility(sa);
         if (sa.hasParam("Adapt")) {
             sa.putParam("CounterType", "P1P1");
+            sa.putParam("CounterNum", sa.getParam("Adapt"));
             sa.putParam("StackDescription", "SpellDescription");
             if (!sa.hasParam("SpellDescription")) {
-                sa.putParam("SpellDescription", "Adapt " + sa.getParam("CounterNum"));
+                sa.putParam("SpellDescription", "Adapt " + sa.getParam("Adapt"));
             }
         }
     }
