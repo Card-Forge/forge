31b55b94925361237c60ca71cff38c9e52c2493f
Eradev <admin@eradev.com>
2026-03-02 03:28:33 -0500

    Spellbook uses Alchemy variant when available

--- forge-game/src/main/java/forge/game/ability/effects/DraftEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/DraftEffect.java
@@ -11,60 +11,68 @@ import forge.game.card.CardZoneTable;
 import forge.game.player.Player;
 import forge.game.spellability.SpellAbility;
 import forge.game.zone.ZoneType;
+import forge.item.PaperCard;
 import forge.util.Localizer;
 
 import java.util.*;
 
- public class DraftEffect extends SpellAbilityEffect {
-     @Override
-     protected String getStackDescription(SpellAbility sa) {
-         final Card source = sa.getHostCard();
-         final Player player = AbilityUtils.getDefinedPlayers(source, sa.getParam("Defined"), sa).get(0);
+public class DraftEffect extends SpellAbilityEffect {
+    @Override
+    protected String getStackDescription(SpellAbility sa) {
+        final Card source = sa.getHostCard();
+        final Player player = AbilityUtils.getDefinedPlayers(source, sa.getParam("Defined"), sa).get(0);
 
-         final StringBuilder sb = new StringBuilder();
+        final StringBuilder sb = new StringBuilder();
 
-         sb.append(player).append(" drafts a card from ").append(source.getDisplayName()).append("'s spellbook.");
+        sb.append(player).append(" drafts a card from ").append(source.getDisplayName()).append("'s spellbook.");
 
-         return sb.toString();
-     }
+        return sb.toString();
+    }
 
-     @Override
-     public void resolve(SpellAbility sa) {
-         final Card source = sa.getHostCard();
-         final Player player = AbilityUtils.getDefinedPlayers(source, sa.getParam("Defined"), sa).get(0);
-         final Game game = player.getGame();
-         List<String> spellbook = Arrays.asList(sa.getParam("Spellbook").split(","));
-         final int numToDraft = AbilityUtils.calculateAmount(source, sa.getParamOrDefault("DraftNum", "1"), sa);
-         Map<AbilityKey, Object> moveParams = AbilityKey.newMap();
-         moveParams.put(AbilityKey.LastStateBattlefield, sa.getLastStateBattlefield());
-         moveParams.put(AbilityKey.LastStateGraveyard, sa.getLastStateGraveyard());
-         CardCollection drafted = new CardCollection();
+    @Override
+    public void resolve(SpellAbility sa) {
+        final Card source = sa.getHostCard();
+        final Player player = AbilityUtils.getDefinedPlayers(source, sa.getParam("Defined"), sa).get(0);
+        final Game game = player.getGame();
+        List<String> spellbook = Arrays.asList(sa.getParam("Spellbook").split(","));
+        final int numToDraft = AbilityUtils.calculateAmount(source, sa.getParamOrDefault("DraftNum", "1"), sa);
+        Map<AbilityKey, Object> moveParams = AbilityKey.newMap();
+        moveParams.put(AbilityKey.LastStateBattlefield, sa.getLastStateBattlefield());
+        moveParams.put(AbilityKey.LastStateGraveyard, sa.getLastStateGraveyard());
+        CardCollection drafted = new CardCollection();
 
-         for (int i = 0; i < numToDraft; i++) {
-             Collections.shuffle(spellbook);
-             List<Card> draftOptions = new ArrayList<>();
-             for (String name : spellbook.subList(0, 3)) {
-                 // Cardnames that include "," must use ";" instead in Spellbook$ (i.e. Tovolar; Dire Overlord)
-                 name = name.replace(";", ",");
-                 Card cardOption = Card.fromPaperCard(StaticData.instance().getCommonCards().getUniqueByName(name), player);
-                 draftOptions.add(cardOption);
-             }
+        for (int i = 0; i < numToDraft; i++) {
+            Collections.shuffle(spellbook);
+            List<Card> draftOptions = new ArrayList<>();
+            for (String name : spellbook.subList(0, 3)) {
+                // Cardnames that include "," must use ";" instead in Spellbook$ (i.e. Tovolar; Dire Overlord)
+                name = name.replace(";", ",");
 
-             Card chosenCard = player.getController().chooseSingleCardForZoneChange(ZoneType.None, new ArrayList<ZoneType>(), sa, new CardCollection(draftOptions), null, Localizer.getInstance().getMessage("lblChooseCardDraft"), false, player);
-             game.getAction().moveTo(ZoneType.None, chosenCard, sa, moveParams);
-             drafted.add(chosenCard);
-         }
+                PaperCard pc = StaticData.instance().getCommonCards().getUniqueByName(name);
+                // Take the balanced version of the card if available.
+                if (pc.isUnRebalanced()) {
+                    pc = StaticData.instance().getCommonCards().getUniqueByName("A-" + name);
+                }
 
-         final CardZoneTable triggerList = new CardZoneTable();
-         for (final Card c : drafted) {
-             Card made = game.getAction().moveToHand(c, sa, moveParams);
-             if (c != null) {
-                 triggerList.put(ZoneType.None, made.getZone().getZoneType(), made);
-             }
-             if (sa.hasParam("RememberDrafted")) {
-                 source.addRemembered(made);
-             }
-         }
-         triggerList.triggerChangesZoneAll(game, sa);
-     }
- }
+                Card cardOption = Card.fromPaperCard(pc, player);
+                draftOptions.add(cardOption);
+            }
+
+            Card chosenCard = player.getController().chooseSingleCardForZoneChange(ZoneType.None, new ArrayList<ZoneType>(), sa, new CardCollection(draftOptions), null, Localizer.getInstance().getMessage("lblChooseCardDraft"), false, player);
+            game.getAction().moveTo(ZoneType.None, chosenCard, sa, moveParams);
+            drafted.add(chosenCard);
+        }
+
+        final CardZoneTable triggerList = new CardZoneTable();
+        for (final Card c : drafted) {
+            Card made = game.getAction().moveToHand(c, sa, moveParams);
+            if (c != null) {
+                triggerList.put(ZoneType.None, made.getZone().getZoneType(), made);
+            }
+            if (sa.hasParam("RememberDrafted")) {
+                source.addRemembered(made);
+            }
+        }
+        triggerList.triggerChangesZoneAll(game, sa);
+    }
+}
