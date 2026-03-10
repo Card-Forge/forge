d512a2733f5c0de48b12331df6a841384e92efb8
Hans Mackowiak <hanmac@gmx.de>
2026-03-10 10:30:02 +0100

    Craft: refactor Title and reminder

--- forge-game/src/main/java/forge/game/keyword/Craft.java
+++ forge-game/src/main/java/forge/game/keyword/Craft.java
@@ -7,43 +7,67 @@ import forge.game.cost.CostPartMana;
 
 public class Craft extends KeywordWithCost {
 
-    String manaString = "Mana?";
-    String exileString = "Exile?";
+    String desc;
+    String reminder;
 
+    @Override
+    public String getTitle() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getTitleWithoutCost());
+        sb.append(" ");
+        sb.append(cost.getCostMana());
+        return sb.toString();
+    }
+
+    @Override
+    public String getTitleWithoutCost() {
+        return "Craft with " + desc;
+    }
 
     @Override
     protected void parse(String details) {
         String[] k = details.split(":");
         super.parse(k[0]);
 
-        final Cost kCost = new Cost(k[0], true);
-        for (CostPart part : kCost.getCostParts()) {
-            if (part instanceof CostPartMana) {
-                manaString = part.toString();
-                break;
-            }
-        }
-
-        final StringBuilder sb = new StringBuilder();
         if (k.length > 2) {
+            desc = k[1];
+            final StringBuilder sb = new StringBuilder();
             sb.append("Exile ").append(k[2]).append(" from among permanents you control and/or cards in your graveyard");
-        } else for (CostPart part : kCost.getCostParts()) {
-            if (part instanceof CostExile) {
-                int xMin = 0;
-                if (k[0].contains("XMin")) {
-                    String cutString = k[0].substring(k[0].indexOf("XMin") + 4);
-                    xMin = Integer.parseInt(cutString.substring(0, cutString.indexOf(" ")));
-                }
-                sb.append(((CostExile) part).exileMultiZoneCostString(true, xMin));
-                break;
+            reminder = sb.toString();
+        } else {
+            CostExile exile = cost.getCostPartByType(CostExile::class);
+            final StringBuilder sb = new StringBuilder();
+            int xmin = cost.getCostMana().getXMin();
+            if (xmin > 0) {
+                sb.append(Lang.getNumeral(xmin)).append(" or more");
+            } else {
+                sb.append(Lang.getNumeral(Integer.parseInt(exile.getAmount())));
             }
+            if (exile.getTypeDescription() != null) {
+                // permanent are skipped in desc
+                if (!"permanent".equals(getTypeDescription())) {
+                    sb.append(" ");
+                    sb.append(getTypeDescription());
+                }
+            } else {
+                sb.append(" ");
+                String partType = exile.getType();
+                //consume .Other from most partTypes
+                if (partType.contains(".Other")) partType = partType.replace(".Other", "");
+                // try to guess plural
+                if (xmin > 0 || !"1".equals(exile.getAmount())) {
+                    sb.append(CardType.getPluralType(partType));
+                } else {
+                    sb.append(Lang.getInstance().formatValidDesc(partType));
+                }
+            }
+            desc = sb.toString();
+            reminder = exile.exileMultiZoneCostString(true, xmin);
         }
-        exileString = sb.toString();
     }
-
 
     @Override
     protected String formatReminderText(String reminderText) {
-        return String.format(reminderText, manaString + ", Exile this artifact, " + exileString);
+        return String.format(reminderText, cost.getCostMana(), reminder);
     }
 }
