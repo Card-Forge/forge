4b1c9a5e03b66a2ea0a5d6b7faf0b5751b775955
kojotak <Tomas.Beneda@gmail.com>
2026-05-04 22:33:16 +0200

    MostProminentRememberedControls

--- forge-ai/src/main/java/forge/ai/ComputerUtilCard.java
+++ forge-ai/src/main/java/forge/ai/ComputerUtilCard.java
@@ -5,6 +5,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.IdentityHashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
@@ -1122,6 +1123,7 @@ public class ComputerUtilCard {
         Player opp = ai.getStrongestOpponent();
         if (sa.hasParam("AILogic")) {
             final String logic = sa.getParam("AILogic");
+            Iterator<Player> rememberedPlayers = IterableUtil.filter(sa.getHostCard().getRemembered(), Player.class).iterator();
 
             if (logic.equals("MostProminentInHumanDeck")) {
                 chosen.add(getMostProminentColor(CardLists.filterControlledBy(game.getCardsInGame(), opp), colorChoices));
@@ -1143,6 +1145,9 @@ public class ComputerUtilCard {
                 chosen.add(getMostProminentColor(ai.getCardsIn(ZoneType.Battlefield), colorChoices));
             } else if (logic.equals("MostProminentHumanControls")) {
                 chosen.add(getMostProminentColor(opp.getCardsIn(ZoneType.Battlefield), colorChoices));
+            } else if (logic.equals("MostProminentRememberedControls") && rememberedPlayers.hasNext()) {
+                Player rememberedPlayer = rememberedPlayers.next();
+                chosen.add(getMostProminentColor(rememberedPlayer.getCardsIn(ZoneType.Battlefield), colorChoices));
             } else if (logic.equals("MostProminentPermanent")) {
                 chosen.add(getMostProminentColor(game.getCardsIn(ZoneType.Battlefield), colorChoices));
             } else if (logic.equals("MostProminentAttackers") && game.getPhaseHandler().inCombat()) {
