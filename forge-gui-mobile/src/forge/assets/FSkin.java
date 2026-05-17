60ebec91babba9a8a099d498703bd04364e386d7
Hans Mackowiak <hanmac@gmx.de>
2026-05-17 18:49:53 +0200

    Keyword: add View Records for Serializing (#10651)
    * Keyword: add View Records for Serializing
    * KeywordCollectionView: make Serializable
    * FSkinProp: use KeywordView for Icon
    * PlayerView: join on KeywordView
    * remove most of the Ability Properties
    * Commander and Ringbearer icons without extra drawSymbol
    * remove old ability icons except for Hexproof and Protection
    * Hexproof logic added and some Card fixes
    * Refactor Protection Icons
    * remove CardFaceSymbols.drawAbilitySymbol on desktop
    
    ---------
    
    Co-authored-by: tool4ever <therealtoolkit@hotmail.com>
    Co-authored-by: tool4EvEr <tool4EvEr@>

--- forge-gui-mobile/src/forge/assets/FSkin.java
+++ forge-gui-mobile/src/forge/assets/FSkin.java
@@ -10,7 +10,6 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.Array;
 import forge.Forge;
 import forge.card.CardFaceSymbols;
-import forge.card.MagicColor;
 import forge.gui.FThreads;
 import forge.gui.GuiBase;
 import forge.localinstance.properties.ForgeConstants;
@@ -368,6 +367,19 @@ public class FSkin {
             }
 
             //load images
+            for (FSkinProp prop : FSkinProp.values()) {
+                if (FSkinProp.PropType.ABILITY == prop.getType()
+                        || FSkinProp.PropType.WATERMARKS == prop.getType()
+                        || FSkinProp.PropType.MANAICONS == prop.getType()
+                        || FSkinProp.PropType.PHYREXIAN == prop.getType()
+                        || FSkinProp.PropType.COLORLESS_HYBRID == prop.getType()
+                        || FSkinProp.PropType.ATTRACTION_LIGHTS == prop.getType()
+                        ) {
+                    FSkinImageImpl image = new FSkinImageImpl(prop);
+                    image.load(preferredIcons);
+                    FSkin.getImages().put(prop, image);
+                }
+            }
             for (FSkinImage image : FSkinImage.values()) {
                 if (GuiBase.isAndroid()) {
                     if (Forge.allowCardBG)
@@ -379,17 +391,6 @@ public class FSkin {
                 } else {
                     image.load(preferredIcons);
                 }
-            }
-            for (FSkinProp prop : FSkinProp.MANA_IMG.values()) {
-                FSkinImageImpl image = new FSkinImageImpl(prop);
-                image.load(preferredIcons);
-                FSkin.getImages().put(prop, image);
-            }
-            for (MagicColor.Color c : MagicColor.Color.values()) {
-                FSkinProp prop = FSkinProp.watermarkFromColor(c);
-                FSkinImageImpl image = new FSkinImageImpl(prop);
-                image.load(preferredIcons);
-                FSkin.getImages().put(prop, image);
             }
 
             //assemble avatar textures
