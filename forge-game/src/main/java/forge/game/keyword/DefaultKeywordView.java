d0118c081ccf6fe17636eb7427672ccd8c4d1067
Hans Mackowiak <hanmac@gmx.de>
2026-05-17 17:06:15 +0200

    ~ add toString for debugging

--- forge-game/src/main/java/forge/game/keyword/DefaultKeywordView.java
+++ forge-game/src/main/java/forge/game/keyword/DefaultKeywordView.java
@@ -2,4 +2,6 @@ package forge.game.keyword;
 
 public record DefaultKeywordView(String original, Keyword keyword, String title, String reminderText) implements KeywordView {
 
+    @Override
+    public String toString() { return title + " (" + reminderText + ")"; }
 }
