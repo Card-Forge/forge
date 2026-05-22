aa1f4c3e60134c4872f8903985110c498920e182
Hans Mackowiak <hanmac@gmx.de>
2026-05-22 09:40:14 +0000

    ITextChanges as combined Map

--- forge-core/src/main/java/forge/card/ResetChangedText.java
+++ forge-core/src/main/java/forge/card/ResetChangedText.java
@@ -2,14 +2,14 @@ package forge.card;
 
 import java.util.Map;
 
-public interface ResetChangedText extends IChangedText {
+public record ResetChangedText() implements IChangedText {
 
     @Override
-    default void applyColorChanges(Map<MagicColor.Color, MagicColor.Color> result) {
+    public void applyColorChanges(Map<MagicColor.Color, MagicColor.Color> result) {
         result.clear();
     }
     @Override
-    default void applyTypeChanges(Map<String, String> result) {
+    public void applyTypeChanges(Map<String, String> result) {
         result.clear();
     }
 
