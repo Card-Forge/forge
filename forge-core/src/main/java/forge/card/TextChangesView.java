ccbfa542a633294c5eddfbd119929ecb5f8656a4
Hans Mackowiak <hanmac@gmx.de>
2026-05-22 10:51:09 +0000

    ITextChanges::combine

--- forge-core/src/main/java/forge/card/TextChangesView.java
+++ forge-core/src/main/java/forge/card/TextChangesView.java
@@ -8,4 +8,36 @@ public record TextChangesView(Map<MagicColor.Color, MagicColor.Color> colorChang
     public boolean isEmpty() {
         return colorChanges.isEmpty() && typeChanges.isEmpty();
     }
+
+    @Override
+    public ITextChanges combine(ITextChanges output) {
+        if (output.isEmpty()) {
+            return this;
+        }
+        if (this.isEmpty()) {
+            return output;
+        };
+
+        return new TextChangesView(
+            _combineChangedMap(colorChanges(), output.colorChanges()),
+            _combineChangedMap(typeChanges(), output.typeChanges()),
+        );
+    }
+
+    private <T> Map<T, T> _combineChangedMap(Map<T, T> input, Map<T, T> output) {
+        // no need to do something, just return hash
+        if (input.isEmpty()) {
+            return output;
+        }
+        if (output.isEmpty()) {
+            return input;
+        }
+        // magic combine them
+        Map<T, T> result = Maps.newHashMap(input);
+        for (Map.Entry<T, T> e : result.entrySet()) {
+            e.setValue(output.getOrDefault(e.getValue(), e.getValue()));
+        }
+        result.putAll(output);
+        return result;
+    }
 }
