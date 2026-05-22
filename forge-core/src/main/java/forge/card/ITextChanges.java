f19740512abf6e727c82636426daebdf03bc6ca0
Hans Mackowiak <hanmac@gmx.de>
2026-05-22 09:40:14 +0000

    ITextChanges as combined Map

---
+++ forge-core/src/main/java/forge/card/ITextChanges.java
@@ -0,0 +1,10 @@
+package forge.card;
+
+import java.util.Map;
+
+public interface ITextChanges {
+    Map<MagicColor.Color, MagicColor.Color> colorChanges();
+    Map<String, String> typeChanges();
+
+    boolean isEmpty();
+}
