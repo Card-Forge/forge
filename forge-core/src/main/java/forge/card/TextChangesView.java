0b0cc65e31d3be95cd736f8dd2587f4ebcd81046
Hans Mackowiak <hanmac@gmx.de>
2026-05-22 09:42:10 +0000

    ~ fix missing }

--- forge-core/src/main/java/forge/card/TextChangesView.java
+++ forge-core/src/main/java/forge/card/TextChangesView.java
@@ -4,9 +4,8 @@ import java.util.Map;
 
 public record TextChangesView(Map<MagicColor.Color, MagicColor.Color> colorChanges, Map<String, String> typeChanges) implements ITextChanges {
 
-
     @Override
     boolean isEmpty() {
         return colorChanges.isEmpty() && typeChanges.isEmpty();
-    
+    }
 }
