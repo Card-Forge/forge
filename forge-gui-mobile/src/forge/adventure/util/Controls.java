bddd490d2b8ae0f48b01a51de099db9cdd520c29
Roger Sloan <1922907+RogerSloan@users.noreply.github.com>
2026-06-25 14:47:42 -0700

    New Game menu bugfix: Correct label alignment and fix text/style setters to properly invalidate the layout on MarqueeButton.

--- forge-gui-mobile/src/forge/adventure/util/Controls.java
+++ forge-gui-mobile/src/forge/adventure/util/Controls.java
@@ -153,6 +153,7 @@ public class Controls {
             minScrollDuration = MIN_SCROLL_DURATION_DEFAULT;
             this.clip(true);
             this.getTextraLabel().setWrap(false);
+            this.align(Align.left);
             this.manageAnimation();
         }
 
@@ -245,19 +246,19 @@ public class Controls {
         @Override
         public void setTextraLabel(TextraLabel label) {
             super.setTextraLabel(label);
-            this.manageAnimation();
+            this.invalidate();
         }
 
         @Override
         public void setText(@Null String text) {
             super.setText(text);
-            this.manageAnimation();
+            this.invalidate();
         }
 
         @Override
         public void setStyle(Button.ButtonStyle style, boolean makeGridGlyphs) {
             super.setStyle(style, makeGridGlyphs);
-            this.manageAnimation();
+            this.invalidate();
         }
 
         @Override
