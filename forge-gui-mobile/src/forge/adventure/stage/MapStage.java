72ddfda6e16179840615329db7223cf95664c63a
BramTeurlings <bram.teurlings@live.nl>
2026-07-01 09:59:30 +0200

    Fixed play movement when dialog is shown

--- forge-gui-mobile/src/forge/adventure/stage/MapStage.java
+++ forge-gui-mobile/src/forge/adventure/stage/MapStage.java
@@ -416,13 +416,8 @@ public class MapStage extends GameStage {
                         } else {
                             otherEntries.add(entry);
                         }
-                        // Todo: This makes the portal appear, we should do some logic when the player enters the portal
-                        if (!prop.containsKey("noExit") || prop.get("noExit").toString().equals("false")) {
-                            if (ArchipelagoRandomizer.getInstance().getSlotData() != null) {
-                                addMapActor(obj, entry);
-                            }
-                            // Notify the user that they must connect to
-                        }
+                        if (!prop.containsKey("noExit") || prop.get("noExit").toString().equals("false"))
+                            addMapActor(obj, entry);
                         break;
                     case "portal":
                         float px = Float.parseFloat(prop.get("x").toString());
