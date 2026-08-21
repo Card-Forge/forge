93b8cac5a725a88ccccceafc88b6294bea5b2a60
kevlahnota <anthonycalosa@gmail.com>
2026-08-21 11:32:32 +0800

    Add tooltip info (#11650)
    * Add tooltip info
    
    * update renders for tooltipinfo

--- forge-gui-mobile/src/forge/adventure/util/RewardActor.java
+++ forge-gui-mobile/src/forge/adventure/util/RewardActor.java
@@ -1533,6 +1533,9 @@ public class RewardActor extends Actor implements Disposable, ImageFetcher.Callb
                     getStage().addActor(switchButton);
             }
             shown = true;
+            RewardScene.instance().showTooltipInfo(
+                "[%95]Swipe Up/Down to toggle Card Detail View.",
+                Reward.Type.Card.equals(reward.type));
         }
 
         public void hide() {
@@ -1545,6 +1548,7 @@ public class RewardActor extends Actor implements Disposable, ImageFetcher.Callb
             if (switchButton != null)
                 switchButton.remove();
             shown = false;
+            RewardScene.instance().showTooltipInfo("", false);
         }
     }
 
