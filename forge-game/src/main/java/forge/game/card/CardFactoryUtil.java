82ec9a3047ede089de9e0a4159a153d2f29273fc
Hans Mackowiak <hanmac@gmx.de>
2026-08-23 16:19:22 +0200

    CardFactoryUtil: use AbilityStatic for Ascend

--- forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
+++ forge-game/src/main/java/forge/game/card/CardFactoryUtil.java
@@ -633,34 +633,19 @@ public class CardFactoryUtil {
                         + " | Secondary$ True | Static$ True | Blessing$ False | IsPresent$ Permanent.YouCtrl | PresentCompare$ GE10"
                         + " | TriggerDescription$ Ascend (" + inst.getReminderText() + ")";
 
-                final String effect = "DB$ Ascend | Defined$ You";
-
                 final Trigger trigger = TriggerHandler.parseTrigger(trig, card, intrinsic);
-                trigger.setOverridingAbility(AbilityFactory.getAbility(effect, card));
-
-                inst.addTrigger(trigger);
-            }
-        } else if (keyword.equals("Storied")) {
-            // Storied trigger only for Permanent, as with Ascend
-            if (card.isPermanent()) {
-                final String trig = "Mode$ Always | TriggerZones$ Battlefield"
-                        + " | Secondary$ True | Static$ True | EnduringStory$ False"
-                        + " | IsPresent$ Permanent.YouCtrl+Historic | PresentCompare$ GE3"
-                        + " | TriggerDescription$ Storied (" + inst.getReminderText() + ")";
-
-                final Trigger trigger = TriggerHandler.parseTrigger(trig, card, intrinsic);
-                final SpellAbility gainStory = new AbilityStatic(card, Cost.Zero, null) {
+                final SpellAbility ascend = new AbilityStatic(card, Cost.Zero, null) {
                     @Override
                     public void resolve() {
                         final Player p = getActivatingPlayer();
                         if (p != null && p.isInGame()) {
-                            p.setEnduringStory(true, getOriginalHost().getSetCode());
+                            p.setBlessing(true, getOriginalHost().getSetCode());
                         }
                     }
                 };
                 // as AbilityFactory would have done, so getOriginalHost resolves on a copied trait
-                gainStory.setCardState(card.getCurrentState());
-                trigger.setOverridingAbility(gainStory);
+                ascend.setCardState(card.getCurrentState());
+                trigger.setOverridingAbility(ascend);
 
                 inst.addTrigger(trigger);
             }
@@ -1802,6 +1787,30 @@ public class CardFactoryUtil {
             squadTrigger.setOverridingAbility(squadAbility);
             squadTrigger.setSVar("SquadAmount", "Count$OptionalKeywordAmount");
             inst.addTrigger(squadTrigger);
+        } else if (keyword.equals("Storied")) {
+            // Storied trigger only for Permanent, as with Ascend
+            if (card.isPermanent()) {
+                final String trig = "Mode$ Always | TriggerZones$ Battlefield"
+                        + " | Secondary$ True | Static$ True | EnduringStory$ False"
+                        + " | IsPresent$ Permanent.YouCtrl+Historic | PresentCompare$ GE3"
+                        + " | TriggerDescription$ Storied (" + inst.getReminderText() + ")";
+
+                final Trigger trigger = TriggerHandler.parseTrigger(trig, card, intrinsic);
+                final SpellAbility gainStory = new AbilityStatic(card, Cost.Zero, null) {
+                    @Override
+                    public void resolve() {
+                        final Player p = getActivatingPlayer();
+                        if (p != null && p.isInGame()) {
+                            p.setEnduringStory(true, getOriginalHost().getSetCode());
+                        }
+                    }
+                };
+                // as AbilityFactory would have done, so getOriginalHost resolves on a copied trait
+                gainStory.setCardState(card.getCurrentState());
+                trigger.setOverridingAbility(gainStory);
+
+                inst.addTrigger(trigger);
+            }
         } else if (keyword.equals("Storm")) {
             final String actualTrigger = "Mode$ SpellCast | ValidCard$ Card.Self | TriggerZones$ Stack | Secondary$ True"
                     + "| TriggerDescription$ Storm (" + inst.getReminderText() + ")";
