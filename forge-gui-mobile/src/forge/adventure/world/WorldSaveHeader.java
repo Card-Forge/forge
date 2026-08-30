8e7bf8f5adb7fa559be8994c2dd42f59fad5fde0
kevlahnota <anthonycalosa@gmail.com>
2026-08-30 16:33:42 +0800

    unused import

--- forge-gui-mobile/src/forge/adventure/world/WorldSaveHeader.java
+++ forge-gui-mobile/src/forge/adventure/world/WorldSaveHeader.java
@@ -1,6 +1,5 @@
 package forge.adventure.world;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
