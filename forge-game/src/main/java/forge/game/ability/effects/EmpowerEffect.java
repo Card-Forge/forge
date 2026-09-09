4a0ad71bfd1fdf924cf9367ee8a74cf5f7aa0a99
Hans Mackowiak <hanmac@gmx.de>
2026-09-09 06:13:33 +0200

    Empower: add new Effect and Jace token (#11827)

--- forge-game/src/main/java/forge/game/ability/effects/EmpowerEffect.java
+++ forge-game/src/main/java/forge/game/ability/effects/EmpowerEffect.java
@@ -4,10 +4,8 @@ import java.util.Map;
 
 import org.apache.commons.lang3.mutable.MutableBoolean;
 
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 
-import forge.card.CardType;
 import forge.game.Game;
 import forge.game.GameEntityCounterTable;
 import forge.game.ability.AbilityUtils;
@@ -26,21 +24,22 @@ import forge.game.zone.ZoneType;
 import forge.util.Lang;
 import forge.util.Localizer;
 
-public class AmassEffect extends TokenEffectBase {
+public class EmpowerEffect extends TokenEffectBase {
 
     @Override
     protected String getStackDescription(SpellAbility sa) {
-        final StringBuilder sb = new StringBuilder("Amass ");
+        final StringBuilder sb = new StringBuilder("Empower ");
         final Card card = sa.getHostCard();
         final int amount = AbilityUtils.calculateAmount(card, sa.getParamOrDefault("Num", "1"), sa);
         final String type = sa.getParam("Type");
 
-        sb.append(CardType.getPluralType(type)).append(" ").append(amount).append(" (Put ");
+        sb.append(type).append(" ").append(amount).append(" (Put ");
 
         sb.append(Lang.nounWithNumeral(amount, "+1/+1 counter"));
 
         // TODO fix reminder after CR
-        sb.append(" on an Army you control. If you don't control one, create a 0/0 black " + type + " Army creature token first.)");
+        sb.append(" on a ").append(type).append(" token you control.");
+        sb.append(" If you don't control one, first create a blue ").append(type).append(" planeswalker token with \"-1: Surveil 1.\" and \"-3: Draw a card.\")");
 
         return sb.toString();
     }
@@ -49,30 +48,29 @@ public class AmassEffect extends TokenEffectBase {
     public void resolve(SpellAbility sa) {
         final Card source = sa.getHostCard();
         final Game game = source.getGame();
-        final Player amasser = getTargetPlayers(sa).getFirst();
-        if (amasser == null) {
+        final Player p = getTargetPlayers(sa).getFirst();
+        if (p == null) {
             return;
         }
         final int amount = AbilityUtils.calculateAmount(source, sa.getParamOrDefault("Num", "1"), sa);
         final String type = sa.getParam("Type");
 
-        // create army token if needed
-        if (!amasser.getCardsIn(ZoneType.Battlefield).anyMatch(CardPredicates.isType("Army"))) {
+        if (!p.getCardsIn(ZoneType.Battlefield).anyMatch(CardPredicates.isType(type).and(CardPredicates.TOKEN))) {
             CardZoneTable triggerList = new CardZoneTable();
             MutableBoolean combatChanged = new MutableBoolean(false);
 
-            StringBuilder sb = new StringBuilder("b_0_0_");
-            sb.append(sa.getOriginalParam("Type").toLowerCase()).append("_army");
+            StringBuilder sb = new StringBuilder("u_empower_");
+            sb.append(sa.getOriginalParam("Type").toLowerCase());
 
-            Card result = TokenInfo.getProtoType(sb.toString(), sa, amasser, false);
-            if (result == null) //Custom Amass type.
-                result = TokenInfo.getProtoType("b_0_0_army", sa, amasser, false);
+            Card result = TokenInfo.getProtoType(sb.toString(), sa, p, false);
+            if (result == null) //Custom Empower type.
+                result = TokenInfo.getProtoType("u_empower", sa, p, false);
             // need to alter the token to add the Type from the Parameter
-            result.setCreatureTypes(Lists.newArrayList(type, "Army"));
-            result.setName(type + " Army Token");
+            result.addType(type);
+            result.setName(type + " Token");
             result.setTokenSpawningAbility(sa);
 
-            makeTokenTable(makeTokenTableInternal(amasser, result, 1), false, triggerList, combatChanged, sa);
+            makeTokenTable(makeTokenTableInternal(p, result, 1), false, triggerList, combatChanged, sa);
 
             triggerList.triggerChangesZoneAll(game, sa);
 
@@ -84,35 +82,18 @@ public class AmassEffect extends TokenEffectBase {
             }
         }
 
-        CardCollectionView tgtCards = CardLists.getType(amasser.getCardsIn(ZoneType.Battlefield), "Army");
+        CardCollectionView tgtCards = CardLists.filter(p.getCardsIn(ZoneType.Battlefield), CardPredicates.isType(type).and(CardPredicates.TOKEN));
         if (tgtCards.isEmpty()) {
             return;
         }
 
         Map<String, Object> params = Maps.newHashMap();
-        params.put("CounterType", CounterEnumType.P1P1);
+        params.put("CounterType", CounterEnumType.LOYALTY);
         params.put("Amount", amount);
-        Card tgt = amasser.getController().chooseSingleEntityForEffect(tgtCards, sa, Localizer.getInstance().getMessage("lblChooseAnArmy"), false, params);
-
-        if (sa.hasParam("RememberAmass")) {
-            source.addRemembered(tgt);
-        }
+        Card tgt = p.getController().chooseSingleEntityForEffect(tgtCards, sa, Localizer.getInstance().getMessage("lblChooseaCard"), false, params);
 
         GameEntityCounterTable table = new GameEntityCounterTable();
-        tgt.addCounter(CounterEnumType.P1P1, amount, amasser, table);
+        tgt.addCounter(CounterEnumType.LOYALTY, amount, p, table);
         table.replaceCounterEffect(game, sa);
-        // 01.44a If it isn’t a [subtype], it becomes a [subtype] in addition to its other types.
-        if (!tgt.getType().hasCreatureType(type)) {
-            final Card eff = createEffect(sa, amasser, "Amass Effect", source.getImageKey());
-            eff.setRenderForUI(false);
-            eff.addRemembered(tgt);
-
-            String s = "Mode$ Continuous | Affected$ Card.IsRemembered | EffectZone$ Command | AddType$ " + type;
-            eff.addStaticAbility(s);
-
-            tgt.addLeavesPlayCommand(() -> game.getAction().exileEffect(eff));
-            game.getAction().moveToCommand(eff, sa);
-        }
     }
-
 }
