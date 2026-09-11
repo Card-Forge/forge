8beda0e1e435f02fa2a07d4da6151e33eb5f187c
BigCrunch22 <22bigcrunch@gmail.com>
2026-09-11 18:15:14 +0800

    Make River Song's draw ability to a static ability

--- forge-game/src/main/java/forge/game/staticability/StaticAbilityMode.java
+++ forge-game/src/main/java/forge/game/staticability/StaticAbilityMode.java
@@ -110,6 +110,9 @@ public enum StaticAbilityMode {
     // StaticAbilityCantDraw
     CantDraw,
 
+    // StaticAbilityDrawFromBottom
+    DrawFromBottom,
+
     // StaticAbilityCantDiscard
     CantDiscard,
 
