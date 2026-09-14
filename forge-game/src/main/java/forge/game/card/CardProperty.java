6a6e70e76dd9e5db9da8c21de24256ba797d1f52
tool4EvEr <tool4EvEr@>
2026-09-14 16:21:25 +0200

    Add support

--- forge-game/src/main/java/forge/game/card/CardProperty.java
+++ forge-game/src/main/java/forge/game/card/CardProperty.java
@@ -1150,14 +1150,6 @@ public class CardProperty {
             if (card.getDamageHistory().getDamageDoneThisTurn(true, true, null, property.split(" ")[1], card, sourceController, spellAbility) == 0) {
                 return false;
             }
-        } else if (property.startsWith("controllerWasDealtCombatDamageByThisTurn")) {
-            if (source.getDamageHistory().getDamageDoneThisTurn(true, true, null, "You", card, controller, spellAbility) == 0) {
-                return false;
-            }
-        } else if (property.startsWith("controllerWasDealtDamageByThisTurn")) {
-            if (source.getDamageHistory().getDamageDoneThisTurn(null, true, null, "You", card, controller, spellAbility) == 0) {
-                return false;
-            }
         } else if (property.startsWith("wasDealtDamageThisTurn")) {
             if (card.getAssignedDamage() == 0) {
                 return false;
