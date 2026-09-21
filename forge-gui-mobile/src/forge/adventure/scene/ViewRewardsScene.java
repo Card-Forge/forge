09ec07abed89f241c8046ff5e79d8d4b7788f8c5
kevlahnota <anthonycalosa@gmail.com>
2026-09-21 15:05:06 +0800

    Refactor Adventure Stages, Scenes, Sprites (#11945)
    * Refactor Adventure Stages
    
    * update more reusable vars
    
    * update adventure scenes
    
    * update adventure scenes
    
    * update adventure actor, sprites
    
    * rename drawFoil -> getFoilIndex
    
    * fix random foil index
    
    * update some needed class for minor refactor

--- forge-gui-mobile/src/forge/adventure/scene/ViewRewardsScene.java
+++ forge-gui-mobile/src/forge/adventure/scene/ViewRewardsScene.java
@@ -1,8 +1,6 @@
 package forge.adventure.scene;
 
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.scenes.scene2d.Actor;
-import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import forge.Adventure;
 import forge.card.CardZoom;
@@ -25,13 +23,27 @@ public class ViewRewardsScene extends ForgeScene {
         index = index0;
         return object;
     }
+
     @Override
     public void enter() {
-        screen = null;
-        getScreen();
         Adventure.getInstance().renderTransitionScreen = false;
-        super.enter();
 
+        // init screen
+        getScreen();
+
+        // access the image and swap if needed
+        RewardScene scene = RewardScene.instance();
+        TextureRegion currentBackground = scene.getUIBackground();
+
+        if (RewardScene.Type.Shop.equals(scene.type)) {
+            if (scene.marketBackgroundImg != null && scene.marketBackgroundImg.getDrawable() instanceof TextureRegionDrawable drawable) {
+                currentBackground = drawable.getRegion();
+            }
+        }
+
+        screen.setBackground(currentBackground);
+
+        super.enter();
         CardZoom.show(list, index, null, true);
     }
 
@@ -44,15 +56,7 @@ public class ViewRewardsScene extends ForgeScene {
     @Override
     public FScreen getScreen() {
         if (screen == null) {
-            RewardScene scene = RewardScene.instance();
-            TextureRegion background = scene.getUIBackground();
-            if (RewardScene.Type.Shop.equals(scene.type)) {
-                Actor actor = scene.getUI().findActor("market_background");
-                if (actor instanceof Image image)
-                    if (image.getDrawable() instanceof TextureRegionDrawable drawable)
-                        background = drawable.getRegion();
-            }
-            screen = new RewardScreen("", background);
+            screen = new RewardScreen("", null);
         }
         return screen;
     }
