8cd226407b1de6589e4d152beed4b29e02ac4d31
Hans Mackowiak <hanmac@gmx.de>
2026-09-25 15:23:47 +0200

    Update Player.java
    fix getMonarchSet

--- forge-game/src/main/java/forge/game/player/Player.java
+++ forge-game/src/main/java/forge/game/player/Player.java
@@ -3440,7 +3440,7 @@ public class Player extends GameEntity implements Comparable<Player> {
     }
 
     public String getMonarchSet() {
-        return monarchEffect == null ? monarchEffect.getSetCode() : null;
+        return monarchEffect != null ? monarchEffect.getSetCode() : null;
     }
 
     public void createMonarchEffect(final String set) {
